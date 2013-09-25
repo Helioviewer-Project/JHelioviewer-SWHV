fbb9e8098818d259f80139f46effe599608462ad
Freek Verstringe <freek.verstringe@oma.be>
2013-09-25 12:26:34 +0000

    data: Changed the configuration of Gong Halpha to hopefully the correct one
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@216 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/viewmodel/src/org/helioviewer/viewmodel/metadata/HelioviewerMetaData.java
+++ src/viewmodel/src/org/helioviewer/viewmodel/metadata/HelioviewerMetaData.java
@@ -71,7 +71,12 @@ public class HelioviewerMetaData extends AbstractMetaData implements SunMetaData
             observatory = m.get("TELESCOP");
             fullName = "AIA " + measurement;
         }
-
+        else if (instrument.contains("GONG")) {
+            instrument = "SWAP";
+            measurement = m.get("WAVELNTH");
+            observatory = m.get("TELESCOP");
+            fullName = "GONG " + measurement;
+        }
         else if (instrument.contains("SWAP")) {
             instrument = "SWAP";
             measurement = m.get("WAVELNTH");
