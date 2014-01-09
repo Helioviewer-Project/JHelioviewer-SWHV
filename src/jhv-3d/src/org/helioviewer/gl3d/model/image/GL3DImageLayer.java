988666bf51bc22da955e26caa5d8cdd08cffffb3
Freek Verstringe <freek.verstringe@oma.be>
2014-01-09 13:16:31 +0000

    client:remove the wrong opacity setting and keep color as is
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@411 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d/src/org/helioviewer/gl3d/model/image/GL3DImageLayer.java
+++ src/jhv-3d/src/org/helioviewer/gl3d/model/image/GL3DImageLayer.java
@@ -146,11 +146,12 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
     }
 
     public void cameraMoved(GL3DCamera camera) {
-        doUpdateROI = true;
+        /*doUpdateROI = true;
         this.accellerationShape.markAsChanged();
-        // markAsChanged();
+        markAsChanged();
 
         cameraMoving(camera);
+        */
     }
 
     public double getLastViewAngle() {
@@ -160,12 +161,14 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
     public void cameraMoving(GL3DCamera camera) {
         // try {
         // Calculate camera direction vector
+    	
         GL3DMat4d camTrans = camera.getRotation().toMatrix().inverse();
         GL3DVec4d camDirection = new GL3DVec4d(0, 0, 1, 1);
         camDirection = camTrans.multiply(camDirection);
         camDirection.w = 0;
         camDirection.normalize();
-
+		
+    	
         // Convert layer orientation to heliocentric coordinate system
         CoordinateVector orientation = coordinateSystemView.getOrientation();
         CoordinateSystem targetSystem = new HeliocentricCartesianCoordinateSystem();
@@ -174,7 +177,7 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
 
         GL3DVec4d layerDirection = new GL3DVec4d(orientation.getValue(0), orientation.getValue(1), orientation.getValue(2), 0);
         layerDirection.normalize();
-
+		
         // Calculate view angle
         double cos = camDirection.dot(layerDirection);
         lastViewAngle = Math.acos(cos) / 3.141592654 * 180.0;
@@ -182,7 +185,7 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
         // angle can be NaN!
         if (Double.isNaN(lastViewAngle))
             lastViewAngle = 0.0;
-
+		/*
         // opacityFilter.setOpacity((float) ((cos+1.0)/2.0));
         opacityFilter.setOpacity((float) ((180.0 - lastViewAngle) / 180.0));
 
@@ -191,7 +194,7 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
         // System.out.println("Layer orientation: " + orientation +
         // " (coord system: " +
         // orientation.getCoordinateSystem().getClass().getName() + ")");
-
+        */
         /*
          * } catch(Exception e) { e.printStackTrace(); }
          */
