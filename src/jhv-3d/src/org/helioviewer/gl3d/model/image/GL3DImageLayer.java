af9fae9d5a29b3071732ab65874a1ad8af028c6a
Bogdan Nicula <bogdan@nicula.net>
2014-04-08 17:42:22 +0000

    client: GL3DImageLayer - dead code elimination; formatting guidelines changes
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@850 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d/src/org/helioviewer/gl3d/model/image/GL3DImageLayer.java
+++ src/jhv-3d/src/org/helioviewer/gl3d/model/image/GL3DImageLayer.java
@@ -59,7 +59,7 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
     private static int nextLayerId = 0;
     private final int layerId;
     private GL3DVec4d direction = new GL3DVec4d(0,0,1,0);
-    
+
     public int getLayerId() {
         return layerId;
     }
@@ -72,20 +72,20 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
     protected GL3DImageLayers layerGroup;
     public double minZ = -Constants.SunRadius;
     public double maxZ = Constants.SunRadius;
-    
+
     protected GL3DNode accellerationShape;
 
     protected boolean doUpdateROI = true;
 
     private ArrayList<Point> points = new ArrayList<Point>();
     private DynamicOpacityFilter opacityFilter;
-   
+
     private double lastViewAngle = 0.0;
-    
+
     protected GL gl;
     protected GL3DImageCoronaFragmentShaderProgram coronaFragmentShader = null;
-	protected GL3DImageFragmentShaderProgram sphereFragmentShader = null;
-    
+    protected GL3DImageFragmentShaderProgram sphereFragmentShader = null;
+
     public GL3DImageLayer(String name, GL3DView mainLayerView) {
         super(name);
         layerId = nextLayerId++;
@@ -126,7 +126,7 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
         while ((filterView = filterView.getAdapter(GLFilterView.class)) != null && opacityFilter == null) {
             Filter filter = ((GLFilterView) filterView).getFilter();
             if (DynamicOpacityFilter.class.isInstance(filter)){
-            	opacityFilter = (DynamicOpacityFilter) filter;
+                opacityFilter = (DynamicOpacityFilter) filter;
             }
         }
 
@@ -146,14 +146,12 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
             phi = Math.acos(orientationXZ.z);
             if (orientationXZ.x < 0) {
                 phi = 0 - phi;
-            }    
+            }
             phi = 0.0;
         }
         this.accellerationShape = new GL3DHitReferenceShape(true, phi);
         this.addNode(this.accellerationShape);
 
-
-        
         super.shapeInit(state);
 
         this.doUpdateROI = true;
@@ -166,11 +164,10 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
     }
 
     protected abstract void createImageMeshNodes(GL gl);
-    
+
     protected abstract GL3DImageMesh getImageCorona();
 
     protected abstract GL3DImageMesh getImageSphere();
-
 
     public void shapeUpdate(GL3DState state) {
         super.shapeUpdate(state);
@@ -192,95 +189,91 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
     public double getLastViewAngle() {
         return lastViewAngle;
     }
-    
+
     public void paint(Graphics g){
-    	
-    		for (Point p : points){
-    			g.fillRect(p.x-1, p.y-1, 2, 2);
-    		}
+        for (Point p : points){
+            g.fillRect(p.x-1, p.y-1, 2, 2);
+        }
     }
 
     public void cameraMoving(GL3DCamera camera) {
-    	GL3DMat4d camTrans = camera.getRotation().toMatrix().inverse();
-    	GL3DVec4d camDirection = new GL3DVec4d(0, 0, 1, 1);
-    	camDirection = camTrans.multiply(camDirection);
-    	camDirection.w = 0;
-    	camDirection.normalize();
+        GL3DMat4d camTrans = camera.getRotation().toMatrix().inverse();
+        GL3DVec4d camDirection = new GL3DVec4d(0, 0, 1, 1);
+        camDirection = camTrans.multiply(camDirection);
+        camDirection.w = 0;
+        camDirection.normalize();
 
-    	double angle = (Math.acos(camDirection.dot(direction))/Math.PI * 180.0);
-	    double maxAngle = 60;
-	    double minAngle = 30;
-    	if (angle != lastViewAngle){
-	    	lastViewAngle = angle;
-	    	float alpha = (float) ((Math.abs(90-lastViewAngle)-minAngle)/(maxAngle-minAngle));
-	    	if (this.coronaFragmentShader != null) this.coronaFragmentShader.changeAlpha(alpha);
-	    	
-	    }
+        double angle = Math.acos(camDirection.dot(direction)) / Math.PI * 180.0;
+        double maxAngle = 60;
+        double minAngle = 30;
+        if (angle != lastViewAngle) {
+            lastViewAngle = angle;
+            float alpha = (float) ((Math.abs(90-lastViewAngle)-minAngle)/(maxAngle-minAngle));
+            if (this.coronaFragmentShader != null) this.coronaFragmentShader.changeAlpha(alpha);
+        }
     }
-    
-    
-    
+
     public GL3DVec4d getLayerDirection()
     {
-		// Convert layer orientation to heliocentric coordinate system
-		/*CoordinateVector orientation = coordinateSystemView.getOrientation();
-		CoordinateSystem targetSystem = new HeliocentricCartesianCoordinateSystem();
-		CoordinateConversion converter = orientation.getCoordinateSystem().getConversion(targetSystem);
-		orientation = converter.convert(orientation);
-		
-    	GL3DVec3d layerDirection = new GL3DVec3d(orientation.getValue(0), orientation.getValue(1), orientation.getValue(2));*/
-    	
+        // Convert layer orientation to heliocentric coordinate system
+        /*CoordinateVector orientation = coordinateSystemView.getOrientation();
+        CoordinateSystem targetSystem = new HeliocentricCartesianCoordinateSystem();
+        CoordinateConversion converter = orientation.getCoordinateSystem().getConversion(targetSystem);
+        orientation = converter.convert(orientation);
+
+        GL3DVec3d layerDirection = new GL3DVec3d(orientation.getValue(0), orientation.getValue(1), orientation.getValue(2));*/
+
         //GL3DVec4d n = new GL3DVec4d(0, 0, 1, 1);
         //n = modelView().multiply(n);
-       
+
         //GL3DVec3d layerDirection = new GL3DVec3d(n.x, n.y, n.z);
-        
+
         //layerDirection.normalize();
         return direction;
     }
-    
-    
+
+
     public void setLayerDirection(GL3DVec4d direction){
-    	this.direction = direction;
+        this.direction = direction;
     }
-    
+
     public CoordinateSystem getCoordinateSystem() {
         return this.coordinateSystemView.getCoordinateSystem();
     }
 
     public CoordinateVector getOrientation() {
-    	HelioviewerMetaData h = (HelioviewerMetaData) this.metaDataView.getMetaData();
-    	CoordinateVector c = this.coordinateSystemView.getOrientation();
-    	return this.coordinateSystemView.getOrientation();
+        HelioviewerMetaData h = (HelioviewerMetaData) this.metaDataView.getMetaData();
+        CoordinateVector c = this.coordinateSystemView.getOrientation();
+        return this.coordinateSystemView.getOrientation();
     }
 
-	private void updateROI(GL3DCamera activeCamera) {
-		
-    	MetaData metaData = metaDataView.getMetaData();
+    private void updateROI(GL3DCamera activeCamera) {
+
+        MetaData metaData = metaDataView.getMetaData();
 
         if (metaData == null) {
             // No Image Data found
             return;
         }
+        /*
         double hh = Math.tan(Math.toRadians(activeCamera.getFOV() / 2)) * activeCamera.getClipNear();
         double hw = hh * activeCamera.getAspect();
         double pixelSize = hw / activeCamera.getWidth() * 2;
-        
-        
+        */
+
         GL3DRayTracer rayTracer = new GL3DRayTracer(this.accellerationShape, activeCamera);
-        
+
         // Shoot Rays in the corners of the viewport
         int width = (int) activeCamera.getWidth();
         int height = (int) activeCamera.getHeight();
         List<GL3DRay> regionTestRays = new ArrayList<GL3DRay>();
-		
+
         //frame.setVisible(true);
         //frame1.setVisible(true);
         for (int i = 0; i <= 10; i++){
-        	for (int j = 0; j <= 10; j++){
-        		
-        		regionTestRays.add(rayTracer.cast(i*(width/10), j*(height/10)));
-        	}
+            for (int j = 0; j <= 10; j++){
+                regionTestRays.add(rayTracer.cast(i*(width/10), j*(height/10)));
+            }
         }
 
         double minPhysicalX = Double.MAX_VALUE;
@@ -337,7 +330,7 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
         minPhysicalY = Math.max(minPhysicalY, metaData.getPhysicalLowerLeft().getY());
         maxPhysicalX = Math.min(maxPhysicalX, metaData.getPhysicalUpperRight().getX());
         maxPhysicalY = Math.min(maxPhysicalY, metaData.getPhysicalUpperRight().getY());
-        
+
         minPhysicalX -= Math.abs(minPhysicalX)*0.1;
         minPhysicalY -= Math.abs(minPhysicalY)*0.1;
         maxPhysicalX += Math.abs(maxPhysicalX)*0.1;
@@ -346,11 +339,10 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
         if (minPhysicalY < metaData.getPhysicalLowerLeft().getY()) minPhysicalY = metaData.getPhysicalLowerLeft().getX();
         if (maxPhysicalX > metaData.getPhysicalUpperRight().getX()) maxPhysicalX = metaData.getPhysicalUpperRight().getX();
         if (maxPhysicalY > metaData.getPhysicalUpperRight().getY()) maxPhysicalY = metaData.getPhysicalUpperRight().getY();
-		
-        
+
         double regionWidth = maxPhysicalX - minPhysicalX;
         double regionHeight = maxPhysicalY - minPhysicalY;
-        
+
         if (regionWidth > 0 && regionHeight > 0) {
             Region newRegion = StaticRegion.createAdaptedRegion(minPhysicalX, minPhysicalY, regionWidth, regionHeight);
             // Log.debug("GL3DImageLayer: '"+getName()+" set its region");
@@ -358,7 +350,6 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
         } else {
             Log.error("Illegal Region calculated! " + regionWidth + ":" + regionHeight + ". x = " + minPhysicalX + " - " + maxPhysicalX + ", y = " + minPhysicalY + " - " + maxPhysicalY);
         }
-        
     }
 
     public void setCoronaVisibility(boolean visible) {
@@ -367,7 +358,6 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
             if (node instanceof GL3DImageCorona) {
                 node.getDrawBits().set(Bit.Hidden, !visible);
             }
-
             node = node.getNext();
         }
     }
@@ -375,15 +365,12 @@ public abstract class GL3DImageLayer extends GL3DOrientedGroup implements GL3DCa
     protected GL3DImageTextureView getImageTextureView() {
         return this.imageTextureView;
     }
-    
-    
-    
+
     public void setLayerGroup(GL3DImageLayers layers) {
-    	layerGroup = layers;
-    }
-    
-    public GL3DImageLayers getLayerGroup() {
-    	return layerGroup;
+        layerGroup = layers;
     }
 
+    public GL3DImageLayers getLayerGroup() {
+        return layerGroup;
+    }
 }
