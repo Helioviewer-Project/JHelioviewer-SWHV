110b607989f81e620f93e21beaf61f2d43b2acfb
Bogdan Nicula <bogdan@nicula.net>
2014-04-11 14:30:52 +0000

    client: conventionally, as set by IAU, Sun has a radius of 696'000 km
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@928 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/base/src/org/helioviewer/base/physics/Constants.java
+++ src/base/src/org/helioviewer/base/physics/Constants.java
@@ -2,7 +2,7 @@ package org.helioviewer.base.physics;
 
 public class Constants {
 
-    public static final double SunRadius = 6.957e8;
+    public static final double SunRadius = 6.96e8;
 
     public static final double SunDiameter = 2 * SunRadius;
 
