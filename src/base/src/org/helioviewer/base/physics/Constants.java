dab82813f3439b48db6577ceba60fffc2033ef70
Freek Verstringe <freek.verstringe@oma.be>
2014-04-14 21:20:25 +0000

    Change constants
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@931 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/base/src/org/helioviewer/base/physics/Constants.java
+++ src/base/src/org/helioviewer/base/physics/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     public static final double SunRadiusInMeter = 6.96e8;
 
     public static final double SunDiameter = 2 * SunRadius;
-
-    public static final double SunMeanDistanceToEarth = 1.496e11;
+    
+    public static final double SunMeanDistanceToEarth = 149597870700.0;
 
     public static final double SunRadius2 = Constants.SunRadius * Constants.SunRadius;
 
