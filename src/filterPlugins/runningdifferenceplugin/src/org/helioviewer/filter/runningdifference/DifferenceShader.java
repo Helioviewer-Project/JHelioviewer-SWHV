ec122cd5e1801c9b923662eb9ea61851a2db0727
Freek Verstringe <freek.verstringe@oma.be>
2014-04-17 14:08:43 +0000

    put difference angle in difference shader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@946 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/filterPlugins/runningdifferenceplugin/src/org/helioviewer/filter/runningdifference/DifferenceShader.java
+++ src/filterPlugins/runningdifferenceplugin/src/org/helioviewer/filter/runningdifference/DifferenceShader.java
@@ -2,6 +2,7 @@ package org.helioviewer.filter.runningdifference;
 
 import javax.media.opengl.GL;
 
+import org.helioviewer.base.physics.Constants;
 import org.helioviewer.viewmodel.view.opengl.shader.GLFragmentShaderProgram;
 import org.helioviewer.viewmodel.view.opengl.shader.GLShaderBuilder;
 import org.helioviewer.viewmodel.view.opengl.shader.GLTextureCoordinate;
@@ -9,13 +10,17 @@ import org.helioviewer.viewmodel.view.opengl.shader.GLShaderBuilder.GLBuildShade
 
 public class DifferenceShader extends GLFragmentShaderProgram {
     private GLTextureCoordinate isDifference;
+    private GLTextureCoordinate differenceAngle;
+
 	private static int ID = 0;
     int mode = -1;
 
     public void setIsDifference(GL gl, float isDifference) {
     	this.isDifference.setValue(gl, isDifference);
     }
-
+    public void setDifferenceAngle(GL gl, float differenceAngle) {
+    	this.differenceAngle.setValue(gl, differenceAngle);
+    }
     /**
      * {@inheritDoc}
      */
@@ -23,7 +28,9 @@ public class DifferenceShader extends GLFragmentShaderProgram {
 
         try {
             isDifference = shaderBuilder.addTexCoordParameter(1);
-            String program = "if(isdifference>0.5){\toutput.r = (output.r - tex2D(differenceImage, texcoord0.xy).r)/output.r;";
+            differenceAngle = shaderBuilder.addTexCoordParameter(1);
+
+            String program = "if(isdifference>0.5){\toutput.r = ( output.r - tex2D(differenceImage, texcoord0.xy).r)/(output.r);";
             program += "\tvec4 tr = vec4(0.05f,0.05f,0.05f,0.05f);";
             program += "output.r = (output.r + 1.0f)/2.0f;}";
             program = program.replaceAll("output", shaderBuilder.useOutputValue("float4", "COLOR"));
@@ -32,6 +39,10 @@ public class DifferenceShader extends GLFragmentShaderProgram {
             program = program.replaceAll("differenceImage", "differenceImage" + 3);
             ID = (ID + 1) & 15;
             program = program.replace("isdifference", isDifference.getIdentifier(1));
+            program = program.replace("differenceAngle", differenceAngle.getIdentifier(1));
+            
+            program = program.replace("physicalPosition", shaderBuilder.useStandardParameter("float4", "TEXCOORD0"));
+            program = program.replace("sunRadius", Double.toString(Constants.SunRadius).replace(',', '.'));            
             shaderBuilder.addMainFragment(program);
             System.out.println("SHADERDIFF: " + shaderBuilder.getCode());
         } catch (GLBuildShaderException e) {
