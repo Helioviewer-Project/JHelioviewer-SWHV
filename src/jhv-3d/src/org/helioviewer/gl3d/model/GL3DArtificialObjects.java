b23a3137efa21183e6f8ff0e816971192e65ceeb
Freek Verstringe <freek.verstringe@oma.be>
2014-04-17 14:08:44 +0000

    temporary remove grid to better see OpenGL traces
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@948 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d/src/org/helioviewer/gl3d/model/GL3DArtificialObjects.java
+++ src/jhv-3d/src/org/helioviewer/gl3d/model/GL3DArtificialObjects.java
@@ -18,10 +18,10 @@ public class GL3DArtificialObjects extends GL3DGroup {
 
     public GL3DArtificialObjects() {
         super("Artificial Objects");
-        GL3DGroup indicatorArrows = new GL3DModel("Arrows", "Arrows indicating the viewspace axes");
-        this.addNode(indicatorArrows);
-        GL3DGrid grid = new GL3DGrid("grid", 20, 20, new GL3DVec4f(1.0f,0.0f,0.0f,1.0f), new GL3DVec4d(0.0,1.0,0.0,1.0));
-        this.addNode(grid);
+        //GL3DGroup indicatorArrows = new GL3DModel("Arrows", "Arrows indicating the viewspace axes");
+        //this.addNode(indicatorArrows);
+        //GL3DGrid grid = new GL3DGrid("grid", 20, 20, new GL3DVec4f(1.0f,0.0f,0.0f,1.0f), new GL3DVec4d(0.0,1.0,0.0,1.0));
+        //this.addNode(grid);
         //GL3DSphere blackSphere = new GL3DSphere(0.990*Constants.SunRadius, 20,20, new GL3DVec4f(0.0f, 0.0f, 0.0f, 1.0f) );
         //this.addNode(blackSphere);
 /*        GL3DShape xAxis = new GL3DArrow("X-Axis", Constants.SunRadius / 20, Constants.SunRadius, 32, new GL3DVec4f(1, 0, 0.5f, 0.2f));
