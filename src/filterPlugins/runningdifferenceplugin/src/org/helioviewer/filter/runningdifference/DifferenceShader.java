0f5ba2ede9919828ec90afcb5e0db1c503668430
Freek Verstringe <freek.verstringe@oma.be>
2014-04-18 13:31:00 +0000

    remove some junk from shader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@983 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/filterPlugins/runningdifferenceplugin/src/org/helioviewer/filter/runningdifference/DifferenceShader.java
+++ src/filterPlugins/runningdifferenceplugin/src/org/helioviewer/filter/runningdifference/DifferenceShader.java
@@ -29,20 +29,19 @@ public class DifferenceShader extends GLFragmentShaderProgram {
         try {
             isDifference = shaderBuilder.addTexCoordParameter(1);
             differenceAngle = shaderBuilder.addTexCoordParameter(1);
-
-            String program = "if(isdifference>0.5){\toutput.r = ( output.r - tex2D(differenceImage, texcoord0.xy).r)/(output.r);";
+            String program = "";
+            program = "if(isdifference>0.5){\toutput.r = ( output.r - tex2D(differenceImage, texcoord0.xy).r)/(output.r);";
             program += "\tvec4 tr = vec4(0.05f,0.05f,0.05f,0.05f);";
             program += "output.r = (output.r + 1.0f)/2.0f;}";
+            //program = "\toutput.rgb = tex2D(differenceImage, texcoord0.xy).rgb;";
+
             program = program.replaceAll("output", shaderBuilder.useOutputValue("float4", "COLOR"));
             
-            mode = shaderBuilder.addTextureParameter("sampler2D differenceImage" + 3);
-            program = program.replaceAll("differenceImage", "differenceImage" + 3);
+            mode = shaderBuilder.addTextureParameter("sampler2D differenceImage");
             ID = (ID + 1) & 15;
             program = program.replace("isdifference", isDifference.getIdentifier(1));
             program = program.replace("differenceAngle", differenceAngle.getIdentifier(1));
             
-            program = program.replace("physicalPosition", shaderBuilder.useStandardParameter("float4", "TEXCOORD0"));
-            program = program.replace("sunRadius", Double.toString(Constants.SunRadius).replace(',', '.'));            
             shaderBuilder.addMainFragment(program);
             System.out.println("SHADERDIFF: " + shaderBuilder.getCode());
         } catch (GLBuildShaderException e) {
