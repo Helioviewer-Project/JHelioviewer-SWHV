798d403c01ed939ee0d7032b2997d6d6d1cd287d
Freek Verstringe <freek.verstringe@oma.be>
2014-05-12 13:08:43 +0000

    Make parameterList accessible
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1125 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/shader/GLShaderBuilder.java
+++ src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/shader/GLShaderBuilder.java
@@ -9,29 +9,29 @@ import org.helioviewer.base.logging.Log;
 
 /**
  * Class to build new OpenGL shader build in Cg.
- * 
+ *
  * <p>
  * Since a graphics card only supports one active vertex shader and one active
  * fragment shader at a time, it is necessary to merge all operations that
  * should be applied to one image into one big, final shader.
- * 
+ *
  * <p>
  * Therefore, this class provides functions to add different input and output
  * parameter to the current shader as well as adding code pieces. After adding
  * all different parts of the shader, they can be compiled into one single
  * shader. For programming, Cg (C for Graphics, developed by NVIDIA) is used as
  * the shading language. To use this class, you have to be familiar with Cg.
- * 
+ *
  * <p>
  * When adding code pieces, it is highly recommended to use a specific prefix,
  * so that your names do not collide with names from other parts.
- * 
+ *
  * <p>
  * For further informations about programming shaders, refer to the Cg User
  * Manual.
- * 
+ *
  * @author Markus Langenberg
- * 
+ *
  */
 public class GLShaderBuilder {
 
@@ -45,23 +45,23 @@ public class GLShaderBuilder {
 
     private static char[] coordinateDimension = { 'x', 'y', 'z', 'w' };
 
-    private LinkedList<String> outputStruct = new LinkedList<String>();
-    private LinkedList<String> standardParameterList = new LinkedList<String>();
-    private LinkedList<String> parameterList = new LinkedList<String>();
-    private HashMap<String, String> outputTypes = new HashMap<String, String>();
-    private HashMap<String, String> outputInit = new HashMap<String, String>();
-    private HashMap<String, String> standardParameterTypes = new HashMap<String, String>();
+    private final LinkedList<String> outputStruct = new LinkedList<String>();
+    private final LinkedList<String> standardParameterList = new LinkedList<String>();
+    private final LinkedList<String> parameterList = new LinkedList<String>();
+    private final HashMap<String, String> outputTypes = new HashMap<String, String>();
+    private final HashMap<String, String> outputInit = new HashMap<String, String>();
+    private final HashMap<String, String> standardParameterTypes = new HashMap<String, String>();
 
-    private int[] componentsAvailableInTexCoord;
+    private final int[] componentsAvailableInTexCoord;
 
     private int nextTexUnit = 1;
     private int nextConstantRegister = 0;
     private int nextVertexAttribute = 0;
     private static int highestTexCoordEverUsed = 0;
 
-    private GL gl;
-    private int shaderID;
-    private int type;
+    private final GL gl;
+    private final int shaderID;
+    private final int type;
 
     private String functions = "";
     private String mainBody = "";
@@ -69,7 +69,7 @@ public class GLShaderBuilder {
     /**
      * Initializes the static members of the shader builder. This has to happen
      * during runtime, since many of this values are driver dependent.
-     * 
+     *
      * @param gl
      *            current GL object
      */
@@ -102,7 +102,7 @@ public class GLShaderBuilder {
 
     /**
      * Generates a new GLShaderBuilder.
-     * 
+     *
      * @param gl
      *            Valid reference to the current gl object
      * @param type
@@ -134,7 +134,7 @@ public class GLShaderBuilder {
      * Returns the GL object assigned to this ShaderBuilder. Warning: Only use
      * this function within a build*Shader-method, otherwise the object might
      * not be valid.
-     * 
+     *
      * @return Reference to the current gl object
      */
     public GL getGL() {
@@ -144,7 +144,7 @@ public class GLShaderBuilder {
     /**
      * Returns OpenGL shader id assigned to this shader. To use this shader,
      * bind shader id.
-     * 
+     *
      * @return assigned shader id
      */
     public int getShaderID() {
@@ -154,7 +154,7 @@ public class GLShaderBuilder {
     /**
      * Advises the GLShaderBuilder to provide this output value in its output.
      * Typical values are for example POSITION, COLOR or TEXCOORD0
-     * 
+     *
      * @param type
      *            the type of the output value to use (since this is not a
      *            1:1-binding to the value)
@@ -185,7 +185,7 @@ public class GLShaderBuilder {
      * Advises the GLShaderBuilder to provide this output value in its output,
      * initialing it the given value. Typical values are for example POSITION,
      * COLOR or TEXCOORD0
-     * 
+     *
      * @param type
      *            the type of the output value to use (since this is not a
      *            1:1-binding to the value)
@@ -220,7 +220,7 @@ public class GLShaderBuilder {
      * parameter list. This function should be used for all kind of parameters,
      * which do not have a special function and may be used by multiple code
      * fragments, such as COLOR, TEXUNIT0, TEXCOORD0 or all members of state.
-     * 
+     *
      * @param type
      *            the type of the standard parameter to use (since this is not a
      *            1:1-binding to the parameter)
@@ -239,7 +239,7 @@ public class GLShaderBuilder {
         if (!parameter.contains(".")) {
             parameter = parameter.toUpperCase();
         }
-        for (String param : parameterList) {
+        for (String param : getParameterList()) {
             if (param.contains(parameter)) {
                 if (!param.toLowerCase().contains(type.toLowerCase())) {
                     throw new GLBuildShaderException("Conflict between parameter types for " + parameter + ": " + type + " vs. " + param.substring(0, param.indexOf(' ')));
@@ -270,7 +270,7 @@ public class GLShaderBuilder {
      * coordinate is added to the parameter list. It returns an abstract
      * representation of the texture coordinate, which has be used to access the
      * values. The object wrappes the call to the actual OpenGL functions
-     * 
+     *
      * @param numDimensions
      *            Number of values to add to texture coordinate
      * @return GLTextureCoordinate presenting the new values
@@ -290,14 +290,14 @@ public class GLShaderBuilder {
 
                 String searchFor = "TEXCOORD" + texCoord;
                 boolean alreadyUsed = standardParameterList.contains(searchFor);
-                for (String parameter : parameterList) {
+                for (String parameter : getParameterList()) {
                     if (parameter.contains(searchFor)) {
                         alreadyUsed = true;
                         break;
                     }
                 }
                 if (!alreadyUsed) {
-                    parameterList.add("float4 " + identifier + " : TEXCOORD" + texCoord);
+                    getParameterList().add("float4 " + identifier + " : TEXCOORD" + texCoord);
                 }
 
                 identifier += ".";
@@ -321,7 +321,7 @@ public class GLShaderBuilder {
      * Adds a new texture unit to the parameter list. It returns the OpenGL
      * constant, which has to be used by glActiveTexture to access this
      * parameter, for example GL_TEXTURE1 or GL_TEXTURE4.
-     * 
+     *
      * @param declaration
      *            declaration of the new parameter, including type and name. for
      *            example "sampler2D myTexture".
@@ -331,7 +331,7 @@ public class GLShaderBuilder {
      */
     public int addTextureParameter(String declaration) throws GLBuildShaderException {
         if (nextTexUnit < maxTexUnits) {
-            parameterList.add("uniform " + declaration.trim() + " : TEXUNIT" + nextTexUnit);
+            getParameterList().add("uniform " + declaration.trim() + " : TEXUNIT" + nextTexUnit);
             return GL.GL_TEXTURE0 + nextTexUnit++;
         } else {
             throw new GLBuildShaderException("Number of available texture units exceeded (Max: " + maxTexUnits + ")");
@@ -342,7 +342,7 @@ public class GLShaderBuilder {
      * Adds a new environment parameter to the parameter list. It returns the
      * index, which has to be used by glProgramEnvParameter*ARB to access this
      * parameter.
-     * 
+     *
      * @param declaration
      *            declaration of the new parameter, including type and name. for
      *            example "float2 myParam".
@@ -353,7 +353,7 @@ public class GLShaderBuilder {
      */
     public int addEnvParameter(String declaration) throws GLBuildShaderException {
         if (nextConstantRegister < maxConstantRegisters) {
-            parameterList.add("uniform " + declaration.trim() + " : C" + nextConstantRegister);
+            getParameterList().add("uniform " + declaration.trim() + " : C" + nextConstantRegister);
             // parameterList.add("uniform " + declaration.trim() +
             // nextConstantRegister);
             return nextConstantRegister++;
@@ -366,7 +366,7 @@ public class GLShaderBuilder {
      * Adds a new generic vertex attribute to the parameter list. It returns the
      * index, which has to be used by glVertexAttrib*ARB to access this
      * parameter.
-     * 
+     *
      * @param declaration
      *            declaration of the new parameter, including type and name. for
      *            example "float2 myParam".
@@ -377,7 +377,7 @@ public class GLShaderBuilder {
      */
     public int addVertexAttribute(String declaration) throws GLBuildShaderException {
         if (nextVertexAttribute < maxVertexAttributes) {
-            parameterList.add(declaration.trim() + " : ATTR" + nextVertexAttribute);
+            getParameterList().add(declaration.trim() + " : ATTR" + nextVertexAttribute);
             return nextVertexAttribute++;
         } else {
             throw new GLBuildShaderException("Number of available vertex attributes exceeded (Max: " + maxVertexAttributes + ")");
@@ -390,7 +390,7 @@ public class GLShaderBuilder {
      * parameters and body. Make sure to add a code fragment to the main
      * function by calling addMainFragment(), where the new function is called
      * in some way.
-     * 
+     *
      * @param code
      *            new code fragment
      */
@@ -402,7 +402,7 @@ public class GLShaderBuilder {
      * Adds a new code fragment to the program code. Use the names returned by
      * useOutputValue and useStandardParameter or the ones assigned by yourself
      * calling a add*-method.
-     * 
+     *
      * @param code
      *            new code fragment
      */
@@ -423,7 +423,7 @@ public class GLShaderBuilder {
             for (int i = 0; i <= highestTexCoordEverUsed; i++) {
                 String searchFor = "TEXCOORD" + i;
                 boolean alreadyUsed = outputStruct.contains(searchFor);
-                for (String parameter : parameterList) {
+                for (String parameter : getParameterList()) {
                     if (parameter.contains(searchFor)) {
                         alreadyUsed = true;
                         break;
@@ -463,7 +463,7 @@ public class GLShaderBuilder {
             }
         }
 
-        for (String parameter : parameterList) {
+        for (String parameter : getParameterList()) {
             finalCode += LINE_SEP + '\t' + parameter + ',';
         }
 
@@ -484,11 +484,11 @@ public class GLShaderBuilder {
 
     /**
      * Compiles the given shaders and creates a new shader builder to continue.
-     * 
+     *
      * The new shader builder is initialized with a GLMinimalXShaderProgram.
      * This function might be useful, if multiple shaders should be used on path
      * path through the view chain.
-     * 
+     *
      * @param shaderBuilder
      * @return new shader builder
      */
@@ -522,14 +522,14 @@ public class GLShaderBuilder {
     /**
      * GLTextureCoordinate implementation for the all texture coordinates
      * managed by GLShaderBuilder
-     * 
+     *
      * @see GLShaderBuilder#addTextureParameter(String)
      */
     private class GLShaderTextureCoordinate extends GLTextureCoordinate {
 
         /**
          * Default constructor.
-         * 
+         *
          * @param target
          *            OpenGL constant, representing the texture coordinate, such
          *            as GL_TEXTURE0 or GL_TEXTURE3
@@ -571,7 +571,7 @@ public class GLShaderBuilder {
             }
         }
 
-        for (String parameter : parameterList) {
+        for (String parameter : getParameterList()) {
             finalCode += LINE_SEP + '\t' + parameter + ',';
         }
 
@@ -588,6 +588,10 @@ public class GLShaderBuilder {
         finalCode += LINE_SEP + mainBody + "\treturn OUT;" + LINE_SEP + '}';
 
         return finalCode;
+    }
+
+    public LinkedList<String> getParameterList() {
+        return parameterList;
     }
 
 }
