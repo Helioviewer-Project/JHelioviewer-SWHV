ef80d7b1b3884b42ec1cf10611cc494c68f00da4
Freek Verstringe <freek.verstringe@oma.be>
2014-05-13 07:15:21 +0000

    Add truncation value, base difference
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1148 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/filterPlugins/runningdifferenceplugin/src/org/helioviewer/filter/runningdifference/RunningDifferencePanel.java
+++ src/filterPlugins/runningdifferenceplugin/src/org/helioviewer/filter/runningdifference/RunningDifferencePanel.java
@@ -1,9 +1,13 @@
 package org.helioviewer.filter.runningdifference;
 
 import javax.swing.JCheckBox;
+import javax.swing.JSpinner;
+import javax.swing.JTextField;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import org.helioviewer.basegui.components.WheelSupport;
 import org.helioviewer.jhv.display.Displayer;
 import org.helioviewer.viewmodel.filter.Filter;
 import org.helioviewer.viewmodelplugin.filter.FilterPanel;
@@ -11,11 +15,11 @@ import org.helioviewer.viewmodelplugin.filter.FilterTabPanelManager.Area;
 
 /**
  * Panel to control running differences
- * 
+ *
  * @author Helge Dietert
- * 
+ *
  */
-public class RunningDifferencePanel extends FilterPanel {
+public class RunningDifferencePanel extends FilterPanel implements ChangeListener {
     /**
      * Generated serial id from Eclipse
      */
@@ -23,11 +27,13 @@ public class RunningDifferencePanel extends FilterPanel {
     /**
      * Box if the running difference filter should be active
      */
-    private JCheckBox activeBox;
+    private final JCheckBox activeBox;
     /**
      * Controlled filter by this panel
      */
     private RunningDifferenceFilter filter;
+    private final JSpinner truncateSpinner;
+    private final JCheckBox baseDifferenceBox;
 
     /**
      * Creates a new panel to control the running difference. Not active until a
@@ -36,17 +42,47 @@ public class RunningDifferencePanel extends FilterPanel {
     public RunningDifferencePanel() {
         activeBox = new JCheckBox("Enable running difference");
         activeBox.addChangeListener(new ChangeListener() {
+            @Override
             public void stateChanged(ChangeEvent e) {
                 if (filter != null) {
                     filter.setActive(activeBox.isSelected());
                     Displayer.getSingletonInstance().render();
+                    Displayer.getSingletonInstance().display();
                 }
             }
         });
         add(activeBox);
+        baseDifferenceBox = new JCheckBox("Base difference");
+        baseDifferenceBox.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                if (filter != null) {
+                    filter.setBaseDifference(baseDifferenceBox.isSelected());
+                    Displayer.getSingletonInstance().display();
+                }
+            }
+        });
+        add(baseDifferenceBox);
+        truncateSpinner = new JSpinner();
+        truncateSpinner.setModel(new SpinnerNumberModel(new Float(1), new Float(0), new Float(1), new Float(0.01f)));
+        truncateSpinner.addChangeListener(this);
+
+        JSpinner.NumberEditor editor = new JSpinner.NumberEditor(truncateSpinner, "0%");
+        truncateSpinner.setEditor(editor);
+        editor.getTextField().setColumns(3);
+        editor.getTextField().setHorizontalAlignment(JTextField.CENTER);
+        editor.getTextField().setValue(0.05);
+        WheelSupport.installMouseWheelSupport(truncateSpinner);
+        add(truncateSpinner);
         setEnabled(false);
     }
-
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        if (filter != null) {
+            float value = ((SpinnerNumberModel) truncateSpinner.getModel()).getNumber().floatValue();
+            filter.setTruncationvalue(value);
+        }
+    }
     /**
      * @see org.helioviewer.viewmodelplugin.filter.FilterPanel#getArea()
      */
