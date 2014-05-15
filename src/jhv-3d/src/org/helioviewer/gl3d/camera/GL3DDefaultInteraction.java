c64daa8aad49040addcb54d19de5e87b67979cb0
Freek Verstringe <freek.verstringe@oma.be>
2014-05-15 21:39:23 +0000

    Do not clear rotation
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1181 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d/src/org/helioviewer/gl3d/camera/GL3DDefaultInteraction.java
+++ src/jhv-3d/src/org/helioviewer/gl3d/camera/GL3DDefaultInteraction.java
@@ -12,9 +12,9 @@ import org.helioviewer.viewmodel.view.MetaDataView;
  * Default {@link GL3DInteraction} class that provides a reference to the
  * {@link GL3DSceneGraphView}. Default behavior includes camera reset on double
  * click.
- * 
+ *
  * @author Simon Spoerri (simon.spoerri@fhnw.ch)
- * 
+ *
  */
 public abstract class GL3DDefaultInteraction extends GL3DInteraction {
     private static final double ZOOM_WHEEL_FACTOR = 1.0 / 20;
@@ -27,10 +27,12 @@ public abstract class GL3DDefaultInteraction extends GL3DInteraction {
         this.reset();
     }
 
+    @Override
     public void reset(GL3DCamera camera) {
         reset();
     }
 
+    @Override
     public void mouseClicked(MouseEvent e, GL3DCamera camera) {
         if (e.getClickCount() == 2) {
             reset();
@@ -46,18 +48,17 @@ public abstract class GL3DDefaultInteraction extends GL3DInteraction {
             double distance = halfWidth * Math.sin(Math.PI / 2 - halfFOVRad) / Math.sin(halfFOVRad);
             distance = -distance - camera.getZTranslation();
             // Log.debug("GL3DZoomFitAction: Distance = "+distance+" Existing Distance: "+camera.getZTranslation());
-            this.camera.getRotation().clear();
             camera.addCameraAnimation(new GL3DCameraZoomAnimation(distance, 500));
             GL3DVec3d cameraTranslation = this.camera.getTranslation().copy();
             cameraTranslation.negate();
             camera.addCameraAnimation(new GL3DCameraPanAnimation(cameraTranslation));
         } else {
             camera.setZTranslation(-GL3DTrackballCamera.DEFAULT_CAMERA_DISTANCE);
-            this.camera.getRotation().clear();
             this.camera.updateCameraTransformation();
         }
     }
 
+    @Override
     public void mouseWheelMoved(MouseWheelEvent e, GL3DCamera camera) {
         double zoomDistance = -(e.getUnitsToScroll()) * camera.getDistanceToSunSurface() * GL3DDefaultInteraction.ZOOM_WHEEL_FACTOR;
         camera.addCameraAnimation(new GL3DCameraZoomAnimation(zoomDistance));
