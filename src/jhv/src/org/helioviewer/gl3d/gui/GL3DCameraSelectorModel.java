8cc530e9ccd472d01464e4eb1db37a59371c1a92
Freek Verstringe <freek.verstringe@oma.be>
2014-05-15 21:39:24 +0000

    Formatting
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1185 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/gui/GL3DCameraSelectorModel.java
+++ src/jhv/src/org/helioviewer/gl3d/gui/GL3DCameraSelectorModel.java
@@ -20,16 +20,16 @@ import org.helioviewer.viewmodel.view.ComponentView;
  * Can be used as the global singleton for all available and the currently
  * active {@link GL3DCamera}. Also it implements the {@link ComboBoxModel} and
  * {@link ListModel} and can thus be used for GUI elements directly.
- * 
+ *
  * @author Simon Spoerri (simon.spoerri@fhnw.ch)
- * 
+ *
  */
 public class GL3DCameraSelectorModel extends AbstractListModel implements ComboBoxModel {
     private static final long serialVersionUID = 1L;
 
     private static GL3DCameraSelectorModel instance;
 
-    private List<GL3DCamera> cameras = new ArrayList<GL3DCamera>();
+    private final List<GL3DCamera> cameras = new ArrayList<GL3DCamera>();
 
     private GL3DCamera defaultCamera;
 
@@ -91,14 +91,17 @@ public class GL3DCameraSelectorModel extends AbstractListModel implements ComboB
         return getCameraView().getCurrentCamera();
     }
 
+    @Override
     public Object getElementAt(int index) {
         return cameras.get(index);
     }
 
+    @Override
     public int getSize() {
         return cameras.size();
     }
 
+    @Override
     public GL3DCamera getSelectedItem() {
         return getCameraView().getCurrentCamera();
     }
@@ -108,6 +111,7 @@ public class GL3DCameraSelectorModel extends AbstractListModel implements ComboB
         getCameraView().setCurrentCamera(camera);
     }
 
+    @Override
     public void setSelectedItem(Object anItem) {
         if (anItem instanceof GL3DCamera) {
             setCurrentCamera((GL3DCamera) anItem);
