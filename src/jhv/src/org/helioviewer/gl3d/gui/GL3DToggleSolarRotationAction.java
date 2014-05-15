e779f8af02848b08d1e6aad1fedc9dd8ce369bac
Freek Verstringe <freek.verstringe@oma.be>
2014-05-15 21:39:25 +0000

    Change camera switch logic
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1186 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/gui/GL3DToggleSolarRotationAction.java
+++ src/jhv/src/org/helioviewer/gl3d/gui/GL3DToggleSolarRotationAction.java
@@ -6,14 +6,15 @@ import javax.swing.AbstractAction;
 
 import org.helioviewer.gl3d.camera.GL3DCamera;
 import org.helioviewer.gl3d.camera.GL3DSolarRotationTrackingTrackballCamera;
+import org.helioviewer.gl3d.camera.GL3DTrackballCamera;
 
 /**
  * Action that enables the Solar Rotation Tracking, which ultimately changes the
  * current {@link GL3DCamera} to the
  * {@link GL3DSolarRotationTrackingTrackballCamera}
- * 
+ *
  * @author Simon Spoerri (simon.spoerri@fhnw.ch)
- * 
+ *
  */
 public class GL3DToggleSolarRotationAction extends AbstractAction {
 
@@ -26,11 +27,12 @@ public class GL3DToggleSolarRotationAction extends AbstractAction {
     /**
      * {@inheritDoc}
      */
+    @Override
     public void actionPerformed(ActionEvent e) {
-        if (GL3DCameraSelectorModel.getInstance().getCurrentCamera() instanceof GL3DSolarRotationTrackingTrackballCamera) {
-            GL3DCameraSelectorModel.getInstance().setCurrentCamera(GL3DCameraSelectorModel.getInstance().getTrackballCamera());
-        } else {
+        if (GL3DCameraSelectorModel.getInstance().getCurrentCamera() instanceof GL3DTrackballCamera) {
             GL3DCameraSelectorModel.getInstance().setCurrentCamera(GL3DCameraSelectorModel.getInstance().getSolarRotationCamera());
+        } else {
+            GL3DCameraSelectorModel.getInstance().setCurrentCamera(GL3DCameraSelectorModel.getInstance().getTrackballCamera());
         }
     }
 
