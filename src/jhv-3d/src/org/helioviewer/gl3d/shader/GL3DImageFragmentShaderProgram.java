6a0c17d4f53c46c590abf974aec02104a371ca48
Freek Verstringe <freek.verstringe@oma.be>
2014-05-16 08:27:30 +0000

    Set difference image params in fragment shader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1206 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d/src/org/helioviewer/gl3d/shader/GL3DImageFragmentShaderProgram.java
+++ src/jhv-3d/src/org/helioviewer/gl3d/shader/GL3DImageFragmentShaderProgram.java
@@ -13,7 +13,14 @@ public class GL3DImageFragmentShaderProgram extends GLFragmentShaderProgram {
     private double phi;
     private double xxTextureScale = 1.0;
     private double yyTextureScale = 1.0;
-
+    private double differenceXTextureScale;
+    private double differenceYTextureScale;
+    private double differenceXOffset;
+    private double differenceYOffset;
+    private double differenceTheta;
+    private double differencePhi;
+    private double differenceXScale;
+    private double differenceYScale;
     public GL3DImageFragmentShaderProgram() {
     }
 
@@ -28,11 +35,12 @@ public class GL3DImageFragmentShaderProgram extends GLFragmentShaderProgram {
         bind(gl, shaderID, cutOffRadius, xxTextureScale, yyTextureScale, theta, phi);
     }
 
-    private static void bind(GL gl, int shader, double cutOffRadius, double xTextureScale, double yTextureScale, double theta, double phi) {
+    private void bind(GL gl, int shader, double cutOffRadius, double xTextureScale, double yTextureScale, double theta, double phi) {
         shaderCurrentlyUsed = shader;
         gl.glBindProgramARB(target, shader);
         gl.glProgramLocalParameter4dARB(target, 0, cutOffRadius, 0.0f, 0.0f, 0.0f);
         gl.glProgramLocalParameter4dARB(target, 1, xTextureScale, yTextureScale, theta, phi);
+        gl.glProgramLocalParameter4dARB(target, 2, differenceXTextureScale, differenceYTextureScale, differenceTheta, differencePhi);
     }
 
     /**
@@ -66,7 +74,7 @@ public class GL3DImageFragmentShaderProgram extends GLFragmentShaderProgram {
         Integer restoreShaderObject = shaderStack.pop();
         int restoreShader = restoreShaderObject == null ? 0 : restoreShaderObject.intValue();
         if (restoreShader >= 0) {
-            bind(gl, restoreShader, 0.0f, 0.0, 0.0, 0.0, 0.0);
+            //bind(gl, restoreShader, 0.0f, 0.0, 0.0, 0.0, 0.0);
         }
     }
 
@@ -74,7 +82,7 @@ public class GL3DImageFragmentShaderProgram extends GLFragmentShaderProgram {
     protected void buildImpl(GLShaderBuilder shaderBuilder) {
         try {
             String program = "\tif(texcoord0.x<0.0||texcoord0.y<0.0||texcoord0.x>textureScaleThetaPhi.x||texcoord0.y>textureScaleThetaPhi.y){" + "\t\tOUT.color = float4(1.0,0.0,0.0,1.0);" + GLShaderBuilder.LINE_SEP + "\t}" + GLShaderBuilder.LINE_SEP;
-            //program = "\tif(texcoord4.x<0.0||texcoord4.y<0.0||texcoord4.x>textureScaleThetaPhi.x||texcoord4.y>textureScaleThetaPhi.y){" + "\t\tOUT.color = float4(0.0,1.0,0.0,1.0);" + GLShaderBuilder.LINE_SEP + "\t}" + GLShaderBuilder.LINE_SEP;
+            program = "\tif((texcoord1.x>0.255&&texcoord1.x<0.995) && (texcoord4.x<0.0||texcoord4.y<0.0||texcoord4.x>diffTextureScaleThetaPhi.x||texcoord4.y>diffTextureScaleThetaPhi.y)){" + "\t\tOUT.color = float4(0.0,1.0,0.0,1.0);" + GLShaderBuilder.LINE_SEP + "\t}" + GLShaderBuilder.LINE_SEP;
 
             //program += "\tOUT.color.a=1.;" + GLShaderBuilder.LINE_SEP;
             program += "\tfloat theta = textureScaleThetaPhi.z;" + GLShaderBuilder.LINE_SEP;
@@ -96,6 +104,8 @@ public class GL3DImageFragmentShaderProgram extends GLFragmentShaderProgram {
 
             shaderBuilder.addEnvParameter("float cutOffRadius");
             shaderBuilder.addEnvParameter("float4 textureScaleThetaPhi");
+            shaderBuilder.addEnvParameter("float4 diffTextureScaleThetaPhi");
+
             program = program.replace("position", shaderBuilder.useStandardParameter("float4", "TEXCOORD3"));
 
             program = program.replace("output", shaderBuilder.useOutputValue("float4", "COLOR"));
@@ -123,4 +133,19 @@ public class GL3DImageFragmentShaderProgram extends GLFragmentShaderProgram {
         this.phi = phi;
     }
 
+    public void changeDifferenceTextureScale(double scaleX, double scaleY) {
+        this.differenceXTextureScale = scaleX;
+        this.differenceYTextureScale = scaleY;
+    }
+    public void changeDifferenceAngles(double theta, double phi) {
+        this.differenceTheta = theta;
+        this.differencePhi = phi;
+    }
+
+    public void setDifferenceRect(double differenceXOffset, double differenceYOffset, double differenceXScale, double differenceYScale) {
+        this.differenceXOffset = differenceXOffset;
+        this.differenceYOffset = differenceYOffset;
+        this.differenceXScale = differenceXScale;
+        this.differenceYScale = differenceYScale;
+    }
 }
