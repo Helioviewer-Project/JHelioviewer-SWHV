29491c24571286f98812e800a67972889a7aa9db
Freek Verstringe <freek.verstringe@oma.be>
2014-05-19 13:44:28 +0000

    Distinguish JP2/JPX take3
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1243 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/AbstractGLView.java
+++ src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/AbstractGLView.java
@@ -9,19 +9,19 @@ import org.helioviewer.viewmodel.view.RegionView;
 import org.helioviewer.viewmodel.view.SubimageDataView;
 import org.helioviewer.viewmodel.view.View;
 import org.helioviewer.viewmodel.view.ViewListener;
-import org.helioviewer.viewmodel.view.jp2view.JHVJPXView;
+import org.helioviewer.viewmodel.view.jp2view.JHVJP2View;
 
 /**
  * Abstract base class implementing GLView, providing some common OpenGL
  * functions.
- * 
+ *
  * <p>
  * This class provides some functions common or useful for all OpenGL views,
  * including the capability to render its successor and handle the case, it the
  * successor is not a GLView.
- * 
+ *
  * @author Markus Langenberg
- * 
+ *
  */
 public abstract class AbstractGLView extends AbstractBasicView implements GLView, ModifiableInnerViewView, ViewListener {
 
@@ -30,18 +30,19 @@ public abstract class AbstractGLView extends AbstractBasicView implements GLView
     /**
      * {@inheritDoc}
      */
+    @Override
     public void viewChanged(View sender, ChangeEvent aEvent) {
         notifyViewListeners(aEvent);
     }
 
     /**
      * Renders the successor (or child) or this view.
-     * 
+     *
      * This is a service function for all GLViews, so that they do not have to
      * take care whether their child is a GLView itself or not. The child is
      * always rendered in the correct way, so other GLViews may call this
      * function during their {@link #renderGL(GL)} function.
-     * 
+     *
      * @param gl
      *            Valid reference to the current gl object
      */
@@ -49,7 +50,7 @@ public abstract class AbstractGLView extends AbstractBasicView implements GLView
         if (view instanceof GLView) {
             ((GLView) view).renderGL(gl, true);
         } else {
-            textureHelper.renderImageDataToScreen(gl, view.getAdapter(RegionView.class).getRegion(), view.getAdapter(SubimageDataView.class).getSubimageData(), view.getAdapter(JHVJPXView.class));
+            textureHelper.renderImageDataToScreen(gl, view.getAdapter(RegionView.class).getRegion(), view.getAdapter(SubimageDataView.class).getSubimageData(), view.getAdapter(JHVJP2View.class));
         }
     }
 
