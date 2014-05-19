89415cbd8f824736d9104469cf92bc3f92729145
Freek Verstringe <freek.verstringe@oma.be>
2014-05-19 13:44:28 +0000

    Distinguish JP2/JPX take 4
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1244 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/GLSynchronizeOverviewChainView.java
+++ src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/GLSynchronizeOverviewChainView.java
@@ -5,31 +5,32 @@ import javax.media.opengl.GL;
 import org.helioviewer.viewmodel.view.RegionView;
 import org.helioviewer.viewmodel.view.SubimageDataView;
 import org.helioviewer.viewmodel.view.SynchronizeOverviewChainView;
-import org.helioviewer.viewmodel.view.jp2view.JHVJPXView;
+import org.helioviewer.viewmodel.view.jp2view.JHVJP2View;
 
 /**
  * Implementation of SynchronizeView for rendering in OpenGL.
- * 
+ *
  * <p>
  * This class behaves exactly like
  * {@link org.helioviewer.viewmodel.view.SynchronizeOverviewChainView} extended
  * by propagating the renderGL call to its successor.
- * 
+ *
  * @author Markus Langenberg
- * 
+ *
  */
 public class GLSynchronizeOverviewChainView extends SynchronizeOverviewChainView implements GLView {
 
-    private GLTextureHelper textureHelper = new GLTextureHelper();
+    private final GLTextureHelper textureHelper = new GLTextureHelper();
 
     /**
      * {@inheritDoc}
      */
+    @Override
     public void renderGL(GL gl, boolean nextView) {
         if (view instanceof GLView) {
             ((GLView) view).renderGL(gl, true);
         } else {
-            textureHelper.renderImageDataToScreen(gl, view.getAdapter(RegionView.class).getRegion(), view.getAdapter(SubimageDataView.class).getSubimageData(), view.getAdapter(JHVJPXView.class));
+            textureHelper.renderImageDataToScreen(gl, view.getAdapter(RegionView.class).getRegion(), view.getAdapter(SubimageDataView.class).getSubimageData(), view.getAdapter(JHVJP2View.class));
         }
     }
 
