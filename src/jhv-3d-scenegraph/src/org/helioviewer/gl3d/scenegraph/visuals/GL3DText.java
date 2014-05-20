8fff24f1f2b2f05f8ee57f9c5a803217ff892eca
Freek Verstringe <freek.verstringe@oma.be>
2014-05-20 13:59:00 +0000

    Load font on the fly take 2
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1276 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d-scenegraph/src/org/helioviewer/gl3d/scenegraph/visuals/GL3DText.java
+++ src/jhv-3d-scenegraph/src/org/helioviewer/gl3d/scenegraph/visuals/GL3DText.java
@@ -3,6 +3,8 @@ package org.helioviewer.gl3d.scenegraph.visuals;
 import java.util.List;
 
 import javax.media.opengl.GL;
+
+import org.helioviewer.base.math.RectangleDouble;
 import org.helioviewer.gl3d.scenegraph.GL3DDrawBits.Bit;
 import org.helioviewer.gl3d.scenegraph.GL3DMesh;
 import org.helioviewer.gl3d.scenegraph.GL3DState;
@@ -12,8 +14,8 @@ import org.helioviewer.gl3d.scenegraph.math.GL3DVec4d;
 import org.helioviewer.viewmodel.view.opengl.GLTextureHelper;
 
 public class GL3DText extends GL3DMesh {
-    private double width;
-    private double height;
+    private final double width;
+    private final double height;
 
     private boolean initiated = false;
     private int texture_id;
@@ -40,56 +42,63 @@ public class GL3DText extends GL3DMesh {
         this.y0 = y0;
         this.z0 = z0;
         this.text = text;
-        this.font = "default";
+        this.font = "Monospaced";
         this.textColor = textColor;
     }
 
     public GL3DText(double width, double height, int resX, int resY) {
-        super("Rectangle");
+        super("RectangleDouble");
         this.width = width;
         this.height = height;
         this.drawBits.set(Bit.Wireframe, false);
     }
 
+    @Override
     public void shapeDraw(GL3DState state) {
         GL gl = state.gl;
+        gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, new float[] { 1.f, 1.f, 1.f }, 0);
+
         if (!initiated) {
             GLTextureHelper th = new GLTextureHelper();
             this.texture_id = th.genTextureID(gl);
             initiated = true;
         }
         GL3DFont.getSingletonInstance().loadFont(this.font, gl);
+
         super.shapeDraw(state);
     }
 
+    @Override
     public GL3DMeshPrimitive createMesh(GL3DState state, List<GL3DVec3d> positions, List<GL3DVec3d> normals, List<GL3DVec2d> textCoords, List<Integer> indices, List<GL3DVec4d> colors) {
+        RectangleDouble[] characters = GL3DFont.getSingletonInstance().getCharacters(this.font);
         double xStart = -this.width * this.text.length() / 2.0;
         double yStart = -this.height / 2.0;
         for (int i = 0; i < this.text.length(); i++) {
-            int ch = (int) (this.text.charAt(i));
-            for (int r = 0; r <= 1; r++) {
-                for (int c = 0; c <= 1; c++) {
-                    positions.add(new GL3DVec3d((x0 + xStart + (c + i) * this.width), (y0 + yStart + (r) * this.height), z0));
-                    textCoords.add(new GL3DVec2d((c + (ch % 16)) / 16.0, (1 - r + (ch / 16)) / 16.0));
-                    normals.add(new GL3DVec3d(0, 0, 1));
-                    colors.add(this.textColor);
-                }
-            }
+            int ch = (this.text.charAt(i));
+            RectangleDouble rect =characters[ch];
+            positions.add(new GL3DVec3d( x0 + xStart +  this.width * i    , y0 - yStart , z0));
+            positions.add(new GL3DVec3d( x0 + xStart +  this.width * i    , y0 + yStart , z0));
+            positions.add(new GL3DVec3d( x0 + xStart +  this.width * (i+1), y0 + yStart , z0));
+            positions.add(new GL3DVec3d( x0 + xStart +  this.width * (i+1), y0 - yStart , z0));
 
-            for (int r = 0; r < 1; r++) {
-                for (int c = 0; c < 1; c++) {
-                    int startIndexThisRow = r * 2 + c + 4 * i;
-                    int startIndexNextRow = (r + 1) * 2 + c + 4 * i;
-                    indices.add(startIndexThisRow);
-                    indices.add(startIndexThisRow + 1);
-                    indices.add(startIndexNextRow);
+            textCoords.add(new GL3DVec2d(rect.getX(), rect.getY() - rect.getHeight()));
+            textCoords.add(new GL3DVec2d(rect.getX(), rect.getY()));
+            textCoords.add(new GL3DVec2d(rect.getX() + rect.getWidth(), rect.getY()));
+            textCoords.add(new GL3DVec2d(rect.getX() + rect.getWidth(), rect.getY() - rect.getHeight()));
 
-                    indices.add(startIndexThisRow + 1);
-                    indices.add(startIndexNextRow + 1);
-                    indices.add(startIndexNextRow);
-                }
-            }
+            normals.add(new GL3DVec3d(0, 0, 1));
+
+
+            int startIndexThisRow = 4 * i;
+            indices.add(startIndexThisRow);
+            indices.add(startIndexThisRow + 1);
+            indices.add(startIndexThisRow + 2);
+
+            indices.add(startIndexThisRow + 2);
+            indices.add(startIndexThisRow + 3);
+            indices.add(startIndexThisRow);
         }
         return GL3DMeshPrimitive.TRIANGLES;
+
     }
 }
