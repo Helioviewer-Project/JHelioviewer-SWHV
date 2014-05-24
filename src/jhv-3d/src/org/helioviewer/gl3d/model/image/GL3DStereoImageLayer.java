755a80b1770764dc017aa3ef8538d39f02b5ae3b
Freek Verstringe <freek.verstringe@oma.be>
2014-05-24 20:40:45 +0000

    Change to match AIA
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1330 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d/src/org/helioviewer/gl3d/model/image/GL3DStereoImageLayer.java
+++ src/jhv-3d/src/org/helioviewer/gl3d/model/image/GL3DStereoImageLayer.java
@@ -2,13 +2,9 @@ package org.helioviewer.gl3d.model.image;
 
 import javax.media.opengl.GL;
 
-import org.helioviewer.base.physics.Constants;
-import org.helioviewer.gl3d.shader.GL3DImageFragmentShaderProgram;
 import org.helioviewer.gl3d.shader.GL3DImageVertexShaderProgram;
 import org.helioviewer.gl3d.shader.GL3DShaderFactory;
 import org.helioviewer.gl3d.view.GL3DView;
-import org.helioviewer.viewmodel.metadata.HelioviewerMetaData;
-import org.helioviewer.viewmodel.view.opengl.shader.GLFragmentShaderProgram;
 import org.helioviewer.viewmodel.view.opengl.shader.GLVertexShaderProgram;
 
 public class GL3DStereoImageLayer extends GL3DImageLayer {
@@ -20,26 +16,13 @@ public class GL3DStereoImageLayer extends GL3DImageLayer {
 
     @Override
     protected void createImageMeshNodes(GL gl) {
-        this.gl = gl;
-        HelioviewerMetaData hvMetaData = (HelioviewerMetaData) metaDataView.getMetaData();
-
-        GL3DImageVertexShaderProgram vertex = new GL3DImageVertexShaderProgram();
-        GLVertexShaderProgram vertexShader = GL3DShaderFactory.createVertexShaderProgram(gl, vertex);
-        this.imageTextureView.setVertexShader(vertex);
-
-        GLFragmentShaderProgram coronaFragmentShader = GL3DShaderFactory.createFragmentShaderProgram(gl, this.coronaFragmentShader);
-
+        this.sphereFragmentShader = imageTextureView.getFragmentShader();
+        GL3DImageVertexShaderProgram vertexShaderProgram = new GL3DImageVertexShaderProgram();
+        GLVertexShaderProgram vertexShader = GL3DShaderFactory.createVertexShaderProgram(gl, vertexShaderProgram);
+        this.imageTextureView.setVertexShader(vertexShaderProgram);
         this.imageTextureView.metadata = this.metaDataView.getMetaData();
-
-        // Don't display sphere for corona images
-        if (!hvMetaData.getDetector().startsWith("COR")) {
-            this.sphereFragmentShader = new GL3DImageFragmentShaderProgram();
-            GLFragmentShaderProgram sphereFragmentShader = GL3DShaderFactory.createFragmentShaderProgram(gl, this.sphereFragmentShader);
-            sphere = new GL3DImageSphere(imageTextureView, vertexShader, sphereFragmentShader, this);
-            this.addNode(sphere);
-            this.sphereFragmentShader.setCutOffRadius(Constants.SunRadius / this.imageTextureView.metadata.getPhysicalImageWidth());
-        }
-
+        sphere = new GL3DImageSphere(imageTextureView, vertexShader, sphereFragmentShader, this);
+        this.addNode(sphere);
     }
 
     @Override
