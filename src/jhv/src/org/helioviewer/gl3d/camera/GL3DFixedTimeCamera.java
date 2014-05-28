f7babc1ca5404e1ade0d1af498f5ea7c3540a6c8
Freek Verstringe <freek.verstringe@oma.be>
2014-05-28 10:33:25 +0000

    Add initial version of fixed time camera
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1381 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DFixedTimeCamera.java
@@ -0,0 +1,87 @@
+package org.helioviewer.gl3d.camera;
+
+import java.util.Date;
+
+import org.helioviewer.base.physics.Constants;
+import org.helioviewer.base.physics.DifferentialRotation;
+import org.helioviewer.gl3d.scenegraph.math.GL3DQuatd;
+import org.helioviewer.gl3d.scenegraph.math.GL3DVec3d;
+import org.helioviewer.gl3d.view.GL3DSceneGraphView;
+import org.helioviewer.gl3d.wcs.CoordinateVector;
+import org.helioviewer.gl3d.wcs.conversion.SolarSphereToStonyhurstHeliographicConversion;
+import org.helioviewer.gl3d.wcs.impl.SolarSphereCoordinateSystem;
+import org.helioviewer.gl3d.wcs.impl.StonyhurstHeliographicCoordinateSystem;
+import org.helioviewer.viewmodel.changeevent.ChangeEvent;
+import org.helioviewer.viewmodel.changeevent.TimestampChangedReason;
+import org.helioviewer.viewmodel.view.LinkedMovieManager;
+import org.helioviewer.viewmodel.view.TimedMovieView;
+import org.helioviewer.viewmodel.view.View;
+import org.helioviewer.viewmodel.view.ViewListener;
+
+/**
+ * This camera is used when solar rotation tracking is enabled. It extends the
+ * {@link GL3DFixedTimeCamera} by automatically rotating the camera around the
+ * Y-Axis (pointing to solar north) by an amount calculated through
+ * {@link DifferentialRotation}.
+ * 
+ * @author Simon Spoerri (simon.spoerri@fhnw.ch)
+ * 
+ */
+public class GL3DFixedTimeCamera extends GL3DSolarRotationTrackingTrackballCamera implements ViewListener {
+
+    private final CoordinateVector startPosition = null;
+
+    private Date currentDate = null;
+    private double currentRotation = 0.0;
+
+    private final StonyhurstHeliographicCoordinateSystem stonyhurstCoordinateSystem = new StonyhurstHeliographicCoordinateSystem();
+    private final SolarSphereCoordinateSystem solarSphereCoordinateSystem = new SolarSphereCoordinateSystem();
+    private final SolarSphereToStonyhurstHeliographicConversion stonyhurstConversion = (SolarSphereToStonyhurstHeliographicConversion) solarSphereCoordinateSystem.getConversion(stonyhurstCoordinateSystem);
+
+    private long timediff;
+
+    public GL3DFixedTimeCamera(GL3DSceneGraphView sceneGraphView) {
+        super(sceneGraphView);
+    }
+
+    @Override
+    public void reset() {
+        this.resetCurrentDragRotation();
+        super.reset();
+    }
+
+    @Override
+    public void activate(GL3DCamera precedingCamera) {
+        super.activate(precedingCamera);
+        sceneGraphView.addViewListener(this);
+    }
+
+    @Override
+    public void deactivate() {
+        sceneGraphView.removeViewListener(this);
+    };
+
+    @Override
+    public String getName() {
+        return "TrackBall";
+    }
+
+    @Override
+    public void viewChanged(View sender, ChangeEvent aEvent) {
+        TimestampChangedReason timestampReason = aEvent.getLastChangedReasonByType(TimestampChangedReason.class);
+        if ((timestampReason != null) && (timestampReason.getView() instanceof TimedMovieView) && LinkedMovieManager.getActiveInstance().isMaster((TimedMovieView) timestampReason.getView())) {
+            currentDate = timestampReason.getNewDateTime().getTime();
+            updateRotation();
+        }
+    }
+
+    public void updateRotation() {
+        this.timediff = (System.currentTimeMillis() + this.getTimeDelay()) / 1000 - Constants.referenceDate;
+        this.currentRotation = DifferentialRotation.calculateRotationInRadians(0., this.timediff) % (Math.PI * 2.0);
+        this.setLocalRotation(GL3DQuatd.createRotation(this.currentRotation, new GL3DVec3d(0, 1, 0)));
+        this.updateCameraTransformation();
+    }
+
+    private void resetStartPosition() {
+    }
+}
