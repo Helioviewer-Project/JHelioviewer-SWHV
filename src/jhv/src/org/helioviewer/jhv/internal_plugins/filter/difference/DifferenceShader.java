d95db195b0a413c7e40cd66178854c8084ee3483
Freek Verstringe <freek.verstringe@oma.be>
2014-05-29 12:28:33 +0000

    comment out unused code
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1408 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/internal_plugins/filter/difference/DifferenceShader.java
+++ src/jhv/src/org/helioviewer/jhv/internal_plugins/filter/difference/DifferenceShader.java
@@ -24,11 +24,13 @@ public class DifferenceShader extends GLFragmentShaderProgram {
     public void setTruncationValue(GL gl, float truncationValue) {
         this.truncationValueFloat[0] = truncationValue;
     }
+
     @Override
-    public void bind(GL gl){
+    public void bind(GL gl) {
         super.bind(gl);
         this.bindEnvVars(gl, this.truncationValueRef, truncationValueFloat);
     }
+
     /**
      * {@inheritDoc}
      */
@@ -40,21 +42,21 @@ public class DifferenceShader extends GLFragmentShaderProgram {
             this.truncationValueRef = shaderBuilder.addEnvParameter("float truncationValue");
             this.truncationValueFloat = this.builder.getEnvParameter(this.truncationValueRef);
 
-
             shaderBuilder.getParameterList().add("float4 " + "texcoord4" + " : TEXCOORD4");
             String program = "";
-            program = "\tif((texcoord4.x<0.0||texcoord4.y<0.0||texcoord4.x>diffTextureScaleThetaPhi.x||texcoord4.y>diffTextureScaleThetaPhi.y)){" + "\t\tOUT.color = float4(0.0,1.0,0.0,1.0);" + GLShaderBuilder.LINE_SEP + "\t}" + GLShaderBuilder.LINE_SEP;
+            // program =
+            // "\tif((texcoord4.x<0.0||texcoord4.y<0.0||texcoord4.x>diffTextureScaleThetaPhi.x||texcoord4.y>diffTextureScaleThetaPhi.y)){"
+            // + "\t\tOUT.color = float4(0.0,1.0,0.0,1.0);" +
+            // GLShaderBuilder.LINE_SEP + "\t}" + GLShaderBuilder.LINE_SEP;
 
-            program = "if(isdifference>0.24 && isdifference<0.27){" + shaderBuilder.LINE_SEP
-                    + "\toutput.r = output.r - tex2D(differenceImage, texcoord0.xy).r;" + shaderBuilder.LINE_SEP;
-            program +="\toutput.r = clamp(output.r,-truncationValue,truncationValue)/truncationValue;" + shaderBuilder.LINE_SEP;
-            program +="\toutput.r = (output.r + 1.0f)/2.0f;" + shaderBuilder.LINE_SEP;
+            program += "if(isdifference>0.24 && isdifference<0.27){" + shaderBuilder.LINE_SEP + "\toutput.r = output.r - tex2D(differenceImage, texcoord0.xy).r;" + shaderBuilder.LINE_SEP;
+            program += "\toutput.r = clamp(output.r,-truncationValue,truncationValue)/truncationValue;" + shaderBuilder.LINE_SEP;
+            program += "\toutput.r = (output.r + 1.0f)/2.0f;" + shaderBuilder.LINE_SEP;
             program += "}" + shaderBuilder.LINE_SEP;
 
-            program += "if(isdifference>0.98 && isdifference<1.01){" + shaderBuilder.LINE_SEP
-                    + "\toutput.r = output.r - tex2D(differenceImage, texcoord4.xy).r;" + shaderBuilder.LINE_SEP;
-            program +="\toutput.r = clamp(output.r,-truncationValue,truncationValue)/truncationValue;" + shaderBuilder.LINE_SEP;
-            program +="\toutput.r = (output.r + 1.0f)/2.0f;" + shaderBuilder.LINE_SEP;
+            program += "if(isdifference>0.98 && isdifference<1.01){" + shaderBuilder.LINE_SEP + "\toutput.r = output.r - tex2D(differenceImage, texcoord4.xy).r;" + shaderBuilder.LINE_SEP;
+            program += "\toutput.r = clamp(output.r,-truncationValue,truncationValue)/truncationValue;" + shaderBuilder.LINE_SEP;
+            program += "\toutput.r = (output.r + 1.0f)/2.0f;" + shaderBuilder.LINE_SEP;
             program += "}" + shaderBuilder.LINE_SEP;
 
             program = program.replaceAll("output", shaderBuilder.useOutputValue("float4", "COLOR"));
@@ -62,8 +64,8 @@ public class DifferenceShader extends GLFragmentShaderProgram {
             mode = shaderBuilder.addTextureParameter("sampler2D differenceImage");
             ID = (ID + 1) & 15;
             program = program.replace("isdifference", isDifference.getIdentifier(1));
-            //program = program.replace("truncationValue", truncationValue.getIdentifier(1));
-
+            // program = program.replace("truncationValue",
+            // truncationValue.getIdentifier(1));
 
             shaderBuilder.addMainFragment(program);
         } catch (GLBuildShaderException e) {
