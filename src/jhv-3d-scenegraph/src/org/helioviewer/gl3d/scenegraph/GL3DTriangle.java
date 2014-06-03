32d1316c8ccd52baba00e732d69d2535dff6e01f
Freek Verstringe <freek.verstringe@oma.be>
2014-06-03 06:50:14 +0000

    create separate planar calculation
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1444 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d-scenegraph/src/org/helioviewer/gl3d/scenegraph/GL3DTriangle.java
+++ src/jhv-3d-scenegraph/src/org/helioviewer/gl3d/scenegraph/GL3DTriangle.java
@@ -17,15 +17,6 @@ public class GL3DTriangle {
     protected GL3DVec3d c;
 
     protected GL3DVec3d center;
-    private boolean planarMode = true;
-
-    public boolean isPlanarMode() {
-        return planarMode;
-    }
-
-    public void setPlanarMode(boolean planarMode) {
-        this.planarMode = planarMode;
-    }
 
     public GL3DTriangle() {
 
@@ -70,19 +61,16 @@ public class GL3DTriangle {
 
         // Calculate barycentric coordinates: u>0 && v>0 && u+v<=1
         u = AO.dot(K) * inv_det;
-        if (!planarMode) {
-            if (u < 0.0f || u > 1.0)
-                return false;
-        }
+        if (u < 0.0f || u > 1.0)
+            return false;
+
         // prepare to test v parameter
         Q = GL3DVec3d.cross(AO, e1);
 
         // calculate v parameter and test bounds
-        if (!planarMode) {
-            v = Q.dot(ray.getDirectionOS()) * inv_det;
-            if (v < 0.0f || u + v > 1.0f)
-                return false;
-        }
+        v = Q.dot(ray.getDirectionOS()) * inv_det;
+        if (v < 0.0f || u + v > 1.0f)
+            return false;
 
         // calculate t, ray intersects triangle
         t = e2.dot(Q) * inv_det;
@@ -90,6 +78,38 @@ public class GL3DTriangle {
         // if intersection is closer replace ray intersection parameters
         if (t > ray.getLength() || t < 0.0f)
             return false;
+
+        ray.setLength(t);
+
+        return true;
+    }
+
+    public boolean intersectsPlanar(GL3DRay ray) {
+        GL3DVec3d e1, e2; // edge 1 and 2
+        GL3DVec3d AO, K, Q;
+
+        // find vectors for two edges sharing the triangle vertex A
+        e1 = GL3DVec3d.subtract(this.b, this.a);
+        e2 = GL3DVec3d.subtract(this.c, this.a);
+
+        // begin calculating determinant - also used to calculate U parameter
+        K = GL3DVec3d.cross(ray.getDirectionOS(), e2);
+
+        // if determinant is near zero, ray lies in plane of triangle
+        final double det = e1.dot(K);
+
+        double inv_det, t, u, v;
+
+        inv_det = 1.0f / det;
+
+        // calculate distance from A to ray origin
+        AO = GL3DVec3d.subtract(ray.getOriginOS(), a);
+
+        // prepare to test v parameter
+        Q = GL3DVec3d.cross(AO, e1);
+
+        // calculate t, ray intersects triangle
+        t = e2.dot(Q) * inv_det;
 
         ray.setLength(t);
 
