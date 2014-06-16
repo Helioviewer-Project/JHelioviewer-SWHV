33d5bc5e3e1f072c6bea6dafcd008b6b75582ddb
Freek Verstringe <freek.verstringe@oma.be>
2014-06-16 21:14:18 +0000

    fire date to camera options panel
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1526 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/camera/GL3DFollowObjectCameraListener.java
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DFollowObjectCameraListener.java
@@ -6,4 +6,6 @@ public interface GL3DFollowObjectCameraListener {
     public void fireLoaded(boolean isLoaded);
 
     public void fireCameraTime(Date cameraDate);
+
+    public void fireNewDate(Date date);
 }
--- src/jhv/src/org/helioviewer/gl3d/camera/GL3DPositionLoadingListener.java
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DPositionLoadingListener.java
@@ -2,4 +2,6 @@ package org.helioviewer.gl3d.camera;
 
 public interface GL3DPositionLoadingListener {
     public void fireNewLoaded(boolean isLoaded);
+
+    public void fireNewDate();
 }
