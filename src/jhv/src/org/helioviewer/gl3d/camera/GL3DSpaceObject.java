3aea8a997313fa398efad6ff3b06646857a1831b
Freek Verstringe <freek.verstringe@oma.be>
2014-06-16 21:14:26 +0000

    Add objects
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1542 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/camera/GL3DSpaceObject.java
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DSpaceObject.java
@@ -13,11 +13,16 @@ public class GL3DSpaceObject {
     }
 
     private static void createObjectList() {
-        objectList = new GL3DSpaceObject[4];
-        objectList[0] = new GL3DSpaceObject("Earth", "Earth");
-        objectList[1] = new GL3DSpaceObject("Mercury", "Mercury");
-        objectList[2] = new GL3DSpaceObject("Venus", "Venus");
-        objectList[3] = new GL3DSpaceObject("Solar%20Orbiter", "Solar Orbiter");
+        objectList = new GL3DSpaceObject[9];
+        objectList[0] = new GL3DSpaceObject("Mercury", "Mercury");
+        objectList[1] = new GL3DSpaceObject("Venus", "Venus");
+        objectList[2] = new GL3DSpaceObject("Earth", "Earth");
+        objectList[3] = new GL3DSpaceObject("Mars", "Mars");
+        objectList[4] = new GL3DSpaceObject("Saturn", "Saturn");
+        objectList[5] = new GL3DSpaceObject("Uranus", "Uranus");
+        objectList[6] = new GL3DSpaceObject("Jupiter", "Jupiter");
+        objectList[7] = new GL3DSpaceObject("Pluto", "Pluto");
+        objectList[8] = new GL3DSpaceObject("Solar%20Orbiter", "Solar Orbiter");
     }
 
     private GL3DSpaceObject(String urlName, String labelName) {
