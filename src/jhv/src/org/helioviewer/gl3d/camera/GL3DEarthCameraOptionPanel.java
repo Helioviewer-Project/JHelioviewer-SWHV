a854040457309a486ea1a49d8332272fb5075e35
Freek Verstringe <freek.verstringe@oma.be>
2014-06-17 10:45:15 +0000

    Create option panel
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1553 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DEarthCameraOptionPanel.java
@@ -0,0 +1,19 @@
+package org.helioviewer.gl3d.camera;
+
+
+public class GL3DEarthCameraOptionPanel extends GL3DCameraOptionPanel {
+
+    private final GL3DEarthCamera camera;
+
+    public GL3DEarthCameraOptionPanel(GL3DEarthCamera camera) {
+        super(camera);
+        this.camera = camera;
+        createGridOptions();
+    }
+
+    @Override
+    public void deactivate() {
+
+    }
+
+}
