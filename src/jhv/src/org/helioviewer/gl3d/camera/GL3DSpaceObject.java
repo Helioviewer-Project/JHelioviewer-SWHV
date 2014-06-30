faf0753bbb52a14707a8cc821bd3f228c2256e5c
Bogdan Nicula <bogdan@nicula.net>
2014-06-30 12:34:15 +0000

    For Mars use barycenter as the target query parameter; Add STEREO Ahead and Behind
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1589 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/camera/GL3DSpaceObject.java
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DSpaceObject.java
@@ -13,20 +13,23 @@ public class GL3DSpaceObject {
     }
 
     private static void createObjectList() {
-        objectList = new GL3DSpaceObject[11];
+        objectList = new GL3DSpaceObject[13];
+
         objectList[0] = new GL3DSpaceObject("Mercury", "Mercury");
         objectList[1] = new GL3DSpaceObject("Venus", "Venus");
         objectList[2] = new GL3DSpaceObject("Earth", "Earth");
         objectList[3] = new GL3DSpaceObject("Moon", "Moon");
 
-        objectList[4] = new GL3DSpaceObject("Mars", "Mars");
+        objectList[4] = new GL3DSpaceObject("Mars%20Barycenter", "Mars");
         objectList[5] = new GL3DSpaceObject("Saturn%20Barycenter", "Saturn");
         objectList[6] = new GL3DSpaceObject("Uranus%20Barycenter", "Uranus");
         objectList[7] = new GL3DSpaceObject("Neptune%20Barycenter", "Neptune");
-
         objectList[8] = new GL3DSpaceObject("Jupiter%20Barycenter", "Jupiter");
         objectList[9] = new GL3DSpaceObject("Pluto%20Barycenter", "Pluto");
-        objectList[10] = new GL3DSpaceObject("Solar%20Orbiter", "Solar Orbiter");
+
+        objectList[10] = new GL3DSpaceObject("STEREO%20Ahead", "STEREO Ahead");
+        objectList[11] = new GL3DSpaceObject("STEREO%20Behind", "STEREO Behind");
+        objectList[12] = new GL3DSpaceObject("Solar%20Orbiter", "Solar Orbiter");
     }
 
     private GL3DSpaceObject(String urlName, String labelName) {
