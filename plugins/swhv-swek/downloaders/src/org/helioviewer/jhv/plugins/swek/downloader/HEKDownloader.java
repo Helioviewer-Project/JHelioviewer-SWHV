577fd1f48de30306bf863e09fa2c45f0118b7748
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-07-29 12:06:47 +0000

    Initial creation of the HEK downloader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1637 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ plugins/swhv-swek/downloaders/src/org/helioviewer/jhv/plugins/swek/downloader/HEKDownloader.java
@@ -0,0 +1,7 @@
+package org.helioviewer.jhv.plugins.swek.downloader;
+
+import org.helioviewer.jhv.plugins.swek.download.SWEKDownloader;
+
+public class HEKDownloader implements SWEKDownloader {
+
+}
