239985b2b03494849b0507e7f60d7ff9350916a5
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-06 09:08:36 +0000

    The jar in the seek configuration file are downloaded if needed. local file can be loaded for class loader creation
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1675 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/SWEKSourceManager.java
+++ plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/SWEKSourceManager.java
@@ -1,25 +1,23 @@
 package org.helioviewer.jhv.plugins.swek.sources;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.helioviewer.base.logging.Log;
-import org.helioviewer.jhv.Settings;
-import org.helioviewer.jhv.plugins.swek.SWEKPlugin;
+import org.helioviewer.jhv.plugins.swek.config.SWEKConfigurationManager;
+import org.helioviewer.jhv.plugins.swek.config.SWEKSource;
 import org.helioviewer.jhv.plugins.swek.settings.SWEKSettings;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * Manages all the downloaders and downloads of the SWEK plugin.
@@ -31,16 +29,19 @@ public class SWEKSourceManager {
     /** Singleton instance of the swek download manager */
     private static SWEKSourceManager instance;
 
-    /** Are the sources loaded */
-    private boolean sourcesLoaded;
+    /** Instance of the SWEK configuration manager */
+    private final SWEKConfigurationManager configManager;
 
-    /**  */
-    private URL sourcesConfigURL;
+    /** Are the sources loaded */
+    private final boolean sourcesLoaded;
 
     /** The swek properties */
     private final Properties swekProperties;
 
+    private final List<URL> jarURLList;
+
     /** The URL classloader */
+    private URLClassLoader urlClassLoader;
 
     /**
      * private constructor
@@ -48,6 +49,8 @@ public class SWEKSourceManager {
     private SWEKSourceManager() {
         this.sourcesLoaded = false;
         this.swekProperties = new Properties();
+        this.configManager = SWEKConfigurationManager.getSingletonInstance();
+        this.jarURLList = new ArrayList<URL>();
     }
 
     /**
@@ -63,155 +66,124 @@ public class SWEKSourceManager {
     }
 
     /**
-     * Loads the SWEK sources.
+     * Loads the SWEK sources. If the download source is remote, the jar are
+     * downloaded to the home directory of the swekplugin and loaded from there.
+     * Local plugins will be loaded from disc.
      */
     public void loadSources() {
         if (!this.sourcesLoaded) {
-            // Check for user set download file and download the jars
-            boolean isSourcesConfigParsed;
-            loadPluginSettings();
-            if (checkAndOpenUserSetSourcesConfig()) {
-                isSourcesConfigParsed = parseSourcesConfig();
-            } else if (checkAndOpenHomeDirectorySourcesConfig()) {
-                isSourcesConfigParsed = parseSourcesConfig();
-            } else if (checkAndOpenOnlineSourcesConfig()) {
-                isSourcesConfigParsed = parseSourcesConfig();
+            // Check the sweksettings file for the downloaders
+            if (checkAndDownloadJars()) {
+                prepareDownloadersClassLoader();
             } else {
-                isSourcesConfigParsed = false;
-            }
-            if (isSourcesConfigParsed) {
-                if (prepareDownloadersClassLoader()) {
-                    this.sourcesLoaded = true;
-                } else {
-                    this.sourcesLoaded = false;
-                }
-            } else {
-                this.sourcesLoaded = false;
+
             }
         }
     }
 
     /**
-     * Loads the overall plugin settings.
-     */
-    private void loadPluginSettings() {
-        InputStream defaultPropStream = SWEKPlugin.class.getResourceAsStream("/SWEK.properties");
-        try {
-            this.swekProperties.load(defaultPropStream);
-        } catch (IOException ex) {
-            Log.error("Could not load the swek settings : ", ex);
-        }
-    }
-
-    /**
-     * Parses the SWEK settings.
-     */
-    private boolean parseSourcesConfig() {
-        try {
-            InputStream configIs = this.sourcesConfigURL.openStream();
-            StringBuilder sb = new StringBuilder();
-            BufferedReader br = new BufferedReader(new InputStreamReader(configIs));
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            JSONObject configJSON = new JSONObject(sb.toString());
-            return parseJSONConfig(configJSON);
-        } catch (IOException e) {
-            Log.debug("The configuration file could not be parsed : " + e);
-        } catch (JSONException e) {
-            Log.debug("Could not parse the given JSON : " + e);
-        }
-        return false;
-    }
-
-    private boolean parseJSONConfig(JSONObject configJSON) {
-        return true;
-    }
-
-    private boolean prepareDownloadersClassLoader() {
-        // TODO Auto-generated method stub
-        return false;
-    }
-
-    /**
-     * Downloads the sources from the Internet and saves it in the plugin home
-     * directory.
+     * Checks the swek configuration file for defined sources. It downloads the
+     * jars to the home directory of the swek plugin if the location of the jar
+     * is external. It builds up a list of URIs with the location of the jars.
      * 
-     * @return true if the the file was found and copied to the home directory,
-     *         false if the file could not be found, copied or something else
-     *         went wrong.
+     * @return True if all the sources were downloaded and added to the list.
      */
-    private boolean checkAndOpenOnlineSourcesConfig() {
-        Log.debug("Download the sources file from the net and copy it to the plugin home directory");
+    private boolean checkAndDownloadJars() {
+        boolean downloadJarOK = true;
+        for (SWEKSource source : this.configManager.getSources().values()) {
+            URI sourceURI;
+            try {
+                sourceURI = new URI(source.getJarLocation());
+                if (sourceURI.getScheme().equals("file")) {
+                    this.jarURLList.add(sourceURI.toURL());
+                } else {
+                    if (downloadJar(sourceURI)) {
+                        downloadJarOK = downloadJarOK && true;
+                    } else {
+                        Log.error("Source with name "
+                                + source.getSourceName()
+                                + " is not loaded. Promblem downloading jar on location: "
+                                + source.getJarLocation()
+                                + ". This will probably cause the programm not being able to check for events deliverd by this source. Check your configuration file and restart.");
+                        downloadJarOK = false;
+                    }
+                }
+            } catch (URISyntaxException e) {
+                Log.error("Source with name "
+                        + source.getSourceName()
+                        + " is not loaded. Promblem downloading jar on location: "
+                        + source.getJarLocation()
+                        + ". This will probably cause the programm not being able to check for events deliverd by this source. Check your configuration file and restart.");
+                downloadJarOK = false;
+            } catch (MalformedURLException e) {
+                Log.error("Source with name "
+                        + source.getSourceName()
+                        + " is not loaded. Promblem downloading jar on location: "
+                        + source.getJarLocation()
+                        + ". This will probably cause the programm not being able to check for events deliverd by this source. Check your configuration file and restart.");
+                downloadJarOK = false;
+            }
+        }
+        return downloadJarOK;
+    }
+
+    /**
+     * Downloads the jar from the network and saves them in the home directory
+     * of the swek plugin. The URI of the local file is added in the list of
+     * jarURIs.
+     * 
+     * @param sourceURI
+     *            The URI describing the location of the jar
+     * @return True if the jar was downloaded and added to the list of URIs.
+     */
+    private boolean downloadJar(URI sourceURI) {
         try {
-            URL url = new URL(this.swekProperties.getProperty("plugin.swek.sources"));
-            ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-            String saveFile = SWEKSettings.SWEK_HOME + this.swekProperties.getProperty("plugin.swek.sourcesfilename");
-            FileOutputStream fos = new FileOutputStream(saveFile);
+            ReadableByteChannel rbc = Channels.newChannel(sourceURI.toURL().openStream());
+            String filename = extractFileName(sourceURI);
+            File dest = new File(SWEKSettings.SWEK_SOURCES + filename);
+            FileOutputStream fos = new FileOutputStream(dest);
             fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-            fos.close();
-            this.sourcesConfigURL = new URL("file://" + saveFile);
+            this.jarURLList.add(dest.toURI().toURL());
             return true;
         } catch (MalformedURLException e) {
-            Log.debug("Could not create a URL from the value found in the properties file: "
-                    + this.swekProperties.getProperty("plugin.swek.sources") + " : " + e);
+            Log.error("The URL was malformed for source " + sourceURI + ". Given error was " + e);
         } catch (IOException e) {
-            Log.debug("Something went wrong downloading the configuration file from the server or saving it to the local machine : " + e);
+            Log.error("IO exception received for source " + sourceURI + ". Given error was " + e);
+            e.printStackTrace();
         }
         return false;
     }
 
     /**
-     * Checks the home directory of the plugin (normally
-     * ~/JHelioviewer/Plugins/swek-plugin/) for the existence of the
-     * sources.json file.
+     * Extracts the name of the file from the URI.
      * 
-     * @return true if the file was found and useful, false if the file was not
-     *         found.
+     * @param sourceURI
+     *            The URI from which to extract the file name.
+     * @return The name of the file.
      */
-    private boolean checkAndOpenHomeDirectorySourcesConfig() {
-        String configFile = SWEKSettings.SWEK_HOME + this.swekProperties.getProperty("plugin.swek.sourcesfilename");
-        try {
-            File f = new File(configFile);
-            if (f.exists()) {
-                this.sourcesConfigURL = new URL("file://" + configFile);
-                return true;
-            } else {
-                Log.debug("File created from the settings : " + configFile + " does not exists on this system.");
-            }
-        } catch (MalformedURLException e) {
-            Log.debug("File at possition " + configFile + " could not be parsed into an URL");
-        }
+    private String extractFileName(URI sourceURI) {
+        String path = sourceURI.getPath();
+        return path.substring(path.lastIndexOf('/') + 1, path.length());
+    }
+
+    /**
+     * Creates a class loader from the given jars
+     * 
+     * @return True if the classloader was created, false if the classloader was
+     *         not created.
+     */
+    private boolean prepareDownloadersClassLoader() {
+        URL[] urls = this.jarURLList.toArray(new URL[0]);
+        this.urlClassLoader = URLClassLoader.newInstance(urls, Thread.currentThread().getContextClassLoader());
         return false;
     }
 
     /**
-     * Checks the jhelioviewer settings file for a swek sources configuration
-     * file.
+     * Gives the downloader object for the given swek source.
      * 
-     * @return true if the file as found and useful, false if the file was not
-     *         found.
+     * @param swekSource
      */
-    private boolean checkAndOpenUserSetSourcesConfig() {
-        Log.debug("Search for a user defined configuration file in the JHelioviewer setting file.");
-        Settings jhvSettings = Settings.getSingletonInstance();
-        String fileName = jhvSettings.getProperty("plugin.swek.sourcesconfigfile");
-        if (fileName == null) {
-            Log.debug("No configured filename found.");
-            return false;
-        } else {
-            try {
-                URI fileLocation = new URI(fileName);
-                this.sourcesConfigURL = fileLocation.toURL();
-                Log.debug("Config file : " + this.sourcesConfigURL.toString());
-                return true;
-            } catch (URISyntaxException e) {
-                Log.debug("Wrong URI syntax for the found file name : " + fileName);
-            } catch (MalformedURLException e) {
-                Log.debug("Could not convert the URI in a correct URL. The found file name : " + fileName);
-            }
-            return false;
-        }
+    public void getDownloader(SWEKSource swekSource) {
+
     }
 }
