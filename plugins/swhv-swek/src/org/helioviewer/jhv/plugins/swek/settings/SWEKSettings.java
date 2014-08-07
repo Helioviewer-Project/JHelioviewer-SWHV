0174c70b57af9251963954225bb835cf8c490e85
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-07 08:28:18 +0000

    Download the plugin resources to the PluginsResources directory
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1685 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/settings/SWEKSettings.java
+++ plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/settings/SWEKSettings.java
@@ -21,5 +21,5 @@ public class SWEKSettings {
     public static final String SWEK_HOME = JHVDirectory.PLUGINS.getPath() + "swek_plugin" + File.separator;
 
     /** Directpry with downloader jar containing the sources */
-    public static final String SWEK_SOURCES = SWEKSettings.SWEK_HOME + "Sources" + File.separator;
+    public static final String SWEK_SOURCES = JHVDirectory.PLUGIN_RESOURCES.getPath() + "swek_plugin" + File.separator;
 }
