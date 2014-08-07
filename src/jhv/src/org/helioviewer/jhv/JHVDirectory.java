5d757073b8f00eec05e08de4145cc1ebd891a15e
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-07 08:10:43 +0000

    Removed the deprecated FILE_SEP
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1683 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/JHVDirectory.java
+++ src/jhv/src/org/helioviewer/jhv/JHVDirectory.java
@@ -15,153 +15,172 @@ public enum JHVDirectory {
     HOME {
         private final String path = System.getProperty("user.home");
 
+        @Override
         public String getPath() {
-            return path + FILE_SEP + "JHelioviewer" + FILE_SEP;
+            return this.path + File.separator + "JHelioviewer" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The image cache directory. */
     CACHE {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Cache" + FILE_SEP;
+            return HOME.getPath() + "Cache" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The shared library directory. */
     LIBS {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Libs" + FILE_SEP;
+            return HOME.getPath() + "Libs" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The shared library directory. */
     LIBS_LAST_CONFIG {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Libs" + FILE_SEP + "LastConfig" + FILE_SEP;
+            return HOME.getPath() + "Libs" + File.separator + "LastConfig" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The JHV state directory. */
     STATES {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "States" + FILE_SEP;
+            return HOME.getPath() + "States" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The exports directory (movies, screenshots, meta data). */
     EXPORTS {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Exports" + FILE_SEP;
+            return HOME.getPath() + "Exports" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The log directory. */
     LOGS {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Logs" + FILE_SEP;
+            return HOME.getPath() + "Logs" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The log settings directory. */
     SETTINGS {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Settings" + FILE_SEP;
+            return HOME.getPath() + "Settings" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The remote files directory. */
     REMOTEFILES {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Downloads" + FILE_SEP;
+            return HOME.getPath() + "Downloads" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The plugins directory. */
     PLUGINS {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Plugins" + FILE_SEP;
+            return HOME.getPath() + "Plugins" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The plugins directory. */
     PLUGINS_LAST_CONFIG {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Plugins" + FILE_SEP + "LastConfig" + FILE_SEP;
+            return HOME.getPath() + "Plugins" + File.separator + "LastConfig" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** Directory of automatically loaded GIMP gradient files. */
     COLOR_PLUGINS {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Colortables" + FILE_SEP;
+            return HOME.getPath() + "Colortables" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** temp directory for the Cg-compiler */
     TEMP {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "Temp" + FILE_SEP;
+            return HOME.getPath() + "Temp" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     },
     /** The remote files directory. */
     VSO_DOWNLOAD {
+        @Override
         public String getPath() {
-            return HOME.getPath() + "VSOData" + FILE_SEP;
+            return HOME.getPath() + "VSOData" + File.separator;
         }
 
+        @Override
         public File getFile() {
             return new File(getPath());
         }
     };
-    /**
-     * The system dependent file separator.
-     * <p>
-     * 
-     * @see File#separator
-     * @see File#separatorChar
-     **/
-    @Deprecated
-    public static final String FILE_SEP = System.getProperty("file.separator");
 
     /** A String representation of the path of the directory. */
     abstract public String getPath();
