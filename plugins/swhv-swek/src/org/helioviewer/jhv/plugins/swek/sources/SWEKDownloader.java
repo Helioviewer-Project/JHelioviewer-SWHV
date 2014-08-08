257dc010ae4ff3336b529f223d08f4434fefb230
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-08 13:59:33 +0000

    Added extra functionality to the swekparser and swekdownloader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1697 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/SWEKDownloader.java
+++ plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/SWEKDownloader.java
@@ -1,5 +1,32 @@
 package org.helioviewer.jhv.plugins.swek.sources;
 
+import java.io.InputStream;
+import java.util.Date;
+
+import org.helioviewer.jhv.plugins.swek.config.SWEKEventType;
+
 public interface SWEKDownloader {
+
+    /**
+     * If called the downloader should try to stop the download process.
+     */
+    public abstract void stopDownload();
+
+    /**
+     * Downloads the data for a given event type over the interval defined by
+     * the startDate and the endDate.
+     * 
+     * @param eventType
+     *            the type that should be downloaded
+     * @param startDate
+     *            the start date of the interval over which the events need to
+     *            be downloaded
+     * @param endDate
+     *            the end date of the interval over which the events need to be
+     *            downloaded
+     * 
+     * @return an inputstream giving acces to the data
+     */
+    public abstract InputStream downloadData(SWEKEventType eventType, Date startDate, Date endDate);
 
 }
--- plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/SWEKParser.java
+++ plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/SWEKParser.java
@@ -1,5 +1,21 @@
 package org.helioviewer.jhv.plugins.swek.sources;
 
+import java.io.InputStream;
+
 public interface SWEKParser {
+
+    /**
+     * If called the parser should stop all parser operations.
+     */
+    public abstract void stopParser();
+
+    /**
+     * Parses the event stream.
+     * 
+     * @param downloadInputStream
+     *            The stream containing the events in specific source format
+     * @return the stream containing the events in standard jhelioviewer format
+     */
+    public abstract SWEKEventStream parseEventStream(InputStream downloadInputStream);
 
 }
