0730632b0a9fd4a3fd70b2339d421c7bd9e0ac2d
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-08 13:59:33 +0000

    Introduced the SWEKEventStream
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1696 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/SWEKEventStream.java
@@ -0,0 +1,19 @@
+package org.helioviewer.jhv.plugins.swek.sources;
+
+import org.helioviewer.jhv.data.datatype.JHVEvent;
+
+public interface SWEKEventStream {
+    /**
+     * Has the stream events left.
+     * 
+     * @return True if the stream still has events left, false if not
+     */
+    public abstract boolean hasEvents();
+
+    /**
+     * Gets the next available event if any.
+     * 
+     * @return The next event or null if the stream has no events.
+     */
+    public abstract JHVEvent next();
+}
