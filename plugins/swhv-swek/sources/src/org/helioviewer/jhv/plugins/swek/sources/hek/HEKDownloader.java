cb16c9afbf442330573258e635e062c9f83259e2
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-08 13:59:33 +0000

    implemented the extra interface functionality
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1698 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKDownloader.java
+++ plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKDownloader.java
@@ -1,7 +1,23 @@
 package org.helioviewer.jhv.plugins.swek.sources.hek;
 
+import java.io.InputStream;
+import java.util.Date;
+
+import org.helioviewer.jhv.plugins.swek.config.SWEKEventType;
 import org.helioviewer.jhv.plugins.swek.sources.SWEKDownloader;
 
 public class HEKDownloader implements SWEKDownloader {
+
+    @Override
+    public void stopDownload() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public InputStream downloadData(SWEKEventType eventType, Date startDate, Date endDate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKParser.java
+++ plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKParser.java
@@ -1,7 +1,22 @@
 package org.helioviewer.jhv.plugins.swek.sources.hek;
 
+import java.io.InputStream;
+
+import org.helioviewer.jhv.plugins.swek.sources.SWEKEventStream;
 import org.helioviewer.jhv.plugins.swek.sources.SWEKParser;
 
 public class HEKParser implements SWEKParser {
+
+    @Override
+    public void stopParser() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public SWEKEventStream parseEventStream(InputStream downloadInputStream) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
