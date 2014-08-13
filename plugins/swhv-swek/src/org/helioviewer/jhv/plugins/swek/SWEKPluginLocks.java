303ca0d1c47b8b28b0c802194a4e500097788172
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-13 13:27:21 +0000

    Introduced the request lock and the tree selection lock
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1710 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/SWEKPluginLocks.java
+++ plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/SWEKPluginLocks.java
@@ -3,4 +3,10 @@ package org.helioviewer.jhv.plugins.swek;
 public class SWEKPluginLocks {
     /** Lock for the downloads */
     public static final Object downloadLock = new Object();
+
+    /** Lock for the requests */
+    public static final Object requestLock = new Object();
+
+    /** Lock for tree selection */
+    public static final Object treeSelectionLock = new Object();
 }
