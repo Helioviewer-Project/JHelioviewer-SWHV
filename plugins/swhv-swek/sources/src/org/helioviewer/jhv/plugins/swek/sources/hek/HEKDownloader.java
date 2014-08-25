e5a89e1964cbd3c9d3c57ffce49afde4172cfaf0
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-08-25 09:00:02 +0000

    Partly implemented HEKDownloader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1762 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKDownloader.java
+++ plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKDownloader.java
@@ -2,11 +2,23 @@ package org.helioviewer.jhv.plugins.swek.sources.hek;
 
 import java.io.InputStream;
 import java.util.Date;
+import java.util.Properties;
 
 import org.helioviewer.jhv.plugins.swek.config.SWEKEventType;
 import org.helioviewer.jhv.plugins.swek.sources.SWEKDownloader;
 
 public class HEKDownloader implements SWEKDownloader {
+
+    /** The hek source properties */
+    private final Properties hekSourceProperties;
+
+    /**
+     * Default constructor.
+     */
+    public HEKDownloader() {
+        HEKSourceProperties hsp = HEKSourceProperties.getSingletonInstance();
+        this.hekSourceProperties = hsp.getHEKSourceProperties();
+    }
 
     @Override
     public void stopDownload() {
@@ -16,6 +28,50 @@ public class HEKDownloader implements SWEKDownloader {
 
     @Override
     public InputStream downloadData(SWEKEventType eventType, Date startDate, Date endDate) {
+        createURL(eventType, startDate, endDate);
+
+        return null;
+    }
+
+    private String createURL(SWEKEventType eventType, Date startDate, Date endDate) {
+        StringBuilder baseURL = new StringBuilder(this.hekSourceProperties.getProperty("heksource.baseurl")).append("?");
+        baseURL = appendCmd(baseURL, eventType, startDate, endDate).append("&");
+        baseURL = appendType(baseURL, eventType, startDate, endDate).append("&");
+        baseURL = appendEventType(baseURL, eventType, startDate, endDate).append("&");
+        baseURL = appendEventCoorSys(baseURL, eventType, startDate, endDate).append("&");
+        baseURL = appendX1X2Y1Y2(baseURL, eventType, startDate, endDate).append("&");
+        baseURL = appendCosec(baseURL, eventType, startDate, endDate).append("&");
+        baseURL = appendParams(baseURL, eventType, startDate, endDate).append("&");
+        return baseURL.toString();
+    }
+
+    private StringBuilder appendCmd(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
+        return baseURL.append("cmd=search");
+    }
+
+    private StringBuilder appendType(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
+        return baseURL.append("type=column");
+    }
+
+    private StringBuilder appendEventType(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
+        return baseURL.append("event_type=").append(HEKEventFactory.getHEKEvent(eventType.getEventName()));
+    }
+
+    private StringBuilder appendEventCoorSys(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
+        return baseURL.append("event_coordsys=helioprojective");
+    }
+
+    private StringBuilder appendX1X2Y1Y2(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private StringBuilder appendCosec(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    private StringBuilder appendParams(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
         // TODO Auto-generated method stub
         return null;
     }
