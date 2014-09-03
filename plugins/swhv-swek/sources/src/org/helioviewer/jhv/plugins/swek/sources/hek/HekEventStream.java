6e66c5f95c7d37d1f028d848f6284cb9a1936314
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-03 09:49:10 +0000

    Introduced the HEKEventStream
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1822 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HekEventStream.java
@@ -0,0 +1,20 @@
+package org.helioviewer.jhv.plugins.swek.sources.hek;
+
+import org.helioviewer.jhv.data.datatype.JHVEvent;
+import org.helioviewer.jhv.plugins.swek.sources.SWEKEventStream;
+
+public class HekEventStream implements SWEKEventStream {
+
+    @Override
+    public boolean hasEvents() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public JHVEvent next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
