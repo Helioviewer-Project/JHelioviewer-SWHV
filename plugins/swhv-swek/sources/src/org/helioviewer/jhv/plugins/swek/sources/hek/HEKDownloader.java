fce41057b0e98b6a6ae52b4147b94d3de6262f0b
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-03 09:49:09 +0000

    implemented the HEK downloader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1821 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKDownloader.java
+++ plugins/swhv-swek/sources/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKDownloader.java
@@ -1,6 +1,7 @@
 package org.helioviewer.jhv.plugins.swek.sources.hek;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Properties;
 
@@ -33,6 +34,18 @@ public class HEKDownloader implements SWEKDownloader {
         return null;
     }
 
+    /**
+     * Creates the download URL for the HEK.
+     * 
+     * @param eventType
+     *            the event type that should be downloaded
+     * @param startDate
+     *            the start date of the interval over which to download the
+     *            event
+     * @param endDate
+     *            the end date of the interval over which to download the event
+     * @return the url represented as string
+     */
     private String createURL(SWEKEventType eventType, Date startDate, Date endDate) {
         StringBuilder baseURL = new StringBuilder(this.hekSourceProperties.getProperty("heksource.baseurl")).append("?");
         baseURL = appendCmd(baseURL, eventType, startDate, endDate).append("&");
@@ -42,38 +55,173 @@ public class HEKDownloader implements SWEKDownloader {
         baseURL = appendX1X2Y1Y2(baseURL, eventType, startDate, endDate).append("&");
         baseURL = appendCosec(baseURL, eventType, startDate, endDate).append("&");
         baseURL = appendParams(baseURL, eventType, startDate, endDate).append("&");
+        baseURL = appendEventStartTime(baseURL, eventType, startDate).append("&");
+        baseURL = appendEventEndTime(baseURL, eventType, endDate).append("&");
         return baseURL.toString();
     }
 
+    /**
+     * Appends the command to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with the command
+     */
     private StringBuilder appendCmd(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
         return baseURL.append("cmd=search");
     }
 
+    /**
+     * Appends the type to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with the type
+     */
     private StringBuilder appendType(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
         return baseURL.append("type=column");
     }
 
+    /**
+     * Appends the event type to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with the event type
+     */
     private StringBuilder appendEventType(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
         return baseURL.append("event_type=").append(HEKEventFactory.getHEKEvent(eventType.getEventName()));
     }
 
+    /**
+     * Appends the event coordinate system to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with the event coordinate system
+     */
     private StringBuilder appendEventCoorSys(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
-        return baseURL.append("event_coordsys=helioprojective");
+        return baseURL.append("event_coordsys=").append(eventType.getCoordinateSystem());
     }
 
+    /**
+     * Appends the spatial region to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with the spatial region
+     */
     private StringBuilder appendX1X2Y1Y2(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
-        // TODO Auto-generated method stub
-        return null;
+        baseURL.append("x1=").append(eventType.getSpatialRegion().getX1()).append("&");
+        baseURL.append("x2=").append(eventType.getSpatialRegion().getX2()).append("&");
+        baseURL.append("y1=").append(eventType.getSpatialRegion().getY1()).append("&");
+        baseURL.append("y2=").append(eventType.getSpatialRegion().getY2()).append("&");
+        return baseURL;
     }
 
+    /**
+     * Append cosec to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with cosec
+     */
     private StringBuilder appendCosec(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
-        // TODO Auto-generated method stub
-        return null;
+        return baseURL.append("cosec=2");
     }
 
+    /**
+     * Appends params to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with the params
+     */
     private StringBuilder appendParams(StringBuilder baseURL, SWEKEventType eventType, Date startDate, Date endDate) {
-        // TODO Auto-generated method stub
-        return null;
+        return baseURL;
     }
 
+    /**
+     * Appends the event start time to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param startDate
+     *            the start date
+     * @return the current URL extended with the start time
+     */
+    private StringBuilder appendEventStartTime(StringBuilder baseURL, SWEKEventType eventType, Date startDate) {
+        return baseURL.append("event_starttime=").append(formatDate(startDate));
+    }
+
+    /**
+     * Appends the event end time to the given URL.
+     * 
+     * @param baseURL
+     *            the current URL
+     * @param eventType
+     *            the event type
+     * @param endDate
+     *            the end date
+     * @return the current URL extended with the end time
+     */
+    private StringBuilder appendEventEndTime(StringBuilder baseURL, SWEKEventType eventType, Date endDate) {
+        return baseURL.append("event_endtime=").append(formatDate(endDate));
+    }
+
+    /**
+     * Formats a date in the yyyy-mm-ddThh:mm:ss format.
+     * 
+     * @param date
+     *            the date to format
+     * @return the date in format yyyy-mm-ddThh:mm-ss
+     */
+    private String formatDate(Date date) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        return sdf.format(date);
+    }
 }
