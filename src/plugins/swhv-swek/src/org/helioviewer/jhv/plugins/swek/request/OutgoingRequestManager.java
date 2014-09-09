d590bc4d077908084bad69a446d1a3c4d54aeac9
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-09 12:44:52 +0000

    Implemented the outgoing request manager
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1861 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/request/OutgoingRequestManager.java
+++ src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/request/OutgoingRequestManager.java
@@ -1,11 +1,16 @@
 package org.helioviewer.jhv.plugins.swek.request;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.helioviewer.base.logging.Log;
+import org.helioviewer.jhv.data.container.JHVEventContainer;
 import org.helioviewer.jhv.layers.LayersListener;
 import org.helioviewer.jhv.layers.LayersModel;
+import org.helioviewer.jhv.plugins.swek.receive.SWEKEventHandler;
 import org.helioviewer.viewmodel.view.View;
 import org.helioviewer.viewmodel.view.jp2view.JHVJPXView;
 import org.helioviewer.viewmodel.view.jp2view.kakadu.JHV_KduException;
@@ -19,17 +24,47 @@ import org.helioviewer.viewmodel.view.jp2view.kakadu.JHV_KduException;
  */
 public class OutgoingRequestManager implements LayersListener {
 
+    /** The singleton instance of the outgoing request manager */
+    private static OutgoingRequestManager instance;
+
+    /** instance of the swek event handler */
+    private final SWEKEventHandler swekEventHandler;
+
+    /**
+     * private constructor
+     */
+    private OutgoingRequestManager() {
+        swekEventHandler = SWEKEventHandler.getSingletonInstace();
+    }
+
+    /**
+     * Gets the singleton instance of the outgoing request manager
+     * 
+     * @return the singleton instance
+     */
+    public static OutgoingRequestManager getSingletonInstance() {
+        if (instance == null) {
+            instance = new OutgoingRequestManager();
+        }
+        return instance;
+    }
+
     @Override
     public void layerAdded(int idx) {
         try {
             View activeView = LayersModel.getSingletonInstance().getActiveView();
-            new ArrayList<Date>();
-            if (activeView instanceof JHVJPXView) {
-                JHVJPXView activeJPXView = (JHVJPXView) activeView;
-                for (int frame = 1; frame <= activeJPXView.getMaximumFrameNumber(); frame++) {
-                    activeJPXView.getJP2Image().getValueFromXML("DATE-OBS", "fits", frame);
+            List<Date> requestDates = new ArrayList<Date>();
+            JHVJPXView jpxView = activeView.getAdapter(JHVJPXView.class);
+            if (jpxView != null) {
+                for (int frame = 1; frame <= jpxView.getMaximumFrameNumber(); frame++) {
+                    String dateOBS = jpxView.getJP2Image().getValueFromXML("DATE-OBS", "fits", frame);
+                    Date parsedDate = parseDate(dateOBS);
+                    if (parsedDate != null) {
+                        requestDates.add(parsedDate);
+                    }
                 }
             }
+            JHVEventContainer.getSingletonInstance().requestForDateList(requestDates, swekEventHandler);
         } catch (JHV_KduException ex) {
             Log.error("Received an kakadu exception. " + ex);
         }
@@ -75,6 +110,24 @@ public class OutgoingRequestManager implements LayersListener {
     public void layerDownloaded(int idx) {
         // TODO Auto-generated method stub
 
+    }
+
+    /**
+     * Parses a date in string with the format yyyy-MM-dd'T'HH:mm:ss.SSS into a
+     * date object.
+     * 
+     * @param dateOBS
+     *            the date to parse
+     * @return The parsed date
+     */
+    private Date parseDate(String dateOBS) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+        try {
+            return sdf.parse(dateOBS);
+        } catch (ParseException e) {
+            Log.warn("Could not parse date:" + dateOBS + ". Returned null.");
+            return null;
+        }
     }
 
 }
