445fe55d511e136922404e0bbc663819c7d6e448
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-09 12:44:52 +0000

    Introduced the SWEKEventHandler and the SWEKEventHAndlerListener
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1860 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/receive/SWEKEventHandler.java
@@ -0,0 +1,60 @@
+package org.helioviewer.jhv.plugins.swek.receive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.helioviewer.jhv.data.container.JHVEventHandler;
+import org.helioviewer.jhv.data.datatype.JHVEvent;
+
+public class SWEKEventHandler implements JHVEventHandler {
+    /** Singleton instance of the SWEKEventHandler */
+    private static SWEKEventHandler instance;
+
+    /** the listeners */
+    private final List<SWEKEventHandlerListener> listeners;
+
+    /**
+     * Private default constructor
+     */
+    private SWEKEventHandler() {
+        listeners = new ArrayList<SWEKEventHandlerListener>();
+    }
+
+    /**
+     * Gets the singleton instance of the SWEKEventHandler
+     * 
+     * @return the singleton instance
+     */
+    public static SWEKEventHandler getSingletonInstace() {
+        if (instance != null) {
+            instance = new SWEKEventHandler();
+        }
+        return instance;
+    }
+
+    /**
+     * Adds a SWEKEventHandlerListener to the SWEKEventHandler
+     * 
+     * @param listener
+     *            the listener to add
+     */
+    public void addSWEKEventHandlerListener(SWEKEventHandlerListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Removes the SWEKEventHandlerListener from the SWEKEventHandler
+     * 
+     * @param listener
+     *            the listener to remove
+     */
+    public void removeSWEKEventHandkerListener(SWEKEventHandlerListener listener) {
+        listeners.remove(listener);
+    }
+
+    @Override
+    public void newEventsReceived(List<JHVEvent> eventList) {
+
+    }
+
+}
