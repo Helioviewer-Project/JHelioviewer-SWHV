815e557a7c2c2577d34ff8cb08454220bdb5906b
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-09 12:44:51 +0000

    Implemented the JHVEventContainer with the event cache and the event handler cache
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1859 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-data/src/org/helioviewer/jhv/data/container/JHVEventContainer.java
+++ src/jhv-data/src/org/helioviewer/jhv/data/container/JHVEventContainer.java
@@ -1,20 +1,35 @@
 package org.helioviewer.jhv.data.container;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import org.helioviewer.jhv.data.container.cache.JHVEventCache;
+import org.helioviewer.jhv.data.container.cache.JHVEventHandlerCache;
+import org.helioviewer.jhv.data.datatype.JHVEvent;
+import org.helioviewer.jhv.data.lock.JHVEventContainerLocks;
 
 public class JHVEventContainer {
 
     /** Singleton instance */
     private static JHVEventContainer singletonInstance;
 
+    /** The handlers of requests */
     private final List<JHVEventContainerRequestHandler> handlers;
+
+    /** the event cache */
+    private final JHVEventCache eventCache;
+
+    /** the event handler cache */
+    private final JHVEventHandlerCache eventHandlerCache;
 
     /**
      * Private constructor.
      */
     private JHVEventContainer() {
-        this.handlers = new ArrayList<JHVEventContainerRequestHandler>();
+        handlers = new ArrayList<JHVEventContainerRequestHandler>();
+        eventHandlerCache = JHVEventHandlerCache.getSingletonInstance();
+        eventCache = JHVEventCache.getSingletonInstance();
     }
 
     /**
@@ -36,7 +51,7 @@ public class JHVEventContainer {
      *            the handler to register
      */
     public void registerHandler(JHVEventContainerRequestHandler handler) {
-        this.handlers.add(handler);
+        handlers.add(handler);
     }
 
     /**
@@ -46,6 +61,65 @@ public class JHVEventContainer {
      *            the handler to remove
      */
     public void removeHandler(JHVEventContainerRequestHandler handler) {
-        this.handlers.remove(handler);
+        handlers.remove(handler);
+    }
+
+    /**
+     * Request the JHVEventContainer for events from a specific date. The events
+     * will be send to the given handler. Events already available will directly
+     * be send to the handler. Events becoming available will also be send to
+     * the handler in the future.
+     * 
+     * @param date
+     *            the date to send events for
+     * @param handler
+     *            the handler to send events to
+     */
+    public void requestForDate(Date date, JHVEventHandler handler) {
+        synchronized (JHVEventContainerLocks.dateLock) {
+            eventHandlerCache.add(handler, date);
+            List<JHVEvent> events = eventCache.get(date);
+            handler.newEventsReceived(events);
+        }
+    }
+
+    /**
+     * Request the JHVEventContainer for events from a specific list of dates.
+     * The events will be send to the given handler. Events already available
+     * will directly be send to the handler. Events becoming available will also
+     * be send to the handler in the future.
+     * 
+     * @param dateList
+     *            the list of dates to send events for
+     * @param handler
+     *            the handler to send events to
+     */
+    public void requestForDateList(List<Date> dateList, JHVEventHandler handler) {
+        synchronized (JHVEventContainerLocks.dateLock) {
+            for (Date date : dateList) {
+                requestForDate(date, handler);
+            }
+        }
+    }
+
+    /**
+     * Request the JHVEventContainer for events from a specific time interval.
+     * The events will be send to the given handler. Events already available
+     * will directly be send to the handler. Events becoming available will also
+     * be send to the handler in the future.
+     * 
+     * @param startDate
+     *            the start date of the interval
+     * @param endDate
+     *            the end date of the interval
+     * @param handler
+     *            the handler
+     */
+    public void requestForInterval(Date startDate, Date endDate, JHVEventHandler handler) {
+        synchronized (JHVEventContainerLocks.intervalLock) {
+            eventHandlerCache.add(handler, startDate, endDate);
+            List<JHVEvent> events = eventCache.get(startDate, endDate);
+            handler.newEventsReceived(events);
+        }
     }
 }
