bde514b338ddca37ee1a00d4c37af35f9697d888
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-09 12:44:43 +0000

    Introduced the JHVEventHandler interface
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1850 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv-data/src/org/helioviewer/jhv/data/container/JHVEventHandler.java
@@ -0,0 +1,21 @@
+package org.helioviewer.jhv.data.container;
+
+import java.util.List;
+
+import org.helioviewer.jhv.data.datatype.JHVEvent;
+
+/**
+ * Interface should be implemented by a class that handles received events by
+ * the JHVEventContainer.
+ * 
+ * @author Bram Bourgoignie (Bram.Bourgoignie)
+ */
+public interface JHVEventHandler {
+    /**
+     * New Events were received by the JHVEventContainer.
+     * 
+     * @param eventList
+     *            the list of events that were received
+     */
+    public abstract void newEventsReceived(List<JHVEvent> eventList);
+}
