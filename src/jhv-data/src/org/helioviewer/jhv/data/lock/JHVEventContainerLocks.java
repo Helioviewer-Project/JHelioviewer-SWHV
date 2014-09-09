84e4b106ffb5f6acf2281bb192f81953e2f038d9
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-09 12:44:44 +0000

    Introduced the JHVEventContainerLocks
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1854 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv-data/src/org/helioviewer/jhv/data/lock/JHVEventContainerLocks.java
@@ -0,0 +1,9 @@
+package org.helioviewer.jhv.data.lock;
+
+public class JHVEventContainerLocks {
+    public static final Object dateLock = new Object();
+
+    public static final Object intervalLock = new Object();
+
+    public static final Object cacheLock = new Object();
+}
