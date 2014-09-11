0bc572fc7aaeaa19441bba70860b0aec586dc275
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-11 07:27:54 +0000

    Implementation of the HEKEventStream.
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1880 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/swhv-swek/sources/HEKSource/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKEventStream.java
@@ -0,0 +1,41 @@
+package org.helioviewer.jhv.plugins.swek.sources.hek;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.helioviewer.jhv.data.datatype.JHVEvent;
+import org.helioviewer.jhv.plugins.swek.sources.SWEKEventStream;
+
+/**
+ * A stream of Hek events. Implementation of the SWEKEventStream.
+ * 
+ * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
+ * 
+ */
+public class HEKEventStream implements SWEKEventStream {
+    /** Queue with events */
+    Queue<JHVEvent> eventQueue = new LinkedList<JHVEvent>();
+
+    public HEKEventStream() {
+        eventQueue = new LinkedList<JHVEvent>();
+    }
+
+    @Override
+    public boolean hasEvents() {
+        return !eventQueue.isEmpty();
+    }
+
+    @Override
+    public JHVEvent next() {
+        return eventQueue.poll();
+    }
+
+    /**
+     * Adds an event to the stream.
+     * 
+     * @param event
+     */
+    public void addJHVEvent(JHVEvent event) {
+        eventQueue.add(event);
+    }
+}
