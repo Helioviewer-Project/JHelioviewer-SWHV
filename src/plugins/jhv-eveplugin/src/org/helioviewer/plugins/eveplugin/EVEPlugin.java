aa0dd59a6b7ddeb7c5b50ba8735b7ca9f8805287
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-12 13:14:55 +0000

    Code cleanup
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1898 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/EVEPlugin.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/EVEPlugin.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 
 import javax.swing.JComponent;
 
-import org.helioviewer.base.logging.Log;
 import org.helioviewer.jhv.JavaHelioViewerLauncher;
 import org.helioviewer.jhv.gui.ImageViewerGui;
 import org.helioviewer.jhv.gui.dialogs.observation.ObservationDialog;
@@ -28,6 +27,7 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
     private final LinkedList<JComponent> pluginPanes = new LinkedList<JComponent>();
     private MainPanel mainPanel;
 
+    @Override
     public void installPlugin() {
         if (mainPanel == null) {
             mainPanel = new MainPanel();
@@ -36,16 +36,21 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
         pluginPanes.add(mainPanel);
 
         ImageViewerGui.getSingletonInstance().getMainContentPanel().addPlugin(this);
-        ObservationDialog.getSingletonInstance().addUserInterface(EVESettings.OBSERVATION_UI_NAME, new ObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
-        ObservationDialog.getSingletonInstance().addUserInterface(EVESettings.RADIO_OBSERVATION_UI_NAME, new SimpleObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
+        ObservationDialog.getSingletonInstance().addUserInterface(EVESettings.OBSERVATION_UI_NAME,
+                new ObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
+        ObservationDialog.getSingletonInstance().addUserInterface(EVESettings.RADIO_OBSERVATION_UI_NAME,
+                new SimpleObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
         // initialize database connection
         DatabaseController.getSingletonInstance();
         RadioPlotModel.getSingletonInstance();
     }
 
+    @Override
     public void uninstallPlugin() {
-        ObservationDialog.getSingletonInstance().removeUserInterface(EVESettings.OBSERVATION_UI_NAME, new ObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
-        ObservationDialog.getSingletonInstance().removeUserInterface(EVESettings.RADIO_OBSERVATION_UI_NAME, new SimpleObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
+        ObservationDialog.getSingletonInstance().removeUserInterface(EVESettings.OBSERVATION_UI_NAME,
+                new ObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
+        ObservationDialog.getSingletonInstance().removeUserInterface(EVESettings.RADIO_OBSERVATION_UI_NAME,
+                new SimpleObservationDialogUIPanel(mainPanel.getPlotContainerPanel()));
         ImageViewerGui.getSingletonInstance().getMainContentPanel().removePlugin(this);
     }
 
@@ -53,19 +58,24 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
         return EVEPlugin.class.getResource(name);
     }
 
+    @Override
     public String getName() {
         return "EVEPlugin";
     }
 
+    @Override
     public String getDescription() {
         return "This plugin visualizes the level 2 data of the EVE project";
     }
 
+    @Override
     public String getAboutLicenseText() {
         String description = "";
 
         description += "<p>The plugin uses the <a href=\"http://www.json.org/java/\">JSON in Java</a> Library, licensed under a <a href=\"http://www.json.org/license.html\">custom License</a>.";
-        description += "<p>The plugin uses <a href=\"http://db.apache.org/derby/\">Apache Derby, an Apache DB subproject</a>,<br>" + '\u00A9' + " 2011, Apache Software Foundation, <a href=\"http://www.apache.org/licenses/\">Apache License, Version 2.0</a><br>";
+        description += "<p>The plugin uses <a href=\"http://db.apache.org/derby/\">Apache Derby, an Apache DB subproject</a>,<br>"
+                + '\u00A9'
+                + " 2011, Apache Software Foundation, <a href=\"http://www.apache.org/licenses/\">Apache License, Version 2.0</a><br>";
 
         return description;
     }
@@ -80,18 +90,22 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
         JavaHelioViewerLauncher.start(EVEPluginLauncher.class, args);
     }
 
+    @Override
     public LinkedList<JComponent> getVisualInterfaces() {
         return pluginPanes;
     }
 
+    @Override
     public String getTabName() {
         return "1-D and 2-D Time Series";
     }
 
+    @Override
     public void setState(String state) {
         // TODO SP: Implement setState for EVEPlugin
     }
 
+    @Override
     public String getState() {
         // TODO SP: Implement getState for EVEPlugin
         return "";
