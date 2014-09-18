b902bf42fe2d6630c90fe38469c9062bfb3f0377
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-18 12:17:47 +0000

    Introduction of the JHVSWEKEventType
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1925 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/download/JHVSWEKEventType.java
@@ -0,0 +1,70 @@
+package org.helioviewer.jhv.plugins.swek.download;
+
+import org.helioviewer.jhv.data.datatype.JHVEventType;
+
+/**
+ * JHV event type implementation for the swek.
+ * 
+ * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
+ * 
+ */
+public class JHVSWEKEventType implements JHVEventType {
+
+    /** The event type */
+    private final String eventType;
+
+    /** The event source */
+    private final String eventSource;
+
+    /** The event provider */
+    private final String eventProvider;
+
+    /**
+     * Default constructor.
+     * 
+     */
+    public JHVSWEKEventType() {
+        eventType = "";
+        eventSource = "";
+        eventProvider = "";
+    }
+
+    /**
+     * Creates a JHVSWEK event type from the given event type, event source,
+     * event provider.
+     * 
+     * @param eventType
+     *            the event type
+     * @param eventSource
+     *            the event source
+     * @param eventProvider
+     *            the event provider
+     */
+    public JHVSWEKEventType(String eventType, String eventSource, String eventProvider) {
+        this.eventType = eventType;
+        this.eventSource = eventSource;
+        this.eventProvider = eventProvider;
+    }
+
+    @Override
+    public String getEventType() {
+        return eventType;
+    }
+
+    @Override
+    public String getEventSource() {
+        return eventSource;
+    }
+
+    @Override
+    public String getEvenProvider() {
+        return eventProvider;
+    }
+
+    @Override
+    public boolean equals(JHVEventType otherEventType) {
+        return otherEventType.getEventType().equals(eventType) && otherEventType.getEventSource().equals(eventSource)
+                && otherEventType.getEvenProvider().equals(eventProvider);
+    }
+
+}
