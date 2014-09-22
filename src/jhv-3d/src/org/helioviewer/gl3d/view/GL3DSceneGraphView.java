6ded2faa534ac997a820757dbb9e68d87d2f5d83
Freek Verstringe <freek.verstringe@oma.be>
2014-09-22 21:35:46 +0000

    merge with jhv-3d-wcs rev 647
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1971 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d/src/org/helioviewer/gl3d/view/GL3DSceneGraphView.java
+++ src/jhv-3d/src/org/helioviewer/gl3d/view/GL3DSceneGraphView.java
@@ -86,30 +86,25 @@ public class GL3DSceneGraphView extends AbstractGL3DView implements GL3DView {
          * root.getDrawBits().toggle(Bit.Wireframe);
          * Displayer.getSingletonInstance().display();
          * Log.debug("Toggling Wireframe"); } }, KeyEvent.VK_W);
+         * 
+         * GL3DKeyController.getInstance().addListener(new GL3DKeyListener() {
+         * 
+         * @Override public void keyHit(KeyEvent e) {
+         * root.getDrawBits().toggle(Bit.Normals);
+         * Displayer.getSingletonInstance().display();
+         * Log.debug("Toggling Normals"); } }, KeyEvent.VK_N);
+         * GL3DKeyController.getInstance().addListener(new GL3DKeyListener() {
+         * 
+         * @Override public void keyHit(KeyEvent e) {
+         * Displayer.getSingletonInstance().display();
+         * Log.debug("Toggling Framebuffer"); } }, KeyEvent.VK_F);
+         * GL3DKeyController.getInstance().addListener(new GL3DKeyListener() {
+         * 
+         * @Override public void keyHit(KeyEvent e) {
+         * imageLayers.getDrawBits().toggle(Bit.Hidden);
+         * Displayer.getSingletonInstance().display();
+         * Log.debug("Toggling Images"); } }, KeyEvent.VK_I);
          */
-        GL3DKeyController.getInstance().addListener(new GL3DKeyListener() {
-            @Override
-            public void keyHit(KeyEvent e) {
-                root.getDrawBits().toggle(Bit.Normals);
-                Displayer.getSingletonInstance().display();
-                Log.debug("Toggling Normals");
-            }
-        }, KeyEvent.VK_N);
-        GL3DKeyController.getInstance().addListener(new GL3DKeyListener() {
-            @Override
-            public void keyHit(KeyEvent e) {
-                Displayer.getSingletonInstance().display();
-                Log.debug("Toggling Framebuffer");
-            }
-        }, KeyEvent.VK_F);
-        GL3DKeyController.getInstance().addListener(new GL3DKeyListener() {
-            @Override
-            public void keyHit(KeyEvent e) {
-                imageLayers.getDrawBits().toggle(Bit.Hidden);
-                Displayer.getSingletonInstance().display();
-                Log.debug("Toggling Images");
-            }
-        }, KeyEvent.VK_I);
         GL3DKeyController.getInstance().addListener(new GL3DKeyListener() {
             @Override
             public void keyHit(KeyEvent e) {
@@ -117,7 +112,7 @@ public class GL3DSceneGraphView extends AbstractGL3DView implements GL3DView {
                 Displayer.getSingletonInstance().display();
                 Log.debug("Toggling Corona Visibility");
             }
-        }, KeyEvent.VK_C);
+        }, KeyEvent.VK_X);
     }
 
     @Override
@@ -382,7 +377,7 @@ public class GL3DSceneGraphView extends AbstractGL3DView implements GL3DView {
         /*
          * GL3DNode sibling = node; while((sibling = sibling.getNext()) != null)
          * { for(int i=0; i<level; ++i) System.out.print("   ");
-         * 
+         *
          * System.out.println("Sibling: " + sibling.getClass().getName() + " ("
          * + node.getName() + ")"); }
          */
