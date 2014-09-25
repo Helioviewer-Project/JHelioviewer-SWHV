18bc0caa0934f82c3f89517eee4ef81f71825d76
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-25 13:28:21 +0000

    Fixed the drawing with the swing worker. No interface interruption anymore
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@1997 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/events/model/EventModel.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/events/model/EventModel.java
@@ -1,6 +1,5 @@
 package org.helioviewer.plugins.eveplugin.events.model;
 
-import java.awt.EventQueue;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -96,14 +95,7 @@ public class EventModel implements ZoomControllerListener, EventRequesterListene
     public void selectedIntervalChanged(final Interval<Date> newInterval) {
         selectedInterval = newInterval;
         if (!EVEState.getSingletonInstance().isMouseTimeIntervalDragging()) {
-            EventQueue.invokeLater(new Runnable() {
-
-                @Override
-                public void run() {
-
-                    createEventPlotConfiguration();
-                }
-            });
+            createEventPlotConfiguration();
         }
     }
 
@@ -149,93 +141,94 @@ public class EventModel implements ZoomControllerListener, EventRequesterListene
     }
 
     private void createEventPlotConfiguration() {
+
         if (currentSwingWorker != null) {
             currentSwingWorker.cancel(true);
-        } else {
-            currentSwingWorker = new SwingWorker<EventTypePlotConfiguration, Void>() {
+        }
 
-                @Override
-                public EventTypePlotConfiguration doInBackground() {
-                    Log.info("Swingworker started");
-                    int maxNrLines = 0;
-                    Map<String, Integer> linesPerEventType = new HashMap<String, Integer>();
-                    Map<String, List<EventPlotConfiguration>> eventPlotConfigPerEventType = new HashMap<String, List<EventPlotConfiguration>>();
-                    for (String eventType : events.keySet()) {
-                        ArrayList<Date> endDates = new ArrayList<Date>();
-                        List<EventPlotConfiguration> plotConfig = new ArrayList<EventPlotConfiguration>();
-                        Date minimalEndDate = null;
-                        Date maximumEndDate = null;
-                        int minimalDateLine = 0;
-                        int maximumDateLine = 0;
-                        int nrLines = 0;
-                        int maxEventLines = 0;
-                        for (Date sDate : events.get(eventType).keySet()) {
-                            for (Date eDate : events.get(eventType).get(sDate).keySet()) {
-                                for (JHVEvent event : events.get(eventType).get(sDate).get(eDate)) {
-                                    int eventPosition = 0;
-                                    if (minimalEndDate == null || minimalEndDate.compareTo(event.getStartDate()) >= 0) {
-                                        // first event or event start before
-                                        // minimal end
-                                        // date so next line
-                                        minimalEndDate = event.getEndDate();
+        currentSwingWorker = new SwingWorker<EventTypePlotConfiguration, Void>() {
+
+            @Override
+            public EventTypePlotConfiguration doInBackground() {
+                int maxNrLines = 0;
+                Map<String, Integer> linesPerEventType = new HashMap<String, Integer>();
+                Map<String, List<EventPlotConfiguration>> eventPlotConfigPerEventType = new HashMap<String, List<EventPlotConfiguration>>();
+                for (String eventType : events.keySet()) {
+                    ArrayList<Date> endDates = new ArrayList<Date>();
+                    List<EventPlotConfiguration> plotConfig = new ArrayList<EventPlotConfiguration>();
+                    Date minimalEndDate = null;
+                    Date maximumEndDate = null;
+                    int minimalDateLine = 0;
+                    int maximumDateLine = 0;
+                    int nrLines = 0;
+                    int maxEventLines = 0;
+                    for (Date sDate : events.get(eventType).keySet()) {
+                        for (Date eDate : events.get(eventType).get(sDate).keySet()) {
+                            for (JHVEvent event : events.get(eventType).get(sDate).get(eDate)) {
+                                int eventPosition = 0;
+                                if (minimalEndDate == null || minimalEndDate.compareTo(event.getStartDate()) >= 0) {
+                                    // first event or event start before
+                                    // minimal end
+                                    // date so next line
+                                    minimalEndDate = event.getEndDate();
+                                    endDates.add(event.getEndDate());
+                                    eventPosition = nrLines;
+                                    nrLines++;
+                                } else {
+                                    if (event.getStartDate().after(maximumEndDate)) {
+                                        // After all other events so start
+                                        // new line
+                                        // and
+                                        // reset everything
+                                        eventPosition = 0;
+                                        nrLines = 1;
+                                        endDates = new ArrayList<Date>();
                                         endDates.add(event.getEndDate());
-                                        eventPosition = nrLines;
-                                        nrLines++;
                                     } else {
-                                        if (event.getStartDate().after(maximumEndDate)) {
-                                            // After all other events so start
-                                            // new line
-                                            // and
-                                            // reset everything
-                                            eventPosition = 0;
-                                            nrLines = 1;
-                                            endDates = new ArrayList<Date>();
-                                            endDates.add(event.getEndDate());
-                                        } else {
-                                            // After minimal date so after
-                                            // minimal end
-                                            // date
-                                            eventPosition = minimalDateLine;
-                                            endDates.set(minimalDateLine, event.getEndDate());
-                                        }
+                                        // After minimal date so after
+                                        // minimal end
+                                        // date
+                                        eventPosition = minimalDateLine;
+                                        endDates.set(minimalDateLine, event.getEndDate());
                                     }
-                                    minimalDateLine = defineMinimalDateLine(endDates);
-                                    minimalEndDate = endDates.get(minimalDateLine);
-                                    maximumDateLine = defineMaximumDateLine(endDates);
-                                    maximumEndDate = endDates.get(maximumDateLine);
-                                    double scaledX0 = defineScaledValue(event.getStartDate());
-                                    double scaledX1 = defineScaledValue(event.getEndDate());
-                                    if (nrLines > maxEventLines) {
-                                        maxEventLines = nrLines;
-                                    }
-                                    plotConfig.add(new EventPlotConfiguration(event, scaledX0, scaledX1, eventPosition));
                                 }
+                                minimalDateLine = defineMinimalDateLine(endDates);
+                                minimalEndDate = endDates.get(minimalDateLine);
+                                maximumDateLine = defineMaximumDateLine(endDates);
+                                maximumEndDate = endDates.get(maximumDateLine);
+                                double scaledX0 = defineScaledValue(event.getStartDate());
+                                double scaledX1 = defineScaledValue(event.getEndDate());
+                                if (nrLines > maxEventLines) {
+                                    maxEventLines = nrLines;
+                                }
+                                plotConfig.add(new EventPlotConfiguration(event, scaledX0, scaledX1, eventPosition));
                             }
                         }
-                        linesPerEventType.put(eventType, maxEventLines);
-                        maxNrLines += maxEventLines;
-                        eventPlotConfigPerEventType.put(eventType, plotConfig);
                     }
-                    Log.info("Swingworker finished");
-                    return new EventTypePlotConfiguration(events.size(), maxNrLines, linesPerEventType, eventPlotConfigPerEventType);
+                    linesPerEventType.put(eventType, maxEventLines);
+                    maxNrLines += maxEventLines;
+                    eventPlotConfigPerEventType.put(eventType, plotConfig);
                 }
+                return new EventTypePlotConfiguration(events.size(), maxNrLines, linesPerEventType, eventPlotConfigPerEventType);
+            }
 
-                @Override
-                public void done() {
-                    try {
-                        Log.info("Swingworker done started");
+            @Override
+            public void done() {
+                try {
+                    if (!isCancelled()) {
                         eventPlotConfiguration = get();
                         DrawController.getSingletonInstance().updateDrawableElement(eventPanel, plot);
-                        Log.info("Swingworker done finished");
-                    } catch (InterruptedException e) {
-                        Log.error("Could not create the event type plot configurations");
-                    } catch (ExecutionException e) {
-                        Log.error("Could not create the event type plot configurations");
+                    } else {
+                        Log.info("Worker was cancelled");
                     }
+                } catch (InterruptedException e) {
+                    Log.error("Could not create the event type plot configurations");
+                } catch (ExecutionException e) {
+                    Log.error("Could not create the event type plot configurations");
                 }
-            };
-            currentSwingWorker.execute();
-        }
+            }
+        };
+        currentSwingWorker.execute();
     }
 
     private int defineMaximumDateLine(ArrayList<Date> endDates) {
