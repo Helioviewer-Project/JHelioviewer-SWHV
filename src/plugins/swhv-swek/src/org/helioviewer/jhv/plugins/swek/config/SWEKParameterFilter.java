cebd344d963b3a1b2b740b011603c61363487df5
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-09-30 07:08:13 +0000

    Added start value to the parameter filter description and let return Double object
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2063 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/config/SWEKParameterFilter.java
+++ src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/config/SWEKParameterFilter.java
@@ -5,47 +5,77 @@ package org.helioviewer.jhv.plugins.swek.config;
 
 /**
  * Describes a SWEK parameter filter
- *
+ * 
  * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
- *
+ * 
  */
 public class SWEKParameterFilter {
-    /** The type of filter*/
+    /** The type of filter */
     private String filterType;
 
-    /** The minium value*/
-    private double min;
+    /** The minium value */
+    private Double min;
     /** The maximum value */
-    private double max;
+    private Double max;
+    /** The start value. */
+    private Double startValue;
 
     /**
-     * Creates a SWEK parameter filter of no type, with minimum value 0.0 and maximum value 0.0.
-     *
+     * Creates a SWEK parameter filter of no type, with minimum value 0.0 and
+     * maximum value 0.0.
+     * 
      */
-    public SWEKParameterFilter(){
-        this.filterType = "";
-        this.min = 0.0;
-        this.max = 0.0;
+    public SWEKParameterFilter() {
+        filterType = null;
+        min = null;
+        max = null;
+        startValue = null;
     }
 
     /**
-     * Creates a SWEK parameter filter description based on the given filter type, the minimum value and the maximum value.
-     *
-     *
-     * @param filterType    The type of filter
-     * @param min           The minimum value
-     * @param max           The maximum value
+     * Creates a SWEK parameter filter description based on the given filter
+     * type, the minimum value, the maximum value and null start value.
+     * 
+     * 
+     * @param filterType
+     *            The type of filter
+     * @param min
+     *            The minimum value
+     * @param max
+     *            The maximum value
      */
     public SWEKParameterFilter(String filterType, double min, double max) {
         super();
         this.filterType = filterType;
         this.min = min;
         this.max = max;
+        startValue = null;
+    }
+
+    /**
+     * Creates a SWEK parameter filter description based on the given filter
+     * type, the minimum value, the maximum value and the start value.
+     * 
+     * @param filterType
+     *            the filter
+     * @param min
+     *            the minimum value
+     * @param max
+     *            the maximum value
+     * @param startValue
+     *            the start value
+     */
+    public SWEKParameterFilter(String filterType, double min, double max, double startValue) {
+        super();
+        this.filterType = filterType;
+        this.min = min;
+        this.max = max;
+        this.startValue = startValue;
     }
 
     /**
      * Gets the type of filter
-     *
+     * 
      * @return the filterType
      */
     public String getFilterType() {
@@ -54,8 +84,9 @@ public class SWEKParameterFilter {
 
     /**
      * Sets the type of filter
-     *
-     * @param filterType the filterType to set
+     * 
+     * @param filterType
+     *            the filterType to set
      */
     public void setFilterType(String filterType) {
         this.filterType = filterType;
@@ -63,17 +94,18 @@ public class SWEKParameterFilter {
 
     /**
      * Gets the minimum value of the parameter filter.
-     *
+     * 
      * @return the min
      */
-    public double getMin() {
+    public Double getMin() {
         return min;
     }
 
     /**
      * Sets the minimum filter of the parameter filter
-     *
-     * @param min the min to set
+     * 
+     * @param min
+     *            the min to set
      */
     public void setMin(double min) {
         this.min = min;
@@ -81,19 +113,39 @@ public class SWEKParameterFilter {
 
     /**
      * Gets the maximum value of the parameter filter
-     *
+     * 
      * @return the max
      */
-    public double getMax() {
+    public Double getMax() {
         return max;
     }
 
     /**
      * Sets the maximum value of the parameter filter.
-     *
-     * @param max the max to set
+     * 
+     * @param max
+     *            the max to set
      */
     public void setMax(double max) {
         this.max = max;
+    }
+
+    /**
+     * Gets the start value.
+     * 
+     * @return the start value
+     */
+    public Double getStartValue() {
+        return startValue;
+    }
+
+    /**
+     * Sets the start value.
+     * 
+     * @param startValue
+     *            the start value
+     */
+    public void setStartValue(Double startValue) {
+        this.startValue = startValue;
     }
 }
