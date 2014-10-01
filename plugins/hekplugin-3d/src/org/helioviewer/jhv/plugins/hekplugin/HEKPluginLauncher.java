2cff7bbbdf90169eb4caad6344bae42f7802a415
Freek Verstringe <freek.verstringe@oma.be>
2014-10-01 11:18:58 +0000

    fix launcher
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2094 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/hekplugin-3d/src/org/helioviewer/jhv/plugins/hekplugin/HEKPluginLauncher.java
+++ plugins/hekplugin-3d/src/org/helioviewer/jhv/plugins/hekplugin/HEKPluginLauncher.java
@@ -2,18 +2,19 @@ package org.helioviewer.jhv.plugins.hekplugin;
 
 import org.helioviewer.base.JavaCompatibility;
 import org.helioviewer.jhv.JavaHelioViewer;
+import org.helioviewer.jhv.JavaHelioViewerLauncher;
 
 /**
  * Class for testing the external plugin
- * 
+ *
  * @author Andre Dau
- * 
+ *
  */
 public class HEKPluginLauncher {
 
     /**
      * Used for testing the plugin
-     * 
+     *
      * @see org.helioviewer.plugins.hekplugin.hekplugin#main(String[])
      * @param args
      */
@@ -24,10 +25,11 @@ public class HEKPluginLauncher {
         System.out.println("================================================================\n\n");
 
         String[] args2 = JavaCompatibility.copyArrayString(args, args.length + 2);
+        JavaHelioViewerLauncher.loadLibs();
 
         args2[args2.length - 2] = "--deactivate-plugin";
         args2[args2.length - 1] = "HEKPlugin.jar";
-        JavaHelioViewer.main(args2, new HEKPlugin(true));
+        JavaHelioViewer.main(args2, new HEKPlugin(false));
         // JavaHelioViewer.main(args2);
     }
 }
