042d692ae8c354659fcf9051e89c90be70e3303d
Freek Verstringe <freek.verstringe@oma.be>
2014-10-01 15:05:14 +0000

    add cactus display
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2114 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- plugins/hekplugin-3d/src/org/helioviewer/gl3d/plugin/hekplugin/HEKPlugin3dRenderer.java
+++ plugins/hekplugin-3d/src/org/helioviewer/gl3d/plugin/hekplugin/HEKPlugin3dRenderer.java
@@ -6,12 +6,10 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Vector;
 
 import javax.media.opengl.GL2;
 import javax.swing.ImageIcon;
 
-import org.helioviewer.base.logging.Log;
 import org.helioviewer.base.math.Vector3dDouble;
 import org.helioviewer.base.physics.Astronomy;
 import org.helioviewer.gl3d.view.GL3DImageTextureView;
@@ -21,10 +19,9 @@ import org.helioviewer.gl3d.wcs.impl.SolarSphereCoordinateSystem;
 import org.helioviewer.gl3d.wcs.impl.SphericalCoordinateSystem;
 import org.helioviewer.jhv.data.datatype.JHVCoordinateSystem;
 import org.helioviewer.jhv.data.datatype.JHVEvent;
+import org.helioviewer.jhv.data.datatype.JHVEventParameter;
 import org.helioviewer.jhv.data.datatype.JHVPoint;
 import org.helioviewer.jhv.data.datatype.JHVPositionInformation;
-import org.helioviewer.jhv.plugins.hekplugin.cache.HEKEvent;
-import org.helioviewer.jhv.plugins.hekplugin.cache.HEKEvent.GenericTriangle;
 import org.helioviewer.jhv.plugins.hekplugin.cache.SWHVHEKData;
 import org.helioviewer.viewmodel.region.Region;
 import org.helioviewer.viewmodel.renderer.physical.PhysicalRenderGraphics;
@@ -54,6 +51,80 @@ public class HEKPlugin3dRenderer extends PhysicalRenderer3d {
     public HEKPlugin3dRenderer() {
     }
 
+    public void drawCactusArc(PhysicalRenderGraphics g, JHVEvent evt, Date now) {
+        GL2 gl = g.getGL();
+        List<JHVEventParameter> params = evt.getAllEventParameters();
+        double principleAngle = 0;
+        double angularWidth = 0;
+        double distSun = 1.;
+        for (JHVEventParameter param : params) {
+            if (param.getParameterName().equals("cme_angularwidth")) {
+                angularWidth = Double.parseDouble(param.getParameterValue()) * Math.PI / 180.;
+            }
+            if (param.getParameterName().equals("event_coord1")) {
+                principleAngle = Double.parseDouble(param.getParameterValue()) * Math.PI / 180.;
+            }
+            if (param.getParameterName().equals("event_coord2")) {
+                distSun = Double.parseDouble(param.getParameterValue());
+            }
+        }
+        double arcResolution = 100;
+        double thetaDelta = Astronomy.getB0InRadians(new Date((evt.getStartDate().getTime() + evt.getEndDate().getTime()) / 2));
+        double thetaStart = principleAngle - angularWidth / 2.;
+        double thetaEnd = principleAngle + angularWidth / 2.;
+        double phi = -Math.PI / 2. - Astronomy.getL0Radians(new Date((evt.getStartDate().getTime() + evt.getEndDate().getTime()) / 2));
+        double lineResolution = 10;
+
+        gl.glColor3d(evt.getColor().getRed(), evt.getColor().getGreen(), evt.getColor().getBlue());
+        gl.glDisable(GL2.GL_TEXTURE_2D);
+        gl.glEnable(GL2.GL_LINE_SMOOTH);
+        gl.glLineWidth(0.5f);
+        gl.glBegin(GL2.GL_LINE_STRIP);
+        for (int i = 0; i <= lineResolution; i++) {
+            double alpha = 1. - 1. * i / arcResolution;
+            double r = alpha * distSun + (1 - alpha) * (distSun + 2);
+            double theta = thetaStart;
+
+            double x = r * Math.cos(theta) * Math.sin(phi);
+            double z = r * Math.cos(theta) * Math.cos(phi);
+            double y = r * Math.sin(theta);
+            double yrot = y * Math.cos(thetaDelta) + z * Math.sin(thetaDelta);
+            double zrot = -y * Math.sin(thetaDelta) + z * Math.cos(thetaDelta);
+            double xrot = x;
+
+            gl.glVertex3d(xrot, yrot, zrot);
+        }
+        for (int i = 0; i <= arcResolution; i++) {
+            double alpha = 1. - 1. * i / arcResolution;
+            double theta = alpha * thetaStart + (1 - alpha) * thetaEnd;
+
+            double x = distSun * Math.cos(theta) * Math.sin(phi);
+            double z = distSun * Math.cos(theta) * Math.cos(phi);
+            double y = distSun * Math.sin(theta);
+            double yrot = y * Math.cos(thetaDelta) + z * Math.sin(thetaDelta);
+            double zrot = -y * Math.sin(thetaDelta) + z * Math.cos(thetaDelta);
+            double xrot = x;
+
+            gl.glVertex3d(xrot, yrot, zrot);
+        }
+        for (int i = 0; i <= lineResolution; i++) {
+            double alpha = 1. - 1. * i / arcResolution;
+            double r = alpha * distSun + (1 - alpha) * (distSun + 2);
+            double theta = thetaEnd;
+
+            double x = r * Math.cos(theta) * Math.sin(phi);
+            double z = r * Math.cos(theta) * Math.cos(phi);
+            double y = r * Math.sin(theta);
+            double yrot = y * Math.cos(thetaDelta) + z * Math.sin(thetaDelta);
+            double zrot = -y * Math.sin(thetaDelta) + z * Math.cos(thetaDelta);
+            double xrot = x;
+
+            gl.glVertex3d(xrot, yrot, zrot);
+        }
+        gl.glEnd();
+        gl.glDisable(GL2.GL_LINE_SMOOTH);
+    }
+
     /**
      * The actual rendering routine
      *
@@ -81,16 +152,6 @@ public class HEKPlugin3dRenderer extends PhysicalRenderer3d {
         }
         String type = evt.getName();
         Color eventColor = evt.getColor();
-
-        Vector<HEKEvent.GenericTriangle<Vector3dDouble>> triangles = null;
-
-        if (triangles != null) {
-            g.setColor(eventColor);
-            for (GenericTriangle<Vector3dDouble> triangle : triangles) {
-                Vector3dDouble tri[] = { triangle.A, triangle.B, triangle.C };
-                g.fillPolygon(tri);
-            }
-        }
 
         // draw bounds
         Vector3dDouble oldBoundaryPoint3d = null;
@@ -178,14 +239,13 @@ public class HEKPlugin3dRenderer extends PhysicalRenderer3d {
 
             if (currentDate != null) {
                 ArrayList<JHVEvent> toDraw = SWHVHEKData.getSingletonInstance().getActiveEvents(currentDate);
-                Log.info(toDraw);
                 for (JHVEvent evt : toDraw) {
-                    if (evt.getName() == "CME") {
-                        System.out.println("CACTUSSSS");
+                    if (evt.getName().equals("Coronal Mass Ejection")) {
+                        drawCactusArc(g, evt, currentDate);
+                    } else {
+                        drawPolygon(g, evt, currentDate);
+                        drawIcon(g, evt, currentDate);
                     }
-                    drawPolygon(g, evt, currentDate);
-                    drawIcon(g, evt, currentDate);
-
                 }
             }
         }
