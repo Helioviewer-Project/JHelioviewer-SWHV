47d3702bb1ce347308587625fadfb209c50df25e
Freek Verstringe <freek.verstringe@oma.be>
2014-10-07 10:34:44 +0000

    Reset old code after merge jhv-3d-wcs
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2164 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-3d-plugin-pfss/src/org/helioviewer/gl3d/plugin/pfss/PfssPluginLauncher.java
+++ src/plugins/jhv-3d-plugin-pfss/src/org/helioviewer/gl3d/plugin/pfss/PfssPluginLauncher.java
@@ -2,18 +2,19 @@ package org.helioviewer.gl3d.plugin.pfss;
 
 import org.helioviewer.base.JavaCompatibility;
 import org.helioviewer.jhv.JavaHelioViewer;
+import org.helioviewer.jhv.JavaHelioViewerLauncher;
 import org.helioviewer.jhv.plugins.pfssplugin.PfssPlugin;
 
 /**
  * Class for testing the external plugin
- * 
+ *
  * @author Stefan Meier (stefan.meier@fhnw.ch)
  */
 public class PfssPluginLauncher {
 
     /**
      * Used for testing the plugin
-     * 
+     *
      * @param args
      */
     public static void main(String[] args) {
@@ -25,6 +26,7 @@ public class PfssPluginLauncher {
         String[] args2 = JavaCompatibility.copyArrayString(args, args.length + 2);
         args2[args2.length - 2] = "--deactivate-plugin";
         args2[args2.length - 1] = "PfssPlugin.jar";
+        JavaHelioViewerLauncher.loadLibs();
         JavaHelioViewer.main(args2, new PfssPlugin(true));
     }
 }
--- src/plugins/jhv-3d-plugin-pfss/src/org/helioviewer/gl3d/plugin/pfss/PfssPluginPanel.java
+++ src/plugins/jhv-3d-plugin-pfss/src/org/helioviewer/gl3d/plugin/pfss/PfssPluginPanel.java
@@ -5,6 +5,8 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,16 +15,22 @@ import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeListener;
 
 import org.helioviewer.base.logging.Log;
+import org.helioviewer.basegui.components.WheelSupport;
 import org.helioviewer.gl3d.plugin.pfss.data.PfssCache;
 import org.helioviewer.gl3d.plugin.pfss.settings.PfssSettings;
+import org.helioviewer.jhv.display.Displayer;
 import org.helioviewer.jhv.layers.LayersListener;
 import org.helioviewer.jhv.layers.LayersModel;
 import org.helioviewer.jhv.plugins.pfssplugin.PfssPlugin;
@@ -45,10 +53,10 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
     private PfssCache pfssCache = null;
     private boolean showAgain = true;
     private boolean retry = false;
-    private final CopyOnWriteArrayList<String> messages = new CopyOnWriteArrayList<String>();
     // UI Components
     private final JButton visibleButton = new JButton(new ImageIcon(PfssPlugin.getResourceUrl("/images/invisible_dm.png")));
     private final JButton reloadButton = new JButton(new ImageIcon(PfssPlugin.getResourceUrl("/images/reload.png")));
+    private JSpinner qualitySpinner;
 
     /**
      * Default constructor
@@ -79,7 +87,7 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
         GridBagLayout gridBagLayout = new GridBagLayout();
         gridBagLayout.columnWidths = new int[] { 0, 0, 0 };
         gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
-        gridBagLayout.columnWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+        gridBagLayout.columnWeights = new double[] { 0.0, 1., 0.0, Double.MIN_VALUE };
         gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
         setLayout(gridBagLayout);
 
@@ -88,20 +96,56 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
 
         setEnabled(true);
 
+        GridBagConstraints c0 = new GridBagConstraints();
+        c0.insets = new Insets(0, 0, 5, 0);
+        c0.gridx = 0;
+        c0.gridy = 1;
+        this.qualitySpinner = new JSpinner();
+        this.qualitySpinner.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), new Integer(8), new Integer(1)));
+
+        this.qualitySpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(javax.swing.event.ChangeEvent e) {
+                PfssSettings.qualityReduction = 8 - ((Integer) qualitySpinner.getValue()).intValue();
+                Displayer.getSingletonInstance().display();
+            }
+
+        });
+        WheelSupport.installMouseWheelSupport(qualitySpinner);
+
+        JPanel helpPanel = new JPanel();
+        helpPanel.add(new JLabel("Level:"));
+        helpPanel.add(qualitySpinner);
+        this.add(helpPanel, c0);
+
+        GridBagConstraints c1 = new GridBagConstraints();
+        c1.insets = new Insets(0, 0, 5, 0);
+        c1.gridx = 0;
+        c1.gridy = 2;
+        JCheckBox fixedColors = new JCheckBox("Fixed colors", false);
+        fixedColors.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                PfssSettings.fixedColor = (e.getStateChange() == ItemEvent.SELECTED);
+                Displayer.getSingletonInstance().display();
+            }
+        });
+        this.add(fixedColors, c1);
+
+        GridBagConstraints c2 = new GridBagConstraints();
+        c2.insets = new Insets(0, 0, 5, 0);
+        c2.gridx = 2;
+        c2.gridy = 0;
+
+        this.add(visibleButton, c2);
+
         GridBagConstraints c3 = new GridBagConstraints();
         c3.insets = new Insets(0, 0, 5, 0);
-        c3.gridx = 1;
+        c3.gridx = 3;
         c3.gridy = 0;
 
-        this.add(visibleButton, c3);
-        visibleButton.setToolTipText("disable/enable PFSS");
-        GridBagConstraints c6 = new GridBagConstraints();
-        c6.insets = new Insets(0, 0, 5, 0);
-        c6.gridx = 2;
-        c6.gridy = 0;
+        this.add(reloadButton, c3);
 
-        this.add(reloadButton, c6);
-        reloadButton.setToolTipText("reload PFSS data");
     }
 
     /**
@@ -117,35 +161,16 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
                 pfssCache.setVisible(false);
                 visibleButton.setIcon(new ImageIcon(PfssPlugin.getResourceUrl("/images/invisible_dm.png")));
             } else {
-                this.showData();
                 pfssCache.setVisible(true);
                 visibleButton.setIcon(new ImageIcon(PfssPlugin.getResourceUrl("/images/visible_dm.png")));
             }
+            Displayer.getSingletonInstance().display();
         }
 
         if (act.getSource().equals(reloadButton)) {
             layerAdded(0);
         }
 
-    }
-
-    private void showData() {
-        if (showAgain) {
-            for (String message : messages) {
-
-                Object[] options = { "Retry", "OK" };
-                Log.error(message);
-                messages.remove(message);
-                JCheckBox checkBox = new JCheckBox("Don't show this message again.");
-                checkBox.setEnabled(showAgain);
-                Object[] params = { message, checkBox };
-                int n = JOptionPane.showOptionDialog(this, params, "Pfss-Data", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
-                showAgain = !checkBox.isSelected();
-                if (n == 0) {
-                    retry = true;
-                }
-            }
-        }
     }
 
     @Override
@@ -158,33 +183,19 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
 
     @Override
     public void layerAdded(int idx) {
-        this.reload();
-    }
+        Date start = LayersModel.getSingletonInstance().getFirstDate();
+        Date end = LayersModel.getSingletonInstance().getLastDate();
 
-    public void reload() {
-        int master = -1000;
-        for (int i = 0; i < LayersModel.getSingletonInstance().getNumLayers(); i++) {
-            if (LayersModel.getSingletonInstance().isMaster(i))
-                master = i;
-        }
-        Date start;
-        Date end;
-        if (master >= 0) {
-            start = LayersModel.getSingletonInstance().getStartDate(master).getTime();
-            end = LayersModel.getSingletonInstance().getStartDate(master).getTime();
-        } else {
-            start = LayersModel.getSingletonInstance().getFirstDate();
-            end = LayersModel.getSingletonInstance().getLastDate();
-        }
+        Calendar startCal = GregorianCalendar.getInstance();
+        startCal.setTime(start);
+
+        Calendar endCal = GregorianCalendar.getInstance();
+        endCal.setTime(end);
 
         if (start != null && end != null) {
-            Calendar startCal = GregorianCalendar.getInstance();
-            startCal.setTime(start);
-
-            Calendar endCal = GregorianCalendar.getInstance();
-            endCal.setTime(end);
             int startYear = startCal.get(Calendar.YEAR);
             int startMonth = startCal.get(Calendar.MONTH);
+
             int endYear = endCal.get(Calendar.YEAR);
             int endMonth = endCal.get(Calendar.MONTH);
             boolean run = true;
@@ -203,6 +214,7 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
                     String[] date;
                     String[] time;
                     while ((inputLine = in.readLine()) != null) {
+
                         splitted = inputLine.split(" ");
                         url = splitted[1];
                         splitted = splitted[0].split("T");
@@ -219,27 +231,21 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
                     if (showAgain) {
 
                         Object[] options = { "Retry", "OK" };
-                        String message = "PFSS data for " + startYear + "-" + (startMonth + 1) + " is not available";
+                        String message = "Pfss-Data for " + startYear + "-" + (startMonth + 1) + " isn't available";
                         Log.error(message);
                         JCheckBox checkBox = new JCheckBox("Don't show this message again.");
-                        if (this.pfssCache.isVisible()) {
-                            checkBox.setEnabled(showAgain);
-                            Object[] params = { message, checkBox };
-                            int n = JOptionPane.showOptionDialog(this, params, "PFSS-Data", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
-                            showAgain = !checkBox.isSelected();
-                            if (n == 0) {
-                                retry = true;
-                            }
-                        } else {
-                            messages.add(message);
+                        checkBox.setEnabled(showAgain);
+                        Object[] params = { message, checkBox };
+                        int n = JOptionPane.showOptionDialog(this, params, "Pfss-Data", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[1]);
+                        showAgain = !checkBox.isSelected();
+                        if (n == 0) {
+                            retry = true;
                         }
                     }
                 }
 
                 if (!retry) {
                     pfssCache.preloadData(startYear, startMonth, startCal.get(Calendar.DAY_OF_MONTH) * 1000000 + startCal.get(Calendar.HOUR_OF_DAY) * 10000 + startCal.get(Calendar.MINUTE) * 100 + startCal.get(Calendar.SECOND));
-                    // pfssCache.addData(startYear, startMonth, dayAndTime,
-                    // url);
                     if (startYear == endYear && startMonth == endMonth)
                         run = false;
                     else if (startYear == endYear && startMonth < endMonth) {
@@ -261,7 +267,6 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
 
     @Override
     public void layerRemoved(View oldView, int oldIdx) {
-        this.reload();
     }
 
     @Override
@@ -276,7 +281,6 @@ public class PfssPluginPanel extends OverlayPanel implements ActionListener, Lay
             Date date = masterView.getCurrentFrameDateTime().getTime();
             Calendar cal = GregorianCalendar.getInstance();
             cal.setTime(date);
-
             pfssCache.updateData(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH) * 1000000 + cal.get(Calendar.HOUR_OF_DAY) * 10000 + cal.get(Calendar.MINUTE) * 100 + cal.get(Calendar.SECOND));
         }
     }
