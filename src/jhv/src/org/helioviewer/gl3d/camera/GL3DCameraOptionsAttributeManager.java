e7eff952abf6d1d4a18b1975b734a6c9f6e4f49b
Freek Verstringe <freek.verstringe@oma.be>
2014-10-08 08:33:09 +0000

    Cache panel, do not loose properties
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2176 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/camera/GL3DCameraOptionsAttributeManager.java
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DCameraOptionsAttributeManager.java
@@ -3,6 +3,9 @@ package org.helioviewer.gl3d.camera;
 public class GL3DCameraOptionsAttributeManager {
 
     private static GL3DCameraOptionsAttributeManager manager = new GL3DCameraOptionsAttributeManager();
+    private GL3DEarthCameraOptionPanel earthCameraOptionPanel;
+    private GL3DFollowObjectCameraOptionPanel followObjectCameraOptionPanel;
+    private GL3DObserverCameraOptionPanel observerCameraOptionPanel;
 
     private GL3DCameraOptionsAttributeManager() {
     }
@@ -13,11 +16,17 @@ public class GL3DCameraOptionsAttributeManager {
 
     public GL3DCameraOptionPanel getCameraOptionAttributePanel(GL3DCamera camera) {
         if (camera instanceof GL3DEarthCamera) {
-            return new GL3DEarthCameraOptionPanel((GL3DEarthCamera) camera);
+            if (earthCameraOptionPanel == null)
+                earthCameraOptionPanel = new GL3DEarthCameraOptionPanel((GL3DEarthCamera) camera);
+            return earthCameraOptionPanel;
         } else if (camera instanceof GL3DFollowObjectCamera) {
-            return new GL3DFollowObjectCameraOptionPanel((GL3DFollowObjectCamera) camera);
+            if (followObjectCameraOptionPanel == null)
+                followObjectCameraOptionPanel = new GL3DFollowObjectCameraOptionPanel((GL3DFollowObjectCamera) camera);
+            return followObjectCameraOptionPanel;
         } else if (camera instanceof GL3DObserverCamera) {
-            return new GL3DObserverCameraOptionPanel((GL3DObserverCamera) camera);
+            if (observerCameraOptionPanel == null)
+                observerCameraOptionPanel = new GL3DObserverCameraOptionPanel((GL3DObserverCamera) camera);
+            return observerCameraOptionPanel;
         }
         return null;
     }
