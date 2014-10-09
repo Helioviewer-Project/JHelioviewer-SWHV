eeb20a0cd48489198e81d6d61ad3fd72706ff513
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-10-09 12:13:16 +0000

    Introduced new controlsPanel for eve
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2202 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/view/ControlsPanel.java
@@ -0,0 +1,24 @@
+package org.helioviewer.plugins.eveplugin.view;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+/**
+ * 
+ * 
+ * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
+ * 
+ */
+public class ControlsPanel extends JPanel {
+
+    public ControlsPanel() {
+        initVisualComponents();
+    }
+
+    private void initVisualComponents() {
+        this.setLayout(new BorderLayout());
+
+    }
+
+}
