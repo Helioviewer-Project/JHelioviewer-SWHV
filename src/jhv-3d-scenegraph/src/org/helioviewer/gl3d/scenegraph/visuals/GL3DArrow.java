23811d7e16eed8c60e4b288bde59ccd87838b61d
Freek Verstringe <freek.verstringe@oma.be>
2014-10-21 09:33:05 +0000

    update arrow color
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2340 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d-scenegraph/src/org/helioviewer/gl3d/scenegraph/visuals/GL3DArrow.java
+++ src/jhv-3d-scenegraph/src/org/helioviewer/gl3d/scenegraph/visuals/GL3DArrow.java
@@ -1,7 +1,10 @@
 package org.helioviewer.gl3d.scenegraph.visuals;
 
+import javax.media.opengl.GL2;
+
 import org.helioviewer.gl3d.scenegraph.GL3DGroup;
 import org.helioviewer.gl3d.scenegraph.GL3DShape;
+import org.helioviewer.gl3d.scenegraph.GL3DState;
 import org.helioviewer.gl3d.scenegraph.math.GL3DVec4f;
 
 public class GL3DArrow extends GL3DGroup {
@@ -19,5 +22,13 @@ public class GL3DArrow extends GL3DGroup {
         GL3DShape cone = new GL3DCone(radius, length / 2, detail, color);
         cone.modelView().setTranslation(0, 0, offset + length / 2);
         addNode(cone);
+    }
+
+    @Override
+    public void shapeDraw(GL3DState state) {
+        GL2 gl = state.get().gl;
+        gl.glDisable(GL2.GL_LIGHTING);
+        super.shapeDraw(state);
+        gl.glEnable(GL2.GL_LIGHTING);
     }
 }
