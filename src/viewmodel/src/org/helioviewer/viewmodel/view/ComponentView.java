a0093fa1669253b9e07f79c8a94dd003e94251ff
Freek Verstringe <freek.verstringe@oma.be>
2014-11-03 22:10:44 +0000

    fix savescreenshot
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2501 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/viewmodel/src/org/helioviewer/viewmodel/view/ComponentView.java
+++ src/viewmodel/src/org/helioviewer/viewmodel/view/ComponentView.java
@@ -12,29 +12,29 @@ import org.helioviewer.viewmodel.renderer.screen.ScreenRenderer;
 /**
  * View to draw the final image to a Component, thus should be the topmost view
  * of the view chain.
- * 
+ *
  * <p>
  * A ComponentView is responsible for rendering the actual image to a Component.
  * The Component can an AWT component as well as a Swing component. If possible,
  * it is recommended to use a Swing component, because it interacts better with
  * the rest of the graphical user interface.
- * 
+ *
  * <p>
  * It is not recommended to use a ComponentView as input for another view, since
  * its purpose is to display the result of all operations, that have taken place
  * within the view chain. But still, it is possible to use it in the middle of
  * the view chain, to display intermediate results, if desired.
- * 
+ *
  * @author Stephan Pagel
  * @author Markus Langenberg
- * 
+ *
  */
 public interface ComponentView extends ModifiableInnerViewView, ViewListener {
 
     /**
      * Deactivate the Component View can be used to clean up the component view
      * when this view chain, i.e. this Component View is not in use anymore
-     * 
+     *
      * @Author Simon Spoerri
      */
     public void deactivate();
@@ -42,7 +42,7 @@ public interface ComponentView extends ModifiableInnerViewView, ViewListener {
     /**
      * Activate is called before the component view will be the active component
      * view displayed on the GUI.
-     * 
+     *
      * @Author Simon Spoerri
      */
     public void activate();
@@ -51,7 +51,7 @@ public interface ComponentView extends ModifiableInnerViewView, ViewListener {
      * Informs the view of the size of the area where the image will be
      * displayed (in pixel). At the moment this class is used to center the
      * image in the main image panel.
-     * 
+     *
      * @param size
      *            The size of the display area in pixel
      */
@@ -59,34 +59,35 @@ public interface ComponentView extends ModifiableInnerViewView, ViewListener {
 
     /**
      * Returns the component where the image will be displayed.
-     * 
+     *
      * To see the result of the view chain, this component has to be build in
      * the graphical user interface somewhere. Apart from that, it is
      * recommended to connect some listeners to the component, if input feedback
      * is required.
-     * 
+     *
      * @return Component which contains the final image
      */
     public Component getComponent();
 
     /**
      * Saves the current screen content to the given file in the given format.
-     * 
+     *
      * @param imageFormat
      *            Desired output format
      * @param outputFile
      *            Desired output destination
+     * @return
      * @throws IOException
      *             is thrown, if the given output file is not valid
      */
-    public void saveScreenshot(String imageFormat, File outputFile) throws IOException;
+    public boolean saveScreenshot(String imageFormat, File outputFile) throws IOException;
 
     /**
      * Sets the background color.
-     * 
+     *
      * This color will be displayed in areas with no image, where images are
      * transparent or when there is no image present at all.
-     * 
+     *
      * @param background
      *            new background color
      */
@@ -95,9 +96,9 @@ public interface ComponentView extends ModifiableInnerViewView, ViewListener {
     /**
      * Sets the displacement of the upper left corner of the image relative to
      * the component.
-     * 
+     *
      * For example, this function can be used to manually center the image.
-     * 
+     *
      * @param offset
      *            new offset
      */
@@ -106,9 +107,9 @@ public interface ComponentView extends ModifiableInnerViewView, ViewListener {
     /**
      * Adds a post renderer, which can draw simple geometric forms on a drawn
      * image and background.
-     * 
+     *
      * The post renderer will be called after every redraw of the actual image.
-     * 
+     *
      * @param postRenderer
      *            new post renderer
      * @see #removePostRenderer(ScreenRenderer)
@@ -118,7 +119,7 @@ public interface ComponentView extends ModifiableInnerViewView, ViewListener {
 
     /**
      * Removes a post renderer.
-     * 
+     *
      * @param postRenderer
      *            post renderer which should be removed
      * @see #addPostRenderer(ScreenRenderer)
@@ -128,10 +129,10 @@ public interface ComponentView extends ModifiableInnerViewView, ViewListener {
 
     /**
      * Returns the list of all post renderer.
-     * 
+     *
      * This function can be used to move the set of post renderers from one
      * ComponentView to another.
-     * 
+     *
      * @return list of all post renderer
      * @see #addPostRenderer(ScreenRenderer)
      * @see #removePostRenderer(ScreenRenderer)
