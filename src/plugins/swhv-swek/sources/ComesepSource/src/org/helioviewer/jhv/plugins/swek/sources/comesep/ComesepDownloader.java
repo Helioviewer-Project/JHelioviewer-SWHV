fe0a0c7704783ebf89be8894af0e6a572c7e10d2
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-11-14 14:19:53 +0000

    implemented part of the Comesep downloader
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2570 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/sources/ComesepSource/src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepDownloader.java
+++ src/plugins/swhv-swek/sources/ComesepSource/src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepDownloader.java
@@ -1,5 +1,25 @@
 package org.helioviewer.jhv.plugins.swek.sources.comesep;
 
-public class ComesepDownloader {
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
+
+import org.helioviewer.jhv.plugins.swek.config.SWEKEventType;
+import org.helioviewer.jhv.plugins.swek.download.SWEKParam;
+import org.helioviewer.jhv.plugins.swek.sources.SWEKDownloader;
+
+public class ComesepDownloader implements SWEKDownloader {
+
+    @Override
+    public void stopDownload() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public InputStream downloadData(SWEKEventType eventType, Date startDate, Date endDate, List<SWEKParam> params, int page) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
