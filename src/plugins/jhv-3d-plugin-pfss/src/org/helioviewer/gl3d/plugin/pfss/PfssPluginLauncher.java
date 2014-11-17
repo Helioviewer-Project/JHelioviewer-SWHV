bb79c0ee9227395f30bb123359c7617a2ab64aa6
Freek Verstringe <freek.verstringe@oma.be>
2014-11-17 07:45:06 +0000

    fix pfss launcher
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2578 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-3d-plugin-pfss/src/org/helioviewer/gl3d/plugin/pfss/PfssPluginLauncher.java
+++ src/plugins/jhv-3d-plugin-pfss/src/org/helioviewer/gl3d/plugin/pfss/PfssPluginLauncher.java
@@ -23,9 +23,16 @@ public class PfssPluginLauncher {
         System.out.println("JHelioviewer developer version with external plugin compiled-in.");
         System.out.println("================================================================\n\n");
 
-        String[] args2 = JavaCompatibility.copyArrayString(args, args.length + 2);
+        String[] args2 = JavaCompatibility.copyArrayString(args, args.length + 8);
+
         args2[args2.length - 2] = "--deactivate-plugin";
-        args2[args2.length - 1] = "PfssPlugin.jar";
+        args2[args2.length - 1] = "SWEKPlugin.jar";
+        args2[args2.length - 4] = "--deactivate-plugin";
+        args2[args2.length - 3] = "EVEPlugin.jar";
+        args2[args2.length - 5] = "--deactivate-plugin";
+        args2[args2.length - 6] = "PFSSPlugin.jar";
+        args2[args2.length - 7] = "--deactivate-plugin";
+        args2[args2.length - 8] = "PfssPlugin.jar";
         JavaHelioViewerLauncher.loadLibs();
         JavaHelioViewer.main(args2, new PfssPlugin(true));
     }
