ac2bdd1ebe5478f7ed928d387a3c4b0c62889e4f
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-12-12 14:10:34 +0000

    Implement the comesep event stream
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2830 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/sources/ComesepSource/src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepEventStream.java
+++ src/plugins/swhv-swek/sources/ComesepSource/src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepEventStream.java
@@ -1,5 +1,37 @@
 package org.helioviewer.jhv.plugins.swek.sources.comesep;
 
-public class ComesepEventStream {
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.helioviewer.jhv.data.datatype.event.JHVEvent;
+import org.helioviewer.jhv.plugins.swek.sources.SWEKEventStream;
+
+public class ComesepEventStream implements SWEKEventStream {
+
+    /** Queue with events */
+    Queue<JHVEvent> eventQueue = new LinkedList<JHVEvent>();
+
+    public ComesepEventStream() {
+        eventQueue = new LinkedList<JHVEvent>();
+    }
+
+    @Override
+    public boolean hasEvents() {
+        return !eventQueue.isEmpty();
+    }
+
+    @Override
+    public JHVEvent next() {
+        return eventQueue.poll();
+    }
+
+    @Override
+    public boolean additionalDownloadNeeded() {
+        return false;
+    }
+
+    public void addJHVEvent(ComesepEvent currentEvent) {
+        eventQueue.add(currentEvent);
+    }
 
 }
