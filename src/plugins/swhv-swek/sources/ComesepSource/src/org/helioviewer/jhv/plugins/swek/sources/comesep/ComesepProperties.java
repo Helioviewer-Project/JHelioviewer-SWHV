eda1ead04344ab2f353e7d531b489c49b9c9230b
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-12-12 14:10:31 +0000

    implemented the comesep properties
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2816 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/sources/ComesepSource/src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepProperties.java
+++ src/plugins/swhv-swek/sources/ComesepSource/src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepProperties.java
@@ -1,5 +1,59 @@
 package org.helioviewer.jhv.plugins.swek.sources.comesep;
 
-public class ComesepProperties {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+/**
+ * Gives access to the COMESEP source properties
+ * 
+ * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
+ * 
+ */
+public class ComesepProperties {
+    private static ComesepProperties singletonInstance;
+
+    /** The COMESEP source properties */
+    private final Properties comesepProperties;
+
+    /**
+     * Private default constructor.
+     */
+    private ComesepProperties() {
+        comesepProperties = new Properties();
+        loadProperties();
+    }
+
+    /**
+     * Gets the singleton instance of the COMESEP source properties
+     * 
+     * @return the COMESEP source properties
+     */
+    public static ComesepProperties getSingletonInstance() {
+        if (singletonInstance == null) {
+            singletonInstance = new ComesepProperties();
+        }
+        return singletonInstance;
+    }
+
+    /**
+     * Gets the COMESEP source properties.
+     * 
+     * @return the comesep source properties
+     */
+    public Properties getComesepProperties() {
+        return comesepProperties;
+    }
+
+    /**
+     * Loads the overall comesep source settings.
+     */
+    private void loadProperties() {
+        InputStream defaultPropStream = ComesepProperties.class.getResourceAsStream("/comesepsource.properties");
+        try {
+            comesepProperties.load(defaultPropStream);
+        } catch (IOException ex) {
+            System.out.println("Could not load the hek settings." + ex);
+        }
+    }
 }
