a7aadad8605fbf2c73bc149e8225202141848e51
Bram Bourgoignie <bram.bourgoignie@oma.be>
2014-12-12 14:10:32 +0000

    created and implemented the Comesep event type
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2824 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/swhv-swek/sources/ComesepSource/src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepEventType.java
@@ -0,0 +1,70 @@
+package org.helioviewer.jhv.plugins.swek.sources.comesep;
+
+import org.helioviewer.jhv.data.datatype.event.JHVEventType;
+
+/**
+ * Defines an event type coming from the comesep source.
+ * 
+ * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
+ * 
+ */
+public class ComesepEventType implements JHVEventType {
+
+    /** The event type */
+    private final String eventType;
+
+    /** The event source */
+    private final String eventSource;
+
+    /** The event provider */
+    private final String eventProvider;
+
+    /**
+     * Default constructor.
+     * 
+     */
+    public ComesepEventType() {
+        eventType = "";
+        eventProvider = "";
+        eventSource = "";
+    }
+
+    /**
+     * Creates a comesep event type with a given event type, event source, event
+     * provider.
+     * 
+     * @param eventType
+     *            the event type
+     * @param eventSource
+     *            the event source
+     * @param eventProvider
+     *            the event provider
+     */
+    public ComesepEventType(String eventType, String eventSource, String eventProvider) {
+        this.eventType = eventType;
+        this.eventSource = eventSource;
+        this.eventProvider = eventProvider;
+    }
+
+    @Override
+    public String getEventType() {
+        return eventType;
+    }
+
+    @Override
+    public String getEventSource() {
+        return eventSource;
+    }
+
+    @Override
+    public String getEventProvider() {
+        return eventProvider;
+    }
+
+    @Override
+    public boolean equals(JHVEventType eventType) {
+        return eventType.getEventType().equals(this.eventType) && eventType.getEventProvider().equals(eventProvider)
+                && eventType.getEventSource().equals(eventSource);
+    }
+
+}
