54004afe3f8c7c8c35cd575308519495307e7404
Freek Verstringe <freek.verstringe@oma.be>
2014-12-16 15:35:05 +0000

    Remove log
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2853 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/gui/GL3DResetCameraAction.java
+++ src/jhv/src/org/helioviewer/gl3d/gui/GL3DResetCameraAction.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
-import org.helioviewer.base.logging.Log;
 import org.helioviewer.gl3d.camera.GL3DCamera;
 import org.helioviewer.jhv.display.Displayer;
 import org.helioviewer.jhv.gui.IconBank;
@@ -13,9 +12,9 @@ import org.helioviewer.jhv.gui.IconBank.JHVIcon;
 /**
  * Action that resets the view transformation of the current {@link GL3DCamera}
  * to its default settings.
- * 
+ *
  * @author Simon Spoerri (simon.spoerri@fhnw.ch)
- * 
+ *
  */
 public class GL3DResetCameraAction extends AbstractAction {
 
@@ -32,7 +31,6 @@ public class GL3DResetCameraAction extends AbstractAction {
     @Override
     public void actionPerformed(ActionEvent e) {
         GL3DCameraSelectorModel.getInstance().getCurrentCamera().reset();
-        Log.debug("Reset Camera");
         Displayer.getSingletonInstance().display();
     }
 }
