d4f3d559c5472c421c315c2b35e0c3729a3b1460
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-01-21 10:54:05 +0000

    Call abstract constructor
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@2942 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/sources/HEKSource/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKEvent.java
+++ src/plugins/swhv-swek/sources/HEKSource/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKEvent.java
@@ -82,6 +82,7 @@ public class HEKEvent extends AbstractJHVEvent {
      * Default constructor
      */
     public HEKEvent() {
+        super();
         initLists();
         eventName = "";
         eventDisplayName = "";
