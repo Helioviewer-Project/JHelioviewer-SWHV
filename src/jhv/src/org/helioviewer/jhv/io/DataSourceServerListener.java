18b16cc6de0894789521addcf561bfdaebac4909
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-02-03 10:43:36 +0000

    Add Datasourceserver and the listener
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3003 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv/src/org/helioviewer/jhv/io/DataSourceServerListener.java
@@ -0,0 +1,5 @@
+package org.helioviewer.jhv.io;
+
+public interface DataSourceServerListener {
+    public abstract void serverChanged();
+}
---
+++ src/jhv/src/org/helioviewer/jhv/io/DataSourceServers.java
@@ -0,0 +1,79 @@
+package org.helioviewer.jhv.io;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.helioviewer.jhv.Settings;
+import org.helioviewer.jhv.gui.ImageViewerGui;
+
+public class DataSourceServers {
+
+    private static DataSourceServers instance;
+
+    private final List<DataSourceServerListener> listeners;
+
+    private String selectedServer;
+
+    private DataSourceServers() {
+        listeners = new ArrayList<DataSourceServerListener>();
+        selectedServer = "";
+    }
+
+    public static DataSourceServers getSingletonInstance() {
+        if (instance == null) {
+            instance = new DataSourceServers();
+        }
+        return instance;
+    }
+
+    public void addListener(DataSourceServerListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(DataSourceServerListener listener) {
+        listeners.remove(listener);
+    }
+
+    public void changeServer(String server, final boolean donotloadStartup) {
+        selectedServer = server;
+        if (server.contains("ROB")) {
+            Settings.getSingletonInstance().setProperty("API.dataSources.path", "http://swhv.oma.be/hv/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
+            Settings.getSingletonInstance().setProperty("API.jp2images.path", "http://swhv.oma.be/hv/api/index.php");
+            Settings.getSingletonInstance().setProperty("API.jp2series.path", "http://swhv.oma.be/hv/api/index.php");
+            Settings.getSingletonInstance().setProperty("default.remote.path", "jpip://swhv.oma.be:8090");
+            Settings.getSingletonInstance().setProperty("API.event.path", "http://swhv.oma.be/hv/api/");
+            Settings.getSingletonInstance().setProperty("default.httpRemote.path", "http://swhv.oma.be/hv/jp2/");
+        } else if (server.contains("Helioviewer")) {
+            Settings.getSingletonInstance().setProperty("API.dataSources.path", "http://helioviewer.org/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
+            Settings.getSingletonInstance().setProperty("API.jp2images.path", "http://helioviewer.org/api/index.php");
+            Settings.getSingletonInstance().setProperty("API.jp2series.path", "http://helioviewer.org/api/index.php");
+            Settings.getSingletonInstance().setProperty("default.remote.path", "jpip://helioviewer.org:8090");
+            Settings.getSingletonInstance().setProperty("API.event.path", "http://helioviewer.org/api/");
+            Settings.getSingletonInstance().setProperty("default.httpRemote.path", "http://helioviewer.org/jp2/");
+        } else if (server.contains("IAS")) {
+            Settings.getSingletonInstance().setProperty("API.dataSources.path", "http://helioviewer.ias.u-psud.fr/helioviewer/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
+            Settings.getSingletonInstance().setProperty("API.jp2images.path", "http://helioviewer.ias.u-psud.fr/helioviewer/api/index.php");
+            Settings.getSingletonInstance().setProperty("API.jp2series.path", "http://helioviewer.ias.u-psud.fr/helioviewer/api/index.php");
+            Settings.getSingletonInstance().setProperty("default.remote.path", "jpip://helioviewer.ias.u-psud.fr:8080");
+            Settings.getSingletonInstance().setProperty("API.event.path", "http://helioviewer.ias.u-psud.fr/helioviewer/api/");
+            Settings.getSingletonInstance().setProperty("default.httpRemote.path", "http://helioviewer.ias.u-psud.fr/helioviewer/jp2/");
+        }
+        Thread t = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                if (!donotloadStartup) {
+                    ImageViewerGui.getSingletonInstance().getObservationDialog().setLoadButtonEnabled(false);
+                }
+                DataSources.getSingletonInstance().reload();
+                for (DataSourceServerListener l : listeners) {
+                    l.serverChanged();
+                }
+                ImageViewerGui.getSingletonInstance().getObservationDialog().setLoadButtonEnabled(true);
+            }
+
+        }, "change server");
+        t.start();
+    }
+
+}
