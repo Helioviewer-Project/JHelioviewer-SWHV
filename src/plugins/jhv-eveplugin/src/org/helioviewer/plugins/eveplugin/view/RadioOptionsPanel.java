113dc0175b5ee8c11c2dbe29e4a7c2fb272834bb
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-02-10 14:52:41 +0000

    Set default lut + functionality to load lut
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3085 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/view/RadioOptionsPanel.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/view/RadioOptionsPanel.java
@@ -5,12 +5,19 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Map;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import org.helioviewer.base.logging.Log;
+import org.helioviewer.base.message.Message;
+import org.helioviewer.jhv.internal_plugins.filter.SOHOLUTFilterPlugin.GGRFilter;
+import org.helioviewer.jhv.internal_plugins.filter.SOHOLUTFilterPlugin.GradientError;
 import org.helioviewer.jhv.internal_plugins.filter.SOHOLUTFilterPlugin.LUT;
 import org.helioviewer.plugins.eveplugin.radio.filter.FilterModel;
 
@@ -18,9 +25,13 @@ public class RadioOptionsPanel extends JPanel implements ActionListener {
 
     private JComboBox lut;
     private JLabel color;
+    private int lastSelectedIndex;
+
+    private final Map<String, LUT> lutMap;
 
     public RadioOptionsPanel() {
         super();
+        lutMap = LUT.getStandardList();
         initVisualComponents();
     }
 
@@ -28,7 +39,9 @@ public class RadioOptionsPanel extends JPanel implements ActionListener {
 
         setLayout(new BorderLayout());
 
-        lut = new JComboBox(LUT.getStandardList().keySet().toArray());
+        lut = new JComboBox(lutMap.keySet().toArray());
+        lut.setSelectedItem("Rainbow 2");
+        lastSelectedIndex = lut.getSelectedIndex();
         lut.addActionListener(this);
         color = new JLabel("Color:");
 
@@ -51,7 +64,46 @@ public class RadioOptionsPanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        FilterModel.getInstance().setLUT(LUT.getStandardList().get((lut.getSelectedItem())));
+        LUT newMap = lutMap.get(lut.getSelectedItem());
+        if (newMap == null) {
+            // Add new color table
+            JFileChooser fc = new JFileChooser();
+            fc.setFileFilter(new GGRFilter());
+            fc.setMultiSelectionEnabled(false);
+            int state = fc.showOpenDialog(null);
+            if (state == JFileChooser.APPROVE_OPTION) {
+                try {
+                    Log.info("Load gradient file " + fc.getSelectedFile());
+                    addLut(LUT.readGimpGradientFile(fc.getSelectedFile()));
+                    lastSelectedIndex = lut.getSelectedIndex();
+                } catch (IOException ex) {
+                    Message.warn("Error loading gradient file", "Error loading gradient file: " + fc.getSelectedFile() + "\n\n" + ex.getMessage());
+                    Log.warn("Error loading gradient file: " + fc.getSelectedFile() + " - " + ex.getMessage());
+                } catch (GradientError ex) {
+                    Message.warn("Error applying gradient file", "Error loading gradient file: " + fc.getSelectedFile() + "\n\n" + ex.getMessage());
+                    Log.warn("Error applying gradient file: " + fc.getSelectedFile() + " - " + ex.getMessage());
+                }
+            } else {
+                lut.setSelectedIndex(lastSelectedIndex);
+            }
+        } else {
+            FilterModel.getInstance().setLUT(LUT.getStandardList().get((lut.getSelectedItem())));
+            lastSelectedIndex = lut.getSelectedIndex();
+        }
+    }
+
+    /**
+     * Adds a color table to the available list and set it active
+     * 
+     * @param lut
+     *            Color table to add
+     */
+    public void addLut(LUT newLut) {
+        if (lutMap.put(newLut.getName(), newLut) == null) {
+            lut.addItem(newLut.getName());
+        }
+        lut.setSelectedItem(newLut.getName());
+        FilterModel.getInstance().setLUT(newLut);
     }
 
 }
