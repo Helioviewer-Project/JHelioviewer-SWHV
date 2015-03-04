836a0410a751d373de16189b90075a1f2f08acc9
Freek Verstringe <freek.verstringe@oma.be>
2015-03-04 08:59:09 +0000

    delete
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3287 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-3d-wcs/src/org/helioviewer/gl3d/wcs/CoordinateSystemChangeListener.java
+++
@@ -1,6 +0,0 @@
-package org.helioviewer.gl3d.wcs;
-
-public interface CoordinateSystemChangeListener {
-
-    public void coordinateSystemChanged(CoordinateSystem coordinateSystem);
-}
--- src/jhv-3d-wcs/src/org/helioviewer/gl3d/wcs/MatrixCoordinateConversion.java
+++
@@ -1,7 +0,0 @@
-package org.helioviewer.gl3d.wcs;
-
-import org.helioviewer.gl3d.scenegraph.math.GL3DMat4d;
-
-public interface MatrixCoordinateConversion extends CoordinateConversion {
-    public GL3DMat4d getConversionMatrix();
-}
