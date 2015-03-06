a60c954fc1a80d976121b873c6cbcb8151aa7278
Freek Verstringe <freek.verstringe@oma.be>
2015-03-06 13:41:23 +0000

    cleanup
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3375 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/shader/ShaderFactory.java
+++ src/viewmodel/src/org/helioviewer/viewmodel/view/opengl/shader/ShaderFactory.java
@@ -1,12 +1,10 @@
 package org.helioviewer.viewmodel.view.opengl.shader;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 import javax.media.opengl.GL2;
 
 import org.helioviewer.base.FileUtils;
-import org.helioviewer.base.logging.Log;
 import org.helioviewer.jhv.gui.states.StateController;
 import org.helioviewer.jhv.gui.states.ViewStateEnum;
 
@@ -95,17 +93,17 @@ public class ShaderFactory {
     }
 
     public static void initShader(GL2 gl) {
-        //if (!init) {
-        //init = true;
-        fragment3dCGID = genShaderID(gl);
-        compileShader(gl, "/data/fragment3d.cg", fragment3dCGID, GL2.GL_FRAGMENT_PROGRAM_ARB);
-        vertex3dCGID = genShaderID(gl);
-        compileShader(gl, "/data/vertex3d.cg", vertex3dCGID, GL2.GL_VERTEX_PROGRAM_ARB);
-        fragment2dCGID = genShaderID(gl);
-        compileShader(gl, "/data/fragment2d.cg", fragment2dCGID, GL2.GL_FRAGMENT_PROGRAM_ARB);
-        vertex2dCGID = genShaderID(gl);
-        compileShader(gl, "/data/vertex2d.cg", vertex2dCGID, GL2.GL_VERTEX_PROGRAM_ARB);
-        //}
+        if (!init) {
+            init = true;
+            fragment3dCGID = genShaderID(gl);
+            compileShader(gl, "/data/fragment3d.cg", fragment3dCGID, GL2.GL_FRAGMENT_PROGRAM_ARB);
+            vertex3dCGID = genShaderID(gl);
+            compileShader(gl, "/data/vertex3d.cg", vertex3dCGID, GL2.GL_VERTEX_PROGRAM_ARB);
+            fragment2dCGID = genShaderID(gl);
+            compileShader(gl, "/data/fragment2d.cg", fragment2dCGID, GL2.GL_FRAGMENT_PROGRAM_ARB);
+            vertex2dCGID = genShaderID(gl);
+            compileShader(gl, "/data/vertex2d.cg", vertex2dCGID, GL2.GL_VERTEX_PROGRAM_ARB);
+        }
     }
 
     public static int genShaderID(GL2 gl) {
@@ -118,11 +116,7 @@ public class ShaderFactory {
     public static void compileShader(GL2 gl, String inputFile, int target, int type) {
         InputStream fragmentStream = FileUtils.getResourceInputStream(inputFile);
         String fragmentText = FileUtils.convertStreamToString(fragmentStream);
-        try {
-            fragmentStream.close();
-        } catch (IOException e) {
-            Log.debug("Stream refuses to close" + e);
-        }
+
         GLShaderHelper shaderHelper = new GLShaderHelper();
         shaderHelper.compileProgram(gl, type, fragmentText, target);
     }
