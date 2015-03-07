a261a2cae1f11941c20f9578d0e475f8bfeeb609
Freek Verstringe <freek.verstringe@oma.be>
2015-03-07 20:44:31 +0000

    add generic pair to return two arguments
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3420 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/base/src/org/helioviewer/base/math/Pair.java
@@ -0,0 +1,11 @@
+package org.helioviewer.base.math;
+
+public class Pair<A, B> {
+    public final A a;
+    public final B b;
+
+    public Pair(A a, B b) {
+        this.a = a;
+        this.b = b;
+    }
+};
