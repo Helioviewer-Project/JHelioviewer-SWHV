c8e6e4f6b5244453254ae4b693fd2d193b36edea
Freek Verstringe <freek.verstringe@oma.be>
2015-03-10 12:10:54 +0000

    always use 3D factory
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3464 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/gui/states/ViewStateEnum.java
+++ src/jhv/src/org/helioviewer/jhv/gui/states/ViewStateEnum.java
@@ -1,18 +1,16 @@
 package org.helioviewer.jhv.gui.states;
 
 import org.helioviewer.jhv.gui.GL3DViewchainFactory;
-import org.helioviewer.jhv.gui.ViewchainFactory;
 
 /**
  * Contains the existing Gui States (2D, 3D)
- * 
+ *
  * @author Robin Oster (robin.oster@students.fhnw.ch)
- * 
+ *
  */
 public enum ViewStateEnum {
 
-    View2D(new GuiState(new ViewchainFactory())),
-    View3D(new GuiState(new GL3DViewchainFactory()));
+    View2D(new GuiState(new GL3DViewchainFactory())), View3D(new GuiState(new GL3DViewchainFactory()));
 
     private final State state;
 
