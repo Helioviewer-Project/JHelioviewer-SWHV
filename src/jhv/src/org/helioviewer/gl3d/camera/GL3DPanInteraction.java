589fac9e8fc3e58a353441b725e9ba0b1e534062
Bogdan Nicula <bogdan@nicula.net>
2015-03-17 00:22:52 +0000

    Fix pan
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3594 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/gl3d/camera/GL3DPanInteraction.java
+++ src/jhv/src/org/helioviewer/gl3d/camera/GL3DPanInteraction.java
@@ -6,9 +6,15 @@ import java.awt.event.MouseEvent;
 import org.helioviewer.base.math.Vector2dDouble;
 import org.helioviewer.base.physics.Constants;
 import org.helioviewer.jhv.display.Displayer;
+import org.helioviewer.jhv.layers.LayersModel;
+import org.helioviewer.viewmodel.region.Region;
 import org.helioviewer.viewmodel.view.RegionView;
+import org.helioviewer.viewmodel.view.View;
 import org.helioviewer.viewmodel.view.ViewHelper;
+import org.helioviewer.viewmodel.view.ViewportView;
 import org.helioviewer.viewmodel.view.opengl.GL3DSceneGraphView;
+import org.helioviewer.viewmodel.viewport.Viewport;
+import org.helioviewer.viewmodel.viewportimagesize.ViewportImageSize;
 
 /**
  * Standard panning interaction, moves the camera proportionally to the mouse
@@ -34,8 +40,20 @@ public class GL3DPanInteraction extends GL3DDefaultInteraction {
     public void mouseDragged(MouseEvent e, GL3DCamera camera) {
         int x = e.getPoint().x - this.lastMousePoint.x;
         int y = e.getPoint().y - this.lastMousePoint.y;
-        if (sceneGraphView.getAdapter(RegionView.class).getRegion() != null) {
-            Vector2dDouble imageDisplacement = ViewHelper.convertScreenToImageDisplacement(x, y, sceneGraphView.getAdapter(RegionView.class).getRegion(), ViewHelper.calculateViewportImageSize(sceneGraphView));
+
+        View view = LayersModel.getSingletonInstance().getActiveView();
+
+        Region region;
+        RegionView regionView = view.getAdapter(RegionView.class);
+
+        Viewport viewport;
+        ViewportView viewportView = view.getAdapter(ViewportView.class);
+
+        if (regionView != null && (region = regionView.getRegion()) != null &&
+            viewportView != null && (viewport = viewportView.getViewport()) != null) {
+
+            ViewportImageSize vis = ViewHelper.calculateViewportImageSize(viewport, region);
+            Vector2dDouble imageDisplacement = ViewHelper.convertScreenToImageDisplacement(x, y, region, vis);
             camera.translation.x += imageDisplacement.getX();
             camera.translation.y += imageDisplacement.getY();
         } else {
