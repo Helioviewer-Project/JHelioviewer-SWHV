7132c1b39ae8b3d1d0647319a1e537b89f06812b
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-03-18 17:51:42 +0000

    Add active events. Don't remove events
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3623 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv-data/src/org/helioviewer/jhv/data/container/cache/JHVEventCache.java
+++ src/jhv-data/src/org/helioviewer/jhv/data/container/cache/JHVEventCache.java
@@ -38,6 +38,8 @@ public class JHVEventCache {
 
     private final List<JHVEvent> eventsWithRelationRules;
 
+    private final Set<JHVEventType> activeEventTypes;
+
     /**
      * private default constructor
      */
@@ -48,6 +50,7 @@ public class JHVEventCache {
         missingEventsInEventRelations = new HashMap<String, List<JHVEvent>>();
         idsPerColor = new HashMap<Color, Set<String>>();
         eventsWithRelationRules = new ArrayList<JHVEvent>();
+        activeEventTypes = new HashSet<JHVEventType>();
     }
 
     /**
@@ -69,6 +72,7 @@ public class JHVEventCache {
      *            the event to add
      */
     public void add(JHVEvent event) {
+        activeEventTypes.add(event.getJHVEventType());
         if (!eventIDs.contains(event.getUniqueID())) {
             allEvents.put(event.getUniqueID(), event);
             Date startDate = DateUtil.getCurrentDate(event.getStartDate());
@@ -88,19 +92,6 @@ public class JHVEventCache {
      * @param date
      *            The date in which the event should have happened
      * @return the list of events happened on the given date
-     */
-    /*
-     * public List<JHVEvent> get(Date date) { synchronized
-     * (JHVEventContainerLocks.cacheLock) { List<JHVEvent> eventsResult = new
-     * ArrayList<JHVEvent>(); Calendar reference = Calendar.getInstance();
-     * reference.setTime(date); for (Date sDate : events.keySet()) { Calendar
-     * tempS = Calendar.getInstance(); tempS.setTime(sDate); if
-     * (tempS.compareTo(reference) <= 0) { Map<Date, List<JHVEvent>>
-     * eventOnEndTime = events.get(sDate); for (Date eDate :
-     * eventOnEndTime.keySet()) { Calendar tempE = Calendar.getInstance();
-     * tempE.setTime(eDate); if (tempE.compareTo(reference) >= 0) {
-     * eventsResult.addAll(eventOnEndTime.get(eDate)); } } } } return
-     * eventsResult; } }
      */
     public Map<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>> get(Date date) {
         Map<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>> eventsResult = new HashMap<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>>();
@@ -154,26 +145,6 @@ public class JHVEventCache {
      *            end date of the interval
      * @return the list of events that are available in the interval
      */
-    /*
-     * public List<JHVEvent> get(Date startDate, Date endDate) { synchronized
-     * (JHVEventContainerLocks.cacheLock) { List<JHVEvent> eventsResult = new
-     * ArrayList<JHVEvent>(); Calendar intervalS = Calendar.getInstance();
-     * intervalS.setTime(startDate); Calendar intervalE =
-     * Calendar.getInstance(); intervalE.setTime(endDate); for (Date sDate :
-     * events.keySet()) { Calendar tempS = Calendar.getInstance();
-     * tempS.setTime(sDate); // event starts after interval start but before
-     * interval end if (intervalS.compareTo(tempS) <= 0 &&
-     * intervalE.compareTo(tempS) >= 0) { for (List<JHVEvent> tempEvent :
-     * events.get(sDate).values()) { eventsResult.addAll(tempEvent); } } //
-     * event start before interval start and end after interval // start
-     * Map<Date, List<JHVEvent>> endDatesEvents = events.get(sDate); for (Date
-     * eDate : endDatesEvents.keySet()) { Calendar tempE =
-     * Calendar.getInstance(); tempE.setTime(eDate); if
-     * (intervalS.compareTo(tempS) >= 0 && intervalS.compareTo(tempE) <= 0) {
-     * eventsResult.addAll(endDatesEvents.get(eDate)); } }
-     * 
-     * } return eventsResult; } }
-     */
     public Map<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>> get(Date startDate, Date endDate) {
         Map<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>> eventsResult = new HashMap<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>>();
         Calendar intervalS = Calendar.getInstance();
@@ -211,26 +182,22 @@ public class JHVEventCache {
      *            the event type to remove
      */
     public void removeEventType(JHVEventType eventType) {
-        for (Map<Date, List<JHVEvent>> endDateEvents : events.values()) {
-            for (List<JHVEvent> eventList : endDateEvents.values()) {
-                List<JHVEvent> deleteList = new ArrayList<JHVEvent>();
-                for (JHVEvent event : eventList) {
-                    if (event.getJHVEventType().equals(eventType)) {
-                        deleteList.add(event);
-                        eventIDs.remove(event.getUniqueID());
-                        allEvents.remove(event.getUniqueID());
-                        missingEventsInEventRelations.remove(event.getUniqueID());
-                        eventsWithRelationRules.remove(event);
-                        for (JHVEventRelation relation : event.getEventRelationShip().getRelatedEventsByRule().values()) {
-                            if (relation.getTheEvent() != null) {
-                                relation.getTheEvent().getEventRelationShip().getRelatedEventsByRule().remove(event.getUniqueID());
-                            }
-                        }
-                    }
-                }
-                eventList.removeAll(deleteList);
-            }
-        }
+        activeEventTypes.remove(eventType);
+        /*
+         * for (Map<Date, List<JHVEvent>> endDateEvents : events.values()) { for
+         * (List<JHVEvent> eventList : endDateEvents.values()) { List<JHVEvent>
+         * deleteList = new ArrayList<JHVEvent>(); for (JHVEvent event :
+         * eventList) { if (event.getJHVEventType().equals(eventType)) {
+         * deleteList.add(event); eventIDs.remove(event.getUniqueID());
+         * allEvents.remove(event.getUniqueID());
+         * missingEventsInEventRelations.remove(event.getUniqueID());
+         * eventsWithRelationRules.remove(event); for (JHVEventRelation relation
+         * : event.getEventRelationShip().getRelatedEventsByRule().values()) {
+         * if (relation.getTheEvent() != null) {
+         * relation.getTheEvent().getEventRelationShip
+         * ().getRelatedEventsByRule().remove(event.getUniqueID()); } } } }
+         * eventList.removeAll(deleteList); } }
+         */
     }
 
     private void checkAndFixRelationShip(JHVEvent event) {
@@ -496,22 +463,24 @@ public class JHVEventCache {
      */
     private void addEventsToResult(Map<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>> eventsResult, List<JHVEvent> tempEvents) {
         for (JHVEvent event : tempEvents) {
-            NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>> datesPerType = new TreeMap<Date, NavigableMap<Date, List<JHVEvent>>>();
-            NavigableMap<Date, List<JHVEvent>> endDatesPerStartDate = new TreeMap<Date, List<JHVEvent>>();
-            List<JHVEvent> events = new ArrayList<JHVEvent>();
-            if (eventsResult.containsKey(event.getJHVEventType().getEventType())) {
-                datesPerType = eventsResult.get(event.getJHVEventType().getEventType());
-                if (datesPerType.containsKey(event.getStartDate())) {
-                    endDatesPerStartDate = datesPerType.get(event.getStartDate());
-                    if (endDatesPerStartDate.containsKey(event.getEndDate())) {
-                        events = endDatesPerStartDate.get(event.getEndDate());
+            if (activeEventTypes.contains(event.getJHVEventType())) {
+                NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>> datesPerType = new TreeMap<Date, NavigableMap<Date, List<JHVEvent>>>();
+                NavigableMap<Date, List<JHVEvent>> endDatesPerStartDate = new TreeMap<Date, List<JHVEvent>>();
+                List<JHVEvent> events = new ArrayList<JHVEvent>();
+                if (eventsResult.containsKey(event.getJHVEventType().getEventType())) {
+                    datesPerType = eventsResult.get(event.getJHVEventType().getEventType());
+                    if (datesPerType.containsKey(event.getStartDate())) {
+                        endDatesPerStartDate = datesPerType.get(event.getStartDate());
+                        if (endDatesPerStartDate.containsKey(event.getEndDate())) {
+                            events = endDatesPerStartDate.get(event.getEndDate());
+                        }
                     }
                 }
+                events.add(event);
+                endDatesPerStartDate.put(event.getEndDate(), events);
+                datesPerType.put(event.getStartDate(), endDatesPerStartDate);
+                eventsResult.put(event.getJHVEventType().getEventType(), datesPerType);
             }
-            events.add(event);
-            endDatesPerStartDate.put(event.getEndDate(), events);
-            datesPerType.put(event.getStartDate(), endDatesPerStartDate);
-            eventsResult.put(event.getJHVEventType().getEventType(), datesPerType);
         }
     }
 
