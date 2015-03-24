04fe23c796161a2e1f7c9fc7fd0df4f7980831e7
Freek Verstringe <freek.verstringe@oma.be>
2015-03-24 09:59:19 +0000

    remove unnecessary swing worker
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3766 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/radio/data/DownloadedJPXData.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/radio/data/DownloadedJPXData.java
@@ -3,11 +3,9 @@ package org.helioviewer.plugins.eveplugin.radio.data;
 import java.awt.Rectangle;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.concurrent.ExecutionException;
 
 import javax.swing.SwingWorker;
 
-import org.helioviewer.base.logging.Log;
 import org.helioviewer.viewmodel.changeevent.ChangeEvent;
 import org.helioviewer.viewmodel.imagedata.ARGBInt32ImageData;
 import org.helioviewer.viewmodel.imagedata.ImageData;
@@ -27,7 +25,7 @@ public class DownloadedJPXData implements ViewListener {
     private final RadioDataManager radioDataManager;
     private final Long downloadID;
     private SwingWorker<DownloadedJPXDataWorkerResult, Void> worker;
-    private int workernumber;
+    private final int workernumber;
 
     public DownloadedJPXData(ImageInfoView view, Long imageID, Date startDate, Date endDate, Long downloadID) {
         super();
@@ -75,55 +73,11 @@ public class DownloadedJPXData implements ViewListener {
 
     @Override
     public void viewChanged(final View sender, final ChangeEvent aEvent) {
-        while (worker != null && !worker.isDone()) {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        DownloadedJPXDataWorkerResult result = getJPXData(sender);
+        if (result != null) {
+            radioDataManager.dataForIDReceived(result.getByteData(), result.getImageID(), result.getDownloadID(), result.getDataSize());
+            radioDataManager.finishedDownloadingID(imageID, downloadID);
         }
-
-        worker = new SwingWorker<DownloadedJPXDataWorkerResult, Void>() {
-
-            private int nr;
-
-            @Override
-            protected DownloadedJPXDataWorkerResult doInBackground() {
-                Thread.currentThread().setName("DownloadedJPXDataWorkerResult1--EVE");
-                // Log.debug("Event " + aEvent);
-                return getJPXData(sender);
-            }
-
-            public SwingWorker<DownloadedJPXDataWorkerResult, Void> init(int workernumber) {
-                nr = workernumber;
-                return this;
-            }
-
-            @Override
-            protected void done() {
-                try {
-                    if (!isCancelled()) {
-                        DownloadedJPXDataWorkerResult result = get();
-                        if (result != null) {
-                            // Log.debug(result.getData().length);
-                            radioDataManager.dataForIDReceived(result.getByteData(), result.getImageID(), result.getDownloadID(), result.getDataSize());
-                        }
-
-                    }
-                    radioDataManager.finishedDownloadingID(imageID, downloadID);
-                } catch (InterruptedException e) {
-                    Log.error("dWorker" + nr + " interrupted " + e.getMessage());
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    Log.error("dWorker " + nr + "execution error " + e.getMessage());
-                    e.printStackTrace();
-                }
-
-            }
-
-        }.init(workernumber++);
-
-        worker.execute();
     }
 
     private DownloadedJPXDataWorkerResult getJPXData(View view) {
