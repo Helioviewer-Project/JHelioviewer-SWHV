b16e8a94dace99d0668df808cc8b82043347f6bb
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-03-25 07:55:02 +0000

    remove synchronised from BandColors
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3786 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/lines/data/BandColors.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/lines/data/BandColors.java
@@ -3,16 +3,11 @@ package org.helioviewer.plugins.eveplugin.lines.data;
 import java.awt.Color;
 
 public class BandColors {
-    private final static Color[] bandColorArray = { new Color(144, 238, 144), new Color(255, 140, 000), new Color(255, 000, 255),
-            new Color(99, 184, 255), new Color(143, 188, 143), new Color(219, 112, 147), new Color(255, 222, 173),
-            new Color(000, 255, 255), new Color(255, 000, 000), new Color(255, 105, 180), new Color(160, 032, 240),
-            new Color(000, 255, 000), new Color(205, 92, 92), new Color(139, 000, 139), new Color(238, 201, 000), new Color(95, 158, 160),
-            new Color(189, 183, 107), new Color(107, 142, 035), new Color(127, 255, 212), new Color(100, 149, 237),
-            new Color(190, 190, 190), new Color(106, 90, 205), };
+    private final static Color[] bandColorArray = { new Color(144, 238, 144), new Color(255, 140, 000), new Color(255, 000, 255), new Color(99, 184, 255), new Color(143, 188, 143), new Color(219, 112, 147), new Color(255, 222, 173), new Color(000, 255, 255), new Color(255, 000, 000), new Color(255, 105, 180), new Color(160, 032, 240), new Color(000, 255, 000), new Color(205, 92, 92), new Color(139, 000, 139), new Color(238, 201, 000), new Color(95, 158, 160), new Color(189, 183, 107), new Color(107, 142, 035), new Color(127, 255, 212), new Color(100, 149, 237), new Color(190, 190, 190), new Color(106, 90, 205), };
     private final static int[] usedArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     private static int minValue = 0;
 
-    public synchronized static Color getNextColor() {
+    public static Color getNextColor() {
         for (int i = 0; i < bandColorArray.length; i++) {
             if (usedArray[i] == minValue) {
                 usedArray[i]++;
@@ -23,7 +18,7 @@ public class BandColors {
         return BandColors.getNextColor();
     }
 
-    public synchronized static void resetColor(Color c) {
+    public static void resetColor(Color c) {
         for (int i = 0; i < bandColorArray.length; i++) {
             if (bandColorArray[i].equals(c)) {
                 usedArray[i]--;
@@ -32,7 +27,7 @@ public class BandColors {
         }
     }
 
-    public synchronized static void setColorUsed(Color c) {
+    public static void setColorUsed(Color c) {
         for (int i = 0; i < bandColorArray.length; i++) {
             if (bandColorArray[i].equals(c)) {
                 usedArray[i]++;
