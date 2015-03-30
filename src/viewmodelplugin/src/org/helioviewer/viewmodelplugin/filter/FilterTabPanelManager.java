1b389d8324f6bd87b65366dd1e14647020b315cf
Freek Verstringe <freek.verstringe@oma.be>
2015-03-30 12:31:57 +0000

    add actions to image options panel
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3899 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/viewmodelplugin/src/org/helioviewer/viewmodelplugin/filter/FilterTabPanelManager.java
+++ src/viewmodelplugin/src/org/helioviewer/viewmodelplugin/filter/FilterTabPanelManager.java
@@ -1,14 +1,21 @@
 package org.helioviewer.viewmodelplugin.filter;
 
 import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import org.helioviewer.jhv.gui.IconBank;
+import org.helioviewer.jhv.gui.IconBank.JHVIcon;
 import org.helioviewer.jhv.gui.filters.AbstractFilterPanel;
 import org.helioviewer.jhv.layers.LayersListener;
 import org.helioviewer.jhv.layers.LayersModel;
@@ -114,68 +121,48 @@ public class FilterTabPanelManager implements LayersListener {
 
         GridBagConstraints c = new GridBagConstraints();
         c.insets = new Insets(2, 5, 2, 5);
+        c.weightx = 1;
+        c.gridx = 0;
+        c.gridwidth = 3;
+        c.anchor = GridBagConstraints.WEST;
+        c.fill = GridBagConstraints.HORIZONTAL;
 
         for (Component comp : centerList) {
             if (comp instanceof FilterAlignmentDetails) {
                 FilterAlignmentDetails details = (FilterAlignmentDetails) comp;
 
-                /*
-                 * This is the nasty part of it all The Layout had to be
-                 * handcrafted and hardcoded :(
-                 */
                 if (details.getDetails() == FilterAlignmentDetails.POSITION_OPACITY) {
-                    c.gridwidth = 3;
-                    c.weightx = 1;
-                    c.gridx = 0;
                     c.gridy = 0;
-                    c.anchor = GridBagConstraints.WEST;
-                    c.fill = GridBagConstraints.HORIZONTAL;
                     compactPanel.add(comp, c);
-                    c.gridy = 0;
                 } else if (details.getDetails() == FilterAlignmentDetails.POSITION_SHARPEN) {
-                    c.gridwidth = 3;
-                    c.weightx = 1;
-                    c.gridx = 0;
                     c.gridy = 1;
-                    c.anchor = GridBagConstraints.WEST;
-                    c.fill = GridBagConstraints.HORIZONTAL;
                     compactPanel.add(comp, c);
                 } else if (details.getDetails() == FilterAlignmentDetails.POSITION_GAMMA) {
-                    c.gridwidth = 3;
-                    c.weightx = 1;
-                    c.gridx = 0;
                     c.gridy = 2;
-                    c.anchor = GridBagConstraints.WEST;
-                    c.fill = GridBagConstraints.HORIZONTAL;
                     compactPanel.add(comp, c);
                 } else if (details.getDetails() == FilterAlignmentDetails.POSITION_CONTRAST) {
-                    c.gridwidth = 3;
-                    c.weightx = 1;
-                    c.gridx = 0;
                     c.gridy = 3;
-                    c.anchor = GridBagConstraints.WEST;
-                    c.fill = GridBagConstraints.HORIZONTAL;
                     compactPanel.add(comp, c);
                 } else if (details.getDetails() == FilterAlignmentDetails.POSITION_COLORTABLES) {
-                    c.gridwidth = 3;
-                    c.weightx = 1;
-                    c.gridx = 0;
                     c.gridy = 4;
-                    c.anchor = GridBagConstraints.WEST;
-                    c.fill = GridBagConstraints.HORIZONTAL;
                     compactPanel.add(comp, c);
                 } else if (details.getDetails() == FilterAlignmentDetails.POSITION_CHANNELMIXER) {
-                    c.gridwidth = 3;
-                    c.weightx = 1;
-                    c.gridx = 0;
                     c.gridy = 5;
-                    c.anchor = GridBagConstraints.WEST;
-                    c.fill = GridBagConstraints.HORIZONTAL;
                     compactPanel.add(comp, c);
                 }
             }
+            c.insets = new Insets(0, 0, 0, 0);
+            c.gridy = 6;
+
+            JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            buttonPanel.setOpaque(false);
+            buttonPanel.add(downloadLayerButton);
+            buttonPanel.add(showMetaButton);
+
+            compactPanel.add(buttonPanel, c);
         }
         LayersModel.getSingletonInstance().addLayersListener(this);
+
         return compactPanel;
     }
 
@@ -190,15 +177,47 @@ public class FilterTabPanelManager implements LayersListener {
     @Override
     public void activeLayerChanged(View view) {
         if (view instanceof JHVJP2View) {
-            setActivejp2((JHVJP2View) view);
+            //setActivejp2((JHVJP2View) view);
         }
     }
 
-    public void setActivejp2(JHVJP2View jp2view) {
+    private Action downloadLayerAction;
+    private Action showMetaAction;
+    private final JButton downloadLayerButton = new JButton();
+    private final JButton showMetaButton = new JButton();
+
+    public void setActivejp2(final JHVJP2View jp2view) {
         for (AbstractFilterPanel c : this.abstractFilterPanels) {
             c.setEnabled(true);
             c.setJP2View(jp2view);
         }
+        downloadLayerAction = new AbstractAction() {
+            private static final long serialVersionUID = 1L;
+            {
+                putValue(SHORT_DESCRIPTION, "Download the selected layer");
+                putValue(SMALL_ICON, IconBank.getIcon(JHVIcon.DOWNLOAD));
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                LayersModel.getSingletonInstance().downloadLayer(jp2view);
+            }
+        };
+        downloadLayerButton.setAction(downloadLayerAction);
+        showMetaAction = new AbstractAction() {
+            private static final long serialVersionUID = 1L;
+            {
+                putValue(SHORT_DESCRIPTION, "Show metadata of the selected layer");
+                putValue(SMALL_ICON, IconBank.getIcon(JHVIcon.INFO));
+            }
+
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                LayersModel.getSingletonInstance().showMetaInfo(jp2view);
+            }
+        };
+        showMetaButton.setAction(showMetaAction);
+        showMetaButton.revalidate();
     }
 
 }
