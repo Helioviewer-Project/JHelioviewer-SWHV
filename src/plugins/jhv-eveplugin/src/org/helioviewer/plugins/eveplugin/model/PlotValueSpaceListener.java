ab6e3205acbc821557f54d8b98fc4946366f417f
Freek Verstringe <freek.verstringe@oma.be>
2015-04-03 06:37:39 +0000

    Remove lost eve code
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3976 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/model/PlotValueSpaceListener.java
+++
@@ -1,9 +0,0 @@
-package org.helioviewer.plugins.eveplugin.model;
-
-public interface PlotValueSpaceListener {
-
-    public abstract void selectedIntervalChanged(double selectedMinValue, double selectedMaxValue);
-
-    public abstract void availableIntervalChanged(double selectedMinValue, double selectedMaxValue);
-
-}
