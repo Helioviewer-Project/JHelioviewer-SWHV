e03103c8b41615116ab466639c44f284d7a399e6
Freek Verstringe <freek.verstringe@oma.be>
2015-04-03 06:37:41 +0000

    change name
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@3986 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhvhekplugin-3d/src/org/helioviewer/jhv/plugins/swhvhekplugin/SWHVHEKSettings.java
+++ src/plugins/swhvhekplugin-3d/src/org/helioviewer/jhv/plugins/swhvhekplugin/SWHVHEKSettings.java
@@ -7,7 +7,7 @@ public class SWHVHEKSettings {
     /**
      * Needed for the JHV plugin initialization
      */
-    public final static String PLUGIN_LOCATION = "HEKPlugin";
+    public final static String PLUGIN_LOCATION = "SWHVHEKPlugin";
     public final static Color[] cactusColorArray = { new Color(144, 238, 144), new Color(255, 140, 000), new Color(255, 255, 000), new Color(255, 000, 255), new Color(255, 255, 255), new Color(99, 184, 255), new Color(143, 188, 143), new Color(220, 220, 220), new Color(219, 112, 147), new Color(255, 222, 173), new Color(000, 255, 255), new Color(255, 000, 000), new Color(255, 250, 205), new Color(255, 105, 180), new Color(160, 032, 240), new Color(255, 225, 255), new Color(000, 139, 139), new Color(000, 255, 000), new Color(205, 92, 92), new Color(238, 224, 229), new Color(139, 000, 139), new Color(238, 201, 000), new Color(211, 211, 211), new Color(153, 153, 153), new Color(95, 158, 160), new Color(189, 183, 107), new Color(107, 142, 035), new Color(127, 255, 212), new Color(100, 149, 237), new Color(190, 190, 190), new Color(106, 90, 205), };
     private static int colorindex = 0;
 
