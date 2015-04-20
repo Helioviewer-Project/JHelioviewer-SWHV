0665cc9ecf78d3516a43cc563a35391fbbb33f4a
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-04-20 15:17:59 +0000

    Load eve again in launcher
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4216 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/SWHVLauncher/src/org/helioviewer/jhv/launcher/SWHVLauncher.java
+++ src/SWHVLauncher/src/org/helioviewer/jhv/launcher/SWHVLauncher.java
@@ -7,6 +7,7 @@ import org.helioviewer.jhv.JavaHelioViewer;
 import org.helioviewer.jhv.JavaHelioViewerLauncher;
 import org.helioviewer.jhv.plugins.swek.SWEKPlugin;
 import org.helioviewer.jhv.plugins.swhvhekplugin.SWHVHEKPlugin;
+import org.helioviewer.plugins.eveplugin.EVEPlugin;
 import org.helioviewer.viewmodelplugin.controller.PluginManager;
 
 /**
@@ -36,6 +37,7 @@ public class SWHVLauncher {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
+                PluginManager.getSingletonInstance().addPlugin(EVEPlugin.class.getClassLoader(), new EVEPlugin(), null);
                 PluginManager.getSingletonInstance().addPlugin(SWHVHEKPlugin.class.getClassLoader(), new SWHVHEKPlugin(), null);
             }
         });
