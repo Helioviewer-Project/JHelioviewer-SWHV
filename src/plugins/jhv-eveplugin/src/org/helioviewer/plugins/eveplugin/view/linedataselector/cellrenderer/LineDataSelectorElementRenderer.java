4d0c072b00c720eb0dc1688ae764c477d90c3637
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-04-22 07:08:41 +0000

    Some extra tablecellrenderers
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4238 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/view/linedataselector/cellrenderer/LineDataSelectorElementRenderer.java
@@ -0,0 +1,24 @@
+package org.helioviewer.plugins.eveplugin.view.linedataselector.cellrenderer;
+
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import org.helioviewer.plugins.eveplugin.view.linedataselector.LineDataSelectorElement;
+
+public class LineDataSelectorElementRenderer extends DefaultTableCellRenderer {
+
+    private static final long serialVersionUID = -8446757044773627214L;
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        String layerName = ((LineDataSelectorElement) value).getName();
+        label.setText(layerName);
+        label.setToolTipText("Line or spectrogram name: " + layerName);
+        return label;
+    }
+
+}
---
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/view/linedataselector/cellrenderer/LoadingCellRenderer.java
@@ -0,0 +1,24 @@
+package org.helioviewer.plugins.eveplugin.view.linedataselector.cellrenderer;
+
+import java.awt.Component;
+
+import javax.swing.JProgressBar;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+import org.helioviewer.plugins.eveplugin.view.linedataselector.LineDataSelectorElement;
+
+public class LoadingCellRenderer extends DefaultTableCellRenderer {
+
+    private static final long serialVersionUID = 2173458369966852891L;
+
+    private final JProgressBar downloadProgressBar = new JProgressBar();
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        LineDataSelectorElement element = (LineDataSelectorElement) value;
+        downloadProgressBar.setIndeterminate(true);
+        downloadProgressBar.setVisible(element.isDownloading());
+        return downloadProgressBar;
+    }
+}
