128e57cafc02c841917cd804d4accda0f10a1cb4
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-04-22 07:09:00 +0000

    Radio options panel as option panel for radio data
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4261 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/radio/data/DownloadRequestData.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/radio/data/DownloadRequestData.java
@@ -5,6 +5,7 @@ import java.awt.Component;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.helioviewer.plugins.eveplugin.view.RadioOptionsPanel;
 import org.helioviewer.plugins.eveplugin.view.linedataselector.LineDataSelectorElement;
 
 public class DownloadRequestData implements LineDataSelectorElement {
@@ -120,7 +121,6 @@ public class DownloadRequestData implements LineDataSelectorElement {
 
     @Override
     public Component getOptionsPanel() {
-        // TODO Auto-generated method stub
-        return null;
+        return new RadioOptionsPanel();
     }
 }
