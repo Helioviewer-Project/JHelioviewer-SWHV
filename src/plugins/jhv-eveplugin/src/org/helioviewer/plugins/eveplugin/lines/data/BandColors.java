6c893f0cb9b944b8abaf5223fca6f39dcab036a2
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-04-23 08:45:28 +0000

    Put black as first color
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4328 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/lines/data/BandColors.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/lines/data/BandColors.java
@@ -3,8 +3,8 @@ package org.helioviewer.plugins.eveplugin.lines.data;
 import java.awt.Color;
 
 public class BandColors {
-    private final static Color[] bandColorArray = { new Color(144, 238, 144), new Color(255, 140, 000), new Color(255, 000, 255), new Color(99, 184, 255), new Color(143, 188, 143), new Color(219, 112, 147), new Color(255, 222, 173), new Color(000, 255, 255), new Color(255, 000, 000), new Color(255, 105, 180), new Color(160, 032, 240), new Color(000, 255, 000), new Color(205, 92, 92), new Color(139, 000, 139), new Color(238, 201, 000), new Color(95, 158, 160), new Color(189, 183, 107), new Color(107, 142, 035), new Color(127, 255, 212), new Color(100, 149, 237), new Color(190, 190, 190), new Color(106, 90, 205), };
-    private final static int[] usedArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    private final static Color[] bandColorArray = { new Color(0, 0, 0), new Color(144, 238, 144), new Color(255, 140, 000), new Color(255, 000, 255), new Color(99, 184, 255), new Color(143, 188, 143), new Color(219, 112, 147), new Color(255, 222, 173), new Color(000, 255, 255), new Color(255, 000, 000), new Color(255, 105, 180), new Color(160, 032, 240), new Color(000, 255, 000), new Color(205, 92, 92), new Color(139, 000, 139), new Color(238, 201, 000), new Color(95, 158, 160), new Color(189, 183, 107), new Color(107, 142, 035), new Color(127, 255, 212), new Color(100, 149, 237), new Color(190, 190, 190), new Color(106, 90, 205), };
+    private final static int[] usedArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     private static int minValue = 0;
 
     public static Color getNextColor() {
