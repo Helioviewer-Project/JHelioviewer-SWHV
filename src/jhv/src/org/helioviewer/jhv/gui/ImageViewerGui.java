06b67868527d3d57a10136ac096f8803e476f2df
Freek Verstringe <freek.verstringe@oma.be>
2015-04-28 19:15:54 +0000

    revert unwanted border
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4423 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/gui/ImageViewerGui.java
+++ src/jhv/src/org/helioviewer/jhv/gui/ImageViewerGui.java
@@ -1,7 +1,6 @@
 package org.helioviewer.jhv.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GraphicsEnvironment;
@@ -16,7 +15,6 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.AbstractList;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
@@ -118,7 +116,6 @@ public class ImageViewerGui {
         imageObservationPanel = new ImageDataPanel();
         observationDialog.addUserInterface("Image data", imageObservationPanel);
         leftPane.add("Image Layers", Displayer.getRenderableContainerPanel(), true);
-        leftPane.setBorder(BorderFactory.createLineBorder(Color.GREEN));
         leftScrollPane = new JScrollPane(leftPane, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         leftScrollPane.setFocusable(false);
         leftScrollPane.getVerticalScrollBar().setUnitIncrement(10);
