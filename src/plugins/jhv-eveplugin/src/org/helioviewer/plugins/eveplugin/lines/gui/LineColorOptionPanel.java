e03c1884931497b097bebc782ef09584c86ac1b3
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-05-08 13:09:16 +0000

    Change layout
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4599 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/lines/gui/LineColorOptionPanel.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/lines/gui/LineColorOptionPanel.java
@@ -1,7 +1,9 @@
 package org.helioviewer.plugins.eveplugin.lines.gui;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -22,9 +24,18 @@ public class LineColorOptionPanel extends JPanel {
     }
 
     private void initVisualComponents() {
-        setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
-
+        // setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(0, 0, 0, 0);
+        c.anchor = GridBagConstraints.WEST;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.fill = GridBagConstraints.NONE;
         JButton pickColor = new JButton("Line color");
+        pickColor.setMargin(new Insets(0, 0, 0, 0));
         pickColor.setToolTipText("Change the color of the current line");
         pickColor.addActionListener(new ActionListener() {
 
@@ -36,7 +47,7 @@ public class LineColorOptionPanel extends JPanel {
                 }
             }
         });
-        add(pickColor);
+        add(pickColor, c);
     }
 
 }
