71d16be6cdba19ad5629a5e3ff0aecad70217b7d
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-05-08 13:09:16 +0000

    remove color label
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4598 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/radio/gui/RadioOptionsPanel.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/plugins/eveplugin/radio/gui/RadioOptionsPanel.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.util.Map;
 
 import javax.swing.JComboBox;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import org.helioviewer.jhv.gui.filters.lut.LUT;
@@ -15,7 +14,6 @@ import org.helioviewer.plugins.eveplugin.radio.model.ColorLookupModel;
 public class RadioOptionsPanel extends JPanel implements ActionListener {
 
     private JComboBox lut;
-    private JLabel color;
 
     private final Map<String, LUT> lutMap;
 
@@ -28,12 +26,10 @@ public class RadioOptionsPanel extends JPanel implements ActionListener {
     private void initVisualComponents() {
         setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 
-        color = new JLabel("Color", JLabel.RIGHT);
         lut = new JComboBox(lutMap.keySet().toArray());
         lut.setSelectedItem("Rainbow 2");
         lut.addActionListener(this);
 
-        add(color);
         add(lut);
     }
 
