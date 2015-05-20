e84e9613c6929a04b15508e76e5072e10261c338
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-05-20 13:24:01 +0000

    Add log information
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4759 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/radio/data/DownloadedJPXData.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/radio/data/DownloadedJPXData.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.swing.SwingWorker;
 
+import org.helioviewer.base.logging.Log;
 import org.helioviewer.viewmodel.imagedata.ImageData;
 import org.helioviewer.viewmodel.imagedata.SingleChannelByte8ImageData;
 import org.helioviewer.viewmodel.imagetransport.Byte8ImageTransport;
@@ -148,6 +149,8 @@ public class DownloadedJPXData implements JHVJP2CallistoViewDataHandler {
         if (result != null) {
             radioDataManager.dataForIDReceived(result.getByteData(), result.getImageID(), result.getDownloadID(), result.getDataSize());
             radioDataManager.finishedDownloadingID(imageID, downloadID);
+        } else {
+            Log.debug("Result is null");
         }
     }
 
