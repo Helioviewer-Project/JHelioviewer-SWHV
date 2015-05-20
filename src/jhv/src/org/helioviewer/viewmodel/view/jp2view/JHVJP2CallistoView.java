6bb0a63729d84802348494e85218f1077843d1a7
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-05-20 13:24:01 +0000

    Fix radio images not drawn in some cases
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4761 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/viewmodel/view/jp2view/JHVJP2CallistoView.java
+++ src/jhv/src/org/helioviewer/viewmodel/view/jp2view/JHVJP2CallistoView.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import org.helioviewer.base.Region;
 import org.helioviewer.base.Viewport;
 import org.helioviewer.base.datetime.ImmutableDateTime;
+import org.helioviewer.base.logging.Log;
 import org.helioviewer.viewmodel.view.jp2view.J2KRender.RenderReasons;
 import org.helioviewer.viewmodel.view.jp2view.image.JP2ImageParameter;
 import org.helioviewer.viewmodel.view.jp2view.image.ResolutionSet.ResolutionLevel;
@@ -13,45 +14,25 @@ import org.helioviewer.viewmodel.view.jp2view.image.SubImage;
 
 public class JHVJP2CallistoView extends JHVJP2View {
 
-    private boolean viewportSet;
-    private boolean regionSet;
     private JHVJP2CallistoViewDataHandler dataHandler;
 
     public JHVJP2CallistoView() {
         region = new Region(0, 0, 86400, 380);
         viewport = new Viewport(2700, 12);
-        viewportSet = false;
-        regionSet = false;
     }
 
     @Override
     public boolean setViewport(Viewport v) {
-        boolean viewportChanged = (viewport == null ? v == null : !viewport.equals(v));
-        viewportSet = true;
         viewport = v;
-
-        if (regionSet) {
-            if (setImageViewParams(calculateParameter())) {
-                return true;
-            } else if (viewportChanged && imageViewParams.resolution.getZoomLevel() == jp2Image.getResolutionSet().getMaxResolutionLevels()) {
-                renderRequestedSignal.signal(RenderReasons.OTHER);
-                return true;
-            }
-        }
-
-        return viewportChanged;
+        renderRequestedSignal.signal(RenderReasons.OTHER);
+        return true;
     }
 
     @Override
     public boolean setRegion(Region r) {
-        boolean changed = region == null ? r == null : !region.equals(r);
         region = r;
-        regionSet = true;
-        if (viewportSet) {
-            changed |= setImageViewParams(calculateParameter());
-        }
-
-        return changed;
+        setImageViewParams(calculateParameter());
+        return true;
     }
 
     @Override
@@ -71,15 +52,9 @@ public class JHVJP2CallistoView extends JHVJP2View {
     protected JP2ImageParameter calculateParameter(int numQualityLayers, int frameNumber) {
         int maxHeight = jp2Image.getResolutionSet().getResolutionLevel(0).getResolutionBounds().height;
         int maxWidth = jp2Image.getResolutionSet().getResolutionLevel(0).getResolutionBounds().width;
-        ResolutionLevel res = jp2Image.getResolutionSet().getClosestResolutionLevel(new Dimension(
-                        (int) Math.ceil(viewport.getWidth() / region.getWidth() * maxWidth),
-                        2 * (int) Math.ceil(viewport.getHeight() / region.getHeight() * maxHeight)));
+        ResolutionLevel res = jp2Image.getResolutionSet().getClosestResolutionLevel(new Dimension((int) Math.ceil(viewport.getWidth() / region.getWidth() * maxWidth), 2 * (int) Math.ceil(viewport.getHeight() / region.getHeight() * maxHeight)));
 
-        SubImage subImage = new SubImage(
-                        (int) (region.getLowerLeftCorner().x / (double) maxWidth * res.getResolutionBounds().width),
-                        (int) (region.getLowerLeftCorner().y / (double) maxHeight * res.getResolutionBounds().height),
-                        (int) (region.getWidth() / (double) maxWidth * res.getResolutionBounds().width),
-                        (int) (region.getHeight() / (double) maxHeight * res.getResolutionBounds().height));
+        SubImage subImage = new SubImage((int) (region.getLowerLeftCorner().x / maxWidth * res.getResolutionBounds().width), (int) (region.getLowerLeftCorner().y / maxHeight * res.getResolutionBounds().height), (int) (region.getWidth() / maxWidth * res.getResolutionBounds().width), (int) (region.getHeight() / maxHeight * res.getResolutionBounds().height));
         return new JP2ImageParameter(subImage, res, numQualityLayers, 0);
     }
 
@@ -88,8 +63,12 @@ public class JHVJP2CallistoView extends JHVJP2View {
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
+                Log.debug("Do run");
                 if (dataHandler != null) {
+                    Log.debug("Handle data");
                     dataHandler.handleData(JHVJP2CallistoView.this);
+                } else {
+                    Log.debug("Data handle is null");
                 }
             }
         });
