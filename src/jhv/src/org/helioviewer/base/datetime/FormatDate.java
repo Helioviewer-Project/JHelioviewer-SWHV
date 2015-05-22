b2ee2e90fd484e79b259871d6ddc00d76a9015a1
Bogdan Nicula <bogdan@nicula.net>
2015-05-22 09:37:08 +0000

    Split out FormatDate
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@4789 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv/src/org/helioviewer/base/datetime/FormatDate.java
@@ -0,0 +1,20 @@
+package org.helioviewer.base.datetime;
+
+import java.util.TimeZone;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+
+public class FormatDate {
+
+    private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
+
+    private static final String SQL_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static final FastDateFormat utcDateFormat = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss", UTC);
+    public static final FastDateFormat sqlDateFormat = FastDateFormat.getInstance(SQL_DATE_FORMAT, UTC);
+    public static final FastDateFormat utcFullDateFormat = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss.SSS", UTC);
+    public static final FastDateFormat apiDateFormat = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss'Z'");
+    public static final FastDateFormat filenameDateFormat = FastDateFormat.getInstance("yyyy-MM-dd_HH.mm.ss");
+    public static final FastDateFormat timeDateFormat = FastDateFormat.getInstance("HH:mm:ss");
+
+}
