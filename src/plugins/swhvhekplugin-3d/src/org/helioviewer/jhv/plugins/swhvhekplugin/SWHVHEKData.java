ac1560c1c1a87be951a9aa80fc3ae7c99134932d
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-06-18 05:53:46 +0000

    Use JHVContainer in better way: should eliminate events not loading
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5046 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhvhekplugin-3d/src/org/helioviewer/jhv/plugins/swhvhekplugin/SWHVHEKData.java
+++ src/plugins/swhvhekplugin-3d/src/org/helioviewer/jhv/plugins/swhvhekplugin/SWHVHEKData.java
@@ -75,27 +75,32 @@ public class SWHVHEKData implements LayersListener, JHVEventHandler {
 
     @Override
     public void newEventsReceived(Map<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>> eventList) {
-        cacheUpdated();
+        displayEvents();
     }
 
     @Override
     public void cacheUpdated() {
         if (beginDate != null && endDate != null) {
-            JHVEventCacheResult result = JHVEventCache.getSingletonInstance().get(beginDate, endDate, beginDate, endDate);
-            data = result.getAvailableEvents();
-            ArrayList<JHVEvent> events = new ArrayList<JHVEvent>();
-            for (String eventType : data.keySet()) {
-                for (Date sDate : data.get(eventType).keySet()) {
-                    for (Date eDate : data.get(eventType).get(sDate).keySet()) {
-                        for (JHVEvent event : data.get(eventType).get(sDate).get(eDate)) {
-                            events.add(event);
-                        }
+            requestEvents();
+            displayEvents();
+        }
+    }
+
+    private void displayEvents() {
+        JHVEventCacheResult result = JHVEventCache.getSingletonInstance().get(beginDate, endDate, beginDate, endDate);
+        data = result.getAvailableEvents();
+        ArrayList<JHVEvent> events = new ArrayList<JHVEvent>();
+        for (String eventType : data.keySet()) {
+            for (Date sDate : data.get(eventType).keySet()) {
+                for (Date eDate : data.get(eventType).get(sDate).keySet()) {
+                    for (JHVEvent event : data.get(eventType).get(sDate).get(eDate)) {
+                        events.add(event);
                     }
                 }
             }
-            this.events = events;
-            Displayer.display();
         }
+        this.events = events;
+        Displayer.display();
     }
 
     public Map<String, NavigableMap<Date, NavigableMap<Date, List<JHVEvent>>>> getData() {
