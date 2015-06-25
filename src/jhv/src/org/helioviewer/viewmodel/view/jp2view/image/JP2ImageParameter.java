2dfaa775255f21cbff7ad637b9f8013ffa5094fa
Bogdan Nicula <bogdan@nicula.net>
2015-06-25 13:55:09 +0000

    Cleanups (from Freek)
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5132 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/viewmodel/view/jp2view/image/JP2ImageParameter.java
+++ src/jhv/src/org/helioviewer/viewmodel/view/jp2view/image/JP2ImageParameter.java
@@ -19,7 +19,7 @@ public class JP2ImageParameter {
     public final ResolutionLevel resolution;
 
     /** Zero based frame number */
-    public int compositionLayer;
+    public final int compositionLayer;
 
     /** This constructor assigns all variables... throw NPE if any args are null */
     public JP2ImageParameter(SubImage _roi, ResolutionLevel _resolution, int _compositionLayer) {
