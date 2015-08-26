9cce89ace6f54792fec1f4fa3e06ed375cdb13c1
Bogdan Nicula <bogdan@nicula.net>
2015-08-26 20:41:53 +0000

    Server selection: Helioviewer.org -> GSFC
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5181 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/io/DataSources.java
+++ src/jhv/src/org/helioviewer/jhv/io/DataSources.java
@@ -359,7 +359,7 @@ public class DataSources {
 
 
     private String selectedServer = "";
-    private final String[] serverList = new String[] { "ROB", "Helioviewer.org", "IAS" };
+    private final String[] serverList = new String[] { "ROB", "GSFC", "IAS" };
 
     public void changeServer(String server, final boolean donotloadStartup) {
         selectedServer = server;
@@ -370,7 +370,7 @@ public class DataSources {
             Settings.getSingletonInstance().setProperty("default.remote.path", "jpip://swhv.oma.be:8090");
             Settings.getSingletonInstance().setProperty("API.event.path", "http://swhv.oma.be/hv/api/");
             Settings.getSingletonInstance().setProperty("default.httpRemote.path", "http://swhv.oma.be/hv/jp2/");
-        } else if (server.contains("Helioviewer")) {
+        } else if (server.contains("GSFC")) {
             Settings.getSingletonInstance().setProperty("API.dataSources.path", "http://helioviewer.org/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
             Settings.getSingletonInstance().setProperty("API.jp2images.path", "http://helioviewer.org/api/index.php");
             Settings.getSingletonInstance().setProperty("API.jp2series.path", "http://helioviewer.org/api/index.php");
