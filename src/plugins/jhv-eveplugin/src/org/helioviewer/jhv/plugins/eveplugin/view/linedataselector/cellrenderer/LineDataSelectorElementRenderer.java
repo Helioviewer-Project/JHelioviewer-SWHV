99edf1541de2cd87fa1349ee3db3aa008a95d05d
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-08-26 10:08:01 +0000

    Simplify tooltip
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5168 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/view/linedataselector/cellrenderer/LineDataSelectorElementRenderer.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/view/linedataselector/cellrenderer/LineDataSelectorElementRenderer.java
@@ -16,11 +16,11 @@ public class LineDataSelectorElementRenderer extends DefaultTableCellRenderer {
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         if (value != null) { // In some case this can be called with value null
-                             // (getAccessibleChild(int i) of JTable )
+            // (getAccessibleChild(int i) of JTable )
             JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
             String layerName = ((LineDataSelectorElement) value).getName();
             label.setText(layerName);
-            label.setToolTipText("Line or spectrogram name: " + layerName);
+            label.setToolTipText(layerName);
             label.setBorder(LineDateSelectorTablePanel.commonBorder);
             return label;
         } else {
