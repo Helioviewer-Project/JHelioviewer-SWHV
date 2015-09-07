e354795800c48b5c5e85c37934dadc4bf87ba0b6
Bogdan Nicula <bogdan@nicula.net>
2015-09-07 13:53:18 +0000

    Add ArrayUtils
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5232 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv/src/org/helioviewer/base/ArrayUtils.java
@@ -0,0 +1,23 @@
+package org.helioviewer.base;
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+
+    public static <T> T[] concatAll(T[] first, T[]... rest) {
+        int totalLength = first.length;
+        for (T[] array : rest) {
+            totalLength += array.length;
+        }
+
+        T[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (T[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+
+        return result;
+    }
+
+}
