6ba67d1522de582f6c207940f3688d8542069287
Freek Verstringe <freek.verstringe@oma.be>
2015-09-08 11:34:45 +0000

    draw fake targets
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5258 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/renderable/components/RenderableMiniview.java
+++ src/jhv/src/org/helioviewer/jhv/renderable/components/RenderableMiniview.java
@@ -11,7 +11,8 @@ import org.helioviewer.jhv.renderable.viewport.GL3DViewport;
 import com.jogamp.opengl.GL2;
 
 public class RenderableMiniview implements Renderable {
-    private final GL3DViewport miniviewViewport = new GL3DViewport(10, 10, 100, 100, new GL3DObserverCamera());
+    private final GL3DViewport miniviewViewport = new GL3DViewport(10, 10, 250, 250, new GL3DObserverCamera());
+
     private final RenderableType type;
     private boolean isVisible = true;
 
@@ -21,8 +22,28 @@ public class RenderableMiniview implements Renderable {
 
     }
 
+    private void drawCircle(GL2 gl, double x, double y, double r, int segments) {
+        gl.glDisable(GL2.GL_TEXTURE_2D);
+        {
+            gl.glBegin(GL2.GL_TRIANGLE_FAN);
+            gl.glVertex2d(x, y);
+            for (int n = 0; n <= segments; ++n) {
+                double t = 2 * Math.PI * n / segments;
+                gl.glVertex2d(x + Math.sin(t) * r, y + Math.cos(t) * r);
+            }
+            gl.glEnd();
+        }
+        gl.glEnable(GL2.GL_TEXTURE_2D);
+    }
+
     @Override
     public void render(GL2 gl, GL3DViewport vp) {
+        if (vp == this.miniviewViewport) {
+            gl.glColor4d(1, 0, 0, 1);
+            drawCircle(gl, 0, 0, 1, 100);
+            gl.glColor4d(0, 0, 0, 1);
+            drawCircle(gl, 0, 0, 10, 100);
+        }
     }
 
     @Override
