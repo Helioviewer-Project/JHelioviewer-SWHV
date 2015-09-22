9553a564cb0580632c82555fb147af0df2fb2482
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-09-22 20:52:05 +0000

    Add missing AbstractValueSpace
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5363 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/draw/AbstractValueSpace.java
@@ -0,0 +1,25 @@
+package org.helioviewer.jhv.plugins.eveplugin.draw;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.helioviewer.jhv.plugins.eveplugin.base.Range;
+
+public abstract class AbstractValueSpace implements ValueSpace {
+
+    protected final Set<ValueSpaceListener> listeners;
+
+    public AbstractValueSpace() {
+        listeners = new HashSet<ValueSpaceListener>();
+    }
+
+    @Override
+    public void resetScaledSelectedRange() {
+        Range availableRange = getScaledAvailableRange();
+        setScaledSelectedRange(new Range(availableRange));
+    }
+
+    public void addValueSpaceListener(ValueSpaceListener valueSpaceListener) {
+        listeners.add(valueSpaceListener);
+    }
+}
