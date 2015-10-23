0b9a4f377e1b10b209c4b2e4588b3a1efa9dadf5
Bogdan Nicula <bogdan@nicula.net>
2015-10-23 21:08:20 +0000

    Extract GLGrab
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5886 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/export/ExportMovie.java
+++ src/jhv/src/org/helioviewer/jhv/export/ExportMovie.java
@@ -1,9 +1,7 @@
 package org.helioviewer.jhv.export;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
 import java.io.File;
-import java.nio.ByteBuffer;
 import java.util.Date;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutionException;
@@ -17,29 +15,21 @@ import org.helioviewer.jhv.JHVDirectory;
 import org.helioviewer.jhv.base.time.TimeUtils;
 import org.helioviewer.jhv.display.Displayer;
 import org.helioviewer.jhv.gui.ImageViewerGui;
-import org.helioviewer.jhv.gui.components.MainComponent;
 import org.helioviewer.jhv.gui.components.MoviePanel;
 import org.helioviewer.jhv.gui.components.MoviePanel.RecordMode;
 import org.helioviewer.jhv.layers.FrameListener;
 import org.helioviewer.jhv.layers.Layers;
-import org.helioviewer.jhv.opengl.GLHelper;
+import org.helioviewer.jhv.opengl.GLGrab;
 import org.helioviewer.jhv.threads.JHVThread;
 
-import com.jogamp.opengl.FBObject;
-import com.jogamp.opengl.FBObject.Attachment.Type;
-import com.jogamp.opengl.FBObject.TextureAttachment;
 import com.jogamp.opengl.GL2;
 
 public class ExportMovie implements FrameListener {
 
     private static MovieExporter exporter;
+    private static GLGrab grabber;
 
-    private static int canvasWidth;
-    private static int canvasHeight;
     private static int exportHeight;
-
-    private final FBObject fbo = new FBObject();
-    private TextureAttachment fboTex;
 
     private static RecordMode mode;
     private static String moviePath;
@@ -72,63 +62,12 @@ public class ExportMovie implements FrameListener {
         }
     }
 
-    private void init(GL2 gl, int w, int h) {
-        inited = true;
-
-        fbo.init(gl, w, h, 0);
-        fboTex = fbo.attachTexture2D(gl, 0, true);
-
-        fbo.attachRenderbuffer(gl, Type.DEPTH, FBObject.CHOSEN_BITS);
-        fbo.unbind(gl);
-    }
-
-    private void dispose(GL2 gl) {
-        inited = false;
-
-        fbo.detachAll(gl);
-        fbo.destroy(gl);
-        gl.glBindFramebuffer(GL2.GL_FRAMEBUFFER, 0);
-    }
-
-    private BufferedImage renderFrame(GL2 gl) {
-        BufferedImage screenshot;
-
-        int _w = Displayer.getGLWidth();
-        int _h = Displayer.getGLHeight();
-
-        GLHelper.unitScale = true;
-        Displayer.setGLSize(canvasWidth, canvasHeight);
-        Displayer.reshapeAll();
-        {
-            fbo.bind(gl);
-            MainComponent.renderScene(gl);
-            MainComponent.renderFloatScene(gl);
-            fbo.unbind(gl);
-
-            fbo.use(gl, fboTex);
-
-            screenshot = new BufferedImage(fbo.getWidth(), fbo.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
-            byte[] array = ((DataBufferByte) screenshot.getRaster().getDataBuffer()).getData();
-            ByteBuffer fb = ByteBuffer.wrap(array);
-            gl.glBindFramebuffer(GL2.GL_READ_FRAMEBUFFER, fbo.getReadFramebuffer());
-            gl.glPixelStorei(GL2.GL_PACK_ALIGNMENT, 1);
-            gl.glReadPixels(0, 0, fbo.getWidth(), fbo.getHeight(), GL2.GL_BGR, GL2.GL_UNSIGNED_BYTE, fb);
-
-            fbo.unuse(gl);
-        }
-        Displayer.setGLSize(_w, _h);
-        Displayer.reshapeAll();
-        GLHelper.unitScale = false;
-
-        return screenshot;
-    }
-
     private void exportMovieFinish(GL2 gl) {
         ImageViewerGui.getMainComponent().detachExport();
         MoviePanel.recordPanelSetEnabled(true);
 
         try {
-            dispose(gl);
+            grabber.dispose(gl);
             disposeMovieWriter(true);
         } catch (Exception e) {
             e.printStackTrace();
@@ -137,15 +76,17 @@ public class ExportMovie implements FrameListener {
 
     public void handleMovieExport(GL2 gl) {
         if (!inited) {
-            init(gl, canvasWidth, canvasHeight);
+            inited = true;
+            grabber.init(gl);
         }
 
         if (stopped) {
             exportMovieFinish(gl);
+            inited = false;
             return;
         }
 
-        BufferedImage screenshot = renderFrame(gl);
+        BufferedImage screenshot = grabber.renderFrame(gl);
         try {
             if (mode == RecordMode.SHOT) {
                 ImageIO.write(ExportUtils.pasteCanvases(screenshot, EVEImage, exportHeight), "png", new File(imagePath));
@@ -169,6 +110,8 @@ public class ExportMovie implements FrameListener {
             scrw = Math.max(1, EVEImage.getWidth());
             scrh = EVEImage.getHeight();
         }
+
+        int canvasWidth, canvasHeight;
 
         mode = _mode;
         if (mode == RecordMode.SHOT)
@@ -197,6 +140,8 @@ public class ExportMovie implements FrameListener {
         imagePath = prefix + ".png";
 
         MoviePanel.recordPanelSetEnabled(false);
+
+        grabber = new GLGrab(canvasWidth, canvasHeight);
         ImageViewerGui.getMainComponent().attachExport(instance);
 
         if (mode == RecordMode.SHOT) {
---
+++ src/jhv/src/org/helioviewer/jhv/opengl/GLGrab.java
@@ -0,0 +1,74 @@
+package org.helioviewer.jhv.opengl;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.nio.ByteBuffer;
+
+import org.helioviewer.jhv.display.Displayer;
+import org.helioviewer.jhv.gui.components.MainComponent;
+
+import com.jogamp.opengl.FBObject;
+import com.jogamp.opengl.FBObject.Attachment.Type;
+import com.jogamp.opengl.FBObject.TextureAttachment;
+import com.jogamp.opengl.GL2;
+
+public class GLGrab {
+
+    private final FBObject fbo = new FBObject();
+    private TextureAttachment fboTex;
+    private int w;
+    private int h;
+
+    public GLGrab(int _w, int _h) {
+        w = _w;
+        h = _h;
+    }
+
+    public void init(GL2 gl) {
+        fbo.init(gl, w, h, 0);
+        fboTex = fbo.attachTexture2D(gl, 0, true);
+
+        fbo.attachRenderbuffer(gl, Type.DEPTH, FBObject.CHOSEN_BITS);
+        fbo.unbind(gl);
+    }
+
+    public void dispose(GL2 gl) {
+        fbo.detachAll(gl);
+        fbo.destroy(gl);
+        gl.glBindFramebuffer(GL2.GL_FRAMEBUFFER, 0);
+    }
+
+    public BufferedImage renderFrame(GL2 gl) {
+        BufferedImage screenshot;
+
+        int _w = Displayer.getGLWidth();
+        int _h = Displayer.getGLHeight();
+
+        GLHelper.unitScale = true;
+        Displayer.setGLSize(fbo.getWidth(), fbo.getHeight());
+        Displayer.reshapeAll();
+        {
+            fbo.bind(gl);
+            MainComponent.renderScene(gl);
+            MainComponent.renderFloatScene(gl);
+            fbo.unbind(gl);
+
+            fbo.use(gl, fboTex);
+
+            screenshot = new BufferedImage(fbo.getWidth(), fbo.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+            byte[] array = ((DataBufferByte) screenshot.getRaster().getDataBuffer()).getData();
+            ByteBuffer fb = ByteBuffer.wrap(array);
+            gl.glBindFramebuffer(GL2.GL_READ_FRAMEBUFFER, fbo.getReadFramebuffer());
+            gl.glPixelStorei(GL2.GL_PACK_ALIGNMENT, 1);
+            gl.glReadPixels(0, 0, fbo.getWidth(), fbo.getHeight(), GL2.GL_BGR, GL2.GL_UNSIGNED_BYTE, fb);
+
+            fbo.unuse(gl);
+        }
+        Displayer.setGLSize(_w, _h);
+        Displayer.reshapeAll();
+        GLHelper.unitScale = false;
+
+        return screenshot;
+    }
+
+}
