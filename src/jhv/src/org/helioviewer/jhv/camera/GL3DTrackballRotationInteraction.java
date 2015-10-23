10a18417179fe9aed9b4ca0d420bb5e80b0646a7
Freek Verstringe <freek.verstringe@oma.be>
2015-10-23 21:08:55 +0000

    rationalize
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@5887 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/camera/GL3DTrackballRotationInteraction.java
+++ src/jhv/src/org/helioviewer/jhv/camera/GL3DTrackballRotationInteraction.java
@@ -6,15 +6,6 @@ import org.helioviewer.jhv.base.math.Quatd;
 import org.helioviewer.jhv.base.math.Vec3d;
 import org.helioviewer.jhv.display.Displayer;
 
-/**
- * This interaction is used by the {@link GL3DEarthCamera} as its rotation
- * interaction. The calculation of the rotation done by creating a rotation
- * Quaternion between two points on a sphere. These points are retrieved by
- * using the raycasting mechanism provided by {@link GL3DRayTracer}.
- *
- * @author Simon Spoerri (simon.spoerri@fhnw.ch)
- *
- */
 public class GL3DTrackballRotationInteraction extends GL3DDefaultInteraction {
 
     private Vec3d currentRotationStartPoint;
@@ -29,10 +20,10 @@ public class GL3DTrackballRotationInteraction extends GL3DDefaultInteraction {
         if (currentRotationStartPoint != null) {
             Vec3d currentRotationEndPoint = camera.getVectorFromSphereTrackball(e.getPoint());
             currentDragRotation = Quatd.calcRotation(currentRotationStartPoint, currentRotationEndPoint);
+            camera.rotateCurrentDragRotation(currentDragRotation);
+            camera.updateCameraTransformation();
+            Displayer.render();
         }
-        camera.rotateCurrentDragRotation(currentDragRotation);
-        camera.updateCameraTransformation();
-        Displayer.render();
     }
 
     @Override
