3c168b526a1456e29d47ad711cb3adf2444cb099
Bogdan Nicula <bogdan@nicula.net>
2015-11-01 17:19:32 +0000

    TimeStamp: use cached TextRenderer
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6007 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/renderable/components/RenderableTimeStamp.java
+++ src/jhv/src/org/helioviewer/jhv/renderable/components/RenderableTimeStamp.java
@@ -1,13 +1,12 @@
 package org.helioviewer.jhv.renderable.components;
 
-import java.awt.Color;
 import java.awt.Component;
 
 import org.helioviewer.jhv.camera.GL3DViewport;
 import org.helioviewer.jhv.display.Displayer;
 import org.helioviewer.jhv.gui.ImageViewerGui;
-import org.helioviewer.jhv.gui.UIGlobals;
 import org.helioviewer.jhv.layers.Layers;
+import org.helioviewer.jhv.opengl.GLText;
 import org.helioviewer.jhv.renderable.gui.AbstractRenderable;
 
 import com.jogamp.opengl.GL2;
@@ -15,14 +14,8 @@ import com.jogamp.opengl.util.awt.TextRenderer;
 
 public class RenderableTimeStamp extends AbstractRenderable {
 
-    private float oldFontSize = -1;
     private static final double vpScale = 0.035;
-    private TextRenderer textRenderer;
-
     private static final String name = "Timestamp";
-
-    public RenderableTimeStamp() {
-    }
 
     @Override
     public void render(GL2 gl, GL3DViewport vp) {
@@ -33,18 +26,6 @@ public class RenderableTimeStamp extends AbstractRenderable {
         if (!isVisible[vp.getIndex()])
             return;
 
-        float fontSize = (int) (vp.getHeight() * vpScale);
-        if (textRenderer == null || fontSize != oldFontSize) {
-            oldFontSize = fontSize;
-            if (textRenderer != null) {
-                textRenderer.dispose();
-            }
-            textRenderer = new TextRenderer(UIGlobals.UIFontRoboto.deriveFont(fontSize), true, true);
-            textRenderer.setUseVertexArrays(true);
-            textRenderer.setSmoothing(false);
-            textRenderer.setColor(Color.WHITE);
-        }
-
         String text = Layers.getLastUpdatedTimestamp().toString();
         if (Displayer.multiview) {
             RenderableImageLayer im = ImageViewerGui.getRenderableContainer().getViewportRenderableImageLayer(vp.getIndex());
@@ -54,9 +35,10 @@ public class RenderableTimeStamp extends AbstractRenderable {
         }
 
         int delta = (int) (vp.getHeight() * 0.01);
-        textRenderer.beginRendering(vp.getWidth(), vp.getHeight(), true);
-        textRenderer.draw(text, delta, delta);
-        textRenderer.endRendering();
+        TextRenderer renderer = GLText.getRenderer((int) (vp.getHeight() * vpScale));
+        renderer.beginRendering(vp.getWidth(), vp.getHeight(), true);
+        renderer.draw(text, delta, delta);
+        renderer.endRendering();
     }
 
     @Override
@@ -90,11 +72,6 @@ public class RenderableTimeStamp extends AbstractRenderable {
 
     @Override
     public void dispose(GL2 gl) {
-        if (textRenderer != null) {
-            textRenderer.dispose();
-            textRenderer = null;
-        }
-        oldFontSize = -1;
     }
 
     @Override
