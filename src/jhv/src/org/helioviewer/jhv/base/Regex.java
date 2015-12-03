9fbcc76d262b25ae92daf0fd1e4ad74141b7d00f
Bogdan Nicula <bogdan@nicula.net>
2015-12-03 22:40:03 +0000

    Replace HTTP pattern
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6250 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/base/Regex.java
+++ src/jhv/src/org/helioviewer/jhv/base/Regex.java
@@ -4,7 +4,8 @@ import java.util.regex.Pattern;
 
 public class Regex {
 
-    public static final Pattern HTTPpattern = Pattern.compile("^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$");
+    public static final Pattern HTTPpattern = Pattern.compile("^https?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
+    // ("^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$"); -- locks up
     public static final Pattern FloatingPointPattern = Pattern.compile("[\\x00-\\x20]*[+-]?(NaN|Infinity|((((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)([eE][+-]?(\\p{Digit}+))?)|(\\.((\\p{Digit}+))([eE][+-]?(\\p{Digit}+))?)|(((0[xX](\\p{XDigit}+)(\\.)?)|(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+)))[pP][+-]?(\\p{Digit}+)))[fFdD]?))[\\x00-\\x20]*");
     public static final Pattern IntegerPattern = Pattern.compile("\\d+");
 
