9197e01a9d4fff89cfefc23c93e93da9d40765e0
Bram Bourgoignie <bram.bourgoignie@oma.be>
2015-12-03 11:24:19 +0000

    Compile integer and floating point pattern
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6240 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/base/Regex.java
+++ src/jhv/src/org/helioviewer/jhv/base/Regex.java
@@ -5,5 +5,7 @@ import java.util.regex.Pattern;
 public class Regex {
 
     public static final Pattern HTTPpattern = Pattern.compile("^(https?:\\/\\/)?([\\da-z\\.-]+)\\.([a-z\\.]{2,6})([\\/\\w \\.-]*)*\\/?$");
+    public static final Pattern FloatingPointPattern = Pattern.compile("[\\x00-\\x20]*[+-]?(NaN|Infinity|((((\\p{Digit}+)(\\.)?((\\p{Digit}+)?)([eE][+-]?(\\p{Digit}+))?)|(\\.((\\p{Digit}+))([eE][+-]?(\\p{Digit}+))?)|(((0[xX](\\p{XDigit}+)(\\.)?)|(0[xX](\\p{XDigit}+)?(\\.)(\\p{XDigit}+)))[pP][+-]?(\\p{Digit}+)))[fFdD]?))[\\x00-\\x20]*");
+    public static final Pattern IntegerPointPattern = Pattern.compile("\\d+");
 
 }
--- src/jhv/src/org/helioviewer/jhv/data/datatype/event/JHVEventParameter.java
+++ src/jhv/src/org/helioviewer/jhv/data/datatype/event/JHVEventParameter.java
@@ -2,7 +2,9 @@ package org.helioviewer.jhv.data.datatype.event;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import org.helioviewer.jhv.base.Regex;
 
 public class JHVEventParameter {
     /** The name of the parameter */
@@ -126,51 +128,12 @@ public class JHVEventParameter {
     }
 
     private boolean isDouble(String value) {
-        final String Digits = "(\\p{Digit}+)";
-        final String HexDigits = "(\\p{XDigit}+)";
-        // an exponent is 'e' or 'E' followed by an optionally
-        // signed decimal integer.
-        final String Exp = "[eE][+-]?" + Digits;
-        final String fpRegex = ("[\\x00-\\x20]*" + // Optional leading
-                // "whitespace"
-                "[+-]?(" + // Optional sign character
-                "NaN|" + // "NaN" string
-                "Infinity|" + // "Infinity" string
-
-                // A decimal floating-point string representing a finite
-                // positive
-                // number without a leading sign has at most five basic pieces:
-                // Digits . Digits ExponentPart FloatTypeSuffix
-                //
-                // Since this method allows integer-only strings as input
-                // in addition to strings of floating-point literals, the
-                // two sub-patterns below are simplifications of the grammar
-                // productions from the Java Language Specification, 2nd
-                // edition, section 3.10.2.
-
-                // Digits ._opt Digits_opt ExponentPart_opt FloatTypeSuffix_opt
-                "(((" + Digits + "(\\.)?(" + Digits + "?)(" + Exp + ")?)|" +
-
-        // . Digits ExponentPart_opt FloatTypeSuffix_opt
-        "(\\.(" + Digits + ")(" + Exp + ")?)|" +
-
-                // Hexadecimal strings
-                "((" +
-                // 0[xX] HexDigits ._opt BinaryExponent FloatTypeSuffix_opt
-                "(0[xX]" + HexDigits + "(\\.)?)|" +
-
-                // 0[xX] HexDigits_opt . HexDigits BinaryExponent
-                // FloatTypeSuffix_opt
-                "(0[xX]" + HexDigits + "?(\\.)" + HexDigits + ")" +
-
-                ")[pP][+-]?" + Digits + "))" + "[fFdD]?))" + "[\\x00-\\x20]*");// Optional
-        // trailing
-        // "whitespace"
-
-        return value != null && Pattern.matches(fpRegex, value);
+        Matcher m = Regex.FloatingPointPattern.matcher(value);
+        return value != null && m.matches();
     }
 
     private boolean isInteger(String value) {
-        return value.matches("\\d+");
+        Matcher m = Regex.IntegerPointPattern.matcher(value);
+        return m.matches();
     }
 }
