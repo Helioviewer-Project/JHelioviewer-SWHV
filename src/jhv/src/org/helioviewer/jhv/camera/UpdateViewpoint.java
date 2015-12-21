0d748a78100393c4e7d2d686f315847a1e2b8e2f
Bogdan Nicula <bogdan@nicula.net>
2015-12-21 10:01:18 +0000

    Reduce files
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6383 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/camera/UpdateViewpoint.java
+++ src/jhv/src/org/helioviewer/jhv/camera/UpdateViewpoint.java
@@ -1,10 +1,72 @@
 package org.helioviewer.jhv.camera;
 
 import org.helioviewer.jhv.base.astronomy.Position;
+import org.helioviewer.jhv.base.astronomy.Sun;
 import org.helioviewer.jhv.base.time.JHVDate;
+import org.helioviewer.jhv.layers.Layers;
+import org.helioviewer.jhv.viewmodel.view.View;
 
-abstract class UpdateViewpoint {
+interface UpdateViewpoint {
 
-    abstract Position.Q update(JHVDate time);
+    Position.Q update(JHVDate time);
+
+    static UpdateViewpoint updateEarth = new UpdateViewpointEarth();
+    static UpdateViewpoint updateExpert = new UpdateViewpointExpert();
+    static UpdateViewpoint updateObserver = new UpdateViewpointObserver();
+
+    static class UpdateViewpointEarth implements UpdateViewpoint {
+        @Override
+        public Position.Q update(JHVDate time) {
+            return Sun.getEarthQuat(time);
+        }
+    }
+
+    static class UpdateViewpointObserver implements UpdateViewpoint {
+        @Override
+        public Position.Q update(JHVDate time) {
+            View view = Layers.getActiveView();
+            if (view == null) {
+                return Sun.getEarthQuat(time);
+            } else {
+                return view.getMetaData(time).getViewpoint();
+            }
+        }
+    }
+
+    static class UpdateViewpointExpert implements UpdateViewpoint {
+
+        private PositionLoad positionLoad;
+
+        void setPositionLoad(PositionLoad _positionLoad) {
+            positionLoad = _positionLoad;
+        }
+
+        @Override
+        public Position.Q update(JHVDate time) {
+            if (positionLoad.isLoaded()) {
+                long currentCameraTime;
+                long tLayerStart = 0, tLayerEnd = 0;
+                // Active layer times
+                View view = Layers.getActiveView();
+                if (view != null) {
+                    tLayerStart = view.getFirstTime().milli;
+                    tLayerEnd = view.getLastTime().milli;
+                }
+
+                // camera times
+                long tPositionStart = positionLoad.getStartTime();
+                long tPositionEnd = positionLoad.getEndTime();
+
+                if (tLayerEnd != tLayerStart) {
+                    currentCameraTime = (long) (tPositionStart + (tPositionEnd - tPositionStart) * (time.milli - tLayerStart) / (double) (tLayerEnd - tLayerStart));
+                } else {
+                    currentCameraTime = tPositionEnd;
+                }
+                return positionLoad.getInterpolatedPosition(currentCameraTime);
+            }
+            return Sun.getEarthQuat(time);
+        }
+
+    }
 
 }
