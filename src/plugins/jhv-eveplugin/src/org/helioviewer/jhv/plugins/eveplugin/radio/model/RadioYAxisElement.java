8654e6a9036f9e471d3ece2a39539fd353c5d6dd
Bram Bourgoignie <bram.bourgoignie@oma.be>
2016-01-08 17:17:25 +0000

    Override fucntions
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6431 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/radio/model/RadioYAxisElement.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/radio/model/RadioYAxisElement.java
@@ -62,4 +62,34 @@ public class RadioYAxisElement extends YAxisElement {
         fireSelectedRangeChanged();
     }
 
+    @Override
+    public void shiftDownPixels(double distanceY, int height) {
+        double scaledMin = scale(selectedRange.min);
+        double scaledMax = scale(selectedRange.max);
+
+        double ratioValue = (scaledMax - scaledMin) / height;
+        double shift = distanceY * ratioValue;
+        double startValue = scaledMin + shift;
+        double endValue = scaledMax + shift;
+        selectedRange.min = invScale(startValue);
+        selectedRange.max = invScale(endValue);
+        fireSelectedRangeChanged();
+    }
+
+    @Override
+    public void zoomSelectedRange(double scrollValue, double relativeY, double height) {
+        double scaledMin = scale(selectedRange.min);
+        double scaledMax = scale(selectedRange.max);
+        double scaled = scaledMin + (scaledMax - scaledMin) * (relativeY / height);
+        double delta = scrollValue * ZOOMSTEP_PERCENTAGE;
+        double newScaledMin = ((1 + delta) * scaledMin - delta * scaled);
+        double newScaledMax = ((1 + delta) * scaledMax - delta * scaled);
+
+        newScaledMin = Math.max(scale(availableRange.min), newScaledMin);
+        newScaledMax = Math.min(scale(availableRange.max), newScaledMax);
+        selectedRange.min = invScale(newScaledMin);
+        selectedRange.max = invScale(newScaledMax);
+        fireSelectedRangeChanged();
+    }
+
 }
