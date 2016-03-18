7e97b9e0692ac74f04aa85a9384544b1c821e0a6
Freek Verstringe <freek.verstringe@oma.be>
2016-03-18 14:11:58 +0000

    reduce
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6609 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/request/IncomingRequestManager.java
+++ src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/request/IncomingRequestManager.java
@@ -1,27 +1,16 @@
 package org.helioviewer.jhv.plugins.swek.request;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import org.helioviewer.jhv.base.interval.Interval;
 import org.helioviewer.jhv.data.container.JHVEventContainerRequestHandler;
 import org.helioviewer.jhv.data.datatype.event.JHVEventType;
+import org.helioviewer.jhv.plugins.swek.download.SWEKDownloadManager;
 
 public class IncomingRequestManager implements JHVEventContainerRequestHandler {
 
     /** The singleton instance */
     private static IncomingRequestManager instance;
-
-    /** The listeners */
-    private final List<IncomingRequestManagerListener> listeners;
-
-    /**
-     * Private constructor.
-     */
-    private IncomingRequestManager() {
-        listeners = new ArrayList<IncomingRequestManagerListener>();
-    }
 
     /**
      * Gets the singleton instance.
@@ -35,45 +24,9 @@ public class IncomingRequestManager implements JHVEventContainerRequestHandler {
         return instance;
     }
 
-    /**
-     * Add a request manager listener.
-     *
-     * @param l
-     *            the listener to add
-     */
-    public void addRequestManagerListener(IncomingRequestManagerListener l) {
-        listeners.add(l);
-    }
-
-    /**
-     * Removes request manager listener.
-     *
-     * @param l
-     *            the listener to remove
-     */
-    public void removeRequestManagerListener(IncomingRequestManagerListener l) {
-        listeners.remove(l);
-    }
-
     @Override
     public void handleRequestForInterval(JHVEventType eventType, Date startDate, Date endDate) {
         Interval<Date> interval = new Interval<Date>(startDate, endDate);
-        fireNewIntervalRequested(eventType, interval);
+        SWEKDownloadManager.getSingletonInstance().newRequestForInterval(eventType, interval);
     }
-
-    /**
-     * Informs the listeners about a new interval that was requested.
-     *
-     * @param eventType
-     *
-     * @param interval
-     *            interval that was requested
-     * @param requestID
-     */
-    private void fireNewIntervalRequested(JHVEventType eventType, Interval<Date> interval) {
-        for (IncomingRequestManagerListener l : listeners) {
-            l.newRequestForInterval(eventType, interval);
-        }
-    }
-
 }
