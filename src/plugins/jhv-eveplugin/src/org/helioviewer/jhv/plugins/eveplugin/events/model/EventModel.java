45ca1fb200bc3bbe7df8e9e1214a226c96cd2d47
Bram Bourgoignie <bram.bourgoignie@oma.be>
2016-03-18 15:15:27 +0000

    Remove background work, add smurf
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6647 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/events/model/EventModel.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/events/model/EventModel.java
@@ -25,6 +25,64 @@ import org.helioviewer.jhv.plugins.eveplugin.view.linedataselector.LineDataSelec
 
 /**
  *
+ * <pre>
+ * "                              _,,aaaaa,,_                           "
+ * "                           _,dP"''    `""""Ya,_                     "
+ * "                        ,aP"'                `"Yb,_                 "
+ * "                      ,8"'                       `"8a,              "
+ * "                    ,8"                             `"8,_           "
+ * "                  ,8"                                  "Yb,         "
+ * "                ,8"                                      `8,        "
+ * "               dP'                                        8I        "
+ * "             ,8"                           bg,_          ,P'        "
+ * "            ,8'                              "Y8"Ya,,,,ad"          "
+ * "           ,d"                            a,_ I8   `"""'            "
+ * "          ,8'                              ""888                    "
+ * "          dP     __                           `Yb,                  "
+ * "         dP'  _,d8P::::Y8b,                     `Ya                 "
+ * "    ,adba8',d88P::;;::;;;:"b:::Ya,_               Ya                "
+ * "   dP":::"Y88P:;P"""YP"""Yb;::::::"Ya,             "Y,              "
+ * "   8:::::::Yb;d" _  "_    dI:::::::::"Yb,__,,gd88ba,db              "
+ * "   Yb:::::::"8(,8P _d8   d8:::::::::::::Y88P"::::::Y8I              "
+ * "   `Yb;:::::::""::"":b,,dP::::::::::::::::::;aaa;:::8(              "
+ * "     `Y8a;:::::::::::::::::::::;;::::::::::8P""Y8)::8I              "
+ * "       8b"ba::::::::::::::::;adP:::::::::::":::dP::;8'              "
+ * "       `8b;::::::::::::;aad888P::::::::::::::;dP::;8'               "
+ * "        `8b;::::::::""""88"  d::::::::::b;:::::;;dP'                "
+ * "          "Yb;::::::::::Y8bad::::::::::;"8Paaa""'                   "
+ * "            `"Y8a;;;:::::::::::::;;aadP""                           "
+ * "                ``""Y88bbbdddd88P""8b,                              "
+ * "                         _,d8"::::::"8b,                            "
+ * "                       ,dP8"::::::;;:::"b,                          "
+ * "                     ,dP"8:::::::Yb;::::"b,                         "
+ * "                   ,8P:dP:::::::::Yb;::::"b,                        "
+ * "                _,dP:;8":::::::::::Yb;::::"b                        "
+ * "      ,aaaaaa,,d8P:::8":::::::::::;dP:::::;8                        "
+ * "   ,ad":;;:::::"::::8"::::::::::;dP::::::;dI                        "
+ * "  dP";adP":::::;:;dP;::::aaaad88"::::::adP:8b,___                   "
+ * " d8:::8;;;aadP"::8'Y8:d8P"::::::::::;dP";d"'`Yb:"b                  "
+ * " 8I:::;""":::::;dP I8P"::::::::::;a8"a8P"     "b:P                  "
+ * " Yb::::"8baa8"""'  8;:;d"::::::::d8P"'         8"                   "
+ * "  "YbaaP::8;P      `8;d::;a::;;;;dP           ,8                    "
+ * "     `"Y8P"'         Yb;;d::;aadP"           ,d'                    "
+ * "                      "YP:::"P'             ,d'                     "
+ * "                        "8bdP'    _        ,8'                      "
+ * "       Normand         ,8"`""Yba,d"      ,d"                        "
+ * "       Veilleux       ,P'     d"8'     ,d"                          "
+ * "                     ,8'     d'8'     ,P'                           "
+ * "                     (b      8 I      8,                            "
+ * "                      Y,     Y,Y,     `b,                           "
+ * "                ____   "8,__ `Y,Y,     `Y""b,                       "
+ * "            ,adP""""b8P""""""""Ybdb,        Y,                      "
+ * "          ,dP"    ,dP'            `""       `8                      "
+ * "         ,8"     ,P'                        ,P                      "
+ * "         8'      8)                        ,8'                      "
+ * "         8,      Yb                      ,aP'                       "
+ * "         `Ya      Yb                  ,ad"'                         "
+ * "           "Ya,___ "Ya             ,ad"'                            "
+ * "             ``""""""`Yba,,,,,,,adP"'                               "
+ * "                        `"""""""'                                   "
+ * </pre>
  *
  * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
  *
@@ -168,52 +226,6 @@ public class EventModel implements TimingListener, JHVEventHandler {
     private Date tempLastDateWithData = null;
     private int maxEventLines = 0;
 
-    public void backgroundWork() {
-
-        final Map<JHVEventType, Integer> linesPerEventType = new HashMap<JHVEventType, Integer>();
-        final Map<JHVEventType, List<EventPlotConfiguration>> eventPlotConfigPerEventType = new HashMap<JHVEventType, List<EventPlotConfiguration>>();
-
-        if (events.size() > 0) {
-            for (JHVEventType eventType : events.keySet()) {
-                endDates = new ArrayList<Date>();
-                plotConfig = new ArrayList<EventPlotConfiguration>();
-                minimalEndDate = null;
-                maximumEndDate = null;
-                minimalDateLine = 0;
-                maximumDateLine = 0;
-                nrLines = 0;
-                maxEventLines = 0;
-                int relatedEventPosition = -1;
-                SortedMap<SortedDateInterval, JHVRelatedEvents> eventMap = events.get(eventType);
-                for (Entry<SortedDateInterval, JHVRelatedEvents> evr : eventMap.entrySet()) {
-                    handleEvent(evr.getValue(), relatedEventPosition, 0);
-                }
-                linesPerEventType.put(eventType, maxEventLines);
-                maxNrLines += maxEventLines;
-                eventPlotConfigPerEventType.put(eventType, plotConfig);
-            }
-
-            eventPlotConfiguration = new EventTypePlotConfiguration(events.size(), maxNrLines, linesPerEventType, eventPlotConfigPerEventType, tempLastDateWithData);
-        } else {
-            eventPlotConfiguration = new EventTypePlotConfiguration();
-        }
-
-        if (!eventPlotConfiguration.getEventPlotConfigurations().isEmpty() && prevNoPlotConfig) {
-            prevNoPlotConfig = false;
-        }
-
-        endDates = new ArrayList<Date>();
-        minimalEndDate = null;
-        maximumEndDate = null;
-        minimalDateLine = 0;
-        maximumDateLine = 0;
-        nrLines = 0;
-        plotConfig = new ArrayList<EventPlotConfiguration>();
-        maxNrLines = 0;
-        tempLastDateWithData = null;
-        maxEventLines = 0;
-    }
-
     private boolean handleEvent(JHVRelatedEvents event, int relatedEventPosition, int relationNr) {
         EventPlotConfiguration epc = creatEventPlotConfiguration(event, relatedEventPosition, relationNr);
         plotConfig.add(epc);
@@ -263,7 +275,48 @@ public class EventModel implements TimingListener, JHVEventHandler {
     }
 
     private void createEventPlotConfiguration() {
-        backgroundWork();
+        final Map<JHVEventType, Integer> linesPerEventType = new HashMap<JHVEventType, Integer>();
+        final Map<JHVEventType, List<EventPlotConfiguration>> eventPlotConfigPerEventType = new HashMap<JHVEventType, List<EventPlotConfiguration>>();
+
+        if (events.size() > 0) {
+            for (JHVEventType eventType : events.keySet()) {
+                endDates = new ArrayList<Date>();
+                plotConfig = new ArrayList<EventPlotConfiguration>();
+                minimalEndDate = null;
+                maximumEndDate = null;
+                minimalDateLine = 0;
+                maximumDateLine = 0;
+                nrLines = 0;
+                maxEventLines = 0;
+                int relatedEventPosition = -1;
+                SortedMap<SortedDateInterval, JHVRelatedEvents> eventMap = events.get(eventType);
+                for (Entry<SortedDateInterval, JHVRelatedEvents> evr : eventMap.entrySet()) {
+                    handleEvent(evr.getValue(), relatedEventPosition, 0);
+                }
+                linesPerEventType.put(eventType, maxEventLines);
+                maxNrLines += maxEventLines;
+                eventPlotConfigPerEventType.put(eventType, plotConfig);
+            }
+
+            eventPlotConfiguration = new EventTypePlotConfiguration(events.size(), maxNrLines, linesPerEventType, eventPlotConfigPerEventType, tempLastDateWithData);
+        } else {
+            eventPlotConfiguration = new EventTypePlotConfiguration();
+        }
+
+        if (!eventPlotConfiguration.getEventPlotConfigurations().isEmpty() && prevNoPlotConfig) {
+            prevNoPlotConfig = false;
+        }
+
+        endDates = new ArrayList<Date>();
+        minimalEndDate = null;
+        maximumEndDate = null;
+        minimalDateLine = 0;
+        maximumDateLine = 0;
+        nrLines = 0;
+        plotConfig = new ArrayList<EventPlotConfiguration>();
+        maxNrLines = 0;
+        tempLastDateWithData = null;
+        maxEventLines = 0;
     }
 
     private int defineMaximumDateLine(ArrayList<Date> endDates) {
