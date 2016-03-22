74e435798f770cc3b69e4d89f3691682ee86d481
Bogdan Nicula <bogdan@nicula.net>
2016-03-22 12:55:50 +0000

    Restore EventTypePlotConfiguration
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6708 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/events/model/EventTypePlotConfiguration.java
@@ -0,0 +1,127 @@
+package org.helioviewer.jhv.plugins.eveplugin.events.model;
+
+import java.awt.Point;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.helioviewer.jhv.data.datatype.event.JHVEventType;
+import org.helioviewer.jhv.data.datatype.event.JHVRelatedEvents;
+
+/**
+ * Combines everything needed to draw the events on the eve plugin.
+ *
+ * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
+ *
+ */
+public class EventTypePlotConfiguration {
+    /** Number of events in this event type plot configuration */
+    private final int nrOfEventTypes;
+
+    /** Maximum number of lines needed for all events */
+    private final int totalNrLines;
+
+    /** Maximum of lines for every event type */
+    private final Map<JHVEventType, Integer> maxLinesPerEventType;
+
+    /** The plot configurations for all events ordered per event type */
+    private final Map<JHVEventType, List<EventPlotConfiguration>> eventPlotConfigurations;
+
+    private final Date lastDateWithData;
+
+    /**
+     * Default constructor.
+     *
+     */
+    public EventTypePlotConfiguration() {
+        nrOfEventTypes = 0;
+        totalNrLines = 0;
+        maxLinesPerEventType = new HashMap<JHVEventType, Integer>();
+        eventPlotConfigurations = new HashMap<JHVEventType, List<EventPlotConfiguration>>();
+        lastDateWithData = null;
+    }
+
+    /**
+     * Creates the event type plot configuration from the given number of
+     * events, total number of lines, maximum lines per event type, the event
+     * plot configurations and the color of the event type.
+     *
+     * @param nrOfEventType
+     *            the total number of event types to be drawn
+     * @param totalNrOfLines
+     *            the total number of lines needed by all event types
+     * @param maxLinesPerEventType
+     *            overview of the total number of lines per event type
+     * @param eventPlotConfigurations
+     *            the event plot configurations
+     */
+    public EventTypePlotConfiguration(int nrOfEventTypes, int totalNrLines, Map<JHVEventType, Integer> maxLinesPerEventType,
+            Map<JHVEventType, List<EventPlotConfiguration>> eventPlotConfigurations, Date lastDateWithData) {
+        this.nrOfEventTypes = nrOfEventTypes;
+        this.totalNrLines = totalNrLines;
+        this.maxLinesPerEventType = maxLinesPerEventType;
+        this.eventPlotConfigurations = eventPlotConfigurations;
+        this.lastDateWithData = lastDateWithData;
+    }
+
+    /**
+     * Gets the number of event types in this configuration
+     *
+     * @return the number of Event types
+     */
+    public int getNrOfEventTypes() {
+        return nrOfEventTypes;
+    }
+
+    /**
+     * Gets the total number of lines.
+     *
+     * @return the total number lines
+     */
+    public int getTotalNrLines() {
+        return totalNrLines;
+    }
+
+    /**
+     * Gets the maximum lines per event type.
+     *
+     * @return the max lines per event type
+     */
+    public Map<JHVEventType, Integer> getMaxLinesPerEventType() {
+        return maxLinesPerEventType;
+    }
+
+    /**
+     * Gets the event plot configurations.
+     *
+     * @return the event plot configurations
+     */
+    public Map<JHVEventType, List<EventPlotConfiguration>> getEventPlotConfigurations() {
+        return eventPlotConfigurations;
+    }
+
+    /**
+     * Gets the event at the given location.
+     *
+     * @param p
+     *            the location to check.
+     * @return null if no event was found, or the event if found
+     */
+    public JHVRelatedEvents getEventOnLocation(Point p) {
+        for (List<EventPlotConfiguration> value : eventPlotConfigurations.values()) {
+            for (EventPlotConfiguration epc : value) {
+                JHVRelatedEvents event = epc.getEventAtPoint(p);
+                if (event != null) {
+                    return event;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Date getLastDateWithData() {
+        return lastDateWithData;
+    }
+
+}
