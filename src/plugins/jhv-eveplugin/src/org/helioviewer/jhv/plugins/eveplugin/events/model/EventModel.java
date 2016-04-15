fee86824002f2c2d85adb7a069de6383b7be3f3e
Freek Verstringe <freek.verstringe@oma.be>
2016-04-15 08:49:19 +0000

    cleanup
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@6953 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/events/model/EventModel.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/events/model/EventModel.java
@@ -18,102 +18,26 @@ import org.helioviewer.jhv.plugins.eveplugin.events.gui.EventPanel;
 import org.helioviewer.jhv.plugins.eveplugin.events.gui.EventsSelectorElement;
 import org.helioviewer.jhv.plugins.eveplugin.view.linedataselector.LineDataSelectorModel;
 
-/**
- *
- * <pre>
- * "                              _,,aaaaa,,_                           "
- * "                           _,dP"''    `""""Ya,_                     "
- * "                        ,aP"'                `"Yb,_                 "
- * "                      ,8"'                       `"8a,              "
- * "                    ,8"                             `"8,_           "
- * "                  ,8"                                  "Yb,         "
- * "                ,8"                                      `8,        "
- * "               dP'                                        8I        "
- * "             ,8"                           bg,_          ,P'        "
- * "            ,8'                              "Y8"Ya,,,,ad"          "
- * "           ,d"                            a,_ I8   `"""'            "
- * "          ,8'                              ""888                    "
- * "          dP     __                           `Yb,                  "
- * "         dP'  _,d8P::::Y8b,                     `Ya                 "
- * "    ,adba8',d88P::;;::;;;:"b:::Ya,_               Ya                "
- * "   dP":::"Y88P:;P"""YP"""Yb;::::::"Ya,             "Y,              "
- * "   8:::::::Yb;d" _  "_    dI:::::::::"Yb,__,,gd88ba,db              "
- * "   Yb:::::::"8(,8P _d8   d8:::::::::::::Y88P"::::::Y8I              "
- * "   `Yb;:::::::""::"":b,,dP::::::::::::::::::;aaa;:::8(              "
- * "     `Y8a;:::::::::::::::::::::;;::::::::::8P""Y8)::8I              "
- * "       8b"ba::::::::::::::::;adP:::::::::::":::dP::;8'              "
- * "       `8b;::::::::::::;aad888P::::::::::::::;dP::;8'               "
- * "        `8b;::::::::""""88"  d::::::::::b;:::::;;dP'                "
- * "          "Yb;::::::::::Y8bad::::::::::;"8Paaa""'                   "
- * "            `"Y8a;;;:::::::::::::;;aadP""                           "
- * "                ``""Y88bbbdddd88P""8b,                              "
- * "                         _,d8"::::::"8b,                            "
- * "                       ,dP8"::::::;;:::"b,                          "
- * "                     ,dP"8:::::::Yb;::::"b,                         "
- * "                   ,8P:dP:::::::::Yb;::::"b,                        "
- * "                _,dP:;8":::::::::::Yb;::::"b                        "
- * "      ,aaaaaa,,d8P:::8":::::::::::;dP:::::;8                        "
- * "   ,ad":;;:::::"::::8"::::::::::;dP::::::;dI                        "
- * "  dP";adP":::::;:;dP;::::aaaad88"::::::adP:8b,___                   "
- * " d8:::8;;;aadP"::8'Y8:d8P"::::::::::;dP";d"'`Yb:"b                  "
- * " 8I:::;""":::::;dP I8P"::::::::::;a8"a8P"     "b:P                  "
- * " Yb::::"8baa8"""'  8;:;d"::::::::d8P"'         8"                   "
- * "  "YbaaP::8;P      `8;d::;a::;;;;dP           ,8                    "
- * "     `"Y8P"'         Yb;;d::;aadP"           ,d'                    "
- * "                      "YP:::"P'             ,d'                     "
- * "                        "8bdP'    _        ,8'                      "
- * "       Normand         ,8"`""Yba,d"      ,d"                        "
- * "       Veilleux       ,P'     d"8'     ,d"                          "
- * "                     ,8'     d'8'     ,P'                           "
- * "                     (b      8 I      8,                            "
- * "                      Y,     Y,Y,     `b,                           "
- * "                ____   "8,__ `Y,Y,     `Y""b,                       "
- * "            ,adP""""b8P""""""""Ybdb,        Y,                      "
- * "          ,dP"    ,dP'            `""       `8                      "
- * "         ,8"     ,P'                        ,P                      "
- * "         8'      8)                        ,8'                      "
- * "         8,      Yb                      ,aP'                       "
- * "         `Ya      Yb                  ,ad"'                         "
- * "           "Ya,___ "Ya             ,ad"'                            "
- * "             ``""""""`Yba,,,,,,,adP"'                               "
- * "                        `"""""""'                                   "
- * </pre>
- *
+/*
  * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
  *
  */
 public class EventModel implements TimingListener, JHVEventHandler {
 
     private static EventModel instance;
-
-    /** event plot configurations */
-    // private final EventTypePlotConfiguration eventPlotConfiguration;
-
-    /** Instance of the event container */
     private final JHVEventContainer eventContainer;
-
-    /** events visible */
     private boolean eventsVisible;
-
-    /** current events */
     private Map<JHVEventType, SortedMap<SortedDateInterval, JHVRelatedEvents>> events;
-
-    /** The event panel */
     private final EventPanel eventPanel;
 
     private final EventsSelectorElement eventSelectorElement;
 
     private boolean eventsActivated;
 
-    // private boolean prevNoPlotConfig;
-
-    // private Date lastDateWithData;
-
     private JHVRelatedEvents eventUnderMouse;
 
     private EventModel() {
         eventContainer = JHVEventContainer.getSingletonInstance();
-        // eventPlotConfiguration = new EventTypePlotConfiguration();
         events = new HashMap<JHVEventType, SortedMap<SortedDateInterval, JHVRelatedEvents>>();
         eventsVisible = false;
         eventPanel = new EventPanel();
@@ -137,11 +61,6 @@ public class EventModel implements TimingListener, JHVEventHandler {
 
     @Override
     public void selectedIntervalChanged(boolean keepFullValueRange) {
-        /*
-        if (!EVEState.getSingletonInstance().isMouseTimeIntervalDragging()) {
-            // createEventPlotConfiguration();
-        }
-         */
     }
 
     @Override
@@ -157,13 +76,6 @@ public class EventModel implements TimingListener, JHVEventHandler {
     public Map<JHVEventType, SortedMap<SortedDateInterval, JHVRelatedEvents>> getEvents() {
         return events;
     }
-
-    /*
-     * public EventTypePlotConfiguration getEventTypePlotConfiguration() {
-     * createEventPlotConfiguration(); if (eventPlotConfiguration != null) {
-     * return eventPlotConfiguration; } else { return new
-     * EventTypePlotConfiguration(); } }
-     */
 
     public boolean isEventsVisible() {
         return eventsVisible;
