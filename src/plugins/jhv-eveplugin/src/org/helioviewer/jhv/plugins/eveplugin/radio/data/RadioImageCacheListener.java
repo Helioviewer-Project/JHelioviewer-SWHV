ba2d7e41427b9c443e7441175864f602c96db61b
Freek Verstringe <freek.verstringe@oma.be>
2016-04-21 10:41:54 +0000

    reduce
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7079 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/radio/data/RadioImageCacheListener.java
+++
@@ -1,9 +0,0 @@
-package org.helioviewer.jhv.plugins.eveplugin.radio.data;
-
-public interface RadioImageCacheListener {
-
-    public abstract void imageRemoved(long ID);
-
-    public abstract void imageAdded(long ID);
-
-}
