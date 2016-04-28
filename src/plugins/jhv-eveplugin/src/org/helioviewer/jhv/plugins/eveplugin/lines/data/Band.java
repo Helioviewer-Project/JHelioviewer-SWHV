78d39623ca311da972191db5dda71f45aa88751a
Freek Verstringe <freek.verstringe@oma.be>
2016-04-28 13:30:28 +0000

    allow downsampling
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7220 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/lines/data/Band.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/lines/data/Band.java
@@ -30,7 +30,6 @@ public class Band implements LineDataSelectorElement {
     private Color graphColor = Color.BLACK;
     private YAxis yAxis;
     private final List<GraphPolyline> graphPolylines = new ArrayList<GraphPolyline>();
-    private long lastMilliWithData;
     private final RequestCache requestCache = new RequestCache();
     public int[] warnLevels;
     public String[] warnLabels;
@@ -111,12 +110,9 @@ public class Band implements LineDataSelectorElement {
         if (!isVisible) {
             return;
         }
+        g.setColor(graphColor);
         for (GraphPolyline line : graphPolylines) {
-            g.setColor(line.color);
-            for (int k = 0; k < line.xPoints.size(); k++) {
-                g.drawPolyline(line.xPointsArray.get(k), line.yPointsArray.get(k), line.yPoints.get(k).size());
-            }
-
+            g.drawPolyline(line.xPoints, line.yPoints, line.yPoints.length);
         }
         for (int j = 0; j < warnLevels.length; j++) {
             g.drawLine(graphArea.x, warnLevels[j], graphArea.x + graphArea.width, warnLevels[j]);
@@ -140,30 +136,56 @@ public class Band implements LineDataSelectorElement {
     private void updateGraphsData() {
         Rectangle graphArea = EVEPlugin.dc.getGraphArea();
         TimeAxis timeAxis = EVEPlugin.dc.selectedAxis;
-        graphPolylines.clear();
         if (isVisible) {
             updateWarnLevels(graphArea);
-            EVEValues values = getValuesInInterval(timeAxis, graphArea);
-            final ArrayList<Point> pointList = new ArrayList<Point>();
-
-            int num = values.getNumberOfValues();
-
-            for (int j = 0; j < num; j++) {
-                float value = values.maxValues[j];
-                long date = values.dates[j];
-                int x = timeAxis.value2pixel(graphArea.x, graphArea.width, date);
-                int y = yAxis.value2pixel(graphArea.y, graphArea.height, value);
-
-                if (date > lastMilliWithData) {
-                    lastMilliWithData = date;
-                }
-
-                final Point point = new Point(x, y);
-                pointList.add(point);
-            }
-            graphPolylines.add(new GraphPolyline(pointList, graphColor, graphArea, timeAxis));
+            graphPolylines.clear();
+            createPolyLines(timeAxis);
         }
 
+    }
+
+    private void createPolyLines(TimeAxis timeAxis) {
+        long keyEnd = date2key(timeAxis.end);
+        long key = date2key(timeAxis.start);
+        int level = 0;
+        double factor = 1;
+
+        double elsz = 1. * MILLIS_PER_CHUNK / CHUNKED_SIZE * factor;
+        double noelements = (timeAxis.end - timeAxis.start) / (elsz);
+        double graphWidth = EVEPlugin.dc.getGraphSize().width;
+        while (level < EVEDataOfChunk.MAX_LEVEL - 1 && noelements > graphWidth) {
+            level++;
+            factor *= EVEDataOfChunk.FACTOR_STEP;
+            elsz = 1. * MILLIS_PER_CHUNK / CHUNKED_SIZE * factor;
+            noelements = (timeAxis.end - timeAxis.start) / (elsz);
+        }
+        Rectangle graphArea = EVEPlugin.dc.getGraphArea();
+        ArrayList<Integer> tvalues = new ArrayList<Integer>();
+        ArrayList<Integer> tdates = new ArrayList<Integer>();
+        while (key <= keyEnd) {
+            EVEDataOfChunk cache = cacheMap.get(key);
+            key++;
+            if (cache == null)
+                continue;
+            float[] values = cache.getValues(level);
+            long[] dates = cache.getDates(level);
+            int i = 0;
+            while (i < values.length) {
+                float value = values[i];
+                if (value <= Float.MIN_VALUE && !tvalues.isEmpty()) {
+                    graphPolylines.add(new GraphPolyline(tdates, tvalues));
+                    tvalues.clear();
+                    tdates.clear();
+                } else if (value > Float.MIN_VALUE) {
+                    tdates.add(timeAxis.value2pixel(graphArea.x, graphArea.width, dates[i]));
+                    tvalues.add(yAxis.value2pixel(graphArea.y, graphArea.height, value));
+                }
+                i++;
+            }
+        }
+        if (!tvalues.isEmpty()) {
+            graphPolylines.add(new GraphPolyline(tdates, tvalues));
+        }
     }
 
     private void setWarn(LinkedList<Integer> _warnLevels, LinkedList<String> _warnLabels) {
@@ -184,58 +206,16 @@ public class Band implements LineDataSelectorElement {
 
     private static class GraphPolyline {
 
-        private final ArrayList<ArrayList<Integer>> xPoints;
-        private final ArrayList<ArrayList<Integer>> yPoints;
-        private final ArrayList<int[]> xPointsArray;
-        private final ArrayList<int[]> yPointsArray;
+        public final int[] xPoints;
+        public final int[] yPoints;
 
-        private final Color color;
-
-        public GraphPolyline(final List<Point> points, final Color color, Rectangle graphArea, TimeAxis timeAxis) {
-            xPoints = new ArrayList<ArrayList<Integer>>();
-            yPoints = new ArrayList<ArrayList<Integer>>();
-            xPointsArray = new ArrayList<int[]>();
-            yPointsArray = new ArrayList<int[]>();
-            this.color = color;
-
-            int counter = -1;
-            double localGraphWidth = graphArea.width > 0 ? graphArea.width : 10000;
-            Integer previousX = null;
-            int len = points.size();
-            int jump = (int) (len / localGraphWidth);
-            if (jump == 0) {
-                jump = 1;
-            }
-            int index = 0;
-            while (index < len) {
-                Point point = points.get(index);
-                index += jump;
-                if (point.y < 10E-32) {
-                    continue;
-                }
-                double timediff = 0;
-                if (previousX != null) {
-                    timediff = timeAxis.value2pixel(graphArea.x, graphArea.width, point.x) - timeAxis.value2pixel(graphArea.x, graphArea.width, previousX);
-                }
-                if (previousX == null || timediff > 2 * TimeUtils.MINUTE_IN_MILLIS) {
-                    xPoints.add(new ArrayList<Integer>());
-                    yPoints.add(new ArrayList<Integer>());
-                    counter++;
-                }
-                xPoints.get(counter).add(point.x);
-                yPoints.get(counter).add(point.y);
-                previousX = point.x;
-            }
-
-            for (int i = 0; i < xPoints.size(); i++) {
-                int[] xPointsArr = new int[xPoints.get(i).size()];
-                int[] yPointsArr = new int[yPoints.get(i).size()];
-                for (int j = 0; j < xPoints.get(i).size(); j++) {
-                    xPointsArr[j] = xPoints.get(i).get(j);
-                    yPointsArr[j] = yPoints.get(i).get(j);
-                }
-                xPointsArray.add(xPointsArr);
-                yPointsArray.add(yPointsArr);
+        public GraphPolyline(List<Integer> dates, List<Integer> values) {
+            int llen = dates.size();
+            xPoints = new int[llen];
+            yPoints = new int[llen];
+            for (int j = 0; j < llen; j++) {
+                xPoints[j] = dates.get(j);
+                yPoints[j] = values.get(j);
             }
         }
     }
@@ -271,8 +251,7 @@ public class Band implements LineDataSelectorElement {
 
     private static final double DISCARD_LOG_LEVEL_LOW = 1e-10;
     private static final double DISCARD_LOG_LEVEL_HIGH = 1e+4;
-    private static long DAYS_PER_CHUNK = 10;
-
+    private static long DAYS_PER_CHUNK = 8;
     static final long MILLIS_PER_TICK = 60000;
     static final long CHUNKED_SIZE = TimeUtils.DAY_IN_MILLIS / MILLIS_PER_TICK * DAYS_PER_CHUNK;
     static final long MILLIS_PER_CHUNK = TimeUtils.DAY_IN_MILLIS * DAYS_PER_CHUNK;
@@ -286,54 +265,17 @@ public class Band implements LineDataSelectorElement {
     public void addToCache(final float[] values, final long[] dates) {
         for (int i = 0; i < values.length; i++) {
             long key = date2key(dates[i]);
-
             EVEDataOfChunk cache = cacheMap.get(key);
             if (cache == null) {
                 cache = new EVEDataOfChunk(key);
                 cacheMap.put(key, cache);
             }
             if (values[i] > DISCARD_LOG_LEVEL_LOW && values[i] < DISCARD_LOG_LEVEL_HIGH) {
-                cache.setValue((int) ((dates[i] % (MILLIS_PER_CHUNK)) / MILLIS_PER_TICK), values[i], dates[i]);
+                cache.setValue((int) ((dates[i] % (MILLIS_PER_CHUNK)) / MILLIS_PER_TICK), values[i]);
             }
         }
         updateGraphsData();
-    }
-
-    private EVEValues getValuesInInterval(TimeAxis timeAxis, Rectangle space) {
-        long intervalStart = timeAxis.start;
-        long intervalEnd = timeAxis.end;
-        long intervalWidth = intervalEnd - intervalStart;
-        int spaceWidth = space.width;
-        long binStart;
-        long binEnd;
-
-        int numberOfBins;
-        long timePerBin;
-        if (space.width < intervalWidth / MILLIS_PER_TICK) {
-            binStart = intervalStart - (intervalWidth / spaceWidth / 2);
-            binEnd = intervalEnd + (intervalWidth / spaceWidth / 2);
-            numberOfBins = spaceWidth + 1;
-            timePerBin = intervalWidth / spaceWidth;
-        } else {
-            numberOfBins = (int) (intervalWidth / MILLIS_PER_TICK) + 1;
-            timePerBin = intervalWidth / numberOfBins;
-            binStart = intervalStart - timePerBin / 2;
-            binEnd = intervalEnd + timePerBin / 2;
-        }
-
-        EVEValues result = new EVEValues(binStart, binEnd, intervalStart, numberOfBins, timePerBin);
-
-        long keyEnd = date2key(binEnd);
-        long key = date2key(binStart);
-        while (key <= keyEnd) {
-            EVEDataOfChunk cache = cacheMap.get(key);
-            if (cache != null) {
-                cache.fillResult(result);
-            }
-            key++;
-        }
-
-        return result;
+        EVEPlugin.dc.fireRedrawRequest();
     }
 
 }
--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/lines/data/EVEDataOfChunk.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/lines/data/EVEDataOfChunk.java
@@ -3,30 +3,50 @@ package org.helioviewer.jhv.plugins.eveplugin.lines.data;
 import java.util.Arrays;
 
 public class EVEDataOfChunk {
-
-    private final float[] values = new float[(int) Band.CHUNKED_SIZE];
-    private final long[] dates = new long[(int) Band.CHUNKED_SIZE];
-    private boolean hasData = false;
+    public static int MAX_LEVEL = 12;
+    public static int FACTOR_STEP = 2;
+    private final float[][] values = new float[MAX_LEVEL][];
+    private final long[][] dates = new long[MAX_LEVEL][];
 
     public EVEDataOfChunk(long key) {
-        Arrays.fill(values, Float.NaN);
+        int factor = 1;
+        for (int i = 0; i < MAX_LEVEL; i++) {
+            values[i] = new float[(int) Band.CHUNKED_SIZE / factor];
+            dates[i] = new long[(int) Band.CHUNKED_SIZE / factor];
+            factor = factor * FACTOR_STEP;
+        }
+        for (int j = 0; j < values.length; j++) {
+            Arrays.fill(values[j], Float.MIN_VALUE);
+        }
         long startdate = key * Band.MILLIS_PER_CHUNK;
-        for (int i = 0; i < values.length; i++) {
-            dates[i] = startdate + i * Band.MILLIS_PER_TICK;
+        factor = 1;
+        for (int j = 0; j < values.length; j++) {
+            for (int i = 0; i < values[j].length; i++) {
+                dates[j][i] = startdate + i * Band.MILLIS_PER_TICK * factor;
+            }
+            factor = factor * FACTOR_STEP;
         }
     }
 
-    public void setValue(int minuteOfDay, float value, long date) {
-        values[minuteOfDay] = value;
-        hasData = true;
+    public void setValue(int minuteOfDay, float value) {
+        int factor = 1;
+        for (int i = 0; i < values.length; i++) {
+            int idx = minuteOfDay / factor;
+            if (idx >= values[i].length) {
+                idx = values[i].length - 1;
+            }
+            values[i][idx] = Math.max(values[i][idx], value);
+            factor = factor * FACTOR_STEP;
+        }
+
     }
 
-    public void fillResult(EVEValues result) {
-        result.addValues(dates, values);
+    public float[] getValues(int level) {
+        return values[level];
     }
 
-    public boolean hasData() {
-        return hasData;
+    public long[] getDates(int level) {
+        return dates[level];
     }
 
 }
