c9345b679ebd38e53000d690952b0d133985cc5f
Freek Verstringe <freek.verstringe@oma.be>
2016-05-02 08:46:13 +0000

    change default colors
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7269 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/lines/BandColors.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/lines/BandColors.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 public class BandColors {
 
-    private final static Color[] bandColorArray = { new Color(0, 0, 0), new Color(144, 238, 144), new Color(255, 140, 0), new Color(255, 0, 255), new Color(99, 184, 255), new Color(143, 188, 143), new Color(219, 112, 147), new Color(255, 222, 173), new Color(0, 255, 255), new Color(255, 0, 0), new Color(255, 105, 180), new Color(160, 32, 240), new Color(0, 255, 0), new Color(205, 92, 92), new Color(139, 0, 139), new Color(238, 201, 0), new Color(95, 158, 160), new Color(189, 183, 107), new Color(107, 142, 35), new Color(127, 255, 212), new Color(100, 149, 237), new Color(190, 190, 190), new Color(106, 90, 205), };
+    private final static Color[] bandColorArray = { new Color(80, 80, 80), new Color(204, 51, 0), new Color(255, 0, 255), new Color(0, 120, 28), new Color(99, 184, 255), new Color(143, 188, 143), new Color(219, 112, 147), new Color(255, 222, 173), new Color(0, 255, 255), new Color(255, 0, 0), new Color(255, 105, 180), new Color(160, 32, 240), new Color(0, 255, 0), new Color(205, 92, 92), new Color(139, 0, 139), new Color(238, 201, 0), new Color(95, 158, 160), new Color(189, 183, 107), new Color(107, 142, 35), new Color(127, 255, 212), new Color(100, 149, 237), new Color(190, 190, 190), new Color(106, 90, 205), };
     private final static int[] usedArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     private static int minValue = 0;
 
