50b6c25b06d235ec76a69e9baeb6e9210c9f5c01
Bogdan Nicula <bogdan@nicula.net>
2016-05-02 12:42:01 +0000

    Give video even more bitrate (#12)
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7282 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/export/XuggleExporter.java
+++ src/jhv/src/org/helioviewer/jhv/export/XuggleExporter.java
@@ -15,41 +15,42 @@ import com.xuggle.xuggler.video.IConverter;
 public class XuggleExporter implements MovieExporter {
 
     private static final int MIN_BITRATE = 2000000;
-    private static final double BPP = 0.25;
+    private static final double BPP = 1;
 
     private IMediaWriter movieWriter;
     private IConverter converter;
-    private long position = 0;
-    private long deltat;
+    private long frameNo = 0;
+    private double deltat;
 
     @Override
     public void open(String path, int w, int h, int fps) throws Exception {
+        int bitRate = (int) Math.max(w * h * fps * BPP, MIN_BITRATE);
+        IRational frameRate = IRational.make(fps, 1);
+        deltat = 1e6 / frameRate.getDouble();
+
         movieWriter = ToolFactory.makeWriter(path);
         movieWriter.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, w, h);
 
-        IRational frameRate = IRational.make(1, fps);
-        deltat = (long) (1e6 * frameRate.getDouble());
-        int bitRate = (int) Math.max(w * h * fps * BPP, MIN_BITRATE);
-
         IPixelFormat.Type pixFmt = IPixelFormat.Type.YUV420P;
+        converter = ConverterFactory.createConverter(ConverterFactory.XUGGLER_BGR_24, pixFmt, w, h);
+
         IStreamCoder coder = movieWriter.getContainer().getStream(0).getStreamCoder();
-        coder.setTimeBase(frameRate);
+        coder.setBitRate(bitRate);
+        coder.setFrameRate(frameRate);
+        coder.setTimeBase(IRational.make(frameRate.getDenominator(), frameRate.getNumerator()));
         coder.setFlag(IStreamCoder.Flags.FLAG_QSCALE, true);
         coder.setGlobalQuality(0);
-        coder.setBitRate(bitRate);
-        // coder.setNumPicturesInGroupOfPictures(fps * 10);
+        // coder.setNumPicturesInGroupOfPictures(fps);
         coder.setPixelType(pixFmt);
-
-        converter = ConverterFactory.createConverter(ConverterFactory.XUGGLER_BGR_24, pixFmt, w, h);
     }
 
     @Override
     public void encode(BufferedImage im) throws Exception {
-        IVideoPicture frame = converter.toPicture(im, position);
+        IVideoPicture frame = converter.toPicture(im, (long) (frameNo * deltat));
         frame.setQuality(0);
 
         movieWriter.encodeVideo(0, frame);
-        position += deltat;
+        frameNo++;
     }
 
     @Override
