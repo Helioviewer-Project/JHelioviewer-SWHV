34a4202c69bb72945e5ef406f31f67d5f83a906a
Freek Verstringe <freek.verstringe@oma.be>
2016-05-12 11:03:30 +0000

    adapt request cache
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7396 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/base/cache/RequestCache.java
+++ src/jhv/src/org/helioviewer/jhv/base/cache/RequestCache.java
@@ -1,31 +1,27 @@
 package org.helioviewer.jhv.base.cache;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import org.helioviewer.jhv.base.interval.Interval;
 
 public class RequestCache {
 
-    private final Map<Long, Interval> requestedAndDownloadedCache;
-    private final Map<Long, Interval> requestedCache;
+    private ArrayList<Interval> cache;
 
     public RequestCache() {
-        requestedAndDownloadedCache = new TreeMap<Long, Interval>();
-        requestedCache = new TreeMap<Long, Interval>();
+        cache = new ArrayList<Interval>();
     }
 
     public List<Interval> adaptRequestCache(long startDate, long endDate) {
         ArrayList<Interval> missingIntervals = new ArrayList<Interval>();
         Interval interval = new Interval(startDate, endDate);
 
-        if (requestedAndDownloadedCache.isEmpty()) {
+        if (cache.isEmpty()) {
             missingIntervals.add(interval);
-            requestedAndDownloadedCache.put(startDate, interval);
-            requestedCache.put(startDate, interval);
+            cache.add(interval);
         } else {
             missingIntervals = getMissingIntervals(interval.start, interval.end);
             updateRequestCache(startDate, endDate);
@@ -34,278 +30,94 @@ public class RequestCache {
     }
 
     private void updateRequestCache(final long startDate, final long endDate) {
-        List<Long> intervalsToRemove = new ArrayList<Long>();
-        long addStart = startDate;
-        long addEnd = endDate;
-
-        Interval previousInterval = null;
-        boolean startFound = false;
-        boolean endFound = false;
-
-        for (Map.Entry<Long, Interval> entry : requestedAndDownloadedCache.entrySet()) {
-            long iStartDate = entry.getKey();
-            Interval rcInterval = entry.getValue();
-
-            // define start
-            if (!startFound) {
-                if (startDate < iStartDate) {
-                    if (previousInterval == null) {
-                        startFound = true;
-                        previousInterval = rcInterval;
-                    } else {
-                        // There was a previous interval. Check if start lies
-                        // within previous interval
-                        if (previousInterval.containsPointInclusive(startDate)) {
-                            addStart = previousInterval.start;
-                            if (previousInterval.containsPointInclusive(endDate)) {
-                                addEnd = previousInterval.end;
-                                endFound = true;
-                                break;
-                            } else {
-                                if (endDate < iStartDate) {
-                                    addEnd = endDate;
-                                    break;
-                                } else {
-                                    intervalsToRemove.add(iStartDate);
-                                    previousInterval = rcInterval;
-                                }
-                            }
-                            startFound = true;
-                        } else {
-                            addStart = startDate;
-                            startFound = true;
-                            if (endDate < iStartDate) {
-                                addEnd = endDate;
-                                endFound = true;
-                                break;
-                            }
-                            previousInterval = rcInterval;
-                        }
-                    }
-                } else {
-                    previousInterval = rcInterval;
-                }
-            } else {
-                // define end
-                if (endDate < previousInterval.start) {
-                    endFound = true;
-                    break;
-                } else {
-                    if (previousInterval.containsPointInclusive(endDate)) {
-                        intervalsToRemove.add(previousInterval.start);
-                        addEnd = previousInterval.end;
-                        endFound = true;
-                        break;
-                    } else {
-                        intervalsToRemove.add(previousInterval.start);
-                        previousInterval = rcInterval;
-                        continue;
-                    }
-                }
-            }
-        }
-        if (!startFound && previousInterval.containsPointInclusive(startDate)) {
-            if (!previousInterval.containsPointInclusive(endDate)) {
-                intervalsToRemove.add(previousInterval.start);
-                addStart = previousInterval.start;
-            } else {
-                addStart = previousInterval.start;
-                addEnd = previousInterval.end;
-                endFound = true;
-            }
-        }
-        if (!endFound) {
-            if (endDate < previousInterval.start) {
-                endFound = true;
-            } else {
-                if (previousInterval.containsPointInclusive(endDate)) {
-                    intervalsToRemove.add(previousInterval.start);
-                    addEnd = previousInterval.end;
-                } else {
-                    if (startDate <= previousInterval.end) {
-                        intervalsToRemove.add(previousInterval.start);
-                    }
-                }
-            }
-        }
-        for (Long toRemove : intervalsToRemove) {
-            requestedAndDownloadedCache.remove(toRemove);
-            requestedCache.remove(toRemove);
-        }
-        Interval intervalToAdd = new Interval(addStart, addEnd);
-        requestedAndDownloadedCache.put(intervalToAdd.start, intervalToAdd);
-        requestedCache.put(intervalToAdd.start, intervalToAdd);
+        cache.add(new Interval(startDate, endDate));
+        cache = merge(cache);
     }
 
-    public void removeRequestedIntervals(Interval remInterval) {
-        List<Interval> intervalsToAdd = new ArrayList<Interval>();
-        List<Long> intervalsToRemove = new ArrayList<Long>();
-        long start = remInterval.start;
-
-        for (Map.Entry<Long, Interval> entry : requestedAndDownloadedCache.entrySet()) {
-            Long isDate = entry.getKey();
-            Interval rcInterval = entry.getValue();
-
-            if (start <= rcInterval.start) {
-                if (remInterval.end > rcInterval.start) {
-                    intervalsToRemove.add(isDate);
-                    if (remInterval.end <= rcInterval.end) {
-                        if (start != rcInterval.start) {
-                            intervalsToAdd.add(new Interval(remInterval.end, rcInterval.end));
-                        }
-                        break;
-                    } else {
-                        start = rcInterval.end;
-                    }
-                }
-            } else {
-                if (rcInterval.end > start) {
-                    intervalsToRemove.add(isDate);
-                    if (remInterval.end < rcInterval.end) {
-                        intervalsToAdd.add(new Interval(rcInterval.start, start));
-                        intervalsToAdd.add(new Interval(remInterval.end, rcInterval.end));
-                        break;
-                    } else {
-                        intervalsToAdd.add(new Interval(rcInterval.start, start));
-                        start = rcInterval.end;
-                    }
-                }
-            }
-        }
-
-        for (Long date : intervalsToRemove) {
-            requestedAndDownloadedCache.remove(date);
-            requestedCache.remove(date);
-        }
-        for (Interval intToAdd : intervalsToAdd) {
-            requestedAndDownloadedCache.put(intToAdd.start, intToAdd);
-            requestedCache.put(intToAdd.start, intToAdd);
-        }
+    public void removeRequestedIntervals(Interval ri) {
+        cache = remove(cache, ri);
     }
 
-    public Collection<Interval> getAllRequestIntervals() {
-        return requestedCache.values();
+    public ArrayList<Interval> getAllRequestIntervals() {
+        return cache;
+    }
+
+    private static ArrayList<Interval> getInvertedCache(ArrayList<Interval> toInvert) {
+        ArrayList<Interval> newCache = new ArrayList<Interval>();
+        int len = toInvert.size();
+        if (len == 0) {
+            newCache.add(new Interval(Long.MIN_VALUE, Long.MAX_VALUE));
+            return newCache;
+        }
+        int i = 0;
+        Interval interval;
+        long currend;
+
+        interval = toInvert.get(0);
+        if (Long.MIN_VALUE != interval.start) {
+            newCache.add(new Interval(Long.MIN_VALUE, interval.start));
+        }
+        currend = interval.end;
+        i++;
+
+        while (i < len) {
+            interval = toInvert.get(i);
+            newCache.add(new Interval(currend, interval.start));
+            currend = interval.end;
+            i++;
+        }
+
+        if (currend != Long.MAX_VALUE) {
+            newCache.add(new Interval(currend, Long.MAX_VALUE));
+        }
+        return newCache;
+    }
+
+    private static ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+        if (intervals == null || intervals.size() <= 1)
+            return intervals;
+
+        Collections.sort(intervals, new IntervalComparator());
+        ArrayList<Interval> result = new ArrayList<Interval>();
+        Interval prev = intervals.get(0);
+        for (int i = 1; i < intervals.size(); i++) {
+            Interval curr = intervals.get(i);
+            if (prev.end >= curr.start) {
+                Interval merged = new Interval(prev.start, Math.max(prev.end, curr.end));
+                prev = merged;
+            } else {
+                result.add(prev);
+                prev = curr;
+            }
+        }
+        result.add(prev);
+        return result;
+    }
+
+    private static ArrayList<Interval> remove(ArrayList<Interval> cache, Interval ri) {
+        ArrayList<Interval> icache = getInvertedCache(cache);
+        icache.add(ri);
+        icache = merge(icache);
+        return getInvertedCache(icache);
     }
 
     public ArrayList<Interval> getMissingIntervals(long start, long end) {
-        ArrayList<Interval> missingIntervals = new ArrayList<Interval>();
-        long startDate = start;
-        long endDate = end;
-        long currentStartDate = start;
-        boolean endDateUsed = false;
-        if (requestedAndDownloadedCache.isEmpty()) {
-            missingIntervals.add(new Interval(startDate, endDate));
-        } else {
-            Interval previousInterval = null;
+        ArrayList<Interval> localCache = getInvertedCache(cache);
+        localCache = remove(localCache, new Interval(Long.MIN_VALUE, start));
+        localCache = remove(localCache, new Interval(end, Long.MAX_VALUE));
+        return localCache;
+    }
 
-            for (Map.Entry<Long, Interval> entry : requestedAndDownloadedCache.entrySet()) {
-                long iStartDate = entry.getKey();
-                Interval rcInterval = entry.getValue();
-
-                if (currentStartDate < iStartDate) {
-                    if (previousInterval == null) {
-                        // No previous interval check if endate is also before
-                        // startdate
-                        if (endDate < iStartDate) {
-                            // complete new interval
-                            missingIntervals.add(new Interval(startDate, endDate));
-                            previousInterval = rcInterval;
-                            break;
-                        } else {
-                            // overlapping interval => missing interval =
-                            // {startDate, iStartDate}
-                            // continue with interval = {iStartDate, endDate}
-                            currentStartDate = iStartDate;
-                            missingIntervals.add(new Interval(startDate, iStartDate));
-                            previousInterval = rcInterval;
-                            continue;
-                        }
-                    } else {
-                        // 1) start time before or equal previous end time
-                        // 2) start time after previous end time
-                        if (previousInterval.containsPointInclusive(currentStartDate)) {
-                            // 1)
-                            // look at end time
-                            // 1) end time before or equal previous end time:
-                            // internal interval => do nothing break.
-                            // 2) end time after previous end time : partial
-                            // overlapping internal continue with interval
-                            // {previousendtime, endtime}
-
-                            if (previousInterval.containsPointInclusive(endDate)) {
-                                // 1))
-                                break;
-                            } else {
-                                if (endDate < iStartDate) {
-                                    missingIntervals.add(new Interval(previousInterval.end, endDate));
-                                    endDateUsed = true;
-                                    break;
-                                } else {
-                                    missingIntervals.add(new Interval(previousInterval.end, iStartDate));
-                                    currentStartDate = iStartDate;
-                                }
-                                previousInterval = rcInterval;
-                                continue;
-                            }
-
-                        } else {
-                            // 2)
-                            // look at end time
-                            // 1) endDate before or equal current start time:
-                            // missing interval: {previousendtime, enddate}
-                            // 2) endDate after current start time : missing
-                            // interval: {previous end date, current start
-                            // date}, continue with interval: {current start
-                            // time, end time}
-                            if (!previousInterval.containsPointInclusive(endDate)) {
-                                if (endDate - iStartDate <= 0) {
-                                    // 1)
-                                    if (currentStartDate > previousInterval.end) {
-                                        missingIntervals.add(new Interval(currentStartDate, endDate));
-                                    } else {
-                                        missingIntervals.add(new Interval(previousInterval.end, endDate));
-                                    }
-                                    endDateUsed = true;
-                                    break;
-                                } else {
-                                    // 2)
-                                    missingIntervals.add(new Interval(currentStartDate, iStartDate));
-                                    previousInterval = rcInterval;
-                                    currentStartDate = iStartDate;
-                                    continue;
-                                }
-                            } else {
-                                endDateUsed = true;
-                                break;
-                            }
-                        }
-                    }
-                } else {
-                    previousInterval = rcInterval;
-                }
-            }
-            // check if current start date is after or equal previous (last
-            // interval) start date
-            if (!endDateUsed && (currentStartDate >= previousInterval.start)) {
-                // Check if start date is after end date of previous (last)
-                // interval
-                // 1) true: missing interval : {currentStartDate, endDate}
-                // 2) false: check end date
-                if (currentStartDate >= previousInterval.end) {
-                    // 1)
-                    missingIntervals.add(new Interval(currentStartDate, endDate));
-                } else {
-                    // 2)
-                    // 1) endDate after previous end date: missing interval =
-                    // {previousenddate, endDate}
-                    // 2) internal interval do nothing
-                    if (endDate > previousInterval.end) {
-                        missingIntervals.add(new Interval(previousInterval.end, endDate));
-                    }
-                }
-            }
+    private static class IntervalComparator implements Comparator<Interval> {
+        @Override
+        public int compare(Interval i1, Interval i2) {
+            if (i1.start > i2.start)
+                return 1;
+            else if (i1.start == i2.start)
+                return 0;
+            else
+                return -1;
         }
-        return missingIntervals;
     }
 
 }
