a0da048bcbe70404fb95f3283966cf51c9fdde1c
Bogdan Nicula <bogdan@nicula.net>
2016-05-20 17:55:00 +0000

    Prepare for streaming
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7476 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/export/JCodecExporter.java
+++ src/jhv/src/org/helioviewer/jhv/export/JCodecExporter.java
@@ -3,14 +3,20 @@ package org.helioviewer.jhv.export;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
+import org.helioviewer.jhv.base.FileUtils;
 import org.jcodec.api.awt.AWTSequenceEncoder8Bit;
+import org.jcodec.containers.mp4.MP4Util;
+import org.jcodec.containers.mp4.boxes.MovieBox;
+import org.jcodec.movtool.Flattern;
 
 public class JCodecExporter implements MovieExporter {
 
     private AWTSequenceEncoder8Bit encoder;
+    private String path;
 
     @Override
     public void open(String path, int w, int h, int fps) throws Exception {
+        this.path = path;
         encoder = AWTSequenceEncoder8Bit.createSequenceEncoder8Bit(new File(path), fps);
         encoder.getEncoder().setKeyInterval(fps);
     }
@@ -23,6 +29,17 @@ public class JCodecExporter implements MovieExporter {
     @Override
     public void close() throws Exception {
         encoder.finish();
+        prepareStream();
+    }
+
+    private void prepareStream() throws Exception {
+        File orig = new File(path);
+        MovieBox movie = MP4Util.createRefMovieFromFile(orig);
+
+        File optim = new File(path + "_optim");
+        new Flattern().flattern(movie, optim);
+        FileUtils.copy(optim, orig); // renameTo doesn't work on Windows
+        optim.delete();
     }
 
 }
