60109d5f91aa153185ef4959fc46dfbde5231fa3
Bogdan Nicula <bogdan@nicula.net>
2016-06-09 23:07:14 +0000

    Split-off icoSphere
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7661 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/layers/ImageLayer.java
+++ src/jhv/src/org/helioviewer/jhv/layers/ImageLayer.java
@@ -4,9 +4,8 @@ import java.awt.Component;
 import java.awt.geom.Rectangle2D;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
-import java.util.ArrayList;
 
-import org.helioviewer.jhv.base.Pair;
+import org.helioviewer.jhv.base.math.IcoSphere;
 import org.helioviewer.jhv.base.math.Mat4;
 import org.helioviewer.jhv.base.math.Quat;
 import org.helioviewer.jhv.base.math.Vec3;
@@ -17,7 +16,6 @@ import org.helioviewer.jhv.camera.CameraHelper;
 import org.helioviewer.jhv.display.Displayer;
 import org.helioviewer.jhv.display.Viewport;
 import org.helioviewer.jhv.gui.ImageViewerGui;
-import org.helioviewer.jhv.layers.Layers;
 import org.helioviewer.jhv.opengl.GLImage;
 import org.helioviewer.jhv.opengl.GLSLSolarShader;
 import org.helioviewer.jhv.opengl.GLText;
@@ -46,8 +44,6 @@ public class ImageLayer extends AbstractRenderable implements ImageDataHandler {
     private static final double vpScale = 0.035;
     private static final String loading = "Loading...";
 
-    private static final Pair<FloatBuffer, IntBuffer> bufferPair = makeIcosphere(2);
-
     public ImageLayer(JHVWorker<?, ?> _worker) {
         worker = _worker;
         setVisible(true);
@@ -57,8 +53,8 @@ public class ImageLayer extends AbstractRenderable implements ImageDataHandler {
     public void init(GL2 gl) {
         glImage.init(gl);
 
-        FloatBuffer positionBuffer = bufferPair.a;
-        IntBuffer indexBuffer = bufferPair.b;
+        FloatBuffer positionBuffer = IcoSphere.IcoSphere.a;
+        IntBuffer indexBuffer = IcoSphere.IcoSphere.b;
 
         positionBufferID = generate(gl);
         int positionBufferSize = positionBuffer.capacity();
@@ -268,122 +264,6 @@ public class ImageLayer extends AbstractRenderable implements ImageDataHandler {
         gl.glDeleteBuffers(1, new int[] { indexBufferID }, 0);
     }
 
-    private static Pair<FloatBuffer, IntBuffer> makeIcosphere(int level) {
-        float t = (float) ((Math.sqrt(5) - 1) / 2);
-        float[][] icosahedronVertexList = new float[][] { new float[] { -1, -t, 0 }, new float[] { 0, 1, t }, new float[] { 0, 1, -t }, new float[] { 1, t, 0 }, new float[] { 1, -t, 0 }, new float[] { 0, -1, -t }, new float[] { 0, -1, t }, new float[] { t, 0, 1 }, new float[] { -t, 0, 1 }, new float[] { t, 0, -1 }, new float[] { -t, 0, -1 }, new float[] { -1, t, 0 }, };
-        for (float[] v : icosahedronVertexList) {
-            float length = (float) Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-            v[0] /= length;
-            v[1] /= length;
-            v[2] /= length;
-        }
-        int[][] icosahedronFaceList = new int[][] { { 3, 7, 1 }, { 4, 7, 3 }, { 6, 7, 4 }, { 8, 7, 6 }, { 7, 8, 1 }, { 9, 4, 3 }, { 2, 9, 3 }, { 2, 3, 1 }, { 11, 2, 1 }, { 10, 2, 11 }, { 10, 9, 2 }, { 9, 5, 4 }, { 6, 4, 5 }, { 0, 6, 5 }, { 0, 11, 8 }, { 11, 1, 8 }, { 10, 0, 5 }, { 10, 5, 9 }, { 0, 8, 6 }, { 0, 10, 11 }, };
-        ArrayList<Float> vertices = new ArrayList<Float>();
-        ArrayList<Integer> faceIndices = new ArrayList<Integer>();
-        for (float[] v : icosahedronVertexList) {
-            vertices.add(v[0]);
-            vertices.add(v[2]);
-            vertices.add(v[1]);
-        }
-        for (int[] f : icosahedronFaceList) {
-            subdivide(f[0], f[1], f[2], vertices, faceIndices, level);
-        }
-        int beginPositionNumberCorona = vertices.size() / 3;
-        float r = 40.f;
-        vertices.add(-r);
-        vertices.add(r);
-        vertices.add(0f);
-
-        vertices.add(r);
-        vertices.add(r);
-        vertices.add(0f);
-
-        vertices.add(r);
-        vertices.add(-r);
-        vertices.add(0f);
-
-        vertices.add(-r);
-        vertices.add(-r);
-        vertices.add(0f);
-
-        faceIndices.add(beginPositionNumberCorona);
-        faceIndices.add(beginPositionNumberCorona + 2);
-        faceIndices.add(beginPositionNumberCorona + 1);
-
-        faceIndices.add(beginPositionNumberCorona + 2);
-        faceIndices.add(beginPositionNumberCorona);
-        faceIndices.add(beginPositionNumberCorona + 3);
-
-        FloatBuffer positionBuffer = FloatBuffer.allocate(vertices.size());
-        for (Float vert : vertices) {
-            if (vert == 0f)
-                vert = Math.nextAfter(vert, vert + 1.0f);
-            positionBuffer.put(vert);
-        }
-        positionBuffer.flip();
-
-        IntBuffer indexBuffer = IntBuffer.allocate(faceIndices.size());
-        for (int i : faceIndices) {
-            indexBuffer.put(i);
-        }
-        indexBuffer.flip();
-
-        return new Pair<FloatBuffer, IntBuffer>(positionBuffer, indexBuffer);
-    }
-
-    private static void subdivide(int vx, int vy, int vz, ArrayList<Float> vertexList, ArrayList<Integer> faceList, int level) {
-        if (level != 0) {
-            float x1 = vertexList.get(3 * vx) + vertexList.get(3 * vy);
-            float y1 = vertexList.get(3 * vx + 1) + vertexList.get(3 * vy + 1);
-            float z1 = vertexList.get(3 * vx + 2) + vertexList.get(3 * vy + 2);
-            float length = (float) Math.sqrt(x1 * x1 + y1 * y1 + z1 * z1);
-            x1 /= length;
-            y1 /= length;
-            z1 /= length;
-            int firstIndex = vertexList.size() / 3;
-            vertexList.add(x1);
-            vertexList.add(y1);
-            vertexList.add(z1);
-
-            float x2 = vertexList.get(3 * vz) + vertexList.get(3 * vy);
-            float y2 = vertexList.get(3 * vz + 1) + vertexList.get(3 * vy + 1);
-            float z2 = vertexList.get(3 * vz + 2) + vertexList.get(3 * vy + 2);
-            length = (float) Math.sqrt(x2 * x2 + y2 * y2 + z2 * z2);
-            x2 /= length;
-            y2 /= length;
-            z2 /= length;
-            int secondIndex = vertexList.size() / 3;
-            vertexList.add(x2);
-            vertexList.add(y2);
-            vertexList.add(z2);
-
-            float x3 = vertexList.get(3 * vx) + vertexList.get(3 * vz);
-            float y3 = vertexList.get(3 * vx + 1) + vertexList.get(3 * vz + 1);
-            float z3 = vertexList.get(3 * vx + 2) + vertexList.get(3 * vz + 2);
-            length = (float) Math.sqrt(x3 * x3 + y3 * y3 + z3 * z3);
-            x3 /= length;
-            y3 /= length;
-            z3 /= length;
-            int thirdIndex = vertexList.size() / 3;
-            vertexList.add(x3);
-            vertexList.add(y3);
-            vertexList.add(z3);
-
-            subdivide(vx, firstIndex, thirdIndex, vertexList, faceList, level - 1);
-            subdivide(firstIndex, vy, secondIndex, vertexList, faceList, level - 1);
-            subdivide(thirdIndex, secondIndex, vz, vertexList, faceList, level - 1);
-            subdivide(firstIndex, secondIndex, thirdIndex, vertexList, faceList, level - 1);
-        } else {
-            faceList.add(vx);
-            faceList.add(vy);
-            faceList.add(vz);
-        }
-    }
-
-    public void setOpacity(float opacity) {
-        optionsPanel.setOpacity(opacity);
-    }
-
     @Override
     public Component getOptionsPanel() {
         return optionsPanel;
@@ -460,6 +340,10 @@ public class ImageLayer extends AbstractRenderable implements ImageDataHandler {
         setImageData(imageData);
         ImageViewerGui.getRenderableContainer().fireTimeUpdated(this);
         Displayer.display();
+    }
+
+    public void setOpacity(float opacity) {
+        optionsPanel.setOpacity(opacity);
     }
 
     GLImage getGLImage() {
