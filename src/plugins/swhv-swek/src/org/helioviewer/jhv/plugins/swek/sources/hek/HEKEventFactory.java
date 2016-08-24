57e8c61c453f6edab90d1e7e06ae9d90901170b2
Freek Verstringe <freek.verstringe@oma.be>
2016-08-24 14:13:03 +0000

    simplify
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7875 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKEventFactory.java
+++ src/plugins/swhv-swek/src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKEventFactory.java
@@ -2,30 +2,24 @@ package org.helioviewer.jhv.plugins.swek.sources.hek;
 
 /**
  * Factory creates a HEKEvent from a String.
- * 
+ *
  * @author Bram Bourgoignie (Bram.Bourgoignie@oma.be)
- * 
+ *
  */
 public class HEKEventFactory {
     /**
      * Creates a HEK event from a String.
-     * 
+     *
      * @param eventType
      *            The string description of the event
      * @return The HEKEvent if known or the unknown hek event.
      */
     public static HEKEventEnum getHEKEvent(String eventType) {
-        HEKEventEnum unknown = HEKEventEnum.UNKNOWN;
-        Object[] possibleEvents = unknown.getDeclaringClass().getEnumConstants();
-        for (Object hekEvent : possibleEvents) {
-            if (hekEvent instanceof HEKEventEnum) {
-                HEKEventEnum event = (HEKEventEnum) hekEvent;
-                if (event.getSWEKEventName().equals(eventType)) {
-                    return event;
-                }
+        for (HEKEventEnum event : HEKEventEnum.values()) {
+            if (event.getSWEKEventName().equals(eventType)) {
+                return event;
             }
         }
-
         return HEKEventEnum.UNKNOWN;
     }
 
