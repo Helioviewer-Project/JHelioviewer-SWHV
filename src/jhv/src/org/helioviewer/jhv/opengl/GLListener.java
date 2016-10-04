3f174794e17217815308bca808e46f4639490f17
Bogdan Nicula <bogdan@nicula.net>
2016-10-04 10:28:03 +0000

    Put render back
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@7914 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/opengl/GLListener.java
+++ src/jhv/src/org/helioviewer/jhv/opengl/GLListener.java
@@ -115,6 +115,7 @@ public class GLListener implements GLEventListener {
                 Displayer.setGLSize(x, y, width, height);
                 Displayer.reshapeAll();
                 ImageViewerGui.getRenderableMiniview().reshapeViewport();
+                Displayer.render(1);
             }
         });
     }
