70305a20845ad2986c36c5c64931f1026769a5a0
Bram Bourgoignie <bram.bourgoignie@oma.be>
2017-03-01 08:56:36 +0000

    Move stuff from eveplugin to temporary timelines plugin
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@8618 b4e469a2-07ce-4b26-9273-4d7d95a670c7

---
+++ src/jhv/src/org/helioviewer/jhv/plugins/eveplugin/Timelines.java
@@ -0,0 +1,97 @@
+package org.helioviewer.jhv.plugins.eveplugin;
+
+import java.util.LinkedList;
+
+import javax.swing.JComponent;
+
+import org.helioviewer.jhv.base.plugin.interfaces.Plugin;
+import org.helioviewer.jhv.data.cache.JHVRelatedEvents;
+import org.helioviewer.jhv.gui.ImageViewerGui;
+import org.helioviewer.jhv.gui.interfaces.MainContentPanelPlugin;
+import org.helioviewer.jhv.layers.Layers;
+import org.helioviewer.jhv.plugins.eveplugin.draw.DrawController;
+import org.helioviewer.jhv.plugins.eveplugin.view.TimelineDialog;
+import org.helioviewer.jhv.plugins.eveplugin.view.chart.PlotPanel;
+import org.helioviewer.jhv.plugins.eveplugin.view.linedataselector.LineDataSelectorModel;
+import org.helioviewer.jhv.plugins.eveplugin.view.linedataselector.LineDataSelectorTablePanel;
+
+public class Timelines implements Plugin, MainContentPanelPlugin {
+
+    public static final LineDataSelectorModel ldsm = new LineDataSelectorModel();
+    private static final DrawController dc = new DrawController();
+    public static final TimelineDialog td = new TimelineDialog();
+    private final LinkedList<JComponent> pluginPanes = new LinkedList<>();
+    private final PlotPanel plotOne = new PlotPanel();
+    private static final LineDataSelectorTablePanel timelinePluginPanel = new LineDataSelectorTablePanel();
+
+    public Timelines() {
+        LineDataSelectorModel.addLineDataSelectorModelListener(dc);
+        LineDataSelectorModel.addLineDataSelectorModelListener(td.getObservationPanel());
+    }
+
+    @Override
+    public String getTabName() {
+        return "Timelines";
+    }
+
+    @Override
+    public LinkedList<JComponent> getVisualInterfaces() {
+        return pluginPanes;
+    }
+
+    @Override
+    public String getName() {
+        return "Timelines Plugin";
+    }
+
+    @Override
+    public String getDescription() {
+        return "This plugin visualizes 1D and 2D time series";
+    }
+
+    @Override
+    public void installPlugin() {
+        pluginPanes.add(plotOne);
+
+        ImageViewerGui.getLeftContentPane().add("Timeline Layers", timelinePluginPanel, true);
+        ImageViewerGui.getLeftContentPane().revalidate();
+
+        ImageViewerGui.getMainContentPanel().addPlugin(this);
+
+        // em.fetchData(DrawController.selectedAxis);
+
+        Layers.addLayersListener(dc);
+        Layers.addTimeListener(dc);
+        Layers.addTimespanListener(dc);
+        JHVRelatedEvents.addHighlightListener(dc);
+    }
+
+    @Override
+    public void uninstallPlugin() {
+        JHVRelatedEvents.removeHighlightListener(dc);
+        Layers.removeTimespanListener(dc);
+        Layers.removeTimeListener(dc);
+        Layers.removeLayersListener(dc);
+
+        ImageViewerGui.getMainContentPanel().removePlugin(this);
+
+        ImageViewerGui.getLeftContentPane().remove(timelinePluginPanel);
+        ImageViewerGui.getLeftContentPane().revalidate();
+        pluginPanes.remove(plotOne);
+    }
+
+    @Override
+    public void setState(String state) {
+    }
+
+    @Override
+    public String getState() {
+        return null;
+    }
+
+    @Override
+    public String getAboutLicenseText() {
+        return "Mozilla Public License Version 2.0";
+    }
+
+}
--- src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/EVEPlugin.java
+++ src/plugins/jhv-eveplugin/src/org/helioviewer/jhv/plugins/eveplugin/EVEPlugin.java
@@ -6,18 +6,11 @@ import java.util.concurrent.ExecutorService;
 import javax.swing.JComponent;
 
 import org.helioviewer.jhv.base.plugin.interfaces.Plugin;
-import org.helioviewer.jhv.data.cache.JHVRelatedEvents;
-import org.helioviewer.jhv.gui.ImageViewerGui;
 import org.helioviewer.jhv.gui.interfaces.MainContentPanelPlugin;
-import org.helioviewer.jhv.layers.Layers;
-import org.helioviewer.jhv.plugins.eveplugin.draw.DrawController;
 import org.helioviewer.jhv.plugins.eveplugin.events.EventModel;
 import org.helioviewer.jhv.plugins.eveplugin.lines.BandTypeAPI;
 import org.helioviewer.jhv.plugins.eveplugin.radio.RadioData;
-import org.helioviewer.jhv.plugins.eveplugin.view.TimelineDialog;
-import org.helioviewer.jhv.plugins.eveplugin.view.chart.PlotPanel;
 import org.helioviewer.jhv.plugins.eveplugin.view.linedataselector.LineDataSelectorModel;
-import org.helioviewer.jhv.plugins.eveplugin.view.linedataselector.LineDataSelectorTablePanel;
 import org.helioviewer.jhv.threads.JHVExecutor;
 import org.helioviewer.jhv.threads.JHVWorker;
 
@@ -26,39 +19,16 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
     private static final int MAX_WORKER_THREADS = 12;
     public static final ExecutorService executorService = JHVExecutor.getJHVWorkersExecutorService("EVE", MAX_WORKER_THREADS);
 
-    private final LinkedList<JComponent> pluginPanes = new LinkedList<>();
-    private final PlotPanel plotOne = new PlotPanel();
-
-    public static final LineDataSelectorModel ldsm = new LineDataSelectorModel();
-    private static final DrawController dc = new DrawController();
     public static final RadioData rd = new RadioData();
     public static final EventModel em = new EventModel();
-    public static final TimelineDialog td = new TimelineDialog();
-
-    private static final LineDataSelectorTablePanel timelinePluginPanel = new LineDataSelectorTablePanel();
 
     public EVEPlugin() {
-        LineDataSelectorModel.addLineDataSelectorModelListener(dc);
-        LineDataSelectorModel.addLineDataSelectorModelListener(td.getObservationPanel());
         LineDataSelectorModel.addLineData(rd);
         LineDataSelectorModel.addLineData(em);
     }
 
     @Override
     public void installPlugin() {
-        pluginPanes.add(plotOne);
-
-        ImageViewerGui.getLeftContentPane().add("Timeline Layers", timelinePluginPanel, true);
-        ImageViewerGui.getLeftContentPane().revalidate();
-
-        ImageViewerGui.getMainContentPanel().addPlugin(this);
-
-        Layers.addLayersListener(dc);
-        Layers.addTimeListener(dc);
-        Layers.addTimespanListener(dc);
-        JHVRelatedEvents.addHighlightListener(dc);
-
-        // em.fetchData(DrawController.selectedAxis);
 
         JHVWorker<Void, Void> loadSources = new JHVWorker<Void, Void>() {
 
@@ -70,7 +40,7 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
 
             @Override
             protected void done() {
-                td.getObservationPanel().setupDatasets();
+                Timelines.td.getObservationPanel().setupDatasets();
             }
 
         };
@@ -81,21 +51,11 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
 
     @Override
     public void uninstallPlugin() {
-        JHVRelatedEvents.removeHighlightListener(dc);
-        Layers.removeTimespanListener(dc);
-        Layers.removeTimeListener(dc);
-        Layers.removeLayersListener(dc);
-
-        ImageViewerGui.getMainContentPanel().removePlugin(this);
-
-        ImageViewerGui.getLeftContentPane().remove(timelinePluginPanel);
-        ImageViewerGui.getLeftContentPane().revalidate();
-        pluginPanes.remove(plotOne);
     }
 
     @Override
     public String getName() {
-        return "Timeline Plugin";
+        return "Timelines, callisto and event displayer Plugin";
     }
 
     @Override
@@ -110,12 +70,12 @@ public class EVEPlugin implements Plugin, MainContentPanelPlugin {
 
     @Override
     public LinkedList<JComponent> getVisualInterfaces() {
-        return pluginPanes;
+        return null;
     }
 
     @Override
     public String getTabName() {
-        return "Timelines";
+        return "Timelines, callisto and event displayer";
     }
 
     @Override
