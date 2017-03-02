fae7e11ad8ef487f48e7a8bbb7fea617810a93bf
Bogdan Nicula <bogdan@nicula.net>
2017-03-02 10:38:43 +0000

    Remove ODDM crap
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@8674 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/gui/dialogs/model/ObservationDialogDateModelListener.java
+++
@@ -1,9 +0,0 @@
-package org.helioviewer.jhv.gui.dialogs.model;
-
-public interface ObservationDialogDateModelListener {
-
-    void startTimeChanged(long startTime);
-
-    void endTimeChanged(long endTime);
-
-}
