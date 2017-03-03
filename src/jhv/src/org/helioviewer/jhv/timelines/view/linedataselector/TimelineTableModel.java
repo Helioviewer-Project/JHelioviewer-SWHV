e1fb4afd05f093a5a1450807185c54258866813c
Bogdan Nicula <bogdan@nicula.net>
2017-03-03 10:48:53 +0000

    Derive from AbstractTableModel
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@8702 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/timelines/view/linedataselector/TimelineTableModel.java
+++ src/jhv/src/org/helioviewer/jhv/timelines/view/linedataselector/TimelineTableModel.java
@@ -1,20 +1,21 @@
 package org.helioviewer.jhv.timelines.view.linedataselector;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-import javax.swing.table.TableModel;
+import javax.swing.table.AbstractTableModel;
 
 import org.helioviewer.jhv.timelines.draw.ClickableDrawable;
 import org.helioviewer.jhv.timelines.draw.DrawController;
 
-public class TimelineTableModel implements TableModel {
+@SuppressWarnings("serial")
+public class TimelineTableModel extends AbstractTableModel {
 
-    private final HashSet<TableModelListener> listeners = new HashSet<>();
     private final ArrayList<TimelineRenderable> elements = new ArrayList<>();
+
+    public List<TimelineRenderable> getAllLineDataSelectorElements() {
+        return elements;
+    }
 
     public void downloadStarted(TimelineRenderable element) {
         fireUpdate(element, TimelinePanel.LOADING_COL);
@@ -26,51 +27,23 @@ public class TimelineTableModel implements TableModel {
 
     public void addLineData(TimelineRenderable element) {
         elements.add(element);
-        fireInsert(elements.size() - 1);
+
+        int row = elements.size() - 1;
+        fireTableRowsInserted(row, row);
+        DrawController.graphAreaChanged();
     }
 
     public void removeLineData(TimelineRenderable element) {
         element.remove();
+        int row = elements.indexOf(element);
         elements.remove(element);
-        fireListeners();
-    }
 
-    public List<TimelineRenderable> getAllLineDataSelectorElements() {
-        return elements;
-    }
-
-    public ClickableDrawable getDrawableUnderMouse() {
-        for (TimelineRenderable el : elements) {
-            ClickableDrawable elUnderMouse = el.getDrawableUnderMouse();
-            if (elUnderMouse != null) {
-                return elUnderMouse;
-            }
-        }
-        return null;
-    }
-
-    private void fireListeners() {
-        TableModelEvent e = new TableModelEvent(this);
-        for (TableModelListener listener : listeners) {
-            listener.tableChanged(e);
-        }
-        DrawController.graphAreaChanged();
-    }
-
-    private void fireInsert(int idx) {
-        TableModelEvent e = new TableModelEvent(this, idx, idx, TableModelEvent.ALL_COLUMNS, TableModelEvent.INSERT);
-        for (TableModelListener listener : listeners) {
-            listener.tableChanged(e);
-        }
+        fireTableRowsDeleted(row, row);
         DrawController.graphAreaChanged();
     }
 
     void fireUpdate(TimelineRenderable element, int col) {
-        int row = elements.indexOf(element);
-        TableModelEvent e = new TableModelEvent(this, row, row, col, TableModelEvent.UPDATE);
-        for (TableModelListener listener : listeners) {
-            listener.tableChanged(e);
-        }
+        fireTableCellUpdated(elements.indexOf(element), col);
     }
 
     @Override
@@ -84,37 +57,18 @@ public class TimelineTableModel implements TableModel {
     }
 
     @Override
-    public String getColumnName(int columnIndex) {
-        return Integer.toString(columnIndex);
+    public Object getValueAt(int row, int col) {
+        return elements.get(row);
     }
 
-    @Override
-    public Class<?> getColumnClass(int columnIndex) {
-        return TimelineRenderable.class;
-    }
-
-    @Override
-    public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return false;
-    }
-
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        return elements.get(rowIndex);
-    }
-
-    @Override
-    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-    }
-
-    @Override
-    public void addTableModelListener(TableModelListener l) {
-        listeners.add(l);
-    }
-
-    @Override
-    public void removeTableModelListener(TableModelListener l) {
-        listeners.remove(l);
+    public ClickableDrawable getDrawableUnderMouse() {
+        for (TimelineRenderable el : elements) {
+            ClickableDrawable elUnderMouse = el.getDrawableUnderMouse();
+            if (elUnderMouse != null) {
+                return elUnderMouse;
+            }
+        }
+        return null;
     }
 
     public int getNumberOfAxes() {
