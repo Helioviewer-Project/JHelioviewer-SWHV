093a04f75523c600dcd58703701de82ebd5d44f3
Bogdan Nicula <bogdan@nicula.net>
2017-03-16 11:19:56 +0000

    Redo time
    git-svn-id: svn+ssh://solsvn.oma.be/home/sidc/ssr/SWHV_repos/trunk/client/jhv-3d-wcs@8760 b4e469a2-07ce-4b26-9273-4d7d95a670c7

--- src/jhv/src/org/helioviewer/jhv/base/time/TimeUtils.java
+++ src/jhv/src/org/helioviewer/jhv/base/time/TimeUtils.java
@@ -6,20 +6,15 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.util.TimeZone;
-
-import org.apache.commons.lang3.time.FastDateFormat;
 
 public class TimeUtils {
 
-    private static final TimeZone UTC = TimeZone.getTimeZone("UTC");
     private static final ZoneOffset ZERO = ZoneOffset.ofTotalSeconds(0);
     private static final DateTimeFormatter sqlFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private static final DateTimeFormatter fileFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss");
 
     public static final long DAY_IN_MILLIS = 86400000;
     public static final long MINUTE_IN_MILLIS = 60000;
-
-    public static final FastDateFormat filenameDateFormat = FastDateFormat.getInstance("yyyy-MM-dd_HH.mm.ss");
 
     public static final JHVDate EPOCH = new JHVDate("2000-01-01T00:00:00");
     public static final JHVDate MINIMAL_DATE = new JHVDate("1970-01-01T00:00:00");
@@ -43,6 +38,10 @@ public class TimeUtils {
 
     public static String formatTime(long milli) {
         return DateTimeFormatter.ISO_LOCAL_TIME.format(Instant.ofEpochMilli(milli).atOffset(ZERO));
+    }
+
+    public static String formatFilename(long milli) {
+        return fileFormatter.format(Instant.ofEpochMilli(milli).atOffset(ZERO));
     }
 
     public static long parse(String date) {
