37cce223aecdc65713de260db07d21662886f8ab
Bogdan Nicula <bogdan@nicula.net>
2017-03-24 20:45:52 +0100

    Cleanup SWEK icons

--- src/org/helioviewer/jhv/plugins/swek/config/SWEKIconBank.java
+++ src/org/helioviewer/jhv/plugins/swek/config/SWEKIconBank.java
@@ -22,7 +22,7 @@ class SWEKIconBank {
         iconBank.put("CoronalHole", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/ch_icon.png")));
         iconBank.put("CoronalWave", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/cw_icon.png")));
         iconBank.put("EmergingFlux", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/ef_icon.png")));
-        iconBank.put("Eruption", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/er_small.gif")));
+        iconBank.put("Eruption", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/er_icon.png")));
         iconBank.put("FilamentActivation", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/fa_icon.png")));
         iconBank.put("FilamentEruption", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/fe_icon.png")));
         iconBank.put("Filament", new ImageIcon(SWEKPlugin.class.getResource("/images/EventIcons/fi_icon.png")));
