32d545819279a099ea1b8dc7531be96677ef9604
Bogdan Nicula <bogdan@nicula.net>
2017-03-24 17:13:30 +0100

    Revert Raygun

--- src/org/helioviewer/jhv/JHVUncaughtExceptionHandler.java
+++ src/org/helioviewer/jhv/JHVUncaughtExceptionHandler.java
@@ -1,275 +1,174 @@
 package org.helioviewer.jhv;
 
-import java.awt.Dialog.ModalityType;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Frame;
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.Image;
 import java.awt.Insets;
-import java.awt.image.BufferedImage;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.lang.Thread.UncaughtExceptionHandler;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Arrays;
 
-import javax.annotation.Nullable;
-import javax.swing.Box;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JCheckBox;
+import javax.swing.BorderFactory;
 import javax.swing.JDialog;
-import javax.swing.JFrame;
+import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.JTextArea;
-import javax.swing.UIManager;
 
-import com.mindscapehq.raygun4java.core.RaygunClient;
+import org.helioviewer.jhv.base.logging.Log;
+import org.helioviewer.jhv.gui.ClipBoardCopier;
 
-/**
- * Routines to catch and handle all runtime exceptions.
- */
-public class JHVUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
-{
-	public static final JHVUncaughtExceptionHandler SINGLETON = new JHVUncaughtExceptionHandler();
-	public static @Nullable String openGLRenderer = null;
-	
-	/**
-	 * This method sets the default uncaught exception handler. Thus, this
-	 * method should be called once when the application starts.
-	 */
-	public static void setupHandlerForThread()
-	{
-		Thread.setDefaultUncaughtExceptionHandler(JHVUncaughtExceptionHandler.SINGLETON);
-	}
+// Catch and handle all runtime exceptions
+public class JHVUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
-	/**
-	 * Generates a simple error Dialog, allowing the user to copy the
-	 * errormessage to the clipboard.
-	 * <p>
-	 * As options it will show {"Quit JHelioviewer", "Continue"} and quit if
-	 * necessary.
-	 * 
-	 * @param title
-	 *            Title of the Dialog
-	 * @param msg
-	 *            Object to display in the main area of the dialog.
-	 */
-	private static void showErrorDialog(final String title, final String msg, final Throwable e, final String log)
-	{
-		List<Object> objects = new ArrayList<>();
+    private static final String BUG_URL = "https://github.com/Helioviewer-Project/JHelioviewer-SWHV/issues";
+    private static final String MAIL_URL = "swhv@sidc.be";
 
-		JLabel head = new JLabel("Dang! You hit a bug in JHelioviewer.");
-		head.setFont(head.getFont().deriveFont(Font.BOLD));
+    private static final int default_width = 600;
+    private static final int default_height = 400;
 
-		objects.add(head);
-		objects.add(Box.createVerticalStrut(10));
-		objects.add(new JLabel("Here are some technical details about the problem:"));
+    private static final JHVUncaughtExceptionHandler instance = new JHVUncaughtExceptionHandler();
 
-		JTextArea textArea = new JTextArea();
-		textArea.setMargin(new Insets(5, 5, 5, 5));
-		textArea.setText(msg);
-		textArea.setEditable(false);
-		JScrollPane sp = new JScrollPane(textArea);
-		sp.setPreferredSize(new Dimension(600, 400));
+    private JHVUncaughtExceptionHandler() {
+    }
 
-		objects.add(sp);
-		JCheckBox allowCrashReport = new JCheckBox("Send this anonymous crash report to the developers.", JHVGlobals.IS_RELEASE_VERSION);
-		objects.add(allowCrashReport);
-		objects.add(Box.createVerticalStrut(10));
+    /**
+     * This method sets the default uncaught exception handler. Thus, this
+     * method should be called once when the application starts.
+     */
+    public static void setupHandlerForThread() {
+        Thread.setDefaultUncaughtExceptionHandler(instance);
+    }
 
-		JOptionPane optionPane = new JOptionPane(title);
-		optionPane.setMessage(objects.toArray());
-		optionPane.setMessageType(JOptionPane.ERROR_MESSAGE);
-		optionPane.setOptions(new String[] { "Quit" });
+    /**
+     * Generates a simple error Dialog, allowing the user to copy the
+     * errormessage to the clipboard.
+     * <p>
+     * As options it will show {"Quit JHelioviewer", "Continue"} and quit if
+     * necessary.
+     * 
+     * @param title
+     *            Title of the Dialog
+     * @param msg
+     *            Object to display in the main area of the dialog.
+     */
+    private static void showErrorDialog(String title, Object msg) {
+        ArrayList<Object> objects = new ArrayList<>();
 
-		JFrame jf = new JFrame();
-		jf.setUndecorated(true);
-		jf.setLocationRelativeTo(null);
-		jf.setIconImage(iconToImage(UIManager.getIcon("OptionPane.errorIcon")));
-		jf.setVisible(true);
+        JLabel fatal = new JLabel("Fatal error detected.");
+        objects.add(fatal);
 
-		JDialog dialog = optionPane.createDialog(jf, title);
-		dialog.setAutoRequestFocus(true);
-		dialog.setIconImage(iconToImage(UIManager.getIcon("OptionPane.errorIcon")));
-		dialog.setResizable(true);
-		dialog.setModalityType(ModalityType.TOOLKIT_MODAL);
-		dialog.setVisible(true);
+        Font font = fatal.getFont();
+        JEditorPane report = new JEditorPane("text/html", "<html><font style=\"font-family: '" + font.getFamily() + "'; font-size: " + font.getSize() + ";\">" +
+                                                          "Please email this report at <a href='mailto:" + MAIL_URL + "'>" + MAIL_URL + "</a> " +
+                                                          "or use it to open an issue at <a href='" + BUG_URL + "'>" + BUG_URL + "</a> " + "</font></html>");
+        report.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 10));
+        report.setEditable(false);
+        report.setOpaque(false);
+        report.addHyperlinkListener(JHVGlobals.hyperOpenURL);
+        objects.add(report);
 
-		jf.dispose();
+        if (msg instanceof String) {
+            JLabel copyToClipboard = new JLabel("Click here to copy the error message to the clipboard.");
 
-		if (allowCrashReport.isSelected())
-		{
-			Map<String, String> customData = new HashMap<>();
-			customData.put("Log", log);
-			if(openGLRenderer!=null)
-				customData.put("OpenGL", openGLRenderer);
-			customData.put("JVM",
-					System.getProperty("java.vm.name") + " "
-							+ System.getProperty("java.vm.version") + " (JRE "
-							+ System.getProperty("java.specification.version")
-							+ ")");
+            copyToClipboard.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mousePressed(MouseEvent me) {
+                    ClipBoardCopier.getSingletonInstance().setString((String) msg);
+                    JOptionPane.showMessageDialog(null, "Error message copied to clipboard.");
+                }
+            });
 
-            // send to raygun
-            // TODO alternative crash-reporting
-			RaygunClient client = new RaygunClient("+rfODAesOh4Col+swQzxYw==");
-			client.SetVersion(JHVGlobals.version);
-			client.Send(e, Arrays.asList(JHVGlobals.RAYGUN_TAG), customData);
-			
-		}
+            font = copyToClipboard.getFont();
+            font = font.deriveFont(font.getStyle() ^ Font.ITALIC);
+            copyToClipboard.setFont(font);
+            copyToClipboard.setForeground(Color.blue);
 
-		Runtime.getRuntime().halt(0);
-	}
+            // copyToClipboard.addMouseListener(l);
 
-	private JHVUncaughtExceptionHandler()
-	{
-	}
+            String text = (String) msg;
 
-	public synchronized void uncaughtException(final @Nullable Throwable e)
-	{
-		uncaughtException(Thread.currentThread(), e);
-	}
-	
-	
-	static volatile boolean alreadyCaughtException=false;
-	
-	// we do not use the logger here, since it should work even before logging
-	// initialization
-	@SuppressWarnings("deprecation")
-	public synchronized void uncaughtException(final @Nullable Thread t, final @Nullable Throwable e)
-	{
-		if(e==null)
-			return;
-		
-		e.printStackTrace();
-		
-		// stop reentrant error reporting
-		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler()
-		{
-			@Override
-			public void uncaughtException(@Nullable Thread _t, @Nullable Throwable _e)
-			{
-				// IGNORE all other exceptions
-			}
-		});
+            JTextArea textArea = new JTextArea();
+            textArea.setMargin(new Insets(5, 5, 5, 5));
+            textArea.setText(text);
+            textArea.setEditable(false);
+            JScrollPane sp = new JScrollPane(textArea);
+            sp.setPreferredSize(new Dimension(default_width, default_height));
 
-		if (!EventQueue.isDispatchThread())
-		{
-			try
-			{
-				EventQueue.invokeLater(() ->
-				{
-					try
-					{
-						uncaughtException(t, e);
-					}
-					catch (ThreadDeath _td)
-					{
-						// ignore concurrent, unhandled exceptions
-					}
-				});
-				return;
-			}
-			catch (Exception _e)
-			{
-			}
-		}
+            objects.add(copyToClipboard);
+            objects.add(new JSeparator());
+            objects.add(sp);
+        } else {
+            objects.add(new JSeparator());
+            objects.add(msg);
+        }
 
-		// STOP THE WORLD to avoid exceptions piling up
-		// Close all threads (excluding systemsthreads, just stop the timer
-		// thread from the system)
-		for (Thread thr : Thread.getAllStackTraces().keySet())
-			if (thr != Thread.currentThread()
-					&& (!thr.getThreadGroup().getName()
-							.equalsIgnoreCase("system") || thr.getName()
-							.contains("Timer")))
-				try
-				{
-					thr.suspend();
-				}
-				catch (Throwable _th)
-				{
-				}
-		for (Thread thr : Thread.getAllStackTraces().keySet())
-			if (thr != Thread.currentThread()
-					&& (!thr.getThreadGroup().getName()
-							.equalsIgnoreCase("system") || thr.getName()
-							.contains("Timer")))
-				try
-				{
-					thr.stop();
-				}
-				catch (Throwable _th)
-				{
-				}
+        JOptionPane optionPane = new JOptionPane(title);
+        optionPane.setMessage(objects.toArray());
+        optionPane.setMessageType(JOptionPane.ERROR_MESSAGE);
+        optionPane.setOptions(new String[] { "Quit JHelioviewer", "Continue" });
+        JDialog dialog = optionPane.createDialog(null, title);
 
-		final String finalLog = ""; //Log.GetLastFewLines(6);
-		String msg = "JHelioviewer: " + JHVGlobals.version + "\n";
-		msg += "Date: " + new Date() + "\n";
-		msg += "JVM: " + System.getProperty("java.vm.name") + " "
-				+ System.getProperty("java.vm.version") + " (JRE "
-				+ System.getProperty("java.specification.version") + ")\n";
-		msg += "OS: " + System.getProperty("os.name") + " "
-				+ System.getProperty("os.arch") + " "
-				+ System.getProperty("os.version") + "\n\n";
-		msg += finalLog;
+        dialog.setVisible(true);
+        if ("Quit JHelioviewer".equals(optionPane.getValue()))
+            System.exit(1);
+    }
 
-		try (StringWriter st = new StringWriter())
-		{
-			try (PrintWriter pw = new PrintWriter(st))
-			{
-				e.printStackTrace(pw);
-				msg += st.toString();
-			}
-		}
-		catch (IOException e1)
-		{
-		}
+    // we do not use the logger here, since it should work even before logging initialization
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        StringBuilder stackTrace = new StringBuilder();
+        stackTrace.append(e.getClass().getCanonicalName()).append('\n');
+        for (StackTraceElement el : e.getStackTrace()) {
+            stackTrace.append("at ").append(el).append('\n');
+        }
 
-		for (Frame f : Frame.getFrames())
-			f.setVisible(false);
+        String msg = "Uncaught Exception detected.\n\nConfiguration:\n";
+        msg += "JHelioviewer - Version: " + JHVGlobals.version + '\n';
+        msg += "JHelioviewer - Revision: " + JHVGlobals.revision + '\n';
+        msg += "Java Virtual Machine - Name: " + System.getProperty("java.vm.name") + '\n';
+        msg += "Java Virtual Machine - Vendor: " + System.getProperty("java.vm.vendor") + '\n';
+        msg += "Java Virtual Machine - Version: " + System.getProperty("java.vm.version") + '\n';
+        msg += "JRE Specification - Version: " + System.getProperty("java.specification.version") + '\n';
+        msg += "Operating System - Name: " + System.getProperty("os.name") + '\n';
+        msg += "Operating System - Architecture: " + System.getProperty("os.arch") + '\n';
+        msg += "Operating System - Version: " + System.getProperty("os.version") + "\n\n";
 
-		final String finalMsg = msg;
-		
-		if(alreadyCaughtException)
-			return;
+        msg += "Date: " + new Date() + '\n';
+        msg += "Thread: " + t + '\n';
+        msg += "Message: " + e.getMessage() + "\n\n";
+        msg += "Stacktrace:\n";
+        msg += stackTrace;
 
-		alreadyCaughtException=true;
+        String logName = Log.getCurrentLogFile();
+        if (logName != null) {
+            Log.fatal("Runtime exception", e);
 
-		// this wizardry forces the creation of a new awt event queue, if needed
-		new Thread(() -> EventQueue.invokeLater(() -> JHVUncaughtExceptionHandler.showErrorDialog(
-							"JHelioviewer: Fatal error", finalMsg, e,
-							finalLog))).start();
-	}
+            msg += "\nLog:\n";
+            try (BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(logName), StandardCharsets.UTF_8))) {
+                String line;
+                StringBuilder sb = new StringBuilder();
+                while ((line = input.readLine()) != null) {
+                    sb.append(line).append('\n');
+                }
+                msg += sb;
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } else {
+            System.err.println("Runtime exception");
+            System.err.println(stackTrace);
+        }
 
-	private static Image iconToImage(Icon icon)
-	{
-		if (icon instanceof ImageIcon)
-			return ((ImageIcon) icon).getImage();
-		else
-		{
-			int w = icon.getIconWidth();
-			int h = icon.getIconHeight();
-			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-			GraphicsDevice gd = ge.getDefaultScreenDevice();
-			GraphicsConfiguration gc = gd.getDefaultConfiguration();
-			BufferedImage image = gc.createCompatibleImage(w, h);
-			Graphics2D g = image.createGraphics();
-			icon.paintIcon(null, g, 0, 0);
-			g.dispose();
-			return image;
-		}
-	}
+        showErrorDialog("JHelioviewer: Fatal Error", msg);
+    }
+
 }
