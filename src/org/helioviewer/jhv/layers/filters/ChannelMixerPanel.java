a7fcb077dcb6561e5d4e82fa2fe376d205d1634f
Bogdan Nicula <bogdan@nicula.net>
2017-05-09 16:43:55 +0200

    Unused imports

--- src/org/helioviewer/jhv/layers/filters/ChannelMixerPanel.java
+++ src/org/helioviewer/jhv/layers/filters/ChannelMixerPanel.java
@@ -3,7 +3,6 @@ package org.helioviewer.jhv.layers.filters;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JCheckBox;
--- src/org/helioviewer/jhv/layers/filters/LUTPanel.java
+++ src/org/helioviewer/jhv/layers/filters/LUTPanel.java
@@ -1,7 +1,6 @@
 package org.helioviewer.jhv.layers.filters;
 
 import java.awt.Component;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JLabel;
