11b7690aa54e11745989fa35b9347c9425b4a30c
freekv <freek.verstringe@oma.be>
2017-05-10 13:24:50 +0200

    refactor pfss code

--- src/org/helioviewer/jhv/plugins/pfss/PfssRenderable.java
+++ src/org/helioviewer/jhv/plugins/pfss/PfssRenderable.java
@@ -9,82 +9,107 @@ import org.helioviewer.jhv.display.Viewport;
 import org.helioviewer.jhv.gui.ImageViewerGui;
 import org.helioviewer.jhv.layers.Layers;
 import org.helioviewer.jhv.layers.TimespanListener;
+import org.helioviewer.jhv.opengl.VBO;
 import org.helioviewer.jhv.plugins.pfss.data.PfssData;
 import org.helioviewer.jhv.plugins.pfss.data.PfssNewDataLoader;
 import org.helioviewer.jhv.renderable.gui.AbstractRenderable;
 import org.helioviewer.jhv.threads.CancelTask;
 
+import com.jogamp.common.nio.Buffers;
 import com.jogamp.opengl.GL2;
 
 public class PfssRenderable extends AbstractRenderable implements TimespanListener {
 
-    private final PfssPluginPanel optionsPanel = new PfssPluginPanel();
-    private PfssData previousPfssData = null;
+	private final PfssPluginPanel optionsPanel = new PfssPluginPanel();
+	private PfssData previousPfssData = null;
+	private static VBO vertexVBO;
 
-    @Override
-    public void render(Camera camera, Viewport vp, GL2 gl) {
-        if (!isVisible[vp.idx])
-            return;
+	@Override
+	public void render(Camera camera, Viewport vp, GL2 gl) {
+		if (!isVisible[vp.idx])
+			return;
 
-        PfssData pfssData = PfssPlugin.getPfsscache().getData(Layers.getLastUpdatedTimestamp().milli);
-        if (pfssData != null) {
-            if (previousPfssData != null && previousPfssData != pfssData && previousPfssData.isInit()) {
-                previousPfssData.clear(gl);
-            }
-            if (!pfssData.isInit())
-                pfssData.init(gl);
-            if (pfssData.isInit()) {
-                pfssData.display(gl);
-                datetime = pfssData.getDateObs();
-                ImageViewerGui.getRenderableContainer().fireTimeUpdated(this);
-            }
-            previousPfssData = pfssData;
-        }
-    }
+		PfssData pfssData = PfssPlugin.getPfsscache().getData(Layers.getLastUpdatedTimestamp().milli);
+		if (pfssData != null) {
+			renderData(gl, pfssData);
+			datetime = pfssData.getDateObs();
+			ImageViewerGui.getRenderableContainer().fireTimeUpdated(this);
 
-    @Override
-    public void remove(GL2 gl) {
-        dispose(gl);
-    }
+			previousPfssData = pfssData;
+		}
+	}
 
-    @Override
-    public Component getOptionsPanel() {
-        return optionsPanel;
-    }
+	@Override
+	public void remove(GL2 gl) {
+		dispose(gl);
+	}
 
-    @Override
-    public String getName() {
-        return "PFSS Model";
-    }
+	@Override
+	public Component getOptionsPanel() {
+		return optionsPanel;
+	}
 
-    private String datetime = null;
+	@Override
+	public String getName() {
+		return "PFSS Model";
+	}
 
-    @Override
-    public String getTimeString() {
-        return datetime;
-    }
+	private String datetime = null;
 
-    @Override
-    public void timespanChanged(long start, long end) {
-        PfssPlugin.getPfsscache().clear();
+	@Override
+	public String getTimeString() {
+		return datetime;
+	}
 
-        FutureTask<Void> dataLoaderTask = new FutureTask<>(new PfssNewDataLoader(start, end), null);
-        PfssPlugin.pfssNewLoadPool.execute(dataLoaderTask);
-        PfssPlugin.pfssReaperPool.schedule(new CancelTask(dataLoaderTask), 60 * 5, TimeUnit.SECONDS);
-    }
+	@Override
+	public void timespanChanged(long start, long end) {
+		PfssPlugin.getPfsscache().clear();
 
-    @Override
-    public boolean isDeletable() {
-        return false;
-    }
+		FutureTask<Void> dataLoaderTask = new FutureTask<>(new PfssNewDataLoader(start, end), null);
+		PfssPlugin.pfssNewLoadPool.execute(dataLoaderTask);
+		PfssPlugin.pfssReaperPool.schedule(new CancelTask(dataLoaderTask), 60 * 5, TimeUnit.SECONDS);
+	}
 
-    @Override
-    public void init(GL2 gl) {
-    }
+	@Override
+	public boolean isDeletable() {
+		return false;
+	}
 
-    @Override
-    public void dispose(GL2 gl) {
-        PfssPlugin.getPfsscache().destroy(gl);
-    }
+	@Override
+	public void init(GL2 gl) {
+		vertexVBO = new VBO(GL2.GL_ARRAY_BUFFER, -1, 3);
+		vertexVBO.init(gl);
+	}
+
+	@Override
+	public void dispose(GL2 gl) {
+		vertexVBO.dispose(gl);
+		previousPfssData = null;
+	}
+
+	public void renderData(GL2 gl, PfssData data) {
+		if (previousPfssData == null || data != previousPfssData || PfssSettings.qualityReduction != data.lastQuality
+				|| PfssSettings.fixedColor != data.lastFixedColor) {
+			data.calculatePositions(PfssSettings.qualityReduction, PfssSettings.fixedColor);
+			vertexVBO.bindBufferData(gl, data.vertices, Buffers.SIZEOF_FLOAT);
+		}
+		gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL2.GL_COLOR_ARRAY);
+
+		gl.glDepthMask(false);
+
+		vertexVBO.bindArray(gl);
+		gl.glColorPointer(4, GL2.GL_FLOAT, 7 * 4, 3 * 4);
+		gl.glVertexPointer(3, GL2.GL_FLOAT, 7 * 4, 0);
+
+		gl.glLineWidth(PfssSettings.LINE_WIDTH);
+		gl.glDrawArrays(GL2.GL_LINE_STRIP, 0, vertexVBO.bufferSize);
+		vertexVBO.unbindArray(gl);
+
+		gl.glDepthMask(true);
+
+		gl.glDisableClientState(GL2.GL_VERTEX_ARRAY);
+		gl.glDisableClientState(GL2.GL_COLOR_ARRAY);
+	}
 
 }
--- src/org/helioviewer/jhv/plugins/pfss/data/PfssData.java
+++ src/org/helioviewer/jhv/plugins/pfss/data/PfssData.java
@@ -11,236 +11,168 @@ import nom.tam.fits.Fits;
 import org.helioviewer.jhv.base.astronomy.Position;
 import org.helioviewer.jhv.base.astronomy.Sun;
 import org.helioviewer.jhv.base.time.JHVDate;
-import org.helioviewer.jhv.opengl.VBO;
 import org.helioviewer.jhv.plugins.pfss.PfssSettings;
 
 import com.jogamp.common.nio.Buffers;
-import com.jogamp.opengl.GL2;
 
 public class PfssData {
+	private enum FieldLineColor {
+		OPENFIELDCOLOR(Color.RED), LOOPCOLOR(Color.WHITE), INSIDEFIELDCOLOR(Color.BLUE);
 
-    private static final Color OPENFIELDCOLOR = Color.RED;
-    private static final Color LOOPCOLOR = Color.WHITE;
-    private static final Color INSIDEFIELDCOLOR = Color.BLUE;
+		public Color color;
 
-    private final byte[] gzipFitsFile;
+		FieldLineColor(Color color) {
+			this.color = color;
+		}
+	}
 
-    private FloatBuffer vertices;
-    private static VBO vertexVBO;
+	private final byte[] gzipFitsFile;
 
-    private int lastQuality;
-    private boolean read = false;
-    private boolean init = false;
+	public FloatBuffer vertices;
 
-    private boolean lastFixedColor;
+	public int lastQuality;
+	public boolean lastFixedColor;
+	public boolean read;
 
-    private String dateObs;
-    final long time;
+	private String dateObs;
+	final long time;
 
-    public PfssData(byte[] _gzipFitsFile, long _time) {
-        gzipFitsFile = _gzipFitsFile;
-        time = _time;
-    }
+	public PfssData(byte[] _gzipFitsFile, long _time) {
+		gzipFitsFile = _gzipFitsFile;
+		time = _time;
+		readFitsFile();
+	}
 
-    private void readFitsFile() {
-        if (gzipFitsFile != null) {
-            calculatePositions();
-        }
-    }
+	private void readFitsFile() {
+		if (gzipFitsFile != null) {
+			calculatePositions(PfssSettings.qualityReduction, PfssSettings.fixedColor);
+		}
+	}
 
-    private void createBuffer(int len) {
-        int numberOfLines = len / PfssSettings.POINTS_PER_LINE;
-        vertices = Buffers.newDirectFloatBuffer((3 + 4) * (len + 2 * numberOfLines));
-    }
+	private void createBuffer(int len) {
+		int numberOfLines = len / PfssSettings.POINTS_PER_LINE;
+		vertices = Buffers.newDirectFloatBuffer((3 + 4) * (len + 2 * numberOfLines));
+	}
 
-    private void addColor(Color color) {
-    	vertices.put(color.getRed() / 255f);
-    	vertices.put(color.getGreen() / 255f);
-    	vertices.put(color.getBlue() / 255f);
-    	vertices.put(color.getAlpha() / 255f);
-    }
+	private void addColor(Color color) {
+		vertices.put(color.getRed() / 255f);
+		vertices.put(color.getGreen() / 255f);
+		vertices.put(color.getBlue() / 255f);
+		vertices.put(color.getAlpha() / 255f);
+	}
 
-    private void addVertex(float x, float y, float z) {
-        vertices.put(x);
-        vertices.put(y);
-        vertices.put(z);
-    }
+	private void addVertex(float x, float y, float z) {
+		vertices.put(x);
+		vertices.put(y);
+		vertices.put(z);
+	}
 
-    private void addColor(double bright, float opacity) {
-        if (bright > 0) {
-            addColor(new Color(1f, (float) (1. - bright), (float) (1. - bright), opacity));
-        } else {
-            addColor(new Color((float) (1. + bright), (float) (1. + bright), 1f, opacity));
-        }
-    }
+	private void addColor(double bright, float opacity) {
+		if (bright > 0) {
+			addColor(new Color(1f, (float) (1. - bright), (float) (1. - bright), opacity));
+		} else {
+			addColor(new Color((float) (1. + bright), (float) (1. + bright), 1f, opacity));
+		}
+	}
 
-    private void calculatePositions() {
-        lastQuality = PfssSettings.qualityReduction;
-        lastFixedColor = PfssSettings.fixedColor;
+	public void calculatePositions(int qualityReduction, boolean fixedColor) {
+		if (lastQuality == qualityReduction && lastFixedColor == fixedColor)
+			return;
 
-        try (Fits fits = new Fits(new ByteArrayInputStream(gzipFitsFile))) {
-            BasicHDU<?> hdus[] = fits.read();
-            if (hdus == null || hdus.length < 2 || !(hdus[1] instanceof BinaryTableHDU))
-                throw new Exception("Could not read FITS");
+		lastQuality = PfssSettings.qualityReduction;
+		lastFixedColor = PfssSettings.fixedColor;
 
-            BinaryTableHDU bhdu = (BinaryTableHDU) hdus[1];
-            short[] fieldlinex = (short[]) bhdu.getColumn("FIELDLINEx");
-            short[] fieldliney = (short[]) bhdu.getColumn("FIELDLINEy");
-            short[] fieldlinez = (short[]) bhdu.getColumn("FIELDLINEz");
-            short[] fieldlines = (short[]) bhdu.getColumn("FIELDLINEs");
+		try (Fits fits = new Fits(new ByteArrayInputStream(gzipFitsFile))) {
+			BasicHDU<?> hdus[] = fits.read();
+			if (hdus == null || hdus.length < 2 || !(hdus[1] instanceof BinaryTableHDU))
+				throw new Exception("Could not read FITS");
 
-            dateObs = bhdu.getHeader().getStringValue("DATE-OBS");
-            if (dateObs == null)
-                throw new Exception("DATE-OBS not found");
+			BinaryTableHDU bhdu = (BinaryTableHDU) hdus[1];
+			short[] fieldlinex = (short[]) bhdu.getColumn("FIELDLINEx");
+			short[] fieldliney = (short[]) bhdu.getColumn("FIELDLINEy");
+			short[] fieldlinez = (short[]) bhdu.getColumn("FIELDLINEz");
+			short[] fieldlines = (short[]) bhdu.getColumn("FIELDLINEs");
 
-            createBuffer(fieldlinex.length);
+			dateObs = bhdu.getHeader().getStringValue("DATE-OBS");
+			if (dateObs == null)
+				throw new Exception("DATE-OBS not found");
 
-            Position.L p = Sun.getEarth(new JHVDate(dateObs));
-            double sphi = Math.sin(p.lon), cphi = Math.cos(p.lon);
+			createBuffer(fieldlinex.length);
 
-            int type = 0;
-            for (int i = 0; i < fieldlinex.length; i++) {
-                if (i / PfssSettings.POINTS_PER_LINE % 9 <= 8 - PfssSettings.qualityReduction) {
-                    int rx = fieldlinex[i] + 32768;
-                    int ry = fieldliney[i] + 32768;
-                    int rz = fieldlinez[i] + 32768;
+			Position.L p = Sun.getEarth(new JHVDate(dateObs));
+			double sphi = Math.sin(p.lon), cphi = Math.cos(p.lon);
 
-                    double x = 3. * (rx * 2. / 65535 - 1.);
-                    double y = 3. * (ry * 2. / 65535 - 1.);
-                    double z = 3. * (rz * 2. / 65535 - 1.);
+			FieldLineColor type = FieldLineColor.LOOPCOLOR;
+			for (int i = 0; i < fieldlinex.length; i++) {
+				if (i / PfssSettings.POINTS_PER_LINE % 9 <= 8 - PfssSettings.qualityReduction) {
+					int rx = fieldlinex[i] + 32768;
+					int ry = fieldliney[i] + 32768;
+					int rz = fieldlinez[i] + 32768;
 
-                    double helpx = cphi * x + sphi * y;
-                    double helpy = -sphi * x + cphi * y;
-                    x = helpx;
-                    y = helpy;
+					double x = 3. * (rx * 2. / 65535 - 1.);
+					double y = 3. * (ry * 2. / 65535 - 1.);
+					double z = 3. * (rz * 2. / 65535 - 1.);
 
-                    int col = fieldlines[i] + 32768;
-                    double bright = (col * 2. / 65535.) - 1.;
-                    if (i % PfssSettings.POINTS_PER_LINE == 0) {
-                        addVertex((float) x, (float) z, (float) -y);
-                        addColor(bright, 0);
-                        addVertex((float) x, (float) z, (float) -y);
-                        if (!PfssSettings.fixedColor) {
-                            addColor(bright, 1);
-                        } else {
-                            int rox = fieldlinex[i + PfssSettings.POINTS_PER_LINE - 1] + 32768;
-                            int roy = fieldliney[i + PfssSettings.POINTS_PER_LINE - 1] + 32768;
-                            int roz = fieldlinez[i + PfssSettings.POINTS_PER_LINE - 1] + 32768;
-                            double xo = 3. * (rox * 2. / 65535 - 1.);
-                            double yo = 3. * (roy * 2. / 65535 - 1.);
-                            double zo = 3. * (roz * 2. / 65535 - 1.);
-                            double ro = Math.sqrt(xo * xo + yo * yo + zo * zo);
-                            double r = Math.sqrt(x * x + y * y + z * z);
+					double helpx = cphi * x + sphi * y;
+					double helpy = -sphi * x + cphi * y;
+					x = helpx;
+					y = helpy;
 
-                            if (Math.abs(r - ro) < 2.5 - 1.0 - 0.2) {
-                                type = 0;
-                                addColor(LOOPCOLOR);
-                            } else if (bright < 0) {
-                                type = 1;
-                                addColor(INSIDEFIELDCOLOR);
-                            } else {
-                                type = 2;
-                                addColor(OPENFIELDCOLOR);
-                            }
-                        }
-                    } else if (i % PfssSettings.POINTS_PER_LINE == PfssSettings.POINTS_PER_LINE - 1) {
-                        addVertex((float) x, (float) z, (float) -y);
-                        if (!PfssSettings.fixedColor) {
-                            addColor(bright, 1);
-                        } else {
-                            if (type == 0) {
-                                addColor(LOOPCOLOR);
-                            } else if (type == 1) {
-                                addColor(INSIDEFIELDCOLOR);
-                            } else {
-                                addColor(OPENFIELDCOLOR);
-                            }
-                        }
-                        addVertex((float) x, (float) z, (float) -y);
-                        addColor(bright, 0);
-                    } else {
-                        addVertex((float) x, (float) z, (float) -y);
-                        if (!PfssSettings.fixedColor) {
-                            addColor(bright, 1);
-                        } else {
-                            if (type == 0) {
-                                addColor(LOOPCOLOR);
-                            } else if (type == 1) {
-                                addColor(INSIDEFIELDCOLOR);
-                            } else {
-                                addColor(OPENFIELDCOLOR);
-                            }
-                        }
-                    }
-                }
-            }
-            vertices.flip();
-            read = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+					int col = fieldlines[i] + 32768;
+					double bright = (col * 2. / 65535.) - 1.;
+					if (i % PfssSettings.POINTS_PER_LINE == 0) {
+						addVertex((float) x, (float) z, (float) -y);
+						addColor(bright, 0);
+						addVertex((float) x, (float) z, (float) -y);
+						if (!PfssSettings.fixedColor) {
+							addColor(bright, 1);
+						} else {
+							int rox = fieldlinex[i + PfssSettings.POINTS_PER_LINE - 1] + 32768;
+							int roy = fieldliney[i + PfssSettings.POINTS_PER_LINE - 1] + 32768;
+							int roz = fieldlinez[i + PfssSettings.POINTS_PER_LINE - 1] + 32768;
+							double xo = 3. * (rox * 2. / 65535 - 1.);
+							double yo = 3. * (roy * 2. / 65535 - 1.);
+							double zo = 3. * (roz * 2. / 65535 - 1.);
+							double ro = Math.sqrt(xo * xo + yo * yo + zo * zo);
+							double r = Math.sqrt(x * x + y * y + z * z);
 
-    public void init(GL2 gl) {
-        if (gzipFitsFile != null) {
-            if (!read)
-                readFitsFile();
-            if (!init && read) {
-            	if(vertexVBO == null) {
-                    vertexVBO = new VBO(GL2.GL_ARRAY_BUFFER, -1, 3);
-                    vertexVBO.init(gl);    
-            	}
-                vertexVBO.bindBufferData(gl, vertices, Buffers.SIZEOF_FLOAT);
-                init = true;
-            }
-        }
-    }
+							if (Math.abs(r - ro) < 2.5 - 1.0 - 0.2) {
+								type = FieldLineColor.LOOPCOLOR;
+							} else if (bright < 0) {
+								type = FieldLineColor.INSIDEFIELDCOLOR;
+							} else {
+								type = FieldLineColor.OPENFIELDCOLOR;
+							}
+							addColor(type.color);
+						}
+					} else if (i % PfssSettings.POINTS_PER_LINE == PfssSettings.POINTS_PER_LINE - 1) {
+						addVertex((float) x, (float) z, (float) -y);
+						if (!PfssSettings.fixedColor) {
+							addColor(bright, 1);
+						} else {
+							addColor(type.color);
+						}
+						addVertex((float) x, (float) z, (float) -y);
+						addColor(bright, 0);
+					} else {
+						addVertex((float) x, (float) z, (float) -y);
+						if (!PfssSettings.fixedColor) {
+							addColor(bright, 1);
+						} else {
+							addColor(type.color);
+						}
+					}
+				}
+			}
+			vertices.flip();
+			read = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    public void clear(GL2 gl) {
-        if (init) {
-        	//vertexVBO.dispose(gl);
-        	//vertexVBO = null;
-            init = false;
-        }
-    }
-
-    public void display(GL2 gl) {
-        if (PfssSettings.qualityReduction != lastQuality || PfssSettings.fixedColor != lastFixedColor) {
-            clear(gl);
-            init = false;
-            read = false;
-            init(gl);
-        }
-        gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
-        gl.glEnableClientState(GL2.GL_COLOR_ARRAY);
-
-        gl.glDepthMask(false);
-
-        vertexVBO.bindArray(gl);
-
-        gl.glColorPointer(4, GL2.GL_FLOAT, 7 * 4, 3 * 4);
-        gl.glVertexPointer(3, GL2.GL_FLOAT, 7 * 4, 0);
-
-        gl.glLineWidth(PfssSettings.LINE_WIDTH);
-        gl.glDrawArrays(GL2.GL_LINE_STRIP, 0, vertexVBO.bufferSize);
-
-        vertexVBO.unbindArray(gl);
-        gl.glDepthMask(true);
-
-        gl.glDisableClientState(GL2.GL_VERTEX_ARRAY);
-        gl.glDisableClientState(GL2.GL_COLOR_ARRAY);
-    }
-
-    public boolean isInit() {
-        return init;
-    }
-
-    public void setInit(boolean _init) {
-        init = _init;
-    }
-
-    public String getDateObs() {
-        return dateObs;
-    }
+	public String getDateObs() {
+		return dateObs;
+	}
 
 }
