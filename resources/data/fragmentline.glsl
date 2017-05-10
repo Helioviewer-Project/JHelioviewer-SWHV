955fdc818e49736a29673a084f6907aa0ea8c648
freekv <freek.verstringe@oma.be>
2017-05-10 09:32:13 +0200

    add line shader

---
+++ resources/data/fragmentline.glsl
@@ -0,0 +1,6 @@
+#version 110
+uniform vec3 color;
+
+void main() {
+  gl_FragColor = vec4(1.0,0.0,0.0, 1.0);
+}
--- src/org/helioviewer/jhv/opengl/GLSLLineShader.java
+++ src/org/helioviewer/jhv/opengl/GLSLLineShader.java
@@ -4,22 +4,72 @@ import com.jogamp.opengl.GL2;
 
 public class GLSLLineShader extends GLSLShader {
 
-    private static final GLSLLineShader line = new GLSLLineShader("/data/vertex.glsl", "/data/fragmentortho.glsl");
+    public static final GLSLLineShader line = new GLSLLineShader("/data/vertexline.glsl", "/data/fragmentline.glsl");
+    public static  int previousLineRef = 0;
+    public static int lineRef = 1;
+    public static int nextLineRef= 2;
+    public static int directionRef= 3;
+
+    public int miterRef;
+    private int thicknessRef;
+    private int aspectRef;
+    
+    private int[] miter = {1};
+    private float[] thickness = {0.005f};
+    private float[] aspect = {1};
+	public boolean inited =false;
+
+
+
 
     public GLSLLineShader(String vertex, String fragment) {
         super(vertex, fragment);
     }
 
     public static void init(GL2 gl) {
-        line._init(gl);
+        line._init(gl, false);
     }
 
     public static void dispose(GL2 gl) {
         line._dispose(gl);
+    }  
+    
+    protected  void _dispose(GL2 gl) {
+    	super._dispose(gl);
+        inited = false;
     }
 
+    protected  void _init(GL2 gl, boolean f) {
+    	super._init(gl, f);
+        inited = true;
+    }
     @Override
     protected void _after_init(GL2 gl) {
+        previousLineRef = gl.glGetAttribLocation(progID, "previousLine");
+        lineRef = gl.glGetAttribLocation(progID, "line");
+        nextLineRef = gl.glGetAttribLocation(progID, "nextLine");
+        directionRef = gl.glGetAttribLocation(progID, "direction");
+        
+        aspectRef = gl.glGetUniformLocation(progID, "aspect");
+        miterRef = gl.glGetUniformLocation(progID, "miter");
+        thicknessRef = gl.glGetUniformLocation(progID, "thickness"); 
+    }
+    
+    public void bindParams(GL2 gl) {
+        gl.glUniform1fv(thicknessRef, 1, thickness, 0);
+        gl.glUniform1fv(aspectRef, 1, aspect, 0);
+        gl.glUniform1iv(miterRef, 1, miter, 0);
+    }
+    
+    public void setAspect(float _aspect) {
+    	aspect[0] = _aspect; 	
+    }
+    
+    protected void bindAttribs(GL2 gl){
+        gl.glBindAttribLocation(progID, previousLineRef, "previousLine");
+        gl.glBindAttribLocation(progID, lineRef, "line");
+        gl.glBindAttribLocation(progID, nextLineRef, "nextLine");
+        gl.glBindAttribLocation(progID, directionRef, "direction");
     }
 
 }
