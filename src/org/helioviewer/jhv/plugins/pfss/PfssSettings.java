1490db293e286f907bc521583d236c83e83ca03b
freekv <freek.verstringe@oma.be>
2017-05-12 14:50:02 +0200

    reduce

--- src/org/helioviewer/jhv/plugins/pfss/PfssSettings.java
+++ src/org/helioviewer/jhv/plugins/pfss/PfssSettings.java
@@ -5,8 +5,6 @@ public class PfssSettings {
     // Maximal preload of cache data
     public static final int CACHE_SIZE = 365 * 4;
 
-    public static final float LINE_ALPHA = 1;
-    public static final float LINE_WIDTH = 2;
     public static final int POINTS_PER_LINE = 40;
 
     public static final String baseURL = "http://swhv.oma.be/magtest/pfss/";
