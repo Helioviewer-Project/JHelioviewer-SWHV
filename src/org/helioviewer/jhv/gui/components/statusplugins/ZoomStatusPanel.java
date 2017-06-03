31c0a139aee515e90f2370dfd7c016b879abf3b2
Bogdan Nicula <bogdan@nicula.net>
2017-06-03 13:49:25 +0200

    Show FOV in au beyond 64Rsun

--- src/org/helioviewer/jhv/gui/components/statusplugins/ZoomStatusPanel.java
+++ src/org/helioviewer/jhv/gui/components/statusplugins/ZoomStatusPanel.java
@@ -10,8 +10,15 @@ public class ZoomStatusPanel extends StatusPanel.StatusPlugin {
         update(1, 1);
     }
 
+    String formatFOV(double r) {
+        if (r < 2 * 32 * Sun.Radius)
+            return String.format("%5.2fR\u2299", r);
+        else
+            return String.format("%.2fau", r * Sun.MeanEarthDistanceInv);
+    }
+
     public void update(double cameraWidth, double distance) {
-        setText(String.format("FOV: %5.2fR\u2299 | D\u2299: %6.3fau", 2 * cameraWidth, distance * Sun.MeanEarthDistanceInv));
+        setText(String.format("FOV: %s | D\u2299: %6.3fau", formatFOV(2 * cameraWidth), distance * Sun.MeanEarthDistanceInv));
     }
 
 }
