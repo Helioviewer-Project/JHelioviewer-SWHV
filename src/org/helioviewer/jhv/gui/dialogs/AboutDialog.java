f18a1447ea5df13c05e3a836de32eba13769e810
Bogdan Nicula <bogdan@nicula.net>
2017-06-30 10:11:21 +0200

    Tinker with about dialog

--- src/org/helioviewer/jhv/gui/dialogs/AboutDialog.java
+++ src/org/helioviewer/jhv/gui/dialogs/AboutDialog.java
@@ -13,9 +13,6 @@ import javax.swing.event.HyperlinkListener;
 
 import org.helioviewer.jhv.JHVGlobals;
 import org.helioviewer.jhv.base.FileUtils;
-import org.helioviewer.jhv.base.plugin.PluginContainer;
-import org.helioviewer.jhv.base.plugin.PluginManager;
-import org.helioviewer.jhv.base.plugin.Plugin;
 import org.helioviewer.jhv.gui.IconBank;
 import org.helioviewer.jhv.gui.IconBank.JHVIcon;
 import org.helioviewer.jhv.gui.ImageViewerGui;
@@ -39,24 +36,22 @@ public class AboutDialog extends StandardDialog implements ShowableDialog, Hyper
 
     @Override
     public JComponent createContentPanel() {
-        StringBuilder text = new StringBuilder("This software uses the <a href=\"http://www.kakadusoftware.com\">Kakadu JPEG2000 Toolkit</a>, ©2015, NewSouth Innovations Ltd (NSI). <a href=Kakadu.txt>License</a>");
-        text.append("<p>This software uses <a href=\"https://jogamp.org\">JogAmp</a>, the Java high performance libraries for 3D Graphics, Multimedia and Processing,<br/>©JogAmp Community and others.");
-        text.append("<p>This software uses <a href=\"http://logging.apache.org/log4j/index.html\">log4j</a> from the Apache Logging Services Project, licensed under the <a href=\"https://www.apache.org/licenses/LICENSE-2.0\">Apache License version 2.0</a>,<br/>©2010, The Apache Software Foundation.");
-        text.append("<p>This software uses the <a href=\"https://github.com/stleary/JSON-java\">JSON in Java</a> Library, licensed under a custom <a href=\"http://www.json.org/license.html\">License</a>.");
-        text.append("<p>This software uses the <a href=\"https://github.com/everit-org/json-schema\">JSON Schema Validator</a> Library, licensed under the <a href=\"https://www.apache.org/licenses/LICENSE-2.0\">Apache License version 2.0</a>.");
-        text.append("<p>This software uses the <a href=\"https://github.com/xerial/sqlite-jdbc\">Xerial SQLite JDBC Driver</a>, licensed under the <a href=\"https://www.apache.org/licenses/LICENSE-2.0\">Apache License version 2.0</a>.");
-        text.append("<p>This software uses <a href=\"http://jcodec.org\">JCodec</a>, licensed under the FreeBSD License.");
-        text.append("<p>This software uses <a href=\"https://github.com/jidesoft/jide-oss\">JIDE Common Layer</a>, ©2002-2017, JIDE Software, Inc.");
-        text.append("<p>This software uses the <a href=\"http://nom-tam-fits.github.io/nom-tam-fits/\">FITS in Java</a> public domain library.");
-
-        for (PluginContainer pluginContainer : PluginManager.getSingletonInstance().getAllPlugins()) {
-            Plugin plugin = pluginContainer.getPlugin();
-            text.append("<hr><b>").append(plugin.getName()).append("</b><br/>").append(plugin.getAboutLicenseText());
-        }
+        String text = "<center>This software was built using several components including:</center><ul>" +
+            "<li>The <a href=\"http://www.kakadusoftware.com\">Kakadu</a> JPEG2000 Toolkit, ©2015, <a href=Kakadu.txt>licensed</a> from NewSouth Innovations Ltd.</li>" +
+            "<li><a href=\"https://jogamp.org\">JogAmp</a>, the Java libraries for 3D Graphics, Multimedia and Processing.</li>" +
+            "<li>The <a href=\"https://github.com/stleary/JSON-java\">JSON in Java</a> library.</li>" +
+            "<li>The JSON <a href=\"https://github.com/everit-org/json-schema\">Schema Validator</a> library.</li>" +
+            "<li>The <a href=\"https://github.com/xerial/sqlite-jdbc\">Xerial</a> SQLite JDBC driver.</li>" +
+            "<li><a href=\"http://jcodec.org\">JCodec</a>, a pure Java video codec library.</li>" +
+            "<li>The <a href=\"http://www.star.bristol.ac.uk/%7Embt/jsamp/index.html\">JSAMP</a> toolkit for the Simple Applications Messaging Protocol.</li>" +
+            "<li>The <a href=\"http://www.ocpsoft.org/prettytime/nlp\">PrettyTime NLP</a> human time parsing library.</li>" +
+            "<li><a href=\"https://github.com/jidesoft/jide-oss\">JIDE Common Layer</a>, ©2002-2017, JIDE Software, Inc.</li>" +
+            "<li>The <a href=\"http://nom-tam-fits.github.io/nom-tam-fits/\">FITS in Java</a> public domain library.</li>" +
+            "<li>The <a href=\"http://logging.apache.org/log4j/index.html\">log4j</a> logging library.</li>";
 
         JTextPane pane = new JTextPane();
         pane.setContentType("text/html");
-        pane.setText(text.toString());
+        pane.setText(text);
         pane.setEditable(false);
         pane.addHyperlinkListener(this);
         pane.putClientProperty(JTextPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
