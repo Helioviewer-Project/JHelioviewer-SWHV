66b182a9a7b9c3891cd8ef74079ef1ffe11d30fb
Bogdan Nicula <bogdan@nicula.net>
2017-11-20 08:45:13 +0100

    Select loaded interval

--- src/org/helioviewer/jhv/plugins/eve/lines/LoadThread.java
+++ src/org/helioviewer/jhv/plugins/eve/lines/LoadThread.java
@@ -6,6 +6,7 @@ import org.helioviewer.jhv.threads.JHVWorker;
 import org.helioviewer.jhv.timelines.Timelines;
 import org.helioviewer.jhv.timelines.band.Band;
 import org.helioviewer.jhv.timelines.band.BandType;
+import org.helioviewer.jhv.timelines.draw.DrawController;
 
 class LoadThread extends JHVWorker<EVEResponse, Void> {
 
@@ -34,6 +35,8 @@ class LoadThread extends JHVWorker<EVEResponse, Void> {
                     Band band = new Band(r.bandType == null ? BandType.getBandType(r.bandName) : r.bandType);
                     band.addToCache(r.values, r.dates);
                     Timelines.getModel().addLineData(band);
+                    if (r.dates.length > 0)
+                        DrawController.setSelectedInterval(r.dates[0], r.dates[r.dates.length - 1]);
                 }
             } catch (Exception e) {
                 e.printStackTrace();
--- src/org/helioviewer/jhv/timelines/view/linedataselector/TimelineTableModel.java
+++ src/org/helioviewer/jhv/timelines/view/linedataselector/TimelineTableModel.java
@@ -97,5 +97,4 @@ public class TimelineTableModel extends AbstractTableModel {
         return ct;
     }
 
-
 }
