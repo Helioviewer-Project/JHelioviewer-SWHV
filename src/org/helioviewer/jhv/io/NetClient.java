7a841119897f69533fb85636f605e482e4aa8f32
Bogdan Nicula <bogdan@nicula.net>
2017-12-01 11:30:48 +0100

    Settings and logging

--- src/org/helioviewer/jhv/io/NetClient.java
+++ src/org/helioviewer/jhv/io/NetClient.java
@@ -4,7 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
+import org.helioviewer.jhv.JHVGlobals;
+import org.helioviewer.jhv.log.Log;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -12,16 +17,23 @@ import okio.BufferedSource;
 
 public class NetClient implements AutoCloseable {
 
-    private static final OkHttpClient client = new OkHttpClient();
+    private static final OkHttpClient client = new OkHttpClient.Builder()
+        .connectTimeout(JHVGlobals.getStdConnectTimeout(), TimeUnit.MILLISECONDS)
+        .readTimeout(JHVGlobals.getStdReadTimeout(), TimeUnit.MILLISECONDS)
+        //.addInterceptor(new LoggingInterceptor())
+        .build();
+
+    private final Request.Builder requestBuilder = new Request.Builder()
+        .header("User-Agent", JHVGlobals.userAgent);
 
     private Response response;
 
     public NetClient(String url) throws IOException {
-        call(new Request.Builder().url(url).build());
+        call(requestBuilder.url(url).build());
     }
 
     public NetClient(URL url) throws IOException {
-        call(new Request.Builder().url(url).build());
+        call(requestBuilder.url(url).build());
     }
 
     private void call(Request request) throws IOException {
@@ -49,6 +61,21 @@ public class NetClient implements AutoCloseable {
     @Override
     public void close() {
         response.close();
+    }
+
+    private static class LoggingInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            long t1 = System.nanoTime();
+            Request r1 = chain.request();
+            Log.info(String.format("Sending request %s on %s%n%s", r1.url(), chain.connection(), r1.headers()));
+
+            Response r2 = chain.proceed(r1);
+            long t2 = System.nanoTime();
+            Log.info(String.format("Received response for %s in %.1fms%n%s", r1.url(), (t2 - t1) / 1e6d, r2.networkResponse().headers()));
+
+            return r2;
+        }
     }
 
 }
