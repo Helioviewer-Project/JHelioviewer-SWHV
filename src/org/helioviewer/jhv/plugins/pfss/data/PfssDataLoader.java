0c8c3bf92b755a3a108925f098fd9330a92309a9
Bogdan Nicula <bogdan@nicula.net>
2017-12-20 13:51:01 +0100

    Mention trouble PFSS URL

--- src/org/helioviewer/jhv/plugins/pfss/data/PfssDataLoader.java
+++ src/org/helioviewer/jhv/plugins/pfss/data/PfssDataLoader.java
@@ -26,28 +26,28 @@ class PfssDataLoader implements Runnable {
         try (NetClient nc = NetClient.of(url); Fits fits = new Fits(nc.getStream())) {
             BasicHDU<?> hdus[] = fits.read();
             if (hdus == null || hdus.length < 2 || !(hdus[1] instanceof BinaryTableHDU))
-                throw new Exception("Could not read FITS");
+                throw new Exception("Could not read FITS: " + url);
 
             BinaryTableHDU bhdu = (BinaryTableHDU) hdus[1];
             Header header = bhdu.getHeader();
 
             String dateFits = header.getStringValue("DATE-OBS");
             if (dateFits == null)
-                throw new Exception("DATE-OBS not found");
+                throw new Exception("DATE-OBS not found: " + url);
             JHVDate date = new JHVDate(dateFits);
             if (time != date.milli)
-                throw new Exception("Inconsistent DATE-OBS. Expected " + new JHVDate(time) + ", got " + date);
+                throw new Exception("Inconsistent DATE-OBS. Expected " + new JHVDate(time) + ", got " + date + ": " + url);
 
             int points = header.getIntValue("HIERARCH.POINTS_PER_LINE");
             if (points == 0)
-                throw new Exception("POINTS_PER_LINE not found");
+                throw new Exception("POINTS_PER_LINE not found: " + url);
 
             short[] flinex = (short[]) bhdu.getColumn("FIELDLINEx");
             short[] fliney = (short[]) bhdu.getColumn("FIELDLINEy");
             short[] flinez = (short[]) bhdu.getColumn("FIELDLINEz");
             short[] flines = (short[]) bhdu.getColumn("FIELDLINEs");
             if (flinex.length != fliney.length || flinex.length != flinez.length || flinex.length != flines.length)
-                throw new Exception("Fieldline arrays not equal " + flinex.length + " " + fliney.length + " " + flinez.length + " " + flinex.length);
+                throw new Exception("Fieldline arrays not equal " + flinex.length + " " + fliney.length + " " + flinez.length + " " + flinex.length + ": " + url);
 
             PfssData pfssData = new PfssData(date, flinex, fliney, flinez, flines, points);
             EventQueue.invokeLater(() -> PfssPlugin.getPfsscache().addData(time, pfssData));
