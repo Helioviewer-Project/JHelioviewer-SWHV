4ecc52964c768e70d6fc531566d9d7c94f819391
Bogdan Nicula <bogdan@nicula.net>
2017-12-31 02:56:47 +0100

    Proper GOES rounding

--- src/org/helioviewer/jhv/base/conversion/GOESLevel.java
+++ src/org/helioviewer/jhv/base/conversion/GOESLevel.java
@@ -3,16 +3,25 @@ package org.helioviewer.jhv.base.conversion;
 public class GOESLevel {
 
     public static String getStringValue(double v) {
-        if (v < 1e-7)
-            return String.format("A%.1f", v * 1e8);
-        else if (v < 1e-6)
-            return String.format("B%.1f", v * 1e7);
-        else if (v < 1e-5)
-            return String.format("C%.1f", v * 1e6);
-        else if (v < 1e-4)
-            return String.format("M%.1f", v * 1e5);
-        else
-            return String.format("X%.1f", v * 1e4);
+        double d;
+
+        d = Math.round(v * 1e9);
+        if (d < 1e2)
+            return String.format("A%.1f", d * 1e-1);
+
+        d = Math.round(v * 1e8);
+        if (d < 1e2)
+            return String.format("B%.1f", d * 1e-1);
+
+        d = Math.round(v * 1e7);
+        if (d < 1e2)
+            return String.format("C%.1f", d * 1e-1);
+
+        d = Math.round(v * 1e6);
+        if (d < 1e2)
+            return String.format("M%.1f", d * 1e-1);
+
+        return String.format("X%.1f", v * 1e4);
     }
 
     public static double getFloatValue(String s) {
