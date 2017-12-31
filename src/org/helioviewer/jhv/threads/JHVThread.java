91a6d20675538008d47fe07bb77cf22a5f645443
Bogdan Nicula <bogdan@nicula.net>
2017-12-31 13:47:36 +0100

    Force event db recreation

--- src/org/helioviewer/jhv/threads/JHVThread.java
+++ src/org/helioviewer/jhv/threads/JHVThread.java
@@ -59,7 +59,7 @@ public class JHVThread {
 
     public static class ConnectionThread extends Thread {
 
-        private static final int CURRENT_VERSION_SCHEMA = 7;
+        private static final int CURRENT_VERSION_SCHEMA = 8;
         private static Connection connection;
 
         ConnectionThread(Runnable r, String name) {
@@ -70,16 +70,16 @@ public class JHVThread {
             try {
                 try (Statement statement = connection.createStatement()) {
                     statement.setQueryTimeout(30);
-                    statement.executeUpdate("CREATE TABLE if not exists event_type (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING , supplier STRING, UNIQUE(name, supplier) ON CONFLICT IGNORE)");
-                    statement.executeUpdate("CREATE TABLE if not exists events (id INTEGER PRIMARY KEY AUTOINCREMENT, type_id INTEGER, uid STRING , start BIGINTEGER, end BIGINTEGER, archiv BIGINTEGER , data BLOB, FOREIGN KEY(type_id) REFERENCES event_type(id), UNIQUE(uid) ON CONFLICT FAIL)");
-                    statement.executeUpdate("CREATE INDEX if not exists evt_uid ON events (uid);");
-                    statement.executeUpdate("CREATE INDEX if not exists evt_end ON events (end);");
-                    statement.executeUpdate("CREATE INDEX if not exists evt_start ON events (start);");
+                    statement.executeUpdate("CREATE TABLE if not exists event_type (id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING, supplier STRING, UNIQUE(name, supplier) ON CONFLICT IGNORE)");
+                    statement.executeUpdate("CREATE TABLE if not exists events (id INTEGER PRIMARY KEY AUTOINCREMENT, type_id INTEGER, uid STRING, start BIGINTEGER, end BIGINTEGER, archiv BIGINTEGER, data BLOB, FOREIGN KEY(type_id) REFERENCES event_type(id), UNIQUE(uid) ON CONFLICT FAIL)");
+                    statement.executeUpdate("CREATE INDEX if not exists evt_uid ON events (uid)");
+                    statement.executeUpdate("CREATE INDEX if not exists evt_end ON events (end)");
+                    statement.executeUpdate("CREATE INDEX if not exists evt_start ON events (start)");
                     statement.executeUpdate("CREATE TABLE if not exists event_link (id INTEGER PRIMARY KEY AUTOINCREMENT, left_id INTEGER, right_id INTEGER, FOREIGN KEY(left_id) REFERENCES events(id), FOREIGN KEY(right_id) REFERENCES events(id), UNIQUE(left_id, right_id) ON CONFLICT IGNORE)");
-                    statement.executeUpdate("CREATE INDEX if not exists evt_left ON event_link (left_id);");
-                    statement.executeUpdate("CREATE INDEX if not exists evt_left ON event_link (right_id);");
-                    statement.executeUpdate("CREATE TABLE if not exists date_range (id INTEGER PRIMARY KEY AUTOINCREMENT, type_id INTEGER , start BIGINTEGER , end BIGINTEGER, FOREIGN KEY(type_id) REFERENCES event_type(id))");
-                    statement.executeUpdate("CREATE TABLE if not exists version (version INTEGER PRIMARY KEY, hash INTEGER );");
+                    statement.executeUpdate("CREATE INDEX if not exists evt_left ON event_link (left_id)");
+                    statement.executeUpdate("CREATE INDEX if not exists evt_left ON event_link (right_id)");
+                    statement.executeUpdate("CREATE TABLE if not exists date_range (id INTEGER PRIMARY KEY AUTOINCREMENT, type_id INTEGER, start BIGINTEGER, end BIGINTEGER, FOREIGN KEY(type_id) REFERENCES event_type(id))");
+                    statement.executeUpdate("CREATE TABLE if not exists version (version INTEGER PRIMARY KEY, hash INTEGER)");
                 }
 
                 try (PreparedStatement pstatement = connection.prepareStatement("INSERT INTO version(version, hash) VALUES(?, ?)")) {
