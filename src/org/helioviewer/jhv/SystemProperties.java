28781552f70d1c7a03235f40eed586d86b32cf5c
Bogdan Nicula <bogdan@nicula.net>
2018-01-04 13:23:34 +0100

    Support only 64bit Java

--- src/org/helioviewer/jhv/SystemProperties.java
+++ src/org/helioviewer/jhv/SystemProperties.java
@@ -1,5 +1,7 @@
 package org.helioviewer.jhv;
 
+import javax.swing.JOptionPane;
+
 import org.helioviewer.jhv.log.Log;
 
 class SystemProperties {
@@ -11,41 +13,36 @@ class SystemProperties {
 
         String os = System.getProperty("os.name");
         String arch = System.getProperty("os.arch");
-        if (os != null && arch != null) {
-            os = os.toLowerCase();
-            arch = arch.toLowerCase();
-            if (os.contains("windows")) {
-                System.setProperty("jhv.os", "windows");
-                if (arch.contains("64"))
-                    System.setProperty("jhv.arch", "x86-64");
-                else if (arch.contains("86"))
-                    System.setProperty("jhv.arch", "x86-32");
-                else
-                    Log.error("Platform > Could not determine platform. OS: " + os + " - arch: " + arch);
-            } else if (os.contains("linux")) {
-                System.setProperty("jhv.os", "linux");
-                if (arch.contains("64"))
-                    System.setProperty("jhv.arch", "x86-64");
-                else if (arch.contains("86"))
-                    System.setProperty("jhv.arch", "x86-32");
-                else
-                    Log.error("Platform > Could not determine platform. OS: " + os + " - arch: " + arch);
-            } else if (os.contains("mac os x")) {
-                System.setProperty("jhv.os", "mac");
-                if (arch.contains("ppc"))
-                    System.setProperty("jhv.arch", "ppc");
-                else if (arch.contains("64"))
-                    System.setProperty("jhv.arch", "x86-64");
-                else if (arch.contains("86"))
-                    System.setProperty("jhv.arch", "x86-32");
-                else
-                    Log.error("Platform > Could not determine platform. OS: " + os + " - arch: " + arch);
-            } else {
-                Log.error("Platform > Could not determine platform. OS: " + os + " - arch: " + arch);
-            }
-        } else {
+        if (os == null || arch == null) {
             Log.error("Platform > Could not determine platform. OS: " + os + " - arch: " + arch);
+            return;
         }
+
+        os = os.toLowerCase();
+        arch = arch.toLowerCase();
+
+        if (arch.contains("32")) {
+            JOptionPane optionPane = new JOptionPane();
+            optionPane.setMessage("Please install Java 64-bit to run JHelioviewer.");
+            optionPane.setMessageType(JOptionPane.ERROR_MESSAGE);
+            optionPane.setOptions(new String[] { "Quit JHelioviewer" });
+            optionPane.createDialog(null, "JHelioviewer: Java 64-bit required").setVisible(true);
+            System.exit(1);
+        }
+
+        if (arch.contains("64") && arch.contains("x86"))
+            System.setProperty("jhv.arch", "x86-64");
+        else
+            Log.error("Platform > Could not determine platform. OS: " + os + " - arch: " + arch);
+
+        if (os.contains("windows"))
+            System.setProperty("jhv.os", "windows");
+        else if (os.contains("linux"))
+            System.setProperty("jhv.os", "linux");
+        else if (os.contains("mac os x"))
+            System.setProperty("jhv.os", "mac");
+        else
+            Log.error("Platform > Could not determine platform. OS: " + os + " - arch: " + arch);
     }
 
 }
