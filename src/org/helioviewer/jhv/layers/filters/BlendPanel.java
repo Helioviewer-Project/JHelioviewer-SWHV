82a4ff645a3d9ba269356ffbbe28ed2dad99f54f
Bogdan Nicula <bogdan@nicula.net>
2018-01-11 21:42:06 +0100

    Show same direction for blend slider as the others

--- src/org/helioviewer/jhv/layers/filters/BlendPanel.java
+++ src/org/helioviewer/jhv/layers/filters/BlendPanel.java
@@ -19,7 +19,7 @@ public class BlendPanel implements FilterDetails {
         label = new JLabel(LevelsPanel.align3(slider.getValue()), JLabel.RIGHT);
         slider.addChangeListener(e -> {
             layer.getGLImage().setBlend(slider.getValue() / 100.);
-            label.setText(LevelsPanel.align3(100 - slider.getValue())); // additivity
+            label.setText(LevelsPanel.align3(slider.getValue())); // additivity
             Displayer.display();
         });
         WheelSupport.installMouseWheelSupport(slider);
