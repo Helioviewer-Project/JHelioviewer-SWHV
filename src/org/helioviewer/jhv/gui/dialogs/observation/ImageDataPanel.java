741b52a8faad687cc2d572a7b9af8d4cbc98d92a
Bogdan Nicula <bogdan@nicula.net>
2018-01-19 21:43:35 +0100

    Split-off panels

--- src/org/helioviewer/jhv/gui/dialogs/observation/ImageDataPanel.java
+++ src/org/helioviewer/jhv/gui/dialogs/observation/ImageDataPanel.java
@@ -1,24 +1,13 @@
 package org.helioviewer.jhv.gui.dialogs.observation;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-
 import javax.swing.BoxLayout;
-import javax.swing.JComboBox;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
 
 import org.helioviewer.jhv.Settings;
 import org.helioviewer.jhv.base.message.Message;
-import org.helioviewer.jhv.gui.components.DateTimePanel;
-import org.helioviewer.jhv.gui.components.calendar.JHVCarringtonPicker;
 import org.helioviewer.jhv.io.APIRequest;
 import org.helioviewer.jhv.io.CommandLine;
 import org.helioviewer.jhv.io.DataSources;
@@ -35,7 +24,7 @@ import org.helioviewer.jhv.time.TimeUtils;
 @SuppressWarnings("serial")
 public class ImageDataPanel extends JPanel {
 
-    private final TimeSelectionPanel timeSelectionPanel = new TimeSelectionPanel();
+    private final TimePanel timePanel = new TimePanel();
     private final CadencePanel cadencePanel = new CadencePanel();
     private final DataSourcesTree sourcesTree = new DataSourcesTree();
     private static boolean first = true;
@@ -43,7 +32,7 @@ public class ImageDataPanel extends JPanel {
     ImageDataPanel() {
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-        add(timeSelectionPanel);
+        add(timePanel);
         add(cadencePanel);
         add(new JScrollPane(sourcesTree));
     }
@@ -64,8 +53,8 @@ public class ImageDataPanel extends JPanel {
         } else if (first) {
             first = false;
 
-            timeSelectionPanel.setStartTime(item.end - 2 * TimeUtils.DAY_IN_MILLIS);
-            timeSelectionPanel.setEndTime(item.end);
+            timePanel.setStartTime(item.end - 2 * TimeUtils.DAY_IN_MILLIS);
+            timePanel.setEndTime(item.end);
 
             if (Boolean.parseBoolean(Settings.getSingletonInstance().getProperty("startup.loadmovie"))) {
                 loadRemote(null, item);
@@ -78,14 +67,6 @@ public class ImageDataPanel extends JPanel {
         return item == null ? null : DataSources.getServerSetting(item.server, "availability.images") + "#IID" + item.sourceId;
     }
 
-    private long getStartTime() {
-        return timeSelectionPanel.getStartTime();
-    }
-
-    private long getEndTime() {
-        return timeSelectionPanel.getEndTime();
-    }
-
     public int getCadence() {
         return cadencePanel.getCadence();
     }
@@ -93,15 +74,15 @@ public class ImageDataPanel extends JPanel {
     private void loadRemote(ImageLayer layer, DataSourcesTree.SourceItem item) { // valid item
         if (layer == null)
             layer = ImageLayer.create(null);
-        layer.load(new APIRequest(item.server, item.sourceId, getStartTime(), getEndTime(), getCadence()));
+        layer.load(new APIRequest(item.server, item.sourceId, timePanel.getStartTime(), timePanel.getEndTime(), getCadence()));
     }
 
     void setupLayer(ImageLayer layer) {
         APIRequest req = layer.getAPIRequest();
         if (req != null) {
             sourcesTree.setSelectedItem(req.server, req.sourceId);
-            timeSelectionPanel.setStartTime(req.startTime);
-            timeSelectionPanel.setEndTime(req.endTime);
+            timePanel.setStartTime(req.startTime);
+            timePanel.setEndTime(req.endTime);
             cadencePanel.setCadence(req.cadence);
         }
     }
@@ -114,136 +95,13 @@ public class ImageDataPanel extends JPanel {
         }
 
         // show message if end date before start date
-        if (getStartTime() > getEndTime()) {
+        if (timePanel.getStartTime() > timePanel.getEndTime()) {
             JOptionPane.showMessageDialog(null, "End date is before start date", "", JOptionPane.ERROR_MESSAGE);
             return false;
         }
 
         loadRemote((ImageLayer) layer, item);
         return true;
-    }
-
-    // Time Selection Panel
-    private static class TimeSelectionPanel extends JPanel {
-
-        private final DateTimePanel startDateTimePanel = new DateTimePanel("Start");
-        private final DateTimePanel endDateTimePanel = new DateTimePanel("End");
-        private final JHVCarringtonPicker startCarrington = new JHVCarringtonPicker();
-        private final JHVCarringtonPicker endCarrington = new JHVCarringtonPicker();
-
-        TimeSelectionPanel() {
-            long milli = (System.currentTimeMillis() / 1000L) * 1000L;
-            setStartTime(milli - 2 * TimeUtils.DAY_IN_MILLIS);
-            setEndTime(milli);
-
-            startDateTimePanel.addListener(e -> setStartTime(startDateTimePanel.getTime()));
-            endDateTimePanel.addListener(e -> setEndTime(endDateTimePanel.getTime()));
-            startCarrington.addJHVCalendarListener(e -> setStartTime(startCarrington.getTime()));
-            endCarrington.addJHVCalendarListener(e -> setEndTime(endCarrington.getTime()));
-
-            startDateTimePanel.add(startCarrington);
-            endDateTimePanel.add(endCarrington);
-
-            setLayout(new GridBagLayout());
-            GridBagConstraints c = new GridBagConstraints();
-            c.anchor = GridBagConstraints.LINE_START;
-            c.fill = GridBagConstraints.HORIZONTAL;
-            c.weighty = 0;
-
-            c.gridy = 0;
-            c.gridx = 0;
-            c.weightx = 1;
-            add(startDateTimePanel, c);
-            c.gridx = 1;
-            c.weightx = 0;
-            add(startCarrington, c);
-
-            c.gridy = 1;
-            c.gridx = 0;
-            c.weightx = 1;
-            add(endDateTimePanel, c);
-            c.gridx = 1;
-            c.weightx = 0;
-            add(endCarrington, c);
-        }
-
-        void setStartTime(long time) {
-            startDateTimePanel.setTime(time);
-            startCarrington.setTime(time);
-        }
-
-        void setEndTime(long time) {
-            endDateTimePanel.setTime(time);
-            endCarrington.setTime(time);
-        }
-
-        long getStartTime() {
-            return startDateTimePanel.getTime();
-        }
-
-        long getEndTime() {
-            return endDateTimePanel.getTime();
-        }
-
-    }
-
-    // Cadence Panel
-    private static class CadencePanel extends JPanel {
-
-        private static final String[] timeStepUnitStrings = { "sec", "min", "hours", "days", "get all" };
-
-        private final JSpinner spinnerCadence = new JSpinner(new SpinnerNumberModel(1, 1, 1000000, 1));
-        private final JComboBox<String> comboUnit = new JComboBox<>(timeStepUnitStrings);
-
-        CadencePanel() {
-            setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 0));
-
-            setCadence(APIRequest.CADENCE_DEFAULT);
-            spinnerCadence.setPreferredSize(new Dimension(50, spinnerCadence.getPreferredSize().height));
-            comboUnit.setSelectedItem("min");
-            comboUnit.addActionListener(e -> spinnerCadence.setEnabled(comboUnit.getSelectedIndex() != 4));
-
-            add(new JLabel("Time step", JLabel.RIGHT));
-            add(spinnerCadence);
-            add(comboUnit);
-        }
-
-        // Returns the number of seconds of the selected cadence
-        int getCadence() {
-            int value = (Integer) spinnerCadence.getValue();
-
-            switch (comboUnit.getSelectedIndex()) {
-            case 1: // min
-                return value * 60;
-            case 2: // hour
-                return value * 3600;
-            case 3: // day
-                return value * 86400;
-            case 4:
-                return APIRequest.CADENCE_ANY;
-            default:
-                return value;
-            }
-        }
-
-        void setCadence(int value) {
-            if (value == APIRequest.CADENCE_ANY)
-                comboUnit.setSelectedItem(timeStepUnitStrings[4]);
-            else if (value / 86400 != 0) {
-                comboUnit.setSelectedItem(timeStepUnitStrings[3]);
-                spinnerCadence.setValue(value / 86400);
-            } else if (value / 3600 != 0) {
-                comboUnit.setSelectedItem(timeStepUnitStrings[2]);
-                spinnerCadence.setValue(value / 3600);
-            } else if (value / 60 != 0) {
-                comboUnit.setSelectedItem(timeStepUnitStrings[1]);
-                spinnerCadence.setValue(value / 60);
-            } else {
-                comboUnit.setSelectedItem(timeStepUnitStrings[0]);
-                spinnerCadence.setValue(value);
-            }
-        }
-
     }
 
 }
