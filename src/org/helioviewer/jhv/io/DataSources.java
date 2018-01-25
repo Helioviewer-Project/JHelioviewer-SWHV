e5d080767a17f77df85deefad46fdfff13502da0
Bogdan Nicula <bogdan@nicula.net>
2018-01-25 12:52:49 +0100

    Fortify sources settings

--- src/org/helioviewer/jhv/io/DataSources.java
+++ src/org/helioviewer/jhv/io/DataSources.java
@@ -20,9 +20,9 @@ public class DataSources {
         "SOHO", "SDO", "STEREO_A", "STEREO_B", "PROBA2", "ROB-USET", "ROB-Humain", "NSO-GONG", "NSO-SOLIS", "Kanzelhoehe", "NRH", "Yohkoh", "Hinode", "TRACE"
     )));
 
-    private static final HashMap<String, HashMap<String, String>> serverSettings = new HashMap<String, HashMap<String, String>>() {
+    private static final Map<String, Map<String, String>> serverSettings = Collections.unmodifiableMap(new HashMap<String, Map<String, String>>() {
         {
-            put("ROB", new HashMap<String, String>() {
+            put("ROB", Collections.unmodifiableMap(new HashMap<String, String>() {
                 {
                     put("API.getDataSources", "http://swhv.oma.be/hv/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
                     put("API.getJP2Image", "http://swhv.oma.be/hv/api/index.php?action=getJP2Image&");
@@ -31,8 +31,8 @@ public class DataSources {
                     put("schema", "/data/sources_v1.0.json");
                     put("availability.images", "http://swhv.oma.be/availability/images/availability/availability.html");
                 }
-            });
-            put("IAS", new HashMap<String, String>() {
+            }));
+            put("IAS", Collections.unmodifiableMap(new HashMap<String, String>() {
                 {
                     put("API.getDataSources", "https://helioviewer-api.ias.u-psud.fr/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
                     put("API.getJP2Image", "https://helioviewer-api.ias.u-psud.fr/v2/getJP2Image/?");
@@ -40,8 +40,8 @@ public class DataSources {
                     put("label", "Institut d'Astrophysique Spatiale");
                     put("schema", "/data/sources_v1.0.json");
                 }
-            });
-            put("GSFC", new HashMap<String, String>() {
+            }));
+            put("GSFC", Collections.unmodifiableMap(new HashMap<String, String>() {
                 {
                     put("API.getDataSources", "https://api.helioviewer.org/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
                     put("API.getJP2Image", "https://api.helioviewer.org/v2/getJP2Image/?");
@@ -49,9 +49,9 @@ public class DataSources {
                     put("label", "Goddard Space Flight Center");
                     put("schema", "/data/sources_v1.0.json");
                 }
-            });
+            }));
             /*
-            put("GSFC SCI Test", new HashMap<String, String>() {
+            put("GSFC SCI Test", Collections.unmodifiableMap(new HashMap<String, String>() {
                 {
                     put("API.getDataSources", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getDataSources&verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
                     put("API.getJP2Image", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getJP2Image&");
@@ -59,8 +59,8 @@ public class DataSources {
                     put("label", "Goddard Space Flight Center SCI Test");
                     put("schema", "/data/sources_v1.0.json");
                 }
-            });
-            put("GSFC NDC Test", new HashMap<String, String>() {
+            }));
+            put("GSFC NDC Test", Collections.unmodifiableMap(new HashMap<String, String>() {
                 {
                     put("API.getDataSources", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getDataSources&verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
                     put("API.getJP2Image", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getJP2Image&");
@@ -68,8 +68,8 @@ public class DataSources {
                     put("label", "Goddard Space Flight Center NDC Test");
                     put("schema", "/data/sources_v1.0.json");
                 }
-            });
-            put("LOCALHOST", new HashMap<String, String>() {
+            }));
+            put("LOCALHOST", Collections.unmodifiableMap(new HashMap<String, String>() {
                 {
                     put("API.getDataSources", "http://localhost:8080/helioviewer/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
                     put("API.getJP2Image", "http://localhost:8080/helioviewer/api/index.php?action=getJP2Image&");
@@ -77,10 +77,10 @@ public class DataSources {
                     put("schema", "/data/sources_v1.0.json");
                     put("label", "Localhost");
                 }
-            });
+            }));
              */
         }
-    };
+    });
 
     public static Set<String> getServers() {
         return serverSettings.keySet();
