cec5ff46ccc82a4dd4c9af7dc51264530c8829b4
Bogdan Nicula <bogdan@nicula.net>
2018-01-25 09:42:56 +0100

    Remove date selection from timelines dialog

--- src/org/helioviewer/jhv/timelines/view/TimelineDataPanel.java
+++ src/org/helioviewer/jhv/timelines/view/TimelineDataPanel.java
@@ -9,30 +9,21 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import org.helioviewer.jhv.gui.components.calendar.JHVCalendarDatePicker;
-import org.helioviewer.jhv.layers.Movie;
-import org.helioviewer.jhv.time.TimeUtils;
 import org.helioviewer.jhv.timelines.TimelineLayer;
 import org.helioviewer.jhv.timelines.TimelineLayers;
 import org.helioviewer.jhv.timelines.Timelines;
 import org.helioviewer.jhv.timelines.band.Band;
 import org.helioviewer.jhv.timelines.band.BandType;
-import org.helioviewer.jhv.timelines.draw.DrawController;
 
 @SuppressWarnings("serial")
 public class TimelineDataPanel extends JPanel {
 
-    private final JHVCalendarDatePicker calendarStartDate = new JHVCalendarDatePicker();
     private final JComboBox<String> comboBoxGroup = new JComboBox<>();
     private final JComboBox<BandType> comboBoxData = new JComboBox<>();
 
-    private boolean userSet;
-
     public TimelineDataPanel() {
         setLayout(new GridBagLayout());
-
         comboBoxGroup.addActionListener(e -> updateGroupValues());
-        calendarStartDate.addJHVCalendarListener(e -> userSet = true);
 
         GridBagConstraints c = new GridBagConstraints();
         c.anchor = GridBagConstraints.WEST;
@@ -41,17 +32,11 @@ public class TimelineDataPanel extends JPanel {
 
         c.gridy = 0;
         c.gridx = 0;
-        add(new JLabel("Start date", JLabel.RIGHT), c);
-        c.gridx = 1;
-        add(calendarStartDate, c);
-
-        c.gridy = 1;
-        c.gridx = 0;
         add(new JLabel("Group", JLabel.RIGHT), c);
         c.gridx = 1;
         add(comboBoxGroup, c);
 
-        c.gridy = 2;
+        c.gridy = 1;
         c.gridx = 0;
         add(new JLabel("Dataset", JLabel.RIGHT), c);
         c.gridx = 1;
@@ -67,9 +52,6 @@ public class TimelineDataPanel extends JPanel {
     }
 
     void updateGroupValues() {
-        if (!userSet)
-            calendarStartDate.setTime(Movie.getStartTime());
-
         String selectedGroup = (String) comboBoxGroup.getSelectedItem();
         if (selectedGroup == null)
             return;
@@ -96,18 +78,7 @@ public class TimelineDataPanel extends JPanel {
         BandType bandType = (BandType) comboBoxData.getSelectedItem();
         if (bandType == null)
             return;
-
         Timelines.getLayers().add(new Band(bandType));
-
-        long time = calendarStartDate.getTime();
-        long movieStart = Movie.getStartTime();
-        long movieEnd = Movie.getEndTime();
-        if (time >= movieStart && time <= movieEnd) {
-            DrawController.setSelectedInterval(movieStart, movieEnd);
-        } else {
-            long now = System.currentTimeMillis();
-            DrawController.setSelectedInterval(Math.min(time, now), Math.min(time + 2 * TimeUtils.DAY_IN_MILLIS, now));
-        }
     }
 
 }
