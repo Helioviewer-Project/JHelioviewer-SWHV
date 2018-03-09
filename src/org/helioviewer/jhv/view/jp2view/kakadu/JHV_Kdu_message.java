979050c600be5d00fca4eebb7a13b44e35cf59d5
Bogdan Nicula <bogdan@nicula.net>
2018-03-09 11:29:58 +0100

    Try harder to not crash on KDU exceptions

--- src/org/helioviewer/jhv/view/jp2view/kakadu/JHV_Kdu_message.java
+++ src/org/helioviewer/jhv/view/jp2view/kakadu/JHV_Kdu_message.java
@@ -1,31 +1,14 @@
 package org.helioviewer.jhv.view.jp2view.kakadu;
 
 import kdu_jni.KduException;
-import kdu_jni.Kdu_message;
+import kdu_jni.Kdu_global;
+import kdu_jni.Kdu_message_queue;
 
-/**
- * This class allows to print Kakadu error messages, throwing Java exceptions if
- * it is necessary.
- */
-class JHV_Kdu_message extends Kdu_message {
+// This class allows to print Kakadu error messages, throwing Java exceptions if necessary.
+class JHV_Kdu_message extends Kdu_message_queue {
 
-    private final boolean raiseException;
-
-    public JHV_Kdu_message(boolean _raiseException) {
-        raiseException = _raiseException;
-    }
-
-/*  Only for debug, may crash
-    @Override
-    public void Put_text(String text) {
-        System.out.print(text);
-    }
-*/
-
-    @Override
-    public void Flush(boolean endOfMessage) throws KduException {
-        if (endOfMessage && raiseException)
-            throw new KduException("Kakadu message error");
+    public JHV_Kdu_message(boolean throwExceptions) throws KduException {
+        Configure(1, true, throwExceptions, Kdu_global.KDU_ERROR_EXCEPTION);
     }
 
 }
