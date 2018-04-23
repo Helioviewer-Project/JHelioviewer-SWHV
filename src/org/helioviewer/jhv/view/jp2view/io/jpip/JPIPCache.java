475734d46b6812d9404c666d7f5dbcfc3fd4c2e7
Bogdan Nicula <bogdan@nicula.net>
2018-04-23 21:17:48 +0200

    Retrieve data from KDU

--- src/org/helioviewer/jhv/view/jp2view/io/jpip/JPIPCache.java
+++ src/org/helioviewer/jhv/view/jp2view/io/jpip/JPIPCache.java
@@ -1,17 +1,15 @@
 package org.helioviewer.jhv.view.jp2view.io.jpip;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 import kdu_jni.KduException;
 import kdu_jni.Kdu_cache;
+import kdu_jni.Kdu_global;
 
 import org.helioviewer.jhv.view.jp2view.kakadu.JHV_KduException;
 import org.helioviewer.jhv.view.jp2view.kakadu.KakaduConstants;
 
 public class JPIPCache extends Kdu_cache {
-
-    private final HashMap<Integer, JPIPStream> map = new HashMap<>();
 
     public boolean isDataBinCompleted(int klassID, long streamID, long binID) throws JHV_KduException {
         boolean complete[] = new boolean[1];
@@ -31,9 +29,37 @@ public class JPIPCache extends Kdu_cache {
         }
     }
 
+    private JPIPStream scan(int frame) throws KduException {
+        int flags = Kdu_global.KDU_CACHE_SCAN_START | Kdu_global.KDU_CACHE_SCAN_FIX_CODESTREAM;
+        int klassID[] = new int[1];
+        long codestreamID[] = { frame };
+        long binID[] = new long[1];
+        int binLen[] = new int[1];
+        boolean complete[] = new boolean[1];
+
+        JPIPStream stream = new JPIPStream();
+        while (Scan_databins(flags, klassID, codestreamID, binID, binLen, complete, null, 0)) {
+            flags &= ~Kdu_global.KDU_CACHE_SCAN_START;
+            byte data[] = new byte[binLen[0]];
+            if (!Scan_databins(flags | Kdu_global.KDU_CACHE_SCAN_NO_ADVANCE, klassID, codestreamID, binID, binLen, complete, data, binLen[0]))
+                break;
+            flags &= ~Kdu_global.KDU_CACHE_SCAN_NO_ADVANCE;
+            if (klassID[0] == KakaduConstants.KDU_META_DATABIN)
+                continue;
+
+            JPIPSegment seg = new JPIPSegment();
+            seg.binID = binID[0];
+            seg.klassID = klassID[0];
+            seg.codestreamID = codestreamID[0];
+            seg.length = binLen[0];
+            seg.data = data;
+            seg.isFinal = complete[0];
+            stream.segments.add(seg);
+        }
+        return stream;
+    }
+
     public void put(int frame, JPIPSegment data) throws IOException {
-        if (data.klassID != KakaduConstants.KDU_META_DATABIN)
-            map.computeIfAbsent(frame, k -> new JPIPStream()).segments.add(data);
         addToKdu(frame, data);
     }
 
@@ -43,7 +69,12 @@ public class JPIPCache extends Kdu_cache {
     }
 
     public JPIPStream get(int frame) {
-        return map.get(frame);
+        try {
+            return scan(frame);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
