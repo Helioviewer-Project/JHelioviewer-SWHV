adc30b54957dba9d949dd8c2b2413df7b04eb928
Bogdan Nicula <bogdan@nicula.net>
2018-05-21 11:06:21 +0200

    Finalize LoadZip

--- src/org/helioviewer/jhv/io/LoadZipTask.java
+++ src/org/helioviewer/jhv/io/LoadZipTask.java
@@ -10,10 +10,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import javax.annotation.Nullable;
 
 import org.helioviewer.jhv.JHVDirectory;
+import org.helioviewer.jhv.io.FileUtils;
 import org.helioviewer.jhv.log.Log;
 import org.helioviewer.jhv.threads.JHVWorker;
 
@@ -29,13 +31,35 @@ class LoadZipTask extends JHVWorker<Void, Void> {
     @Nullable
     @Override
     protected Void backgroundWork() {
-        try (FileSystem zipfs = FileSystems.newFileSystem(URI.create("jar:" + uri), new HashMap<>())) {
-            for (Path root : zipfs.getRootDirectories()) {
-                try (DirectoryStream<Path> stream = Files.newDirectoryStream(root)) {
-                    for (Path entry: stream) {
-                        Path ext = Paths.get(JHVDirectory.TEMP.getPath() + entry);
-                        Files.copy(entry, ext, StandardCopyOption.REPLACE_EXISTING);
-                        Load.image.get(ext.toUri());
+        try {
+            HashSet<Path> exclude = new HashSet<>();
+            String tmpDir = FileUtils.tempDir(JHVDirectory.TEMP.getFile(), uri.getPath().substring(Math.max(0, uri.getPath().lastIndexOf('/') + 1))).toString();
+            try (FileSystem zipfs = FileSystems.newFileSystem(URI.create("jar:" + uri), new HashMap<>())) {
+                for (Path root : zipfs.getRootDirectories()) {
+                    try (DirectoryStream<Path> stream = Files.newDirectoryStream(root, "*.jhv")) {
+                        for (Path entry: stream) {
+                            Path ext = Paths.get(tmpDir + entry);
+                            exclude.add(ext);
+                            Files.copy(entry, ext, StandardCopyOption.REPLACE_EXISTING);
+                            Load.state.get(ext.toUri());
+                        }
+                    }
+                    try (DirectoryStream<Path> stream = Files.newDirectoryStream(root, "*.json")) {
+                        for (Path entry: stream) {
+                            Path ext = Paths.get(tmpDir + entry);
+                            exclude.add(ext);
+                            Files.copy(entry, ext, StandardCopyOption.REPLACE_EXISTING);
+                            Load.request.get(ext.toUri());
+                        }
+                    }
+                    try (DirectoryStream<Path> stream = Files.newDirectoryStream(root)) {
+                        for (Path entry: stream) {
+                            Path ext = Paths.get(tmpDir + entry);
+                            if (exclude.contains(ext))
+                                continue;
+                            Files.copy(entry, ext, StandardCopyOption.REPLACE_EXISTING);
+                            Load.image.get(ext.toUri());
+                        }
                     }
                 }
             }
