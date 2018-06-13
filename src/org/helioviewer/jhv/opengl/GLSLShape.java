1eb3f11e68a3fbccf49914838bcdfdc5528aabb0
Bogdan Nicula <bogdan@nicula.net>
2018-06-13 10:48:24 +0200

    Reduce

--- src/org/helioviewer/jhv/opengl/GLSLShape.java
+++ src/org/helioviewer/jhv/opengl/GLSLShape.java
@@ -1,9 +1,7 @@
 package org.helioviewer.jhv.opengl;
 
 import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
 
-import org.helioviewer.jhv.base.BufferUtils;
 import org.helioviewer.jhv.log.Log;
 
 import com.jogamp.common.nio.Buffers;
@@ -15,37 +13,23 @@ public class GLSLShape {
     private final int[] vboAttribLens = { 4, 4 };
 
     private final VBO[] vbos = new VBO[2];
-    private VBO ivbo;
-    private boolean hasPoints = false;
+    private int count;
     private boolean inited = false;
 
     public void setData(GL2 gl, FloatBuffer points, FloatBuffer colors) {
-        hasPoints = false;
-        int plen = points.limit() / 4;
-        if (plen * 4 != points.limit() || points.limit() != colors.limit()) {
+        count = 0;
+        int plen = points.limit() / vboAttribLens[0];
+        if (plen * vboAttribLens[0] != points.limit() || points.limit() != colors.limit()) {
             Log.error("Something is wrong with the vertices or colors from this GLShape");
             return;
         }
         vbos[0].bindBufferData(gl, points, Buffers.SIZEOF_FLOAT);
         vbos[1].bindBufferData(gl, colors, Buffers.SIZEOF_FLOAT);
-
-        IntBuffer indexBuffer = gen_indices(plen);
-        ivbo.bindBufferData(gl, indexBuffer, Buffers.SIZEOF_INT);
-
-        hasPoints = true;
-    }
-
-    private static IntBuffer gen_indices(int plen) {
-        IntBuffer indicesBuffer = BufferUtils.newIntBuffer(plen);
-        for (int j = 0; j < plen; j++) {
-            indicesBuffer.put(j);
-        }
-        indicesBuffer.rewind();
-        return indicesBuffer;
+        count = plen;
     }
 
     public void renderPoints(GL2 gl, double factor) {
-        if (!hasPoints)
+        if (count == 0)
             return;
 
         GLSLShapeShader.point.bind(gl);
@@ -53,21 +37,21 @@ public class GLSLShape {
         GLSLShapeShader.point.bindParams(gl);
 
         bindVBOs(gl);
-        gl.glDrawElements(GL2.GL_POINTS, ivbo.bufferSize, GL2.GL_UNSIGNED_INT, 0);
+        gl.glDrawArrays(GL2.GL_POINTS, 0, count);
         unbindVBOs(gl);
 
         GLSLShader.unbind(gl);
     }
 
     public void renderShape(GL2 gl, int mode) {
-        if (!hasPoints)
+        if (count == 0)
             return;
 
         GLSLShapeShader.shape.bind(gl);
         GLSLShapeShader.shape.bindParams(gl);
 
         bindVBOs(gl);
-        gl.glDrawElements(mode, ivbo.bufferSize, GL2.GL_UNSIGNED_INT, 0);
+        gl.glDrawArrays(mode, 0, count);
         unbindVBOs(gl);
 
         GLSLShader.unbind(gl);
@@ -77,14 +61,12 @@ public class GLSLShape {
         for (VBO vbo : vbos) {
             vbo.bindArray(gl);
         }
-        ivbo.bindArray(gl);
     }
 
     private void unbindVBOs(GL2 gl) {
         for (VBO vbo : vbos) {
             vbo.unbindArray(gl);
         }
-        ivbo.unbindArray(gl);
     }
 
     private void initVBOs(GL2 gl) {
@@ -92,8 +74,6 @@ public class GLSLShape {
             vbos[i] = VBO.gen_float_VBO(vboAttribRefs[i], vboAttribLens[i]);
             vbos[i].init(gl);
         }
-        ivbo = VBO.gen_index_VBO();
-        ivbo.init(gl);
     }
 
     private void disposeVBOs(GL2 gl) {
@@ -102,10 +82,6 @@ public class GLSLShape {
                 vbos[i].dispose(gl);
                 vbos[i] = null;
             }
-        }
-        if (ivbo != null) {
-            ivbo.dispose(gl);
-            ivbo = null;
         }
     }
 
