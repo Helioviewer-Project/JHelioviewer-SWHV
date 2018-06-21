a2d6a646d49215dbae9783064a6c0239db45ed13
Bogdan Nicula <bogdan@nicula.net>
2018-06-21 22:57:48 +0200

    Reduce

--- resources/data/LineVertex.glsl
+++ resources/data/LineVertex.glsl
@@ -15,28 +15,20 @@ uniform mat4 view;
 
 // https://mattdesl.svbtle.com/drawing-lines-is-hard
 void main() {
-  vec2 aspectVec = vec2(aspect, 1.);
-
   mat4 ModelViewProjectionMatrix = projection * view;
   vec4 previousProjected = ModelViewProjectionMatrix * vec4(previousLine, 1.0);
   vec4 currentProjected = ModelViewProjectionMatrix * vec4(line, 1.0);
   vec4 nextProjected = ModelViewProjectionMatrix * vec4(nextLine, 1.0);
 
-  vec2 currentScreen = currentProjected.xy / currentProjected.w * aspectVec;
-  vec2 previousScreen = previousProjected.xy / previousProjected.w * aspectVec;
-  vec2 nextScreen = nextProjected.xy / nextProjected.w * aspectVec;
-
-  float len = thickness;
-
-  vec2 dir = vec2(0.0);
-  if (currentScreen == previousScreen) {
-    dir = normalize(nextScreen - currentScreen);
+  vec4 dir = vec4(0.0);
+  if (currentProjected == previousProjected) {
+    dir = normalize(nextProjected - currentProjected);
   } else {
-    dir = normalize(currentScreen - previousScreen);
+    dir = normalize(currentProjected - previousProjected);
   }
 
   vec2 normal = vec2(-dir.y, dir.x);
-  normal *= len / 2.;
+  normal *= thickness / 2.;
   normal.x /= aspect;
 
   vec4 offset = vec4(-normal * direction, 0.0, 0.0);
