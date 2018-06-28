0b79b11bb3139a70269d20f6dcb83b80a0922865
Bogdan Nicula <bogdan@nicula.net>
2018-06-28 09:34:37 +0200

    Give save state a key shortcut

--- src/org/helioviewer/jhv/gui/actions/SaveStateAction.java
+++ src/org/helioviewer/jhv/gui/actions/SaveStateAction.java
@@ -1,9 +1,13 @@
 package org.helioviewer.jhv.gui.actions;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
 
+import org.helioviewer.jhv.gui.UIGlobals;
+import org.helioviewer.jhv.input.KeyShortcuts;
 import org.helioviewer.jhv.layers.selector.State;
 
 @SuppressWarnings("serial")
@@ -11,6 +15,10 @@ public class SaveStateAction extends AbstractAction {
 
     public SaveStateAction() {
         super("Save State");
+
+        KeyStroke key = KeyStroke.getKeyStroke(KeyEvent.VK_S, UIGlobals.menuShortcutMask);
+        putValue(ACCELERATOR_KEY, key);
+        KeyShortcuts.registerKey(key, this);
     }
 
     @Override
