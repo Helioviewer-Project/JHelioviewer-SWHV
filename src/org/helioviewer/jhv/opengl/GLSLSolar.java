d6d3f312ccf1a72c55bb88ed59d1003137f905e1
Bogdan Nicula <bogdan@nicula.net>
2018-07-27 20:39:24 +0200

    Retire icosphere

--- src/org/helioviewer/jhv/opengl/GLSLSolar.java
+++ src/org/helioviewer/jhv/opengl/GLSLSolar.java
@@ -1,40 +1,34 @@
 package org.helioviewer.jhv.opengl;
 
-import java.nio.IntBuffer;
 import java.nio.FloatBuffer;
-
-import org.helioviewer.jhv.math.IcoSphere;
 
 import com.jogamp.opengl.GL2;
 
 public class GLSLSolar {
 
-    private static final FloatBuffer positionBuffer = IcoSphere.IcoSphere.a;
-    private static final IntBuffer indexBuffer = IcoSphere.IcoSphere.b;
-    private static final int indexSize = indexBuffer.limit();
-
-    private static final VBO positionVBO = VBO.gen_float_VBO(0, 4);
-    private static final VBO indexVBO = VBO.gen_index_VBO();
+    private static final float R = 32; // > C3
+    private static final FloatBuffer vertexBuffer = FloatBuffer.wrap(new float[] {
+        -R, -R, 0, 1, R, R, 0, 1, -R, R, 0, 1,
+        R, R, 0, 1, -R, -R, 0, 1, R, -R, 0, 1
+    });
+    private static final int vertexSize = vertexBuffer.limit();
+    private static final VBO vertexVBO = VBO.gen_float_VBO(0, 4);
 
     public static void render(GL2 gl) {
-        gl.glDrawElements(GL2.GL_TRIANGLES, 6, GL2.GL_UNSIGNED_INT, 4 * (indexSize - 6));
+        gl.glDrawArrays(GL2.GL_TRIANGLES, 0, vertexSize);
     }
 
     public static void bindVBOs(GL2 gl) {
-        positionVBO.bindArray(gl);
-        indexVBO.bindArray(gl);
+        vertexVBO.bindArray(gl);
     }
 
     static void init(GL2 gl) {
-        positionVBO.init(gl);
-        positionVBO.setData4(gl, positionBuffer);
-        indexVBO.init(gl);
-        indexVBO.setData4(gl, indexBuffer);
+        vertexVBO.init(gl);
+        vertexVBO.setData4(gl, vertexBuffer);
     }
 
     static void dispose(GL2 gl) {
-        positionVBO.dispose(gl);
-        indexVBO.dispose(gl);
+        vertexVBO.dispose(gl);
     }
 
 }
