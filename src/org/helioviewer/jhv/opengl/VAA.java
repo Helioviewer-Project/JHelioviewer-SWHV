5e1432129c133bef8b2c6cf8b03d69f3bc5e4304
Bogdan Nicula <bogdan@nicula.net>
2018-08-20 01:11:31 +0200

    Reduce

--- src/org/helioviewer/jhv/opengl/VAA.java
+++ src/org/helioviewer/jhv/opengl/VAA.java
@@ -8,21 +8,23 @@ class VAA {
     private final int size;
     private final int type;
     private final boolean normalized;
+    private final int stride;
     private final long offset;
     private final int divisor;
 
-    VAA(int _index, int _size, boolean _normalized, long _offset, int _divisor) {
+    VAA(int _index, int _size, boolean _normalized, int _stride, long _offset, int _divisor) {
         index = _index;
         size = _size;
         type = _normalized ? GL2.GL_UNSIGNED_BYTE : GL2.GL_FLOAT;
         normalized = _normalized;
+        stride = _stride;
         offset = _offset;
         divisor = _divisor;
     }
 
     void enable(GL2 gl) {
         gl.glEnableVertexAttribArray(index);
-        gl.glVertexAttribPointer(index, size, type, normalized, 0, offset);
+        gl.glVertexAttribPointer(index, size, type, normalized, stride, offset);
         gl.glVertexAttribDivisor(index, divisor);
     }
 
