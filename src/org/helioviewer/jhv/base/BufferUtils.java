b35707317ee23c337c9c9474e3cb4dff8b97d55c
Bogdan Nicula <bogdan@nicula.net>
2018-08-22 19:17:23 +0200

    Reduce

--- src/org/helioviewer/jhv/base/BufferUtils.java
+++ src/org/helioviewer/jhv/base/BufferUtils.java
@@ -6,8 +6,6 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
 
-import org.helioviewer.jhv.math.Vec3;
-
 public class BufferUtils {
 
     public static ByteBuffer newByteBuffer(int len) {
@@ -24,22 +22,6 @@ public class BufferUtils {
 
     public static ShortBuffer newShortBuffer(int len) {
         return newByteBuffer(2 * len).asShortBuffer();
-    }
-
-    public static void put4f(FloatBuffer buf, Vec3 v) {
-        buf.put((float) v.x).put((float) v.y).put((float) v.z).put(1);
-    }
-
-    public static void put4f(FloatBuffer buf, float x, float y, float z, float w) {
-        buf.put(x).put(y).put(z).put(w);
-    }
-
-    public static void put2f(FloatBuffer buf, float[] f) {
-        buf.put(f[0]).put(f[1]);
-    }
-
-    public static void put2f(FloatBuffer buf, float f0, float f1) {
-        buf.put(f0).put(f1);
     }
 
 }
