5274c662295e4368e6a7de1b53a9e8e22a34422d
Bogdan Nicula <bogdan@nicula.net>
2018-09-19 12:23:24 +0200

    Move complete into DecodeParams

--- src/org/helioviewer/jhv/view/j2k/image/DecodeParams.java
+++ src/org/helioviewer/jhv/view/j2k/image/DecodeParams.java
@@ -7,13 +7,15 @@ import org.helioviewer.jhv.imagedata.SubImage;
 public class DecodeParams {
 
     public final int serialNo;
+    public final boolean complete;
     public final SubImage subImage;
     public final ResolutionSet.ResolutionLevel resolution;
     public final int frame;
     public final double factor;
 
-    public DecodeParams(int _serialNo, SubImage _roi, ResolutionSet.ResolutionLevel _resolution, int _frame, double _factor) {
+    public DecodeParams(int _serialNo, boolean _complete, SubImage _roi, ResolutionSet.ResolutionLevel _resolution, int _frame, double _factor) {
         serialNo = _serialNo;
+        complete = _complete;
         subImage = _roi;
         resolution = _resolution;
         frame = _frame;
@@ -25,12 +27,12 @@ public class DecodeParams {
         if (!(o instanceof DecodeParams))
             return false;
         DecodeParams p = (DecodeParams) o;
-        return frame == p.frame && factor == p.factor && subImage.equals(p.subImage) && resolution.equals(p.resolution); // serialNo doesn't participate
+        return frame == p.frame && factor == p.factor && subImage.equals(p.subImage) && resolution.equals(p.resolution); // serialNo, complete don't participate
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(frame, factor, subImage, resolution); // serialNo doesn't participate
+        return Objects.hash(frame, factor, subImage, resolution); // serialNo, complete don't participate
     }
 
     @Override
