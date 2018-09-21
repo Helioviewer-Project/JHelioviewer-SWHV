f57b05e87110cd9d913ec589ca6b3f1a28e9f3eb
Bogdan Nicula <bogdan@nicula.net>
2018-09-21 17:10:59 +0200

    Move coordinates together

--- src/org/helioviewer/jhv/gui/components/statusplugin/PositionStatusPanel.java
+++ src/org/helioviewer/jhv/gui/components/statusplugin/PositionStatusPanel.java
@@ -96,7 +96,7 @@ public class PositionStatusPanel extends StatusPanel.StatusPlugin implements Mou
     private static String formatOrtho(@Nonnull Vec2 coord, double r, double pa, double px, double py, int value) {
         String coordStr = coord == Vec2.NAN ? nanOrtho : String.format("%+7.2f\u00B0,%+7.2f\u00B0", coord.x, coord.y);
         String valueStr = value == ImageData.BAD_PIXEL ? nanValue : String.format("%7d", value);
-        return String.format("(\u03C6,\u03B8) : (%s) | (\u03c1,\u03c8) : (%s,%+7.2f\u00B0) | (x,y) : (%s,%s) | %s", coordStr, formatR(r), pa, formatXY(px), formatXY(py), valueStr);
+        return String.format("(\u03c1,\u03c8) : (%s,%+7.2f\u00B0) | (\u03C6,\u03B8) : (%s) | (x,y) : (%s,%s) | %s", formatR(r), pa, coordStr, formatXY(px), formatXY(py), valueStr);
     }
 
     @Override
