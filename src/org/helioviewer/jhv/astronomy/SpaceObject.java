ac9e76e5ee176eb9d5b59ed2a7ae93fb35ceb3a4
Bogdan Nicula <bogdan@nicula.net>
2018-10-11 15:22:29 +0200

    Some immutable maps

--- src/org/helioviewer/jhv/astronomy/SpaceObject.java
+++ src/org/helioviewer/jhv/astronomy/SpaceObject.java
@@ -1,10 +1,7 @@
 package org.helioviewer.jhv.astronomy;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.Nullable;
 import javax.swing.border.Border;
@@ -12,7 +9,8 @@ import javax.swing.border.Border;
 import org.helioviewer.jhv.base.Colors;
 import org.helioviewer.jhv.gui.components.base.JHVTableCellRenderer;
 
-@SuppressWarnings("serial")
+import com.google.common.collect.ImmutableMap;
+
 public class SpaceObject {
 
     private final String urlName;
@@ -23,33 +21,31 @@ public class SpaceObject {
 
     public static final SpaceObject Sol = new SpaceObject("SUN", "Sun", 1, Colors.Yellow, JHVTableCellRenderer.cellEmphasisBorder);
 
-    private static final Map<String, SpaceObject> objectMap = Collections.unmodifiableMap(new LinkedHashMap<String, SpaceObject>() {
-        {
-            put("Sun", Sol);
-            put("Mercury", new SpaceObject("Mercury", "Mercury", 2439700 / Sun.RadiusMeter, Colors.Gray, JHVTableCellRenderer.cellBorder));
-            put("Venus", new SpaceObject("Venus", "Venus", 6051800 / Sun.RadiusMeter, Colors.bytes(181, 110, 26), JHVTableCellRenderer.cellBorder));
-            put("Earth", new SpaceObject("Earth", "Earth", 6371000 / Sun.RadiusMeter, Colors.Blue, JHVTableCellRenderer.cellBorder));
-            put("Moon", new SpaceObject("Moon", "Moon", 1737400 / Sun.RadiusMeter, Colors.LightGray, JHVTableCellRenderer.cellBorder));
-            put("Mars", new SpaceObject("Mars%20Barycenter", "Mars", 3389500 / Sun.RadiusMeter, Colors.bytes(135, 37, 18), JHVTableCellRenderer.cellBorder));
-            put("Jupiter", new SpaceObject("Jupiter%20Barycenter", "Jupiter", 69911000 / Sun.RadiusMeter, Colors.bytes(168, 172, 180), JHVTableCellRenderer.cellBorder));
-            put("Saturn", new SpaceObject("Saturn%20Barycenter", "Saturn", 58232000 / Sun.RadiusMeter, Colors.bytes(208, 198, 173), JHVTableCellRenderer.cellBorder));
-            put("Uranus", new SpaceObject("Uranus%20Barycenter", "Uranus", 25362000 / Sun.RadiusMeter, Colors.bytes(201, 239, 242), JHVTableCellRenderer.cellBorder));
-            put("Neptune", new SpaceObject("Neptune%20Barycenter", "Neptune", 24622000 / Sun.RadiusMeter, Colors.bytes(124, 157, 226), JHVTableCellRenderer.cellBorder));
-            put("Pluto", new SpaceObject("Pluto%20Barycenter", "Pluto", 1195000 / Sun.RadiusMeter, Colors.bytes(205, 169, 140), JHVTableCellRenderer.cellEmphasisBorder));
+    private static final ImmutableMap<String, SpaceObject> objectMap = new ImmutableMap.Builder<String, SpaceObject>().
+            put("Sun", Sol).
+            put("Mercury", new SpaceObject("Mercury", "Mercury", 2439700 / Sun.RadiusMeter, Colors.Gray, JHVTableCellRenderer.cellBorder)).
+            put("Venus", new SpaceObject("Venus", "Venus", 6051800 / Sun.RadiusMeter, Colors.bytes(181, 110, 26), JHVTableCellRenderer.cellBorder)).
+            put("Earth", new SpaceObject("Earth", "Earth", 6371000 / Sun.RadiusMeter, Colors.Blue, JHVTableCellRenderer.cellBorder)).
+            put("Moon", new SpaceObject("Moon", "Moon", 1737400 / Sun.RadiusMeter, Colors.LightGray, JHVTableCellRenderer.cellBorder)).
+            put("Mars", new SpaceObject("Mars%20Barycenter", "Mars", 3389500 / Sun.RadiusMeter, Colors.bytes(135, 37, 18), JHVTableCellRenderer.cellBorder)).
+            put("Jupiter", new SpaceObject("Jupiter%20Barycenter", "Jupiter", 69911000 / Sun.RadiusMeter, Colors.bytes(168, 172, 180), JHVTableCellRenderer.cellBorder)).
+            put("Saturn", new SpaceObject("Saturn%20Barycenter", "Saturn", 58232000 / Sun.RadiusMeter, Colors.bytes(208, 198, 173), JHVTableCellRenderer.cellBorder)).
+            put("Uranus", new SpaceObject("Uranus%20Barycenter", "Uranus", 25362000 / Sun.RadiusMeter, Colors.bytes(201, 239, 242), JHVTableCellRenderer.cellBorder)).
+            put("Neptune", new SpaceObject("Neptune%20Barycenter", "Neptune", 24622000 / Sun.RadiusMeter, Colors.bytes(124, 157, 226), JHVTableCellRenderer.cellBorder)).
+            put("Pluto", new SpaceObject("Pluto%20Barycenter", "Pluto", 1195000 / Sun.RadiusMeter, Colors.bytes(205, 169, 140), JHVTableCellRenderer.cellEmphasisBorder)).
 
-            put("Comet 67P", new SpaceObject("CHURYUMOV-GERASIMENKO", "Comet 67P", 2200 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellEmphasisBorder));
+            put("Comet 67P", new SpaceObject("CHURYUMOV-GERASIMENKO", "Comet 67P", 2200 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellEmphasisBorder)).
 
-            put("SOHO", new SpaceObject("SOHO", "SOHO", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder));
-            put("STEREO Ahead", new SpaceObject("STEREO%20Ahead", "STEREO Ahead", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder));
-            put("STEREO Behind", new SpaceObject("STEREO%20Behind", "STEREO Behind", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder));
-            put("PROBA-2", new SpaceObject("PROBA2", "PROBA-2", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder));
-            put("SDO", new SpaceObject("SDO", "SDO", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellEmphasisBorder));
+            put("SOHO", new SpaceObject("SOHO", "SOHO", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder)).
+            put("STEREO Ahead", new SpaceObject("STEREO%20Ahead", "STEREO Ahead", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder)).
+            put("STEREO Behind", new SpaceObject("STEREO%20Behind", "STEREO Behind", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder)).
+            put("PROBA-2", new SpaceObject("PROBA2", "PROBA-2", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder)).
+            put("SDO", new SpaceObject("SDO", "SDO", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellEmphasisBorder)).
 
-            put("Solar Orbiter", new SpaceObject("Solar%20Orbiter", "Solar Orbiter", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder));
-            put("Parker Solar Probe", new SpaceObject("SPP", "Parker Solar Probe", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder));
-            put("PROBA-3", new SpaceObject("PROBA3", "PROBA-3", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder));
-        }
-    });
+            put("Solar Orbiter", new SpaceObject("Solar%20Orbiter", "Solar Orbiter", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder)).
+            put("Parker Solar Probe", new SpaceObject("SPP", "Parker Solar Probe", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder)).
+            put("PROBA-3", new SpaceObject("PROBA3", "PROBA-3", 2 / Sun.RadiusMeter, Colors.White, JHVTableCellRenderer.cellBorder)).
+            build();
 
     public static List<SpaceObject> getTargets(SpaceObject observer) {
         ArrayList<SpaceObject> list = new ArrayList<>(objectMap.values());
--- src/org/helioviewer/jhv/view/j2k/io/jpip/DatabinMap.java
+++ src/org/helioviewer/jhv/view/j2k/io/jpip/DatabinMap.java
@@ -1,19 +1,15 @@
 package org.helioviewer.jhv.view.j2k.io.jpip;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import com.google.common.collect.ImmutableMap;
 
-@SuppressWarnings("serial")
 public class DatabinMap {
 
-    private static final Map<Integer, Integer> classMap = Collections.unmodifiableMap(new HashMap<Integer, Integer>() {{
-        put(JPIPConstants.PRECINCT_DATA_BIN_CLASS, KakaduConstants.KDU_PRECINCT_DATABIN);
-        put(JPIPConstants.TILE_HEADER_DATA_BIN_CLASS, KakaduConstants.KDU_TILE_HEADER_DATABIN);
-        put(JPIPConstants.TILE_DATA_BIN_CLASS, JPIPConstants.TILE_DATA_BIN_CLASS);
-        put(JPIPConstants.MAIN_HEADER_DATA_BIN_CLASS, KakaduConstants.KDU_MAIN_HEADER_DATABIN);
-        put(JPIPConstants.META_DATA_BIN_CLASS, KakaduConstants.KDU_META_DATABIN);
-    }});
+    private static final ImmutableMap<Integer, Integer> classMap = ImmutableMap.of(
+            JPIPConstants.PRECINCT_DATA_BIN_CLASS, KakaduConstants.KDU_PRECINCT_DATABIN,
+            JPIPConstants.TILE_HEADER_DATA_BIN_CLASS, KakaduConstants.KDU_TILE_HEADER_DATABIN,
+            JPIPConstants.TILE_DATA_BIN_CLASS, JPIPConstants.TILE_DATA_BIN_CLASS,
+            JPIPConstants.MAIN_HEADER_DATA_BIN_CLASS, KakaduConstants.KDU_MAIN_HEADER_DATABIN,
+            JPIPConstants.META_DATA_BIN_CLASS, KakaduConstants.KDU_META_DATABIN);
 
     public static Integer getKlass(int classID) {
         return classMap.get(classID);
