48f72908bb3c8f81a0b5d9a0e8ce05a9da220a35
Bogdan Nicula <bogdan@nicula.net>
2018-10-11 15:59:57 +0200

    More immutable maps

--- src/org/helioviewer/jhv/io/DataSources.java
+++ src/org/helioviewer/jhv/io/DataSources.java
@@ -1,10 +1,6 @@
 package org.helioviewer.jhv.io;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -14,92 +10,76 @@ import javax.annotation.Nullable;
 import org.helioviewer.jhv.JHVGlobals;
 
 import org.everit.json.schema.Validator;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
 
-@SuppressWarnings("serial")
 public class DataSources {
 
-    static final Set<String> SupportedObservatories = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
-            "SOHO", "SDO", "STEREO_A", "STEREO_B", "PROBA2", "ROB-USET", "ROB-Humain", "NSO-GONG", "NSO-SOLIS", "Kanzelhoehe", "NRH", "Yohkoh", "Hinode", "TRACE"
-    )));
+    static final Set<String> SupportedObservatories = ImmutableSet.of(
+            "SOHO", "SDO", "STEREO_A", "STEREO_B", "PROBA2",
+            "ROB-USET", "ROB-Humain", "NSO-GONG", "NSO-SOLIS", "Kanzelhoehe",
+            "NRH", "Yohkoh", "Hinode", "TRACE");
 
-    private static final Map<String, Map<String, String>> serverSettings = Collections.unmodifiableMap(new LinkedHashMap<String, Map<String, String>>() {
-        {
-            put("ROB", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "http://swhv.oma.be/hv/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "http://swhv.oma.be/hv/api/index.php?action=getJP2Image&");
-                    put("API.getJPX", "http://swhv.oma.be/hv/api/index.php?action=getJPX&");
-                    put("label", "Royal Observatory of Belgium");
-                    put("schema", "/data/sources_v1.0.json");
-                    put("availability.images", "http://swhv.oma.be/availability/images/availability/availability.html");
-                }
-            }));
-/*          put("ROB Test", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "http://swhv2.oma.be:8083/index.php?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "http://swhv2.oma.be:8083/index.php?action=getJP2Image&");
-                    put("API.getJPX", "http://swhv2.oma.be:8083/index.php?action=getJPX&");
-                    put("label", "Royal Observatory of Belgium");
-                    put("schema", "/data/sources_v1.0.json");
-                    put("availability.images", "http://swhv2.oma.be/availability/images/availability/availability.html");
-                }
-            })); */
-            put("IAS", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "https://helioviewer-api.ias.u-psud.fr/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "https://helioviewer-api.ias.u-psud.fr/v2/getJP2Image/?");
-                    put("API.getJPX", "https://helioviewer-api.ias.u-psud.fr/v2/getJPX/?");
-                    put("label", "Institut d'Astrophysique Spatiale");
-                    put("schema", "/data/sources_v1.0.json");
-                }
-            }));
-/*          put("IAS Test", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "https://inf-helio-test-api.ias.u-psud.fr/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "https://inf-helio-test-api.ias.u-psud.fr/v2/getJP2Image/?");
-                    put("API.getJPX", "https://inf-helio-test-api.ias.u-psud.fr/v2/getJPX/?");
-                    put("label", "Institut d'Astrophysique Spatiale");
-                    put("schema", "/data/sources_v1.0.json");
-                }
-            })); */
-            put("GSFC", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "https://api.helioviewer.org/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "https://api.helioviewer.org/v2/getJP2Image/?");
-                    put("API.getJPX", "https://api.helioviewer.org/v2/getJPX/?");
-                    put("label", "Goddard Space Flight Center");
-                    put("schema", "/data/sources_v1.0.json");
-                }
-            }));
-/*          put("GSFC SCI Test", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getDataSources&verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getJP2Image&");
-                    put("API.getJPX", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getJPX&");
-                    put("label", "Goddard Space Flight Center SCI Test");
-                    put("schema", "/data/sources_v1.0.json");
-                }
-            }));
-            put("GSFC NDC Test", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getDataSources&verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getJP2Image&");
-                    put("API.getJPX", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getJPX&");
-                    put("label", "Goddard Space Flight Center NDC Test");
-                    put("schema", "/data/sources_v1.0.json");
-                }
-            })); */
-/*          put("LOCALHOST", Collections.unmodifiableMap(new HashMap<String, String>() {
-                {
-                    put("API.getDataSources", "http://localhost:8080/helioviewer/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]");
-                    put("API.getJP2Image", "http://localhost:8080/helioviewer/api/index.php?action=getJP2Image&");
-                    put("API.getJPX", "http://localhost:8080/helioviewer/api/index.php?action=getJPX&");
-                    put("schema", "/data/sources_v1.0.json");
-                    put("label", "Localhost");
-                }
-            })); */
-        }
-    });
+    private static final ImmutableMap<String, Map<String, String>> serverSettings = new ImmutableMap.Builder<String, Map<String, String>>().
+            put("ROB", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "http://swhv.oma.be/hv/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "http://swhv.oma.be/hv/api/index.php?action=getJP2Image&").
+                    put("API.getJPX", "http://swhv.oma.be/hv/api/index.php?action=getJPX&").
+                    put("label", "Royal Observatory of Belgium").
+                    put("schema", "/data/sources_v1.0.json").
+                    put("availability.images", "http://swhv.oma.be/availability/images/availability/availability.html").
+                    build()).
+/*          put("ROB Test", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "http://swhv2.oma.be:8083/index.php?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "http://swhv2.oma.be:8083/index.php?action=getJP2Image&").
+                    put("API.getJPX", "http://swhv2.oma.be:8083/index.php?action=getJPX&").
+                    put("label", "Royal Observatory of Belgium").
+                    put("schema", "/data/sources_v1.0.json").
+                    put("availability.images", "http://swhv2.oma.be/availability/images/availability/availability.html").
+                    build()). */
+            put("IAS", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "https://helioviewer-api.ias.u-psud.fr/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "https://helioviewer-api.ias.u-psud.fr/v2/getJP2Image/?").
+                    put("API.getJPX", "https://helioviewer-api.ias.u-psud.fr/v2/getJPX/?").
+                    put("label", "Institut d'Astrophysique Spatiale").
+                    put("schema", "/data/sources_v1.0.json").
+                    build()).
+/*          put("IAS Test", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "https://inf-helio-test-api.ias.u-psud.fr/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "https://inf-helio-test-api.ias.u-psud.fr/v2/getJP2Image/?").
+                    put("API.getJPX", "https://inf-helio-test-api.ias.u-psud.fr/v2/getJPX/?").
+                    put("label", "Institut d'Astrophysique Spatiale").
+                    put("schema", "/data/sources_v1.0.json").
+                    build()). */
+            put("GSFC", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "https://api.helioviewer.org/v2/getDataSources/?verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "https://api.helioviewer.org/v2/getJP2Image/?").
+                    put("API.getJPX", "https://api.helioviewer.org/v2/getJPX/?").
+                    put("label", "Goddard Space Flight Center").
+                    put("schema", "/data/sources_v1.0.json").
+                    build()).
+/*          put("GSFC SCI Test", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getDataSources&verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getJP2Image&").
+                    put("API.getJPX", "http://helioviewer.sci.gsfc.nasa.gov/api.php?action=getJPX&").
+                    put("label", "Goddard Space Flight Center SCI Test").
+                    put("schema", "/data/sources_v1.0.json").
+                    build()). */
+/*          put("GSFC NDC Test", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getDataSources&verbose=true&enable=[TRACE,Hinode,Yohkoh,STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getJP2Image&").
+                    put("API.getJPX", "http://gs671-heliovw7.ndc.nasa.gov/api.php?action=getJPX&").
+                    put("label", "Goddard Space Flight Center NDC Test");.
+                    put("schema", "/data/sources_v1.0.json").
+                    build()). */
+/*          put("LOCALHOST", new ImmutableMap.Builder<String, String>().
+                    put("API.getDataSources", "http://localhost:8080/helioviewer/api/?action=getDataSources&verbose=true&enable=[STEREO_A,STEREO_B,PROBA2]").
+                    put("API.getJP2Image", "http://localhost:8080/helioviewer/api/index.php?action=getJP2Image&").
+                    put("API.getJPX", "http://localhost:8080/helioviewer/api/index.php?action=getJPX&").
+                    put("schema", "/data/sources_v1.0.json").
+                    put("label", "Localhost").
+                    build()). */
+            build();
 
     public static Set<String> getServers() {
         return serverSettings.keySet();
--- src/org/helioviewer/jhv/metadata/AIAResponse.java
+++ src/org/helioviewer/jhv/metadata/AIAResponse.java
@@ -2,52 +2,47 @@ package org.helioviewer.jhv.metadata;
 
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.HashMap;
 
 import org.helioviewer.jhv.io.FileUtils;
 import org.helioviewer.jhv.io.JSONUtils;
 import org.json.JSONObject;
 
-@SuppressWarnings("serial")
+import com.google.common.collect.ImmutableMap;
+
 public class AIAResponse {
     /*
         // https://github.com/mjpauly/aia/blob/master/mov_img.py
         private static final HashMap<String, Double> STANDARD_INT = new HashMap<String, Double>() {
-            {
-                put("131", 6.99685);
-                put("171", 4.99803);
-                put("193", 2.9995);
-                put("211", 4.99801);
-                put("304", 4.99941);
-                put("335", 6.99734);
-                put("94", 4.99803);
-            }
-        };
+        private static final ImmutableMap<String, Double> STANDARD_INT = new ImmutableMap.Builder<String, Double>().
+                put("131", 6.99685).
+                put("171", 4.99803).
+                put("193", 2.9995).
+                put("211", 4.99801).
+                put("304", 4.99941).
+                put("335", 6.99734).
+                put("94", 4.99803).
+                build();
 
-        private static final HashMap<String, Double> MAX = new HashMap<String, Double>() {
-            {
-                put("131", 1200.);
-                put("171", 6000.);
-                put("193", 6000.);
-                put("211", 13000.);
-                put("304", 2000.);
-                put("335", 1000.);
-                put("94",  50.);
-            }
-        };
+        private static final ImmutableMap<String, Double> MAX = new ImmutableMap.Builder<String, Double>().
+                put("131", 1200.).
+                put("171", 6000.).
+                put("193", 6000.).
+                put("211", 13000.).
+                put("304", 2000.).
+                put("335", 1000.).
+                put("94",  50.).
+                build();
     */
     // https://github.com/Helioviewer-Project/jp2gen/blob/master/idl/sdo/aia/hvs_version5_aia.pro
-    private static final HashMap<String, Double> HV_MAX = new HashMap<String, Double>() {
-        {
-            put("131", 500.);
-            put("171", 14000.);
-            put("193", 2500.);
-            put("211", 1500.);
-            put("304", 250.);
-            put("335", 80.);
-            put("94", 30.);
-        }
-    };
+    private static final ImmutableMap<String, Double> HV_MAX = new ImmutableMap.Builder<String, Double>().
+            put("131", 500.).
+            put("171", 14000.).
+            put("193", 2500.).
+            put("211", 1500.).
+            put("304", 250.).
+            put("335", 80.).
+            put("94", 30.).
+            build();
 
 //    private static final HashMap<String, Double> LMSAL_MAX = new HashMap<>();
 //    private static final String extPath = "https://raw.githubusercontent.com/mjpauly/aia/master/aia_rescaling_data.json";
