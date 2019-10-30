a909c41dc35a5156105254a60bf0e71ada7d361b
Bogdan Nicula <bogdan@nicula.net>
2019-10-30 10:30:14 +0100

    Sync docs templates

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,6 @@
 doc=ddf
 pandoc='pandoc --standalone --wrap=none --listings --number-sections --toc --toc-depth=6'
-tex_options='--top-level-division=chapter -V classoption=oneside --template=eisvogel.latex --highlight-style=kate --pdf-engine=xelatex'
+tex_options='--top-level-division=chapter -V classoption=oneside --template=eisvogel.tex --highlight-style=kate --pdf-engine=xelatex'
 
 gpp -x -T ${doc}.md | ${pandoc} ${tex_options} -o ${doc}.pdf
 #gpp -x -T ${doc}.md | ${pandoc} -o ${doc}.docx
---
+++ docs/preamble.md
@@ -0,0 +1,24 @@
+---
+author: [SWHV Team]
+tags: [SWHV, JHV]
+book: true
+fontsize: 9pt
+papersize: A4
+lot: true
+lof: true
+toc-depth: 6
+secnumdepth: 6
+colorlinks: true
+code-block-font-size: \footnotesize
+footnotes-pretty: true
+table-use-row-colors: true
+titlepage: true
+titlepage-color: "0088cc"
+titlepage-text-color: "ffffff"
+titlepage-rule-color: "ffffff"
+titlepage-rule-height: 5
+listings-disable-line-numbers: true
+logo: hvLogo.png
+logo-width: 40
+---
+\frenchspacing
