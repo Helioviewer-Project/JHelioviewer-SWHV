8bbcff07f1dd56f9304fb06aa74002f89bb86435
Bogdan Nicula <bogdan@nicula.net>
2020-01-01 20:30:38 +0100

    Reduce

--- src/org/helioviewer/jhv/timelines/band/BandDataProvider.java
+++ src/org/helioviewer/jhv/timelines/band/BandDataProvider.java
@@ -9,10 +9,12 @@ import javax.annotation.Nullable;
 
 import org.helioviewer.jhv.base.interval.Interval;
 import org.helioviewer.jhv.io.JSONUtils;
+import org.helioviewer.jhv.log.Log;
 import org.helioviewer.jhv.threads.JHVExecutor;
 import org.helioviewer.jhv.threads.JHVWorker;
 import org.helioviewer.jhv.time.TimeUtils;
 import org.helioviewer.jhv.timelines.Timelines;
+import org.helioviewer.jhv.timelines.TimelineSettings;
 import org.json.JSONObject;
 
 public class BandDataProvider {
@@ -61,6 +63,7 @@ public class BandDataProvider {
             startTime = _startTime;
             endTime = _endTime;
             Timelines.getLayers().downloadStarted(band);
+            setThreadName("EVE--BandDownload");
         }
 
         @Nullable
@@ -72,7 +75,7 @@ public class BandDataProvider {
                         "&timeline=" + type.getName();
                 return new BandResponse(JSONUtils.get(request));
             } catch (Exception e) {
-                e.printStackTrace();
+                Log.error("Error downloading band", e);
             }
             return null;
         }
@@ -92,6 +95,67 @@ public class BandDataProvider {
                 }
             }
             Timelines.getLayers().downloadFinished(band);
+        }
+
+    }
+
+    private static class BandLoadTask extends JHVWorker<BandResponse, Void> {
+
+        private final JSONObject jo;
+
+        BandLoadTask(JSONObject _jo) {
+            jo = _jo;
+        }
+
+        @Nullable
+        @Override
+        protected BandResponse backgroundWork() {
+            try {
+                return new BandResponse(jo);
+            } catch (Exception e) {
+                Log.error("Error loading band", e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void done() {
+            if (!isCancelled()) {
+                try {
+                    BandResponse r = get();
+                    if (r != null) {
+                        Band band = new Band(r.bandType == null ? BandType.getBandType(r.bandName) : r.bandType);
+                        band.addToCache(r.values, r.dates);
+                        Timelines.getLayers().add(band);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    private static class BandTypeTask extends JHVWorker<Void, Void> {
+
+        BandTypeTask() {
+            setThreadName("EVE--LoadSources");
+        }
+
+        @Nullable
+        @Override
+        protected Void backgroundWork() {
+            try {
+                BandType.loadBandTypes(JSONUtils.get(TimelineSettings.baseURL).getJSONArray("objects"));
+            } catch (Exception e) {
+                Log.error("Error downloading band types", e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void done() {
+            Timelines.td.getObservationPanel().setupDatasets();
         }
 
     }
