d3d6b520b71e53063a6380885216f8a0d215d75e
Bogdan Nicula <bogdan@nicula.net>
2020-01-17 11:23:26 +0100

    Convert timelines to callback pool

--- src/org/helioviewer/jhv/timelines/band/BandDataProvider.java
+++ src/org/helioviewer/jhv/timelines/band/BandDataProvider.java
@@ -1,14 +1,14 @@
 package org.helioviewer.jhv.timelines.band;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 
-import org.helioviewer.jhv.JHVGlobals;
 import org.helioviewer.jhv.base.interval.Interval;
 import org.helioviewer.jhv.io.JSONUtils;
 import org.helioviewer.jhv.log.Log;
-import org.helioviewer.jhv.threads.JHVWorker;
+import org.helioviewer.jhv.threads.EventQueueCallbackExecutor;
 import org.helioviewer.jhv.time.TimeUtils;
 import org.helioviewer.jhv.timelines.Timelines;
 import org.helioviewer.jhv.timelines.TimelineSettings;
@@ -16,23 +16,25 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.ListenableFuture;
 
 public class BandDataProvider {
 
-    private static final ArrayListMultimap<Band, BandDownloadTask> workerMap = ArrayListMultimap.create();
+    private static final ArrayListMultimap<Band, ListenableFuture<BandResponse>> workerMap = ArrayListMultimap.create();
 
     public static void loadBandTypes() {
-        JHVGlobals.getExecutorService().execute(new BandTypeTask());
+        EventQueueCallbackExecutor.pool.submit(new BandTypeDownload(), new BandTypeDownloadCallback());
     }
 
     public static void loadBand(JSONObject jo) {
-        JHVGlobals.getExecutorService().execute(new BandLoadTask(jo));
+        EventQueueCallbackExecutor.pool.submit(new BandLoad(jo), new BandLoadCallback());
     }
 
     static void addDownloads(Band band, List<Interval> intervals) {
         for (Interval interval : intervals) {
-            BandDownloadTask worker = new BandDownloadTask(band, interval.start, interval.end);
-            JHVGlobals.getExecutorService().submit(worker);
+            ListenableFuture<BandResponse> worker = EventQueueCallbackExecutor.pool.submit(
+                    new BandDownload(band, interval.start, interval.end), new BandDownloadCallback(band));
             workerMap.put(band, worker);
         }
     }
@@ -43,127 +45,115 @@ public class BandDataProvider {
     }
 
     static boolean isDownloadActive(Band band) {
-        for (BandDownloadTask worker : workerMap.get(band)) {
+        for (ListenableFuture<BandResponse> worker : workerMap.get(band)) {
             if (!worker.isDone())
                 return true;
         }
         return false;
     }
 
-    private static class BandDownloadTask extends JHVWorker<BandResponse, Void> {
+    private static class BandDownload implements Callable<BandResponse> {
 
         private final Band band;
         private final long startTime;
         private final long endTime;
 
-        BandDownloadTask(Band _band, long _startTime, long _endTime) {
+        BandDownload(Band _band, long _startTime, long _endTime) {
             band = _band;
             startTime = _startTime;
             endTime = _endTime;
             Timelines.getLayers().downloadStarted(band);
-            setThreadName("EVE--BandDownload");
-        }
-
-        @Nullable
-        @Override
-        protected BandResponse backgroundWork() {
-            try {
-                BandType type = band.getBandType();
-                String request = type.getBaseURL() + "start_date=" + TimeUtils.formatDate(startTime) + "&end_date=" + TimeUtils.formatDate(endTime) +
-                        "&timeline=" + type.getName();
-                return new BandResponse(JSONUtils.get(request));
-            } catch (Exception e) {
-                Log.error("Error downloading band", e);
-            }
-            return null;
         }
 
         @Override
-        protected void done() {
-            if (!isCancelled()) {
-                try {
-                    BandResponse r = get();
-                    if (r != null) {
-                        if (!r.bandName.equals(band.getBandType().getName()))
-                            throw new Exception("Expected " + band.getBandType().getName() + ", got " + r.bandName);
-                        band.addToCache(r.values, r.dates);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            Timelines.getLayers().downloadFinished(band);
+        public BandResponse call() throws Exception {
+            BandType type = band.getBandType();
+            String request = type.getBaseURL() +
+                    "start_date=" + TimeUtils.formatDate(startTime) +
+                    "&end_date=" + TimeUtils.formatDate(endTime) +
+                    "&timeline=" + type.getName();
+            return new BandResponse(JSONUtils.get(request));
         }
 
     }
 
-    private static class BandLoadTask extends JHVWorker<BandResponse, Void> {
+    private static class BandDownloadCallback implements FutureCallback<BandResponse> {
+
+        private final Band band;
+
+        BandDownloadCallback(Band _band) {
+            band = _band;
+        }
+
+        @Override
+        public void onSuccess(BandResponse result) {
+            if (result.bandName.equals(band.getBandType().getName()))
+                band.addToCache(result.values, result.dates);
+            else
+                Log.error("Expected " + band.getBandType().getName() + ", got " + result.bandName);
+        }
+
+        @Override
+        public void onFailure(@Nonnull Throwable t) {
+            Log.error("Error downloading band", t);
+            // t.printStackTrace();
+        }
+
+    }
+
+    private static class BandLoad implements Callable<BandResponse> {
 
         private final JSONObject jo;
 
-        BandLoadTask(JSONObject _jo) {
+        BandLoad(JSONObject _jo) {
             jo = _jo;
         }
 
-        @Nullable
         @Override
-        protected BandResponse backgroundWork() {
-            try {
-                return new BandResponse(jo);
-            } catch (Exception e) {
-                Log.error("Error loading band", e);
-            }
-            return null;
-        }
-
-        @Override
-        protected void done() {
-            if (!isCancelled()) {
-                try {
-                    BandResponse r = get();
-                    if (r != null) {
-                        Band band = new Band(r.bandType == null ? BandType.getBandType(r.bandName) : r.bandType);
-                        band.addToCache(r.values, r.dates);
-                        Timelines.getLayers().add(band);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+        public BandResponse call() {
+            return new BandResponse(jo);
         }
 
     }
 
-    private static class BandTypeTask extends JHVWorker<JSONArray, Void> {
+    private static class BandLoadCallback implements FutureCallback<BandResponse> {
 
-        BandTypeTask() {
-            setThreadName("EVE--LoadSources");
-        }
-
-        @Nullable
         @Override
-        protected JSONArray backgroundWork() {
-            try {
-                return JSONUtils.get(TimelineSettings.baseURL).getJSONArray("objects");
-            } catch (Exception e) {
-                Log.error("Error downloading band types", e);
-            }
-            return null;
+        public void onSuccess(BandResponse result) {
+            Band band = new Band(result.bandType == null ? BandType.getBandType(result.bandName) : result.bandType);
+            band.addToCache(result.values, result.dates);
+            Timelines.getLayers().add(band);
         }
 
         @Override
-        protected void done() {
-            if (!isCancelled()) {
-                try {
-                    JSONArray ja = get();
-                    if (ja != null) {
-                        BandType.loadBandTypes(ja);
-                        Timelines.td.getObservationPanel().setupDatasets();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+        public void onFailure(@Nonnull Throwable t) {
+            Log.error("Error loading band", t);
+            // t.printStackTrace();
+        }
+
+    }
+
+    private static class BandTypeDownload implements Callable<JSONArray> {
+
+        @Override
+        public JSONArray call() throws Exception {
+            return JSONUtils.get(TimelineSettings.baseURL).getJSONArray("objects");
+        }
+
+    }
+
+    private static class BandTypeDownloadCallback implements FutureCallback<JSONArray> {
+
+        @Override
+        public void onSuccess(JSONArray result) {
+            BandType.loadBandTypes(result);
+            Timelines.td.getObservationPanel().setupDatasets();
+        }
+
+        @Override
+        public void onFailure(@Nonnull Throwable t) {
+            Log.error("Error downloading band types", t);
+            // t.printStackTrace();
         }
 
     }
