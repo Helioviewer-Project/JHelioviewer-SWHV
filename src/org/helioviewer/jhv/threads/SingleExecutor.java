4f8679bbcdf88b251bda4af12496723e73106777
Bogdan Nicula <bogdan@nicula.net>
2020-01-20 12:45:12 +0100

    Rename

--- src/org/helioviewer/jhv/threads/SingleExecutor.java
+++ src/org/helioviewer/jhv/threads/SingleExecutor.java
@@ -5,16 +5,20 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class SerialExecutor {
+public class SingleExecutor {
 
     private final ExecutorService executor;
 
-    public SerialExecutor(JHVThread.NamedClassThreadFactory factory) {
+    public SingleExecutor(JHVThread.NamedClassThreadFactory factory) {
         executor = Executors.newSingleThreadExecutor(factory);
     }
 
     public <T> T invokeAndWait(Callable<T> callable) throws InterruptedException, ExecutionException {
         return executor.submit(callable).get();
+    }
+
+    public void invokeLater(Runnable runnable) {
+        executor.submit(runnable);
     }
 
 }
