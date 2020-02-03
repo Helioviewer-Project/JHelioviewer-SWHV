c160ba74006fd158f3bd3b50329c25b0fa3642fc
Bogdan Nicula <bogdan@nicula.net>
2020-02-04 00:20:25 +0100

    Add UI for FOV selection

--- src/org/helioviewer/jhv/layers/FOVLayer.java
+++ src/org/helioviewer/jhv/layers/FOVLayer.java
@@ -1,17 +1,26 @@
 package org.helioviewer.jhv.layers;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 import javax.annotation.Nullable;
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.table.AbstractTableModel;
 
 import org.helioviewer.jhv.astronomy.Position;
 import org.helioviewer.jhv.base.Colors;
@@ -19,6 +28,8 @@ import org.helioviewer.jhv.camera.Camera;
 import org.helioviewer.jhv.camera.CameraHelper;
 import org.helioviewer.jhv.display.Viewport;
 import org.helioviewer.jhv.gui.ComponentUtils;
+import org.helioviewer.jhv.gui.components.TableValue;
+import org.helioviewer.jhv.gui.components.base.JHVTableCellRenderer;
 import org.helioviewer.jhv.gui.components.base.TerminatedFormatterFactory;
 import org.helioviewer.jhv.gui.components.base.WheelSupport;
 import org.helioviewer.jhv.math.Transform;
@@ -30,45 +41,64 @@ import org.json.JSONObject;
 
 import com.jogamp.opengl.GL2;
 
+@SuppressWarnings("serial")
 public class FOVLayer extends AbstractLayer {
 
-    private enum FOVType { RECTANGULAR, CIRCULAR }
+    private enum FOVType {RECTANGULAR, CIRCULAR}
 
     private static class FOV {
 
-        final String name;
-        final FOVType type;
-        final double inner;
-        final double wide;
-        final double high;
+        private final String name;
+        private final FOVType type;
+        private final double inner;
+        private final double wide;
+        private final double high;
+        private final byte[] color;
+        private boolean selected;
 
-        FOV(String _name, FOVType _type, double innerDeg, double wideDeg, double highDeg) {
+        FOV(String _name, FOVType _type, double innerDeg, double wideDeg, double highDeg, byte[] _color) {
             name = _name;
             type = _type;
             inner = 0.5 * Math.tan(innerDeg * (Math.PI / 180.));
-            wide  = 0.5 * Math.tan(wideDeg * (Math.PI / 180.));
-            high  = 0.5 * Math.tan(highDeg * (Math.PI / 180.));
+            wide = 0.5 * Math.tan(wideDeg * (Math.PI / 180.));
+            high = 0.5 * Math.tan(highDeg * (Math.PI / 180.));
+            color = _color;
         }
 
-        void putFOV(FOVShape f, BufVertex buf, byte[] color) {
+        void putFOV(FOVShape f, double distance, BufVertex buf) {
+            if (!selected)
+                return;
             if (inner > 0)
-                f.putCircLine(inner, buf, color);
+                f.putCircLine(inner * distance, buf, color);
             if (type == FOVType.RECTANGULAR)
-                f.putRectLine(wide, high, buf, color);
+                f.putRectLine(wide * distance, high * distance, buf, color);
             else
-                f.putCircLine(wide, buf, color);
+                f.putCircLine(wide * distance, buf, color);
+        }
+
+        boolean isSelected() {
+            return selected;
+        }
+
+        void select() {
+            selected = !selected;
+        }
+
+        @Override
+        public String toString() {
+            return name;
         }
 
     }
 
     private static final List<FOV> FOVs = List.of(
-        new FOV("SOLO/EUI/HRI", FOVType.RECTANGULAR, 0, 16.6 / 60., 16.6 / 60.),
-        new FOV("SOLO/EUI/FSI", FOVType.RECTANGULAR, 0,  228 / 60.,  228 / 60.),
-        new FOV("SOLO/METIS",   FOVType.CIRCULAR,    3,        5.8,        5.8),
-        new FOV("SOLO/PHI/HRT", FOVType.RECTANGULAR, 0,       0.28,       0.28),
-        new FOV("SOLO/PHI/FDT", FOVType.RECTANGULAR, 0,          2,          2),
-        new FOV("SOLO/SPICE",   FOVType.RECTANGULAR, 0,    16 / 60.,  11 / 60.),
-        new FOV("SOLO/STIX",    FOVType.RECTANGULAR, 0,           2,         2)
+            new FOV("SOLO/EUI/HRI", FOVType.RECTANGULAR, 0, 16.6 / 60., 16.6 / 60., Colors.Blue),
+            new FOV("SOLO/EUI/FSI", FOVType.RECTANGULAR, 0, 228 / 60., 228 / 60., Colors.Blue),
+            new FOV("SOLO/METIS", FOVType.CIRCULAR, 3, 5.8, 5.8, Colors.Blue),
+            new FOV("SOLO/PHI/HRT", FOVType.RECTANGULAR, 0, 0.28, 0.28, Colors.Blue),
+            new FOV("SOLO/PHI/FDT", FOVType.RECTANGULAR, 0, 2, 2, Colors.Blue),
+            new FOV("SOLO/SPICE", FOVType.RECTANGULAR, 0, 16 / 60., 11 / 60., Colors.Blue),
+            new FOV("SOLO/STIX", FOVType.RECTANGULAR, 0, 2, 2, Colors.Blue)
     );
 
     private static final double LINEWIDTH_FOV = GLSLLine.LINEWIDTH_BASIC;
@@ -82,6 +112,7 @@ public class FOVLayer extends AbstractLayer {
 
     private final JPanel optionsPanel;
 
+    private boolean drawCustom;
     private double fovAngle = Camera.INITFOV / Math.PI * 180;
 
     @Override
@@ -99,7 +130,6 @@ public class FOVLayer extends AbstractLayer {
 
         double pixFactor = CameraHelper.getPixelFactor(camera, vp);
         Position viewpoint = camera.getViewpoint();
-        double halfSide = 0.5 * viewpoint.distance * Math.tan(fovAngle * (Math.PI / 180.));
 
         Transform.pushView();
         Transform.rotateViewInverse(viewpoint.toQuat());
@@ -113,7 +143,11 @@ public class FOVLayer extends AbstractLayer {
         center.setData(gl, centerBuf);
         center.renderPoints(gl, pixFactor);
 
-        fov.putRectLine(halfSide, halfSide, fovBuf, fovColor);
+        FOVs.forEach(f -> f.putFOV(fov, viewpoint.distance, fovBuf));
+        if (drawCustom) {
+            double halfSide = 0.5 * viewpoint.distance * Math.tan(fovAngle * (Math.PI / 180.));
+            fov.putRectLine(halfSide, halfSide, fovBuf, fovColor);
+        }
         fovLine.setData(gl, fovBuf);
         fovLine.render(gl, vp.aspect, LINEWIDTH_FOV);
 
@@ -165,6 +199,12 @@ public class FOVLayer extends AbstractLayer {
         return false;
     }
 
+    private static final int ICON_WIDTH = 12;
+    private static final int NUMBEROFVISIBLEROWS = 5;
+
+    private static final int SELECTED_COL = 0;
+    private static final int OBJECT_COL = 1;
+
     private JPanel optionsPanel() {
         double fovMin = 0, fovMax = 180;
         JSpinner spinner = new JSpinner(new SpinnerNumberModel(Double.valueOf(fovAngle), Double.valueOf(fovMin), Double.valueOf(fovMax), Double.valueOf(0.01)));
@@ -177,20 +217,147 @@ public class FOVLayer extends AbstractLayer {
         f.setFormatterFactory(new TerminatedFormatterFactory("%.2f", "\u00B0", fovMin, fovMax));
         WheelSupport.installMouseWheelSupport(spinner);
 
-        JPanel panel = new JPanel(new GridBagLayout());
+        JCheckBox customCheckBox = new JCheckBox("Custom angle", false);
+        customCheckBox.addChangeListener(e -> {
+            drawCustom = !drawCustom;
+            MovieDisplay.display();
+        });
+
+        JPanel customPanel = new JPanel(new GridBagLayout());
         GridBagConstraints c0 = new GridBagConstraints();
         c0.anchor = GridBagConstraints.LINE_END;
         c0.weightx = 1.;
         c0.weighty = 1.;
         c0.gridy = 0;
         c0.gridx = 0;
-        panel.add(new JLabel("Custom angle", JLabel.RIGHT), c0);
+        customPanel.add(customCheckBox, c0);
         c0.anchor = GridBagConstraints.LINE_START;
         c0.gridx = 1;
-        panel.add(spinner, c0);
+        customPanel.add(spinner, c0);
+
+        FOVModel model = new FOVModel();
+        JTable grid = new JTable(model);
+        grid.setTableHeader(null);
+        grid.setShowGrid(false);
+        grid.setRowSelectionAllowed(true);
+        grid.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        grid.setColumnSelectionAllowed(false);
+        grid.setIntercellSpacing(new Dimension(0, 0));
+
+        grid.getColumnModel().getColumn(SELECTED_COL).setCellRenderer(new SelectedRenderer());
+        grid.getColumnModel().getColumn(SELECTED_COL).setPreferredWidth(ICON_WIDTH + 8);
+        grid.getColumnModel().getColumn(SELECTED_COL).setMaxWidth(ICON_WIDTH + 8);
+        grid.getColumnModel().getColumn(OBJECT_COL).setCellRenderer(new FOVRenderer());
+
+        grid.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (!grid.isEnabled())
+                    return;
+                TableValue v = TableValue.tableValueAtPoint(grid, e.getPoint());
+                if (v == null || !(v.value instanceof FOV))
+                    return;
+
+                if (v.col == SELECTED_COL) {
+                    ((FOV) v.value).select();
+                    model.fireTableRowsUpdated(v.row, v.row);
+                }
+                MovieDisplay.display();
+            }
+        });
+
+        JScrollPane scroll = new JScrollPane();
+        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scroll.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY));
+
+        scroll.setViewportView(grid);
+        scroll.getViewport().setBackground(grid.getBackground());
+        scroll.setPreferredSize(new Dimension(-1, getGridRowHeight(grid) * NUMBEROFVISIBLEROWS + 1));
+        grid.setRowHeight(getGridRowHeight(grid));
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c1 = new GridBagConstraints();
+        c1.fill = GridBagConstraints.BOTH;
+        c1.weightx = 1;
+        c1.gridx = 0;
+
+        c1.weighty = 0;
+        c1.gridy = 0;
+        panel.add(customPanel, c1);
+
+        c1.weighty = 1;
+        c1.gridy = 1;
+        panel.add(scroll, c1);
 
         ComponentUtils.smallVariant(panel);
         return panel;
+    }
+
+    private int rowHeight = -1;
+
+    private int getGridRowHeight(JTable grid) {
+        if (rowHeight == -1) {
+            rowHeight = grid.getRowHeight() + 4;
+        }
+        return rowHeight;
+    }
+
+    private static class SelectedRenderer extends JHVTableCellRenderer {
+
+        private final JCheckBox checkBox = new JCheckBox();
+
+        SelectedRenderer() {
+            setHorizontalAlignment(CENTER);
+            checkBox.putClientProperty("JComponent.sizeVariant", "small");
+            checkBox.setBorderPainted(true);
+        }
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            if (value instanceof FOV) {
+                FOV fov = (FOV) value;
+                checkBox.setSelected(fov.isSelected());
+                checkBox.setBorder(JHVTableCellRenderer.cellBorder);
+            }
+            checkBox.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
+            return checkBox;
+        }
+
+    }
+
+    private static class FOVRenderer extends JHVTableCellRenderer {
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+            if (value instanceof FOV) {
+                FOV fov = (FOV) value;
+                label.setText(fov.toString());
+                label.setBorder(JHVTableCellRenderer.cellBorder);
+            }
+            return label;
+        }
+    }
+
+    private static class FOVModel extends AbstractTableModel {
+
+        private final List<FOV> fovs = FOVs;
+
+        @Override
+        public int getRowCount() {
+            return fovs.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 2;
+        }
+
+        @Override
+        public Object getValueAt(int row, int column) {
+            return fovs.get(row);
+        }
+
     }
 
 }
