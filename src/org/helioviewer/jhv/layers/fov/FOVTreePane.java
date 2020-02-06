3bf4b3370f4f1aac53ac96765ea3a0d0c5bbf64b
Bogdan Nicula <bogdan@nicula.net>
2020-02-06 18:18:40 +0100

    More logic and less Swing-crazy with a tree

---
+++ src/org/helioviewer/jhv/layers/fov/FOVTreePane.java
@@ -0,0 +1,75 @@
+package org.helioviewer.jhv.layers.fov;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.util.EventObject;
+
+import javax.swing.BorderFactory;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellEditor;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.DefaultTreeModel;
+
+import org.helioviewer.jhv.base.Colors;
+import org.helioviewer.jhv.layers.fov.FOVTreeElement.FOVType;
+import org.helioviewer.jhv.opengl.BufVertex;
+import org.helioviewer.jhv.opengl.FOVShape;
+import org.helioviewer.jhv.opengl.text.JhvTextRenderer;
+
+@SuppressWarnings("serial")
+public class FOVTreePane extends JScrollPane {
+
+    private final FOVTreeRoot root;
+
+    public FOVTreePane() {
+        root = new FOVTreeRoot("SOLO");
+        root.add(new FOVTreeElement("EUI/HRI", FOVType.RECTANGULAR, 0, 16.6 / 60., 16.6 / 60., Colors.Blue));
+        root.add(new FOVTreeElement("EUI/FSI", FOVType.RECTANGULAR, 0, 228 / 60., 228 / 60., Colors.Blue));
+        root.add(new FOVTreeElement("METIS", FOVType.CIRCULAR, 3, 5.8, 5.8, Colors.Blue));
+        root.add(new FOVTreeElement("PHI/HRT", FOVType.RECTANGULAR, 0, 0.28, 0.28, Colors.Blue));
+        root.add(new FOVTreeElement("PHI/FDT", FOVType.RECTANGULAR, 0, 2, 2, Colors.Blue));
+        root.add(new FOVTreeElement("SPICE", FOVType.RECTANGULAR, 0, 16 / 60., 11 / 60., Colors.Blue));
+        root.add(new FOVTreeElement("STIX", FOVType.RECTANGULAR, 0, 2, 2, Colors.Blue));
+
+        DefaultTreeModel dtm = new DefaultTreeModel(root);
+        FOVTreeRenderer ftr = new FOVTreeRenderer();
+
+        JTree tree = new JTree(dtm);
+        tree.setEditable(true);
+        tree.setShowsRootHandles(true);
+        tree.setCellRenderer(ftr);
+        tree.setCellEditor(new MyTreeCellEditor(tree, ftr));
+        tree.setRowHeight(0); // force calculation of nodes heights
+
+        setViewportView(tree);
+        setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY));
+        setPreferredSize(new Dimension(-1, 90));
+    }
+
+    public void putFOV(FOVShape f, double distance, BufVertex lineBuf, BufVertex centerBuf, JhvTextRenderer renderer) {
+        root.children().asIterator().forEachRemaining(c -> ((FOVTreeElement) c).putFOV(f, distance, lineBuf, centerBuf, renderer));
+    }
+
+    private static class MyTreeCellEditor extends DefaultTreeCellEditor {
+
+        MyTreeCellEditor(JTree tree, DefaultTreeCellRenderer renderer) {
+            super(tree, renderer);
+        }
+
+        @Override
+        public Component getTreeCellEditorComponent(JTree tree, Object value, boolean isSelected, boolean expanded, boolean leaf, int row) {
+            return renderer.getTreeCellRendererComponent(tree, value, true, expanded, leaf, row, true);
+        }
+
+        @Override
+        public boolean isCellEditable(EventObject e) {
+            return true;
+        }
+
+    }
+
+}
---
+++ src/org/helioviewer/jhv/layers/fov/FOVTreeRenderer.java
@@ -0,0 +1,88 @@
+package org.helioviewer.jhv.layers.fov;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JCheckBox;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.JTree;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+import org.helioviewer.jhv.gui.ComponentUtils;
+import org.helioviewer.jhv.gui.components.base.TerminatedFormatterFactory;
+import org.helioviewer.jhv.gui.components.base.WheelSupport;
+import org.helioviewer.jhv.layers.MovieDisplay;
+
+@SuppressWarnings("serial")
+class FOVTreeRenderer extends DefaultTreeCellRenderer {
+
+    private static final double min = -60;
+    private static final double max = 60;
+
+    private final JSpinner spinnerX;
+    private final JSpinner spinnerY;
+
+    FOVTreeRenderer() {
+        spinnerX = new JSpinner(new SpinnerNumberModel(0, min, max, 0.1));
+        JFormattedTextField fX = ((JSpinner.DefaultEditor) spinnerX.getEditor()).getTextField();
+        fX.setFormatterFactory(new TerminatedFormatterFactory("%.2f", "\u2032", min, max));
+        WheelSupport.installMouseWheelSupport(spinnerX);
+
+        spinnerY = new JSpinner(new SpinnerNumberModel(0, min, max, 0.1));
+        JFormattedTextField fY = ((JSpinner.DefaultEditor) spinnerY.getEditor()).getTextField();
+        fY.setFormatterFactory(new TerminatedFormatterFactory("%.2f", "\u2032", min, max));
+        WheelSupport.installMouseWheelSupport(spinnerY);
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object obj, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        if (obj instanceof FOVTreeElement) {
+            return createLeaf((FOVTreeElement) obj, tree.getBackground());
+        } else if (obj instanceof FOVTreeRoot) {
+            return createRoot((FOVTreeRoot) obj, tree.getBackground());
+        } else {
+            return super.getTreeCellRendererComponent(tree, obj, selected, expanded, leaf, row, hasFocus);
+        }
+    }
+
+    private static JCheckBox createLeaf(FOVTreeElement f, Color back) {
+        JCheckBox checkBox = new JCheckBox(f.toString(), f.isEnabled());
+        checkBox.setBackground(back);
+        checkBox.addActionListener(e -> {
+            f.toggle();
+            MovieDisplay.display();
+        });
+        checkBox.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3 && checkBox.isSelected()) {
+                    f.zoom();
+                    MovieDisplay.render(1);
+                }
+            }
+        });
+        ComponentUtils.smallVariant(checkBox);
+        return checkBox;
+    }
+
+    private JPanel createRoot(FOVTreeRoot r, Color back) {
+        spinnerX.addChangeListener(e -> r.setCenterX((Double) spinnerX.getValue()));
+        spinnerY.addChangeListener(e -> r.setCenterY((Double) spinnerY.getValue()));
+
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 0));
+        panel.setOpaque(false);
+        panel.add(new JLabel(r.toString()));
+        panel.add(spinnerX);
+        panel.add(spinnerY);
+        ComponentUtils.smallVariant(panel);
+        return panel;
+    }
+
+}
