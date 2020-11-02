51cf0aa17d4ec596e56df5dd08dd74179dd429ca
Bogdan Nicula <bogdan@nicula.net>
2020-11-02 09:54:19 +0100

    Reduce

--- src/org/helioviewer/jhv/gui/components/Buttons.java
+++ src/org/helioviewer/jhv/gui/components/Buttons.java
@@ -6,80 +6,73 @@ import org.helioviewer.jhv.gui.UIGlobals;
 
 public class Buttons {
 
-    private static String uc2ent(MaterialDesign... uc) {
-        StringBuilder out = new StringBuilder();
-        for (MaterialDesign c : uc)
-            out.append(c.code);
-        return out.toString();
-    }
-
     public static Font getMaterialFont(float size) {
         return UIGlobals.uiFontMDI.deriveFont(size);
     }
 
-    public static final String close = String.valueOf(MaterialDesign.MDI_CLOSE.code);
-    public static final String play = String.valueOf(MaterialDesign.MDI_PLAY.code);
-    public static final String pause = String.valueOf(MaterialDesign.MDI_PAUSE.code);
-    public static final String backward = String.valueOf(MaterialDesign.MDI_STEP_BACKWARD.code);
-    public static final String forward = String.valueOf(MaterialDesign.MDI_STEP_FORWARD.code);
-    public static final String record = String.valueOf(MaterialDesign.MDI_RECORD.code);
-    public static final String check = String.valueOf(MaterialDesign.MDI_CHECK.code);
+    public static final String close = MaterialDesign.CLOSE.toString();
+    public static final String play = MaterialDesign.PLAY.toString();
+    public static final String pause = MaterialDesign.PAUSE.toString();
+    public static final String backward = MaterialDesign.STEP_BACKWARD.toString();
+    public static final String forward = MaterialDesign.STEP_FORWARD.toString();
+    public static final String record = MaterialDesign.RECORD.toString();
+    public static final String check = MaterialDesign.CHECK.toString();
 
     public static final String chevronRight = "<html><font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_CHEVRON_RIGHT) + "</font>&nbsp;";
+            MaterialDesign.CHEVRON_RIGHT + "</font>&nbsp;";
     public static final String chevronDown = "<html><font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_CHEVRON_DOWN) + "</font>&nbsp;";
+            MaterialDesign.CHEVRON_DOWN + "</font>&nbsp;";
 
     public static final String optionsRight = "<html>Options<font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_CHEVRON_RIGHT);
+            MaterialDesign.CHEVRON_RIGHT;
     public static final String optionsDown = "<html>Options<font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_CHEVRON_DOWN);
+            MaterialDesign.CHEVRON_DOWN;
 
     public static final String newLayer = "<html><font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_PLUS_CIRCLE) + "</font>&nbsp;New Layer";
+            MaterialDesign.PLUS_CIRCLE + "</font>&nbsp;New Layer";
     public static final String syncLayers = "<html>&nbsp;<font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_SYNC) + "</font>&nbsp;Sync";
+            MaterialDesign.SYNC + "</font>&nbsp;Sync";
 
     public static final String lock = "<html>&nbsp;<font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_LOCK) + "</font>&nbsp;";
+            MaterialDesign.LOCK + "</font>&nbsp;";
     public static final String unlock = "<html>&nbsp;<font face='Material Design Icons' size=4>" +
-            uc2ent(MaterialDesign.MDI_LOCK_OPEN) + "</font>&nbsp;";
+            MaterialDesign.LOCK_OPEN + "</font>&nbsp;";
 
-    public static final String info = button(MaterialDesign.MDI_INFORMATION_VARIANT);
-    public static final String download = button(MaterialDesign.MDI_DOWNLOAD);
+    public static final String info = button(MaterialDesign.INFORMATION_VARIANT);
+    public static final String download = button(MaterialDesign.DOWNLOAD);
 
-    public static final String invert = button(MaterialDesign.MDI_INVERT_COLORS);
-    public static final String corona = button(MaterialDesign.MDI_WHITE_BALANCE_SUNNY);
+    public static final String invert = button(MaterialDesign.INVERT_COLORS);
+    public static final String corona = button(MaterialDesign.WHITE_BALANCE_SUNNY);
 
-    public static final String calendar = button(MaterialDesign.MDI_CALENDAR);
-    public static final String skipBack = button(MaterialDesign.MDI_SKIP_BACKWARD);
-    public static final String skipFore = button(MaterialDesign.MDI_SKIP_FORWARD);
+    public static final String calendar = button(MaterialDesign.CALENDAR);
+    public static final String skipBack = button(MaterialDesign.SKIP_BACKWARD);
+    public static final String skipFore = button(MaterialDesign.SKIP_FORWARD);
 
     private static String button(MaterialDesign uc) {
-        return "<html><span style='font-size:12px'>&nbsp;<font face='Material Design Icons'>" + uc2ent(uc) + "</font>&nbsp;";
+        return "<html><span style='font-size:12px'>&nbsp;<font face='Material Design Icons'>" + uc + "</font>&nbsp;";
     }
 
     // toolbar
 
-    private static String toolBar(MaterialDesign... uc) {
-        return "<html><center><font face='Material Design Icons' size=5>" + uc2ent(uc) + "</font>";
+    private static String toolBar(MaterialDesign uc) {
+        return "<html><center><font face='Material Design Icons' size=5>" + uc + "</font>";
     }
 
-    public static final String annotate = toolBar(MaterialDesign.MDI_SHAPE_POLYGON_PLUS);
-    public static final String axis = toolBar(MaterialDesign.MDI_BACKUP_RESTORE);
-    public static final String cutOut = toolBar(MaterialDesign.MDI_VECTOR_CIRCLE_VARIANT);
-    public static final String diffRotation = toolBar(MaterialDesign.MDI_CHART_GANTT);
-    public static final String multiview = toolBar(MaterialDesign.MDI_BORDER_ALL);
-    public static final String offDisk = toolBar(MaterialDesign.MDI_WEATHER_SUNNY);
-    public static final String pan = toolBar(MaterialDesign.MDI_CURSOR_MOVE);
-    public static final String projection = toolBar(MaterialDesign.MDI_CUBE_OUTLINE);
-    public static final String resetCamera = toolBar(MaterialDesign.MDI_CROSSHAIRS);
-    public static final String rotate = toolBar(MaterialDesign.MDI_ROTATE_3D);
-    public static final String samp = toolBar(MaterialDesign.MDI_SHARE_VARIANT);
-    public static final String track = toolBar(MaterialDesign.MDI_CROSSHAIRS_GPS);
-    public static final String zoomFit = toolBar(MaterialDesign.MDI_CROP_LANDSCAPE);
-    public static final String zoomIn = toolBar(MaterialDesign.MDI_MAGNIFY_PLUS);
-    public static final String zoomOne = toolBar(MaterialDesign.MDI_PLUS_ONE);
-    public static final String zoomOut = toolBar(MaterialDesign.MDI_MAGNIFY_MINUS);
+    public static final String annotate = toolBar(MaterialDesign.SHAPE_POLYGON_PLUS);
+    public static final String axis = toolBar(MaterialDesign.BACKUP_RESTORE);
+    public static final String cutOut = toolBar(MaterialDesign.VECTOR_CIRCLE_VARIANT);
+    public static final String diffRotation = toolBar(MaterialDesign.CHART_GANTT);
+    public static final String multiview = toolBar(MaterialDesign.BORDER_ALL);
+    public static final String offDisk = toolBar(MaterialDesign.WEATHER_SUNNY);
+    public static final String pan = toolBar(MaterialDesign.CURSOR_MOVE);
+    public static final String projection = toolBar(MaterialDesign.CUBE_OUTLINE);
+    public static final String resetCamera = toolBar(MaterialDesign.CROSSHAIRS);
+    public static final String rotate = toolBar(MaterialDesign.ROTATE_3D);
+    public static final String samp = toolBar(MaterialDesign.SHARE_VARIANT);
+    public static final String track = toolBar(MaterialDesign.CROSSHAIRS_GPS);
+    public static final String zoomFit = toolBar(MaterialDesign.CROP_LANDSCAPE);
+    public static final String zoomIn = toolBar(MaterialDesign.MAGNIFY_PLUS);
+    public static final String zoomOne = toolBar(MaterialDesign.PLUS_ONE);
+    public static final String zoomOut = toolBar(MaterialDesign.MAGNIFY_MINUS);
 
 }
--- src/org/helioviewer/jhv/gui/components/MaterialDesign.java
+++ src/org/helioviewer/jhv/gui/components/MaterialDesign.java
@@ -1,52 +1,57 @@
 package org.helioviewer.jhv.gui.components;
 
-public enum MaterialDesign {
+enum MaterialDesign {
 
     // https://github.com/aalmiray/ikonli
-    MDI_BACKUP_RESTORE("mdi-backup-restore", '\uf06f'),
-    MDI_BORDER_ALL("mdi-border-all", '\uf0c7'),
-    // MDI_BRIGHTNESS_AUTO("mdi-brightness-auto", '\uf0e1'),
-    MDI_CALENDAR("mdi-calendar", '\uf0ed'),
-    MDI_CHART_GANTT("mdi-chart-gantt", '\uf66c'),
-    MDI_CHECK("mdi-check", '\uf12c'),
-    MDI_CHEVRON_DOWN("mdi-chevron-down", '\uf140'),
-    MDI_CHEVRON_RIGHT("mdi-chevron-right", '\uf142'),
-    MDI_CLOSE("mdi-close", '\uf156'),
-    MDI_CROP_LANDSCAPE("mdi-crop-landscape", '\uf1a0'),
-    MDI_CROSSHAIRS("mdi-crosshairs", '\uf1a3'),
-    MDI_CROSSHAIRS_GPS("mdi-crosshairs-gps", '\uf1a4'),
-    MDI_CUBE_OUTLINE("mdi-cube-outline", '\uf1a7'),
-    MDI_CURSOR_MOVE("mdi-cursor-move", '\uf1b6'),
-    MDI_DOWNLOAD("mdi-download", '\uf1da'),
-    MDI_INFORMATION_VARIANT("mdi-information-variant", '\uf64e'),
-    MDI_INVERT_COLORS("mdi-invert-colors", '\uf301'),
-    MDI_LOCK("mdi-lock", '\uf33e'),
-    MDI_LOCK_OPEN("mdi-lock-open", '\uf33f'),
-    MDI_MAGNIFY_MINUS("mdi-magnify-minus", '\uf34a'),
-    MDI_MAGNIFY_PLUS("mdi-magnify-plus", '\uf34b'),
-    MDI_PAUSE("mdi-pause", '\uf3e4'),
-    MDI_PLAY("mdi-play", '\uf40a'),
-    MDI_PLUS_CIRCLE("mdi-plus-circle", '\uf417'),
-    MDI_PLUS_ONE("mdi-plus-one", '\uf41b'),
-    MDI_RECORD("mdi-record", '\uf44a'),
-    MDI_ROTATE_3D("mdi-rotate-3d", '\uf464'),
-    MDI_SHAPE_POLYGON_PLUS("mdi-shape-polygon-plus", '\uf65e'),
-    MDI_SHARE_VARIANT("mdi-share-variant", '\uf497'),
-    MDI_SKIP_BACKWARD("mdi-skip-backward", '\uf4ab'),
-    MDI_SKIP_FORWARD("mdi-skip-forward", '\uf4ac'),
-    // MDI_SLACK("mdi-slack", '\uf4b1'),
-    MDI_STEP_BACKWARD("mdi-step-backward", '\uf4d5'),
-    MDI_STEP_FORWARD("mdi-step-forward", '\uf4d7'),
-    MDI_SYNC("mdi-sync", '\uf4e6'),
-    // MDI_UPLOAD("mdi-upload", '\uf552'),
-    MDI_VECTOR_CIRCLE_VARIANT("mdi-vector-circle-variant", '\uf557'),
-    MDI_WEATHER_SUNNY("mdi-weather-sunny", '\uf599'),
-    MDI_WHITE_BALANCE_SUNNY("mdi-white-balance-sunny", '\uf5a8');
+    BACKUP_RESTORE("mdi-backup-restore", '\uf06f'),
+    BORDER_ALL("mdi-border-all", '\uf0c7'),
+    // BRIGHTNESS_AUTO("mdi-brightness-auto", '\uf0e1'),
+    CALENDAR("mdi-calendar", '\uf0ed'),
+    CHART_GANTT("mdi-chart-gantt", '\uf66c'),
+    CHECK("mdi-check", '\uf12c'),
+    CHEVRON_DOWN("mdi-chevron-down", '\uf140'),
+    CHEVRON_RIGHT("mdi-chevron-right", '\uf142'),
+    CLOSE("mdi-close", '\uf156'),
+    CROP_LANDSCAPE("mdi-crop-landscape", '\uf1a0'),
+    CROSSHAIRS("mdi-crosshairs", '\uf1a3'),
+    CROSSHAIRS_GPS("mdi-crosshairs-gps", '\uf1a4'),
+    CUBE_OUTLINE("mdi-cube-outline", '\uf1a7'),
+    CURSOR_MOVE("mdi-cursor-move", '\uf1b6'),
+    DOWNLOAD("mdi-download", '\uf1da'),
+    INFORMATION_VARIANT("mdi-information-variant", '\uf64e'),
+    INVERT_COLORS("mdi-invert-colors", '\uf301'),
+    LOCK("mdi-lock", '\uf33e'),
+    LOCK_OPEN("mdi-lock-open", '\uf33f'),
+    MAGNIFY_MINUS("mdi-magnify-minus", '\uf34a'),
+    MAGNIFY_PLUS("mdi-magnify-plus", '\uf34b'),
+    PAUSE("mdi-pause", '\uf3e4'),
+    PLAY("mdi-play", '\uf40a'),
+    PLUS_CIRCLE("mdi-plus-circle", '\uf417'),
+    PLUS_ONE("mdi-plus-one", '\uf41b'),
+    RECORD("mdi-record", '\uf44a'),
+    ROTATE_3D("mdi-rotate-3d", '\uf464'),
+    SHAPE_POLYGON_PLUS("mdi-shape-polygon-plus", '\uf65e'),
+    SHARE_VARIANT("mdi-share-variant", '\uf497'),
+    SKIP_BACKWARD("mdi-skip-backward", '\uf4ab'),
+    SKIP_FORWARD("mdi-skip-forward", '\uf4ac'),
+    // SLACK("mdi-slack", '\uf4b1'),
+    STEP_BACKWARD("mdi-step-backward", '\uf4d5'),
+    STEP_FORWARD("mdi-step-forward", '\uf4d7'),
+    SYNC("mdi-sync", '\uf4e6'),
+    // UPLOAD("mdi-upload", '\uf552'),
+    VECTOR_CIRCLE_VARIANT("mdi-vector-circle-variant", '\uf557'),
+    WEATHER_SUNNY("mdi-weather-sunny", '\uf599'),
+    WHITE_BALANCE_SUNNY("mdi-white-balance-sunny", '\uf5a8');
 
-    public final char code;
+    private final char code;
 
     MaterialDesign(String _description, char _code) {
         code = _code;
+    }
+
+    @Override
+    public String toString() {
+        return /*code <= '\uffff' ?*/ String.valueOf(code) /*: new String(Character.toChars(code))*/;
     }
 
 }
