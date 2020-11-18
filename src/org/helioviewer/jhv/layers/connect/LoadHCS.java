5365f1e430f941d8894ccd0b8f69b4ed7aed3a9f
Bogdan Nicula <bogdan@nicula.net>
2020-11-18 09:20:23 +0100

    Encapsulate HCS

--- src/org/helioviewer/jhv/layers/connect/LoadHCS.java
+++ src/org/helioviewer/jhv/layers/connect/LoadHCS.java
@@ -11,15 +11,18 @@ import javax.annotation.Nonnull;
 import org.helioviewer.jhv.base.Regex;
 import org.helioviewer.jhv.gui.Message;
 import org.helioviewer.jhv.io.NetClient;
+import org.helioviewer.jhv.layers.connect.ReceiverHCS.HCS;
 import org.helioviewer.jhv.log.Log;
 import org.helioviewer.jhv.math.Vec3;
 import org.helioviewer.jhv.threads.EventQueueCallbackExecutor;
 
 import com.google.common.util.concurrent.FutureCallback;
 
-public class LoadHCS implements Callable<List<Vec3>> {
+public class LoadHCS implements Callable<HCS> {
 
-    public static Void submit(@Nonnull URI uri, ReceiverVecList receiver) {
+    private static final double radius = 1.01;
+
+    public static Void submit(@Nonnull URI uri, ReceiverHCS receiver) {
         EventQueueCallbackExecutor.pool.submit(new LoadHCS(uri), new Callback(receiver));
         return null;
     }
@@ -31,8 +34,8 @@ public class LoadHCS implements Callable<List<Vec3>> {
     }
 
     @Override
-    public List<Vec3> call() throws Exception {
-        List<Vec3> vecList = new ArrayList<>();
+    public HCS call() throws Exception {
+        List<Vec3> hcsList = new ArrayList<>();
 
         try (NetClient nc = NetClient.of(uri); BufferedReader br = new BufferedReader(nc.getReader())) {
             int lineNo = 0;
@@ -54,7 +57,7 @@ public class LoadHCS implements Callable<List<Vec3>> {
                         double x = Math.cos(lat) * Math.sin(lon);
                         double y = Math.sin(lat);
                         double z = Math.cos(lat) * Math.cos(lon);
-                        vecList.add(new Vec3(x, y, z));
+                        hcsList.add(new Vec3(x, y, z));
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -62,22 +65,30 @@ public class LoadHCS implements Callable<List<Vec3>> {
             }
         }
 
-        if (vecList.isEmpty())
+        int size = hcsList.size();
+        if (size == 0)
             return null;
-        return vecList;
+
+        List<Vec3> ortho = new ArrayList<>(size);
+        List<Vec3> scale = new ArrayList<>(size);
+        hcsList.forEach(v -> {
+            ortho.add(new Vec3(radius * v.x, radius * v.y, radius * v.z));
+            scale.add(new Vec3(v.x, -v.y, v.z));
+        });
+        return new HCS(ortho, scale);
     }
 
-    private static class Callback implements FutureCallback<List<Vec3>> {
+    private static class Callback implements FutureCallback<HCS> {
 
-        private final ReceiverVecList receiver;
+        private final ReceiverHCS receiver;
 
-        Callback(ReceiverVecList _receiver) {
+        Callback(ReceiverHCS _receiver) {
             receiver = _receiver;
         }
 
         @Override
-        public void onSuccess(List<Vec3> result) {
-            receiver.setVecList(result);
+        public void onSuccess(HCS result) {
+            receiver.setHCS(result);
         }
 
         @Override
