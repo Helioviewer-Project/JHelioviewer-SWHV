fe12d91a4bcda1b73bd7feabd90175942644921c
Bogdan Nicula <bogdan@nicula.net>
2021-05-02 14:42:45 +0200

    Re-capsulate

--- src/org/helioviewer/jhv/imagedata/ImageFilter.java
+++ src/org/helioviewer/jhv/imagedata/ImageFilter.java
@@ -8,101 +8,105 @@ import java.util.ArrayList;
 
 import org.helioviewer.jhv.math.MathUtils;
 
-// derived from http://dev.ipol.im/~getreuer/code/
 public class ImageFilter {
 
-    private static final int SII_MIN_K = 3;
-    private static final int SII_MAX_K = 5;
+    // derived from http://dev.ipol.im/~getreuer/code/
+    private static class GaussFilter {
 
-    private static final double sigma0 = 100.0 / Math.PI;
-    private static final short[][] radii0 = {
-            {76, 46, 23, 0, 0},
-            {82, 56, 37, 19, 0},
-            {85, 61, 44, 30, 16}};
-    private static final float[][] weights0 = {
-            {0.1618f, 0.5502f, 0.9495f, 0, 0},
-            {0.0976f, 0.3376f, 0.6700f, 0.9649f, 0},
-            {0.0739f, 0.2534f, 0.5031f, 0.7596f, 0.9738f}};
+        private static final int SII_MIN_K = 3;
+        private static final int SII_MAX_K = 5;
 
-    // Box weights
-    private final float[] weights = new float[SII_MAX_K];
-    // Box radii
-    private final int[] radii = new int[SII_MAX_K];
-    // Number of boxes
-    private final int K;
+        private static final double sigma0 = 100 / Math.PI;
+        private static final short[][] radii0 = {
+                {76, 46, 23, 0, 0},
+                {82, 56, 37, 19, 0},
+                {85, 61, 44, 30, 16}};
+        private static final float[][] weights0 = {
+                {0.1618f, 0.5502f, 0.9495f, 0, 0},
+                {0.0976f, 0.3376f, 0.6700f, 0.9649f, 0},
+                {0.0739f, 0.2534f, 0.5031f, 0.7596f, 0.9738f}};
 
-    private final float[] buffer;
+        // Box weights
+        private final float[] weights = new float[SII_MAX_K];
+        // Box radii
+        private final int[] radii = new int[SII_MAX_K];
+        // Number of boxes
+        private final int K;
 
-    public ImageFilter(double sigma, int _K, int N) {
-        K = _K;
+        private final float[] buffer;
 
-        int i = K - SII_MIN_K;
-        double sum = 0;
+        GaussFilter(double sigma, int _K, int N) {
+            K = _K;
 
-        for (int k = 0; k < K; ++k) {
-            radii[k] = (int) (radii0[i][k] * (sigma / sigma0) + 0.5);
-            sum += weights0[i][k] * (2 * radii[k] + 1);
+            int i = K - SII_MIN_K;
+            double sum = 0;
+
+            for (int k = 0; k < K; ++k) {
+                radii[k] = (int) (radii0[i][k] * (sigma / sigma0) + 0.5);
+                sum += weights0[i][k] * (2 * radii[k] + 1);
+            }
+
+            for (int k = 0; k < K; ++k)
+                weights[k] = (float) (weights0[i][k] / sum);
+
+            int pad = radii[0] + 1;
+            buffer = new float[N + 2 * pad];
         }
 
-        for (int k = 0; k < K; ++k)
-            weights[k] = (float) (weights0[i][k] / sum);
+        private static int extension(int N, int n) {
+            while (true) {
+                if (n < 0)
+                    n = -1 - n;         // Reflect over n = -1/2
+                else if (n >= N)
+                    n = 2 * N - 1 - n;  // Reflect over n = N - 1/2
+                else
+                    break;
+            }
+            return n;
+        }
 
-        int pad = radii[0] + 1;
-        buffer = new float[N + 2 * pad];
+        private void gaussianConv(float[] dst, float[] src, int N, int stride, int offset) {
+            int pad = radii[0] + 1;
+            float accum = 0;
+
+            // Compute cumulative sum of src over n = -pad,..., N + pad - 1
+            for (int n = -pad; n < 0; ++n) {
+                accum += src[offset + stride * extension(N, n)];
+                buffer[pad + n] = accum;
+            }
+            for (int n = 0; n < N; ++n) {
+                accum += src[offset + stride * n];
+                buffer[pad + n] = accum;
+            }
+            for (int n = N; n < N + pad; ++n) {
+                accum += src[offset + stride * extension(N, n)];
+                buffer[pad + n] = accum;
+            }
+
+            // Compute stacked box filters
+            for (int n = 0; n < N; ++n) {
+                accum = weights[0] * (buffer[pad + n + radii[0]] - buffer[pad + n - radii[0] - 1]);
+                for (int k = 1; k < K; ++k)
+                    accum += weights[k] * (buffer[pad + n + radii[k]] - buffer[pad + n - radii[k] - 1]);
+                dst[offset + stride * n] = accum;
+            }
+        }
+
+        void gaussianConvImage(float[] dst, float[] src, int width, int height) {
+            // Filter each row
+            for (int y = 0; y < height; ++y)
+                gaussianConv(dst, src, width, 1, width * y);
+            // Filter each column
+            for (int x = 0; x < width; ++x)
+                gaussianConv(dst, dst, height, width, x);
+        }
+
     }
 
-    private static int extension(int N, int n) {
-        while (true) {
-            if (n < 0)
-                n = -1 - n;         // Reflect over n = -1/2
-            else if (n >= N)
-                n = 2 * N - 1 - n;  // Reflect over n = N - 1/2
-            else
-                break;
-        }
-        return n;
-    }
-
-    private void gaussianConv(float[] dst, float[] src, int N, int stride, int offset) {
-        int pad = radii[0] + 1;
-        float accum = 0;
-
-        // Compute cumulative sum of src over n = -pad,..., N + pad - 1
-        for (int n = -pad; n < 0; ++n) {
-            accum += src[offset + stride * extension(N, n)];
-            buffer[pad + n] = accum;
-        }
-        for (int n = 0; n < N; ++n) {
-            accum += src[offset + stride * n];
-            buffer[pad + n] = accum;
-        }
-        for (int n = N; n < N + pad; ++n) {
-            accum += src[offset + stride * extension(N, n)];
-            buffer[pad + n] = accum;
-        }
-
-        // Compute stacked box filters
-        for (int n = 0; n < N; ++n) {
-            accum = weights[0] * (buffer[pad + n + radii[0]] - buffer[pad + n - radii[0] - 1]);
-            for (int k = 1; k < K; ++k)
-                accum += weights[k] * (buffer[pad + n + radii[k]] - buffer[pad + n - radii[k] - 1]);
-            dst[offset + stride * n] = accum;
-        }
-    }
-
-    private void gaussianConvImage(float[] dst, float[] src, int width, int height) {
-        // Filter each row
-        for (int y = 0; y < height; ++y)
-            gaussianConv(dst, src, width, 1, width * y);
-        // Filter each column
-        for (int x = 0; x < width; ++x)
-            gaussianConv(dst, dst, height, width, x);
-    }
-
-    private static final int _K = 3;
+    private static final int K = 3;
     private static final float H = 0.7f;
     private static final double[] sigmas = {1, 4, 16, 64};
-    private static final double[] weight = {0.25, 0.5, 0.75, 1};
+    private static final double[] weights = {0.25, 0.5, 0.75, 1};
 
     @SuppressWarnings("serial")
     private static class ScaleTask extends RecursiveTask<float[]> {
@@ -123,7 +127,7 @@ public class ImageFilter {
 
         @Override
         protected float[] compute() {
-            ImageFilter filter = new ImageFilter(sigma, _K, Math.max(width, height));
+            GaussFilter filter = new GaussFilter(sigma, K, Math.max(width, height));
 
             int size = width * height;
             float[] conv = new float[size];
@@ -148,7 +152,7 @@ public class ImageFilter {
     private static float[] multiScale(float[] data, int width, int height) {
         ArrayList<ForkJoinTask<float[]>> tasks = new ArrayList<>(sigmas.length);
         for (int i = 0; i < sigmas.length; ++i)
-            tasks.add(new ScaleTask(data, width, height, sigmas[i], weight[i]).fork());
+            tasks.add(new ScaleTask(data, width, height, sigmas[i], weights[i]).fork());
 
         int size = width * height;
         float[] image = new float[size];
@@ -167,7 +171,7 @@ public class ImageFilter {
                 min = v;
         }
 
-        if (max == min)
+        if (min == max)
             return data;
 
         float k = (1 - H) / (max - min);
