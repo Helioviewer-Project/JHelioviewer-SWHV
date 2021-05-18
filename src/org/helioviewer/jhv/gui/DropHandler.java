7a46eab70b4b1d4d9fb79702416d7cbbbfc983cd
Bogdan Nicula <bogdan@nicula.net>
2021-05-18 14:27:58 +0200

    Accept dnd URL also

--- src/org/helioviewer/jhv/gui/DropHandler.java
+++ src/org/helioviewer/jhv/gui/DropHandler.java
@@ -1,6 +1,7 @@
 package org.helioviewer.jhv.gui;
 
 import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 import java.io.File;
 import java.net.URI;
 import java.util.ArrayList;
@@ -10,47 +11,69 @@ import java.util.Locale;
 import javax.swing.TransferHandler;
 
 import org.helioviewer.jhv.io.Load;
+import org.helioviewer.jhv.log.Log;
+
+import org.apache.commons.validator.routines.UrlValidator;
 
 @SuppressWarnings("serial")
 class DropHandler extends TransferHandler {
 
     @Override
-    public boolean canImport(TransferHandler.TransferSupport info) {
-        return info.isDrop() && info.getTransferable().isDataFlavorSupported(DataFlavor.javaFileListFlavor);
+    public boolean canImport(TransferHandler.TransferSupport support) {
+        Transferable transferable = support.getTransferable();
+        return support.isDrop() && (transferable.isDataFlavorSupported(DataFlavor.javaFileListFlavor) ||
+                transferable.isDataFlavorSupported(DataFlavor.stringFlavor));
     }
 
     @Override
-    public boolean importData(TransferHandler.TransferSupport info) {
-        if (!canImport(info)) {
+    public boolean importData(TransferHandler.TransferSupport support) {
+        if (!canImport(support))
             return false;
-        }
 
         try {
-            List<?> objects = (List<?>) info.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
+            Transferable transferable = support.getTransferable();
+            if (transferable.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                List<?> objects = (List<?>) transferable.getTransferData(DataFlavor.javaFileListFlavor);
 
-            ArrayList<URI> imageUris = new ArrayList<>(objects.size());
-            ArrayList<URI> requestUris = new ArrayList<>(objects.size());
-            for (Object object : objects) {
-                if (object instanceof File) {
-                    File f = (File) object;
-                    if (f.isFile() && f.canRead()) {
-                        URI uri = f.toURI();
-                        String loc = uri.toString().toLowerCase(Locale.ENGLISH);
-                        if (loc.endsWith(".json"))
-                            requestUris.add(uri);
-                        else
-                            imageUris.add(uri);
+                ArrayList<URI> imageUris = new ArrayList<>(objects.size());
+                ArrayList<URI> requestUris = new ArrayList<>(objects.size());
+                for (Object object : objects) {
+                    if (object instanceof File) {
+                        File f = (File) object;
+                        if (f.isFile() && f.canRead()) {
+                            URI uri = f.toURI();
+                            String loc = uri.toString().toLowerCase(Locale.ENGLISH);
+                            if (loc.endsWith(".json"))
+                                requestUris.add(uri);
+                            else
+                                imageUris.add(uri);
+                        }
                     }
                 }
+
+                requestUris.forEach(Load.request::get);
+                if (!imageUris.isEmpty())
+                    Load.Image.getAll(imageUris);
+
+                return true;
+            } else if (transferable.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                String loc = (String) transferable.getTransferData(DataFlavor.stringFlavor);
+                System.out.println(">>> " + loc);
+
+                if (UrlValidator.getInstance().isValid(loc)) {
+                    URI uri = new URI(loc);
+                    if (loc.endsWith(".json"))
+                        Load.request.get(uri);
+                    else if (loc.endsWith(".jhv"))
+                        Load.state.get(uri);
+                    else
+                        Load.image.get(uri);
+
+                    return true;
+                }
             }
-
-            requestUris.forEach(Load.request::get);
-            if (!imageUris.isEmpty())
-                Load.Image.getAll(imageUris);
-
-            return true;
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.error("Import error", e);
         }
 
         return false;
