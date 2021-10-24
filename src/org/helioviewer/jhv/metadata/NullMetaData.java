8d18f64c4c64db492c6c41d851268e45884ca77c
Bogdan Nicula <bogdan@nicula.net>
2021-10-24 23:53:43 +0200

    Avoid 1:1 zoom crash

--- src/org/helioviewer/jhv/metadata/NullMetaData.java
+++ src/org/helioviewer/jhv/metadata/NullMetaData.java
@@ -1,12 +1,17 @@
 package org.helioviewer.jhv.metadata;
 
 import org.helioviewer.jhv.astronomy.Sun;
+import org.helioviewer.jhv.base.Region;
 import org.helioviewer.jhv.time.JHVTime;
 
 public class NullMetaData extends BaseMetaData {
 
     public NullMetaData(JHVTime time) {
         viewpoint = Sun.getEarth(time);
+
+        pixelW = 1024;
+        pixelH = 1024;
+        region = new Region(-0.5, -0.5, 1, 1);
     }
 
 }
