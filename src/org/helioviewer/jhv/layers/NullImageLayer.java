79ba304bd772d5d281b2ac55e6c1d56ee0954ee1
Bogdan Nicula <bogdan@nicula.net>
2021-10-25 08:08:31 +0200

    Avoid NullImageLayer options panel

--- src/org/helioviewer/jhv/layers/NullImageLayer.java
+++ src/org/helioviewer/jhv/layers/NullImageLayer.java
@@ -1,11 +1,15 @@
 package org.helioviewer.jhv.layers;
 
-import org.helioviewer.jhv.view.ManyView;
+import org.helioviewer.jhv.view.View;
 
 public class NullImageLayer extends ImageLayer {
 
-    public NullImageLayer(ManyView _view) {
-        super(null);
+    public NullImageLayer(View _view) {
+        super(_view);
+    }
+
+    @Override
+    public void setView(View _view) {
         view = _view;
     }
 
