87db72176cd3273052abe96ec23423c5a3d01b58
Bogdan Nicula <bogdan@nicula.net>
2021-11-23 09:00:57 +0100

    Reduce

--- src/org/helioviewer/jhv/JHVDirectory.java
+++ src/org/helioviewer/jhv/JHVDirectory.java
@@ -13,21 +13,11 @@ public enum JHVDirectory {
         public String getPath() {
             return path + File.separator + "JHelioviewer-SWHV" + File.separator;
         }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
-        }
     },
     CACHE {
         @Override
         public String getPath() {
             return HOME.getPath() + "Cache" + File.separator;
-        }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
         }
     },
     // The JHV state directory
@@ -36,22 +26,12 @@ public enum JHVDirectory {
         public String getPath() {
             return HOME.getPath() + "States" + File.separator;
         }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
-        }
     },
     // The exports directory (movies, screenshots, meta data)
     EXPORTS {
         @Override
         public String getPath() {
             return HOME.getPath() + "Exports" + File.separator;
-        }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
         }
     },
     // The log directory
@@ -60,22 +40,12 @@ public enum JHVDirectory {
         public String getPath() {
             return HOME.getPath() + "Logs" + File.separator;
         }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
-        }
     },
     // The log settings directory
     SETTINGS {
         @Override
         public String getPath() {
             return HOME.getPath() + "Settings" + File.separator;
-        }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
         }
     },
     // The remote files directory
@@ -84,11 +54,6 @@ public enum JHVDirectory {
         public String getPath() {
             return HOME.getPath() + "Downloads" + File.separator;
         }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
-        }
     },
     // Directory of automatically loaded GIMP gradient files
     COLOR_PLUGINS {
@@ -96,21 +61,11 @@ public enum JHVDirectory {
         public String getPath() {
             return HOME.getPath() + "Colortables" + File.separator;
         }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
-        }
     },
     TEMP {
         @Override
         public String getPath() {
             return HOME.getPath() + "Temp" + File.separator;
-        }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
         }
     },
     EVENTS {
@@ -118,17 +73,14 @@ public enum JHVDirectory {
         public String getPath() {
             return HOME.getPath() + "Events" + File.separator;
         }
-
-        @Override
-        public File getFile() {
-            return new File(getPath());
-        }
     };
 
     // A String representation of the path of the directory
     public abstract String getPath();
 
     // A File representation of the path of the directory
-    public abstract File getFile();
+    public File getFile() {
+        return new File(getPath());
+    }
 
 }
