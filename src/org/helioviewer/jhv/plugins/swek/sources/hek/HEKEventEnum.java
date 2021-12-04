54c6a466023bcb871809648b750880b3ef13f199
Bogdan Nicula <bogdan@nicula.net>
2021-12-04 02:04:51 +0100

    Record reduce

--- src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKEventEnum.java
+++ src/org/helioviewer/jhv/plugins/swek/sources/hek/HEKEventEnum.java
@@ -1,7 +1,6 @@
 package org.helioviewer.jhv.plugins.swek.sources.hek;
 
 enum HEKEventEnum {
-
     ACTIVE_REGION("ActiveRegion", "Active Region", "AR"), CORONAL_MASS_EJECTION("CME", "Coronal Mass Ejection", "CE"), CORONAL_DIMMING(
             "CoronalDimming", "Coronal Dimming", "CD"), CORONAL_HOLE("CoronalHole", "Coronal Hole", "CH"), CORONAL_WAVE("CoronalWave",
             "Coronal Wave", "CW"), FILAMENT("Filament", "Filament", "FI"), FILAMENT_ERUPTION("FilamentEruption", "Filament Eruption", "FE"), FLARE(
