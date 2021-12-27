a3d8a183a868b98f206e988ac173965fb78c9384
Bogdan Nicula <bogdan@nicula.net>
2021-12-27 09:56:09 +0100

    Reduce

--- src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepHandler.java
+++ src/org/helioviewer/jhv/plugins/swek/sources/comesep/ComesepHandler.java
@@ -3,7 +3,9 @@ package org.helioviewer.jhv.plugins.swek.sources.comesep;
 import java.io.ByteArrayOutputStream;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 
 import org.helioviewer.jhv.Log;
 import org.helioviewer.jhv.base.Pair;
@@ -90,10 +92,35 @@ public class ComesepHandler extends SWEKHandler {
     @Override
     public JHVEvent parseEventJSON(JSONObject json, SWEKSupplier supplier, int id, long start, long end, boolean full) throws JSONException {
         JHVEvent currentEvent = new JHVEvent(supplier, id, start, end);
-        ComesepParser.parseResult(json, currentEvent);
+        parseResult(json, currentEvent);
         currentEvent.finishParams();
 
         return currentEvent;
+    }
+
+    private static void parseResult(JSONObject result, JHVEvent currentEvent) throws JSONException {
+        Iterator<String> keys = result.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            if (result.isNull(key))
+                return;
+
+            String lowKey = key.toLowerCase(Locale.ENGLISH);
+            if (!(lowKey.equals("atearliest") || lowKey.equals("atlatest") ||
+                    lowKey.equals("begin_time_value") || lowKey.equals("end_time_value") ||
+                    lowKey.startsWith("liftoff"))) {
+                String value = result.optString(key).trim();
+                if (!value.isEmpty()) {
+                    if (lowKey.equals("atstrongest")) {
+                        try {
+                            value = TimeUtils.format(Long.parseLong(value) * 1000L);
+                        } catch (Exception ignore) {
+                        }
+                    }
+                    currentEvent.addParameter(lowKey, value, true);
+                }
+            }
+        }
     }
 
 }
