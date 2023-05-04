d014882d0352232f3ad95f865b77cfeb356a50a2
Bogdan Nicula <bogdan@nicula.net>
2023-05-04 11:30:13 +0200

    Reduce

--- src/org/helioviewer/jhv/view/j2k/io/jpip/JPIPSocket.java
+++ src/org/helioviewer/jhv/view/j2k/io/jpip/JPIPSocket.java
@@ -87,7 +87,7 @@ public class JPIPSocket extends HTTPSocket {
         req.setHeader("Connection", "keep-alive");
         req.setHeader("Accept-Encoding", "gzip");
         req.setHeader("Cache-Control", "no-cache");
-        req.setHeader("Host", usedHost + ':' + usedPort);
+        req.setHeader("Host", host);
         queryStr = "GET " + jpipPath + '?' + queryStr + " HTTP/1.1\r\n" + req + "\r\n";
         write(queryStr);
     }
