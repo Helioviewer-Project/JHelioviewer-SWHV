0e2d4dc344e7ca51628e95480fc21dc11aa14cab
Bogdan Nicula <bogdan@nicula.net>
2023-05-18 10:23:35 +0200

    Implement HAPI manually

--- src/org/helioviewer/jhv/io/HapiClient.java
+++ src/org/helioviewer/jhv/io/HapiClient.java
@@ -1,10 +1,7 @@
 package org.helioviewer.jhv.io;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URI;
-import java.util.Iterator;
 import java.util.concurrent.Callable;
 
 import javax.annotation.Nonnull;
@@ -12,19 +9,18 @@ import javax.annotation.Nonnull;
 import org.helioviewer.jhv.Log;
 import org.helioviewer.jhv.threads.EventQueueCallbackExecutor;
 
-import org.hapiserver.HapiClientCSVIterator;
-import org.hapiserver.HapiRecord;
-//import org.hapiserver.TimeUtil;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import com.google.common.util.concurrent.FutureCallback;
 
 public class HapiClient {
 
     public static void submit() {
-        String server = "https://jfaden.net/HapiServerDemo/hapi";
-        String id = "Iowa+City+Conditions";
-        String parameters = "Temperature,Humidity";
-        String startTime = "2019-10-20T00:00";
-        String endTime = "2019-10-24T05:00";
+        String server = "https://cdaweb.gsfc.nasa.gov/hapi";
+        String id = "SOLO_L2_MAG-RTN-NORMAL-1-MINUTE";
+        String parameters = "B_RTN";
+        String startTime = "2022-01-01T00:00:00";
+        String endTime = "2022-01-02T00:00:00";
 
         EventQueueCallbackExecutor.pool.submit(new LoadHapi(server, id, parameters, startTime, endTime), new Callback(server));
     }
@@ -33,13 +29,13 @@ public class HapiClient {
                             String endTime) implements Callable<Void> {
         @Override
         public Void call() throws Exception {
+            URI infoURI = new URI(server + "/info?id=" + id + "&parameters=" + parameters);
+            JSONUtils.get(infoURI);
+
             URI dataURI = new URI(server + "/data?id=" + id + "&parameters=" + parameters + "&time.min=" + startTime + "&time.max=" + endTime);
-            try (NetClient nc = NetClient.of(dataURI); InputStream is = nc.getStream()) {
-                URI infoURI = new URI(server + "/info?id=" + id + "&parameters=" + parameters);
-                Iterator<HapiRecord> it = new HapiClientCSVIterator(JSONUtils.get(infoURI), new BufferedReader(new InputStreamReader(is)));
-                while (it.hasNext()) {
-                    HapiRecord rec = it.next();
-                    System.out.printf("%s %9.3f %9.3f%n", rec.getIsoTime(0), rec.getDouble(1), rec.getDouble(2));
+            try (NetClient nc = NetClient.of(dataURI); Reader reader = nc.getReader()) {
+                for (CSVRecord rec : CSVFormat.DEFAULT.parse(reader)) {
+                    System.out.printf("%s %s %s %s%n", rec.get(0), rec.get(1), rec.get(2), rec.get(3));
                 }
             }
             return null;
