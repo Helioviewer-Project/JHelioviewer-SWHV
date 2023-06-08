5ce95c592e201159229cf909345d38f5e0eeeda7
Bogdan Nicula <bogdan@nicula.net>
2023-06-08 11:26:27 +0200

    Extract method

--- src/org/helioviewer/jhv/gui/dialogs/SoarDialog.java
+++ src/org/helioviewer/jhv/gui/dialogs/SoarDialog.java
@@ -4,7 +4,6 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.util.List;
 
