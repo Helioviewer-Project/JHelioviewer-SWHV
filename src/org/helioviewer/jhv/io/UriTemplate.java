58b79218711e1fd4fd455c7fd5ebe753b6092538
Bogdan Nicula <bogdan@nicula.net>
2023-07-27 14:50:07 +0200

    Reduce

--- src/org/helioviewer/jhv/io/UriTemplate.java
+++ src/org/helioviewer/jhv/io/UriTemplate.java
@@ -10,18 +10,18 @@ public class UriTemplate {
     private final HashMap<String, String> params = new HashMap<>();
 
     public UriTemplate(String _base) {
-        base = _base;
+        base = _base + '?';
     }
 
     public UriTemplate set(String param, Object value) {
-        params.put(param, URLEncoder.encode(value.toString(), StandardCharsets.UTF_8));
+        params.put('&' + param, '=' + URLEncoder.encode(value.toString(), StandardCharsets.UTF_8));
         return this;
     }
 
     @Override
     public String toString() {
-        StringBuilder builder = new StringBuilder(base + '?');
-        params.forEach((key, value) -> builder.append('&').append(key).append('=').append(value));
+        StringBuilder builder = new StringBuilder(base);
+        params.forEach((key, value) -> builder.append(key).append(value));
         return builder.toString();
     }
 
