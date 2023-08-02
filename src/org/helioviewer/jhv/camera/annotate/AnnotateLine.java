e134cf353682b7c676de74b01aa6e467e6029686
Bogdan Nicula <bogdan@nicula.net>
2023-08-02 12:05:46 +0200

    Draw line annotation

--- src/org/helioviewer/jhv/camera/annotate/AnnotateLine.java
+++ src/org/helioviewer/jhv/camera/annotate/AnnotateLine.java
@@ -2,6 +2,7 @@ package org.helioviewer.jhv.camera.annotate;
 
 import javax.annotation.Nullable;
 
+import org.helioviewer.jhv.base.Colors;
 import org.helioviewer.jhv.camera.Camera;
 import org.helioviewer.jhv.camera.CameraHelper;
 import org.helioviewer.jhv.camera.Interaction;
@@ -13,30 +14,30 @@ import org.json.JSONObject;
 
 public class AnnotateLine extends AbstractAnnotateable {
 
-    private final FOVShape fov = new FOVShape();
+    private static final int SUBDIVISIONS = 12;
 
     public AnnotateLine(JSONObject jo) {
         super(jo);
     }
 
+    private static void drawLine(double centerX, double centerY, double bw, double bh, BufVertex buf, byte[] color) {
+        for (int i = 0; i <= SUBDIVISIONS; i++) {
+            double x = -bw + 2 * bw / SUBDIVISIONS * i + centerX;
+            double y = -bh + 2 * bh / SUBDIVISIONS * i + centerY;
+            double z = FOVShape.computeZ(x, y);
+            if (i == 0) { // first
+                buf.putVertex((float) x, (float) y, (float) z, 1, Colors.Null);
+            }
+            buf.putVertex((float) x, (float) y, (float) z, 1, color);
+             if (i == SUBDIVISIONS) { // last
+                buf.putVertex((float) x, (float) y, (float) z, 1, Colors.Null);
+            }
+        }
+    }
+
     @Nullable
     private static Vec3 computePointFOV(Camera camera, int x, int y) {
         return CameraHelper.getVectorFromSphereOrPlane(camera, Display.getActiveViewport(), x, y, camera.getDragRotation());
-    }
-
-    public void zoom(Camera camera) {
-        boolean dragged = beingDragged();
-        if ((startPoint == null || endPoint == null) && !dragged)
-            return;
-
-        Vec3 p0 = dragged ? dragStartPoint : startPoint;
-        Vec3 p1 = dragged ? dragEndPoint : endPoint;
-        double dx = 0.5 * (p1.x - p0.x);
-        double dy = 0.5 * (p1.y - p0.y);
-
-        camera.setTranslation(-(p0.x + dx), -(p0.y + dy));
-        camera.resetDragRotation();
-        camera.setFOV(2 * Math.atan2(Math.sqrt(dx * dx + dy * dy), camera.getViewpoint().distance));
     }
 
     @Override
@@ -51,9 +52,7 @@ public class AnnotateLine extends AbstractAnnotateable {
         double dx = 0.5 * (p1.x - p0.x);
         double dy = 0.5 * (p1.y - p0.y);
 
-        fov.setCenter(p0.x + dx, p0.y + dy);
-        fov.putCenter(centerBuf, color);
-        fov.putRectLine(dx, dy, lineBuf, color);
+        drawLine(p0.x + dx, p0.y + dy, dx, dy, lineBuf, color);
     }
 
     @Override
