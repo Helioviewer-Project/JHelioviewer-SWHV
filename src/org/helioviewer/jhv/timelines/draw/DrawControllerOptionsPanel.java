a0457f9f950633a81e34a5511c97c1993a65995d
Bogdan Nicula <bogdan@nicula.net>
2023-08-03 10:55:29 +0200

    Reduce

--- src/org/helioviewer/jhv/timelines/draw/DrawControllerOptionsPanel.java
+++ src/org/helioviewer/jhv/timelines/draw/DrawControllerOptionsPanel.java
@@ -66,61 +66,37 @@ class DrawControllerOptionsPanel extends JPanel {
         @Override
         public String toString() {
             String plural = number > 1 ? "s" : "";
-
-            switch (zoom) {
-                case All:
-                    return "Maximum interval";
-                case Hour:
-                    return number + " hour" + plural;
-                case Day:
-                    return number + " day" + plural;
-                case Month:
-                    return number + " month" + plural;
-                case Year:
-                    return number + " year" + plural;
-                case Carrington:
-                    return "Carrington rotation" + plural;
-                case Movie:
-                    return "Movie interval";
-                default:
-                    break;
-            }
-            return "Custom interval";
+            return switch (zoom) {
+                case CUSTOM -> "Custom interval";
+                case All -> "Maximum interval";
+                case Year -> number + " year" + plural;
+                case Month -> number + " month" + plural;
+                case Day -> number + " day" + plural;
+                case Hour -> number + " hour" + plural;
+                case Carrington -> "Carrington rotation" + plural;
+                case Movie -> "Movie interval";
+            };
         }
     }
 
     private static void zoomTo(ZOOM zoom, long value) {
-        TimeAxis selectedInterval = DrawController.selectedAxis;
         TimeAxis availableInterval = DrawController.availableAxis;
+        long end = DrawController.selectedAxis.end();
 
         switch (zoom) {
-            case All:
-                DrawController.setSelectedInterval(availableInterval.start(), availableInterval.end());
-                break;
-            case Day:
-                computeZoomInterval(selectedInterval.end(), TimeUtils.DAY_IN_MILLIS * value);
-                break;
-            case Hour:
-                computeZoomInterval(selectedInterval.end(), 60 * 60 * 1000L * value);
-                break;
-            case Month:
-                computeZoomInterval(selectedInterval.end(), (long) (30.6001 * TimeUtils.DAY_IN_MILLIS * value));
-                break;
-            case Year:
-                computeZoomInterval(selectedInterval.end(), (long) (365.25 * TimeUtils.DAY_IN_MILLIS * value));
-                break;
-            case Carrington:
-                computeZoomInterval(selectedInterval.end(), (long) (Carrington.CR_SYNODIC_MEAN * TimeUtils.DAY_IN_MILLIS * value));
-                break;
-            case Movie:
-                DrawController.setSelectedInterval(Movie.getStartTime(), Movie.getEndTime());
-                break;
-            case CUSTOM:
-                break;
+            case CUSTOM -> {
+            }
+            case All -> DrawController.setSelectedInterval(availableInterval.start(), availableInterval.end());
+            case Year -> setZoom(end, (long) (365.25 * TimeUtils.DAY_IN_MILLIS * value));
+            case Month -> setZoom(end, (long) (30.6001 * TimeUtils.DAY_IN_MILLIS * value));
+            case Day -> setZoom(end, TimeUtils.DAY_IN_MILLIS * value);
+            case Hour -> setZoom(end, 60 * 60 * 1000L * value);
+            case Carrington -> setZoom(end, (long) (Carrington.CR_SYNODIC_MEAN * TimeUtils.DAY_IN_MILLIS * value));
+            case Movie -> DrawController.setSelectedInterval(Movie.getStartTime(), Movie.getEndTime());
         }
     }
 
-    private static void computeZoomInterval(long end, long diff) {
+    private static void setZoom(long end, long diff) {
         long now = System.currentTimeMillis();
         if (end > now)
             end = now;
