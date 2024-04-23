4c99c701bb83eaa4c2ae6feccb52e13c525469a2
Bogdan Nicula <bogdan@nicula.net>
2024-04-23 21:06:30 +0200

    Hack to preserve support of fits.gz files

--- src/org/helioviewer/jhv/io/NetFileCache.java
+++ src/org/helioviewer/jhv/io/NetFileCache.java
@@ -19,6 +19,13 @@ public class NetFileCache {
 
     private static final Tika tika = new Tika();
 
+    private static DataUri.Format detect(Path path) throws IOException {
+        if (path.toString().toLowerCase().endsWith(".fits.gz"))
+            return DataUri.Format.FITS;
+        else
+            return DataUri.Format.get(tika.detect(path));
+    }
+
     private static final LoadingCache<URI, DataUri> cache = Caffeine.newBuilder().softValues().
             build(uri -> {
                 String scheme = uri.getScheme().toLowerCase();
@@ -27,13 +34,13 @@ public class NetFileCache {
 
                 if ("file".equals(scheme)) {
                     Path path = Path.of(uri);
-                    return new DataUri(uri, uri, DataUri.Format.get(tika.detect(path)));
+                    return new DataUri(uri, uri, detect(path));
                 } else {
                     Path path = Files.createTempFile(JHVGlobals.fileCacheDir.toPath(), "jhv", null);
                     try (NetClient nc = NetClient.of(uri, false, NetClient.NetCache.BYPASS); BufferedSink sink = Okio.buffer(Okio.sink(path))) {
                         sink.writeAll(nc.getSource());
                     }
-                    return new DataUri(path.toUri(), uri, DataUri.Format.get(tika.detect(path)));
+                    return new DataUri(path.toUri(), uri, detect(path));
                 }
             });
 
