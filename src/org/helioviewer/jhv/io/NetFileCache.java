c86faba00593781fe06b0867d181568fe050d08f
Bogdan Nicula <bogdan@nicula.net>
2024-04-24 10:05:22 +0200

    Avoid troubles for URIs with authority

--- src/org/helioviewer/jhv/io/NetFileCache.java
+++ src/org/helioviewer/jhv/io/NetFileCache.java
@@ -1,5 +1,6 @@
 package org.helioviewer.jhv.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
@@ -19,28 +20,29 @@ public class NetFileCache {
 
     private static final Tika tika = new Tika();
 
-    private static DataUri.Format detect(Path path) throws IOException {
-        if (path.toString().toLowerCase().endsWith(".fits.gz")) // hack
+    private static DataUri.Format detect(File file) throws IOException {
+        if (file.getPath().toLowerCase().endsWith(".fits.gz")) // hack
             return DataUri.Format.FITS;
         else
-            return DataUri.Format.get(tika.detect(path));
+            return DataUri.Format.get(tika.detect(file));
     }
 
     private static final LoadingCache<URI, DataUri> cache = Caffeine.newBuilder().softValues().
             build(uri -> {
                 String scheme = uri.getScheme().toLowerCase();
                 if ("jpip".equals(scheme) || "jpips".equals(scheme))
-                    return new DataUri(uri, uri, DataUri.Format.JPIP);
+                    return new DataUri(uri, uri, null, DataUri.Format.JPIP);
                 if ("file".equals(scheme)) {
-                    Path path = Path.of(uri);
-                    return new DataUri(uri, uri, detect(path));
+                    File file = new File(uri.getPath()); // for files with authority (//localhost) and Windows
+                    return new DataUri(uri, uri, file, detect(file));
                 }
 
                 Path path = Files.createTempFile(JHVGlobals.fileCacheDir.toPath(), "jhv", null);
                 try (NetClient nc = NetClient.of(uri, false, NetClient.NetCache.BYPASS); BufferedSink sink = Okio.buffer(Okio.sink(path))) {
                     sink.writeAll(nc.getSource());
                 }
-                return new DataUri(path.toUri(), uri, detect(path));
+                File file = path.toFile();
+                return new DataUri(path.toUri(), uri, file, detect(file));
             });
 
     public static DataUri get(@Nonnull URI uri) throws IOException {
