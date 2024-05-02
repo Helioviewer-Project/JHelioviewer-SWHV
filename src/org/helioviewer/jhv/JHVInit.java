af9b1b972b3faddfeef131b1c7f89424bd6d1055
Bogdan Nicula <bogdan@nicula.net>
2024-05-02 11:11:18 +0200

    Update resources

--- src/org/helioviewer/jhv/JHVInit.java
+++ src/org/helioviewer/jhv/JHVInit.java
@@ -92,15 +92,15 @@ class JHVInit {
         List<String> kernels = List.of(
                 "de432s_reduced.bsp",
                 "ahead_2017_061_5295day_predict.epm.bsp",
-                "solo_ANC_soc-orbit-stp_20200210-20301120_306_V1_00343_V01.bsp",
+                "solo_ANC_soc-orbit-stp_20200210-20301120_310_V1_00348_V01.bsp",
                 "naif0012.tls",
                 "pck00011.tpc",
                 "solo_ANC_soc-ops-fk_V02.tf",
                 "solo_ANC_soc-sci-fk_V08.tf");
 
-        kernels.parallelStream().forEach(x -> {
-            try (InputStream in = FileUtils.getResource("/kernels/" + x)) {
-                Files.copy(in, Path.of(JHVGlobals.dataCacheDir, x));
+        kernels.parallelStream().forEach(k -> {
+            try (InputStream in = FileUtils.getResource("/kernels/" + k)) {
+                Files.copy(in, Path.of(JHVGlobals.dataCacheDir, k));
             } catch (Exception e) {
                 Log.error("SPICE kernel copy error", e);
             }
