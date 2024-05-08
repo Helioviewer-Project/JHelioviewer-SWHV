a7198b4c5de22419e44804f0b05fe9ee45be2f08
Bogdan Nicula <bogdan@nicula.net>
2024-05-08 14:33:17 +0200

    Cleanup

--- src/org/helioviewer/jhv/view/j2k/jpip/JPIPSegment.java
+++ src/org/helioviewer/jhv/view/j2k/jpip/JPIPSegment.java
@@ -1,10 +1,6 @@
 package org.helioviewer.jhv.view.j2k.jpip;
 
 import java.io.Serializable;
-//import java.nio.ByteBuffer;
-//import java.nio.ByteOrder;
-
-//import com.google.common.primitives.Shorts;
 
 // The class JPIPSegment is used to construct objects to store
 // segments of JPIP data. These segments can be data-bin segments as well as EOR
@@ -40,52 +36,6 @@ class JPIPSegment implements Serializable {
 
     // Indicates if this segment is an End-Of-Response message
     public boolean isEOR;
-
-/*
-    public ByteBuffer toBuffer() {
-        ByteBuffer buffer = ByteBuffer.allocate(length + 6).order(ByteOrder.nativeOrder());
-
-        buffer.putShort(Shorts.checkedCast(binID));
-        // buffer.putLong(aux); not used
-        // buffer.putShort(Shorts.checkedCast(codestreamID)); don't need
-        // buffer.putShort(Shorts.checkedCast(offset)); don't need
-
-        buffer.putShort(Shorts.checkedCast(length));
-        if (length > 0)
-            buffer.put(data);
-
-        buffer.put((byte) klassID); // fits surely
-
-        byte flags = 0;
-        if (isFinal)
-            flags += 1;
-        buffer.put(flags);
-        buffer.rewind();
-
-        return buffer;
-    }
-
-    public static JPIPSegment fromBuffer(ByteBuffer buffer) {
-        JPIPSegment seg = new JPIPSegment();
-        seg.binID = buffer.getShort();
-        // seg.codestreamID = buffer.getShort();
-        // seg.offset = buffer.getShort();
-
-        seg.length = buffer.getShort();
-        if (seg.length > 0) {
-            seg.data = new byte[seg.length];
-            buffer.get(seg.data);
-        }
-
-        seg.klassID = buffer.get();
-
-        byte flags = buffer.get();
-        if ((flags & 1) != 0)
-            seg.isFinal = true;
-
-        return seg;
-    }
-*/
 
     // Returns a string representation of the JPIP segment
     @Override
