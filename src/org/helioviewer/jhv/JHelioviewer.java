44c865494c488a8478eb0fb85297e24ea802c3ac
Bogdan Nicula <bogdan@nicula.net>
2024-05-09 08:52:05 +0200

    Remove traces of Sentry

--- src/org/helioviewer/jhv/JHelioviewer.java
+++ src/org/helioviewer/jhv/JHelioviewer.java
@@ -59,7 +59,6 @@ public class JHelioviewer {
         System.setProperty("org.lwjgl.system.SharedLibraryExtractPath", JHVGlobals.libCacheDir);
         // System.setProperty("flatlaf.nativeLibraryPath", JHVGlobals.libCacheDir);
         // System.setProperty("jsamp.nosystray", "true");
-        // if (true) throw new RuntimeException("This is a Sentry test. Please ignore.");
 
         JHVInit.init();
 
