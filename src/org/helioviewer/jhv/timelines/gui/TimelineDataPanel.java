ed0610707976b90152ec2c03d65f3570aa67740c
Bogdan Nicula <bogdan@nicula.net>
2024-05-22 13:56:55 +0200

    Better UI for timeline dialog

--- src/org/helioviewer/jhv/timelines/gui/TimelineDataPanel.java
+++ src/org/helioviewer/jhv/timelines/gui/TimelineDataPanel.java
@@ -1,16 +1,18 @@
 package org.helioviewer.jhv.timelines.gui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.util.HashSet;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BoxLayout;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
-import org.helioviewer.jhv.timelines.TimelineLayer;
-import org.helioviewer.jhv.timelines.TimelineLayers;
 import org.helioviewer.jhv.timelines.Timelines;
 import org.helioviewer.jhv.timelines.band.Band;
 import org.helioviewer.jhv.timelines.band.BandType;
@@ -18,67 +20,51 @@ import org.helioviewer.jhv.timelines.band.BandType;
 @SuppressWarnings("serial")
 public class TimelineDataPanel extends JPanel {
 
-    private final JComboBox<String> comboBoxGroup = new JComboBox<>();
-    private final JComboBox<BandType> comboBoxData = new JComboBox<>();
+    private final JComboBox<String> comboGroup = new JComboBox<>();
+    private final JList<BandType> listBand = new JList<>();
 
     TimelineDataPanel() {
-        setLayout(new GridBagLayout());
-        comboBoxGroup.addActionListener(e -> updateGroupValues());
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-        GridBagConstraints c = new GridBagConstraints();
-        c.anchor = GridBagConstraints.LINE_START;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 1;
+        comboGroup.addActionListener(e -> updateGroupValues());
+        listBand.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    loadButtonPressed();
+                }
+            }
+        });
 
-        c.gridy = 0;
-        c.gridx = 0;
-        add(new JLabel("Group", JLabel.RIGHT), c);
-        c.gridx = 1;
-        add(comboBoxGroup, c);
+        JPanel groupPanel = new JPanel(new FlowLayout(FlowLayout.LEADING, 5, 0));
+        groupPanel.add(new JLabel("Group", JLabel.RIGHT));
+        groupPanel.add(comboGroup);
 
-        c.gridy = 1;
-        c.gridx = 0;
-        add(new JLabel("Dataset", JLabel.RIGHT), c);
-        c.gridx = 1;
-        add(comboBoxData, c);
+        com.jidesoft.swing.SearchableUtils.installSearchable(listBand);
+        JScrollPane scrollPane = new JScrollPane(listBand);
+        scrollPane.setPreferredSize(new Dimension(350, 350));
+
+        add(groupPanel);
+        add(scrollPane);
     }
 
     public void setupDatasets() {
         DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>(BandType.getGroups());
         if (model.getSize() > 0) {
-            comboBoxGroup.setModel(model);
-            comboBoxGroup.setSelectedIndex(0);
+            comboGroup.setModel(model);
+            comboGroup.setSelectedIndex(0);
         }
     }
 
     void updateGroupValues() {
-        String selectedGroup = (String) comboBoxGroup.getSelectedItem();
-        if (selectedGroup == null)
-            return;
-
-        HashSet<BandType> bandTypesInSelector = new HashSet<>();
-        for (TimelineLayer tl : TimelineLayers.get()) {
-            if (tl instanceof Band)
-                bandTypesInSelector.add(((Band) tl).getBandType());
-        }
-
-        DefaultComboBoxModel<BandType> model = new DefaultComboBoxModel<>();
-        for (BandType value : BandType.getBandTypes(selectedGroup)) {
-            if (!bandTypesInSelector.contains(value))
-                model.addElement(value);
-        }
-
-        if (model.getSize() > 0) {
-            comboBoxData.setModel(model);
-            comboBoxData.setSelectedIndex(0);
-        }
+        if (comboGroup.getSelectedItem() instanceof String group)
+            listBand.setListData(BandType.getBandTypes(group).toArray(BandType[]::new));
     }
 
     void loadButtonPressed() {
-        BandType bandType = (BandType) comboBoxData.getSelectedItem();
-        if (bandType == null)
-            return;
-        Timelines.getLayers().add(Band.createFromType(bandType));
+        for (BandType bandType : listBand.getSelectedValuesList()) {
+            Timelines.getLayers().add(Band.createFromType(bandType));
+        }
     }
 
 }
