be6c387f007efda8f0581430d016b457003690c9
Bogdan Nicula <bogdan@nicula.net>
2024-06-26 10:53:52 +0200

    Reduce

--- src/org/helioviewer/jhv/math/Quat.java
+++ src/org/helioviewer/jhv/math/Quat.java
@@ -86,7 +86,7 @@ public class Quat {
 
     public static Quat rotateWithConjugate(Quat q1, Quat q2) {
         return new Quat(
-                 q1.w * q2.w + q1.x * q2.x + q1.y * q2.y + q1.z * q2.z,
+                q1.w * q2.w + q1.x * q2.x + q1.y * q2.y + q1.z * q2.z,
                 -q1.w * q2.x + q1.x * q2.w - q1.y * q2.z + q1.z * q2.y,
                 -q1.w * q2.y + q1.y * q2.w - q1.z * q2.x + q1.x * q2.z,
                 -q1.w * q2.z + q1.z * q2.w - q1.x * q2.y + q1.y * q2.x);
