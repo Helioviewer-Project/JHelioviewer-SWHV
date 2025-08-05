6670c2538e0f434c686d5162748b00b79337e331
Bogdan Nicula <bogdan@nicula.net>
2025-08-05 11:35:23 +0200

    Update libs & SOLO kernel

--- src/org/helioviewer/jhv/JHVInit.java
+++ src/org/helioviewer/jhv/JHVInit.java
@@ -95,7 +95,7 @@ class JHVInit {
         List<String> kernels = List.of(
                 "de432s_reduced.bsp",
                 "ahead_2017_061_5295day_predict.epm.bsp",
-                "solo_ANC_soc-orbit-stp_20200210-20301120_368_V1_00455_V01.bsp",
+                "solo_ANC_soc-orbit-stp_20200210-20301120_376_V1_00463_V01.bsp",
                 "naif0012.tls",
                 "pck00011.tpc",
                 "solo_ANC_soc-ops-fk_V02.tf",
