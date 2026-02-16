0842ff3d3efadc3332a6c3c0935378018f36806d
Bogdan Nicula <bogdan@nicula.net>
2026-02-16 21:09:28 +0100

    Be more careful setting the cadence

--- src/org/helioviewer/jhv/gui/components/CadencePanel.java
+++ src/org/helioviewer/jhv/gui/components/CadencePanel.java
@@ -8,13 +8,15 @@ import javax.swing.JPanel;
 
 import org.helioviewer.jhv.gui.components.base.JHVSpinner;
 import org.helioviewer.jhv.io.APIRequest;
+import org.helioviewer.jhv.math.MathUtils;
 
 @SuppressWarnings("serial")
 public final class CadencePanel extends JPanel {
 
     private static final String[] timeStepUnits = {"sec", "min", "hours", "days", "get all"};
+    private static final int CADENCE_MIN = 1, CADENCE_MAX = 10000;
 
-    private final JHVSpinner cadenceSpinner = new JHVSpinner(1, 1, 10000, 1);
+    private final JHVSpinner cadenceSpinner = new JHVSpinner(1, CADENCE_MIN, CADENCE_MAX, 1);
     private final JComboBox<String> unitCombo = new JComboBox<>(timeStepUnits);
 
     public CadencePanel() {
@@ -43,20 +45,23 @@ public final class CadencePanel extends JPanel {
     }
 
     public void setCadence(int value) {
-        if (value == APIRequest.CADENCE_ALL)
+        if (value == APIRequest.CADENCE_ALL) {
             unitCombo.setSelectedItem(timeStepUnits[4]);
-        else if (value / 86400 != 0) {
+            return;
+        }
+
+        if (value / 86400 != 0) {
             unitCombo.setSelectedItem(timeStepUnits[3]);
-            cadenceSpinner.setValue(value / 86400);
+            cadenceSpinner.setValue(MathUtils.clip(value / 86400, CADENCE_MIN, CADENCE_MAX));
         } else if (value / 3600 != 0) {
             unitCombo.setSelectedItem(timeStepUnits[2]);
-            cadenceSpinner.setValue(value / 3600);
+            cadenceSpinner.setValue(MathUtils.clip(value / 3600, CADENCE_MIN, CADENCE_MAX));
         } else if (value / 60 != 0) {
             unitCombo.setSelectedItem(timeStepUnits[1]);
-            cadenceSpinner.setValue(value / 60);
+            cadenceSpinner.setValue(MathUtils.clip(value / 60, CADENCE_MIN, CADENCE_MAX));
         } else {
             unitCombo.setSelectedItem(timeStepUnits[0]);
-            cadenceSpinner.setValue(value);
+            cadenceSpinner.setValue(MathUtils.clip(value, CADENCE_MIN, CADENCE_MAX));
         }
     }
 
