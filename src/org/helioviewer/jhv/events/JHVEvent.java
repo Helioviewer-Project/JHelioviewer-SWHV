4c8d9f428175817b6cdbebcbeb203b5e7dbe800c
freekv <freek.verstringe@oma.be>
2026-02-18 16:42:40 +0100

    Fixing fetching/displaying events (1)

--- src/org/helioviewer/jhv/events/JHVEvent.java
+++ src/org/helioviewer/jhv/events/JHVEvent.java
@@ -2,26 +2,22 @@ package org.helioviewer.jhv.events;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.annotation.Nullable;
-
 import org.apache.commons.validator.routines.UrlValidator;
 
 public class JHVEvent {
-
     public final long start;
     public final long end;
+    private final int id;
+    private final SWEKSupplier supplier;
 
-    private JHVEventParameter[] allParameters;
-    private JHVEventParameter[] visibleParameters;
-    private JHVEventParameter[] simpleVisibleParameters;
+    private JHVEventParameter[] allParameters = new JHVEventParameter[0];
+    private JHVEventParameter[] visibleParameters = new JHVEventParameter[0];
+    private JHVEventParameter[] simpleVisibleParameters = new JHVEventParameter[0];
 
     private List<JHVEventParameter> allParametersArray = new ArrayList<>();
     private List<JHVEventParameter> visibleParametersArray = new ArrayList<>();
-
-    private final SWEKSupplier supplier;
     private JHVPositionInformation positionInformation = null;
-    private final int id;
 
     public JHVEvent(SWEKSupplier _supplier, int _id, long _start, long _end) {
         supplier = _supplier;
@@ -30,109 +26,65 @@ public class JHVEvent {
         id = _id;
     }
 
-    public JHVEventParameter[] getAllEventParameters() {
-        return allParameters;
-    }
+    public JHVEventParameter[] getAllEventParameters() { return allParameters; }
+    public JHVEventParameter[] getVisibleEventParameters() { return visibleParameters; }
+    public JHVEventParameter[] getSimpleVisibleEventParameters() { return simpleVisibleParameters; }
+    public JHVPositionInformation getPositionInformation() { return positionInformation; }
+    public boolean isCactus() { return supplier.isCactus(); }
+    public int getUniqueID() { return id; }
+    public SWEKSupplier getSupplier() { return supplier; }
 
-    public JHVEventParameter[] getVisibleEventParameters() {
-        return visibleParameters;
-    }
-
-    public JHVEventParameter[] getSimpleVisibleEventParameters() {
-        return simpleVisibleParameters;
+    public void addPositionInformation(JHVPositionInformation pi) {
+        this.positionInformation = pi;
     }
 
     @Nullable
     public JHVEventParameter getParameter(String key) {
-        int i = 0;
-        while (i < allParameters.length) {
-            JHVEventParameter p = allParameters[i];
-            if (p.getParameterName() == key)
-                return p;
-            i++;
+        for (JHVEventParameter p : allParameters) {
+            if (p.getParameterName().equals(key)) return p;
         }
         return null;
-    }
-
-    public SWEKSupplier getSupplier() {
-        return supplier;
-    }
-
-    public JHVPositionInformation getPositionInformation() {
-        return positionInformation;
-    }
-
-    public void addPositionInformation(JHVPositionInformation _positionInformation) {
-        positionInformation = _positionInformation;
-    }
-
-    public int getUniqueID() {
-        return id;
     }
 
     public void addParameter(JHVEventParameter parameter, boolean visible, boolean configured, boolean full) {
-        String keyString = parameter.getParameterName(); // interned
-        if (!visible && !full)
-            if (keyString != "cme_radiallinvel" && keyString != "event_coord1" && keyString != "cme_angularwidth") {
+        String key = parameter.getParameterName();
+        if (!visible && !full) {
+            if (!"cme_radiallinvel".equals(key) && !"event_coord1".equals(key) && !"cme_angularwidth".equals(key)) {
                 return;
             }
-        allParametersArray.add(parameter);
-        if (configured && visible) {
-            visibleParametersArray.add(parameter);
         }
+        allParametersArray.add(parameter);
+        if (configured && visible) visibleParametersArray.add(parameter);
     }
 
-    @Nullable
-    private static SWEK.Parameter parameterFromList(String name, List<SWEK.Parameter> parameterList) {
-        for (SWEK.Parameter p : parameterList) {
-            if (p.name().equalsIgnoreCase(name)) {
-                return p;
-            }
-        }
+    public void addParameter(String key, String value, boolean full) {
+        SWEK.Parameter p = parameterFromList(key, supplier.getGroup().getParameterList());
+        if (p == null) p = parameterFromList(key, supplier.getSource().generalParameters());
+
+        boolean visible = (p != null) && p.visible();
+        boolean configured = (p != null);
+        String displayName = (p != null) ? p.displayName() : key.replace("_", " ").trim();
+
+        addParameter(new JHVEventParameter(key, displayName, value), visible, configured, full);
+    }
+
+    private static SWEK.Parameter parameterFromList(String name, List<SWEK.Parameter> list) {
+        for (SWEK.Parameter p : list) if (p.name().equalsIgnoreCase(name)) return p;
         return null;
     }
 
-    public void addParameter(String keyString, String value, boolean full) {
-        boolean visible = false;
-        boolean configured = false;
-        String displayName;
-
-        SWEK.Parameter p = parameterFromList(keyString, supplier.getGroup().getParameterList());
-        if (p == null) {
-            p = parameterFromList(keyString, supplier.getSource().generalParameters());
-        }
-
-        if (p != null) {
-            configured = true;
-            visible = p.visible();
-            displayName = p.displayName();
-        } else {
-            displayName = keyString.replaceAll("_", " ").trim();
-        }
-
-        JHVEventParameter parameter = new JHVEventParameter(keyString, displayName, value);
-        addParameter(parameter, visible, configured, full);
-    }
-
     public void finishParams() {
-        allParameters = allParametersArray.toArray(JHVEventParameter[]::new);
-        allParametersArray = new ArrayList<>();
-        visibleParameters = visibleParametersArray.toArray(JHVEventParameter[]::new);
+        allParameters = allParametersArray.toArray(new JHVEventParameter[0]);
+        visibleParameters = visibleParametersArray.toArray(new JHVEventParameter[0]);
 
-        UrlValidator urlValidator = UrlValidator.getInstance();
-        // maybe should be configured
-        List<JHVEventParameter> simpleVisibleParametersArray = new ArrayList<>();
-        for (JHVEventParameter param : visibleParametersArray) {
-            if (!urlValidator.isValid(param.getParameterValue()))
-                simpleVisibleParametersArray.add(param);
+        UrlValidator uv = UrlValidator.getInstance();
+        List<JHVEventParameter> simple = new ArrayList<>();
+        for (JHVEventParameter p : visibleParameters) {
+            if (!uv.isValid(p.getParameterValue())) simple.add(p);
         }
-        simpleVisibleParameters = simpleVisibleParametersArray.toArray(JHVEventParameter[]::new);
+        simpleVisibleParameters = simple.toArray(new JHVEventParameter[0]);
 
+        allParametersArray = new ArrayList<>();
         visibleParametersArray = new ArrayList<>();
     }
-
-    public boolean isCactus() {
-        return supplier.isCactus();
-    }
-
 }
--- src/org/helioviewer/jhv/events/JHVRelatedEvents.java
+++ src/org/helioviewer/jhv/events/JHVRelatedEvents.java
@@ -5,13 +5,10 @@ import java.awt.Point;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedMap;
 import java.util.TreeMap;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.ImageIcon;
-
 import org.helioviewer.jhv.base.Colors;
 import org.helioviewer.jhv.base.Pair;
 import org.helioviewer.jhv.base.interval.Interval;
@@ -31,54 +28,40 @@ public class JHVRelatedEvents implements ClickableDrawable {
     private Interval interval;
     private boolean highlighted;
 
-    JHVRelatedEvents(JHVEvent event, Map<SWEKSupplier, SortedMap<Interval, JHVRelatedEvents>> eventsMap) {
-        supplier = event.getSupplier();
-        color = eventColors.getNextColor();
-        highlighted = false;
-
-        events.add(event);
-        eventsMap.computeIfAbsent(supplier, k -> new TreeMap<>());
-
-        interval = new Interval(event.start, event.end);
-        eventsMap.get(supplier).put(interval, this);
+    JHVRelatedEvents(JHVEvent event, Map<SWEKSupplier, TreeMap<Long, List<JHVRelatedEvents>>> eventsMap) {
+        this.supplier = event.getSupplier();
+        this.color = eventColors.getNextColor();
+        this.events.add(event);
+        this.interval = new Interval(event.start, event.end);
+        addToMap(eventsMap);
     }
 
-    public List<JHVEvent> getEvents() {
-        return events;
+    private void addToMap(Map<SWEKSupplier, TreeMap<Long, List<JHVRelatedEvents>>> eventsMap) {
+        eventsMap.computeIfAbsent(supplier, k -> new TreeMap<>())
+                 .computeIfAbsent(interval.start, k -> new ArrayList<>())
+                 .add(this);
     }
 
-    public long getEnd() {
-        return interval.end;
+    private void removeFromMap(Map<SWEKSupplier, TreeMap<Long, List<JHVRelatedEvents>>> eventsMap) {
+        TreeMap<Long, List<JHVRelatedEvents>> supplierMap = eventsMap.get(supplier);
+        if (supplierMap != null) {
+            List<JHVRelatedEvents> list = supplierMap.get(interval.start);
+            if (list != null) {
+                list.remove(this);
+                if (list.isEmpty()) supplierMap.remove(interval.start);
+            }
+        }
     }
 
-    public long getStart() {
-        return interval.start;
-    }
-
-    public Color getColor() {
-        return color;
-    }
+    public List<JHVEvent> getEvents() { return events; }
+    public long getEnd() { return interval.end; }
+    public long getStart() { return interval.start; }
+    public Color getColor() { return color; }
+    public SWEKSupplier getSupplier() { return supplier; }
+    public boolean isHighlighted() { return highlighted; }
 
     @Nonnull
-    public ImageIcon getIcon() {
-        return supplier.getGroup().getIcon();
-    }
-
-    void merge(JHVRelatedEvents found, Map<SWEKSupplier, SortedMap<Interval, JHVRelatedEvents>> eventsMap) {
-        events.addAll(found.events);
-        associations.addAll(found.associations);
-
-        eventsMap.computeIfAbsent(supplier, k -> new TreeMap<>());
-        eventsMap.get(supplier).remove(found.interval);
-        eventsMap.get(supplier).remove(interval);
-
-        interval = new Interval(Math.min(interval.start, found.interval.start), Math.max(interval.end, found.interval.end));
-        eventsMap.get(supplier).put(interval, this);
-    }
-
-    public SWEKSupplier getSupplier() {
-        return supplier;
-    }
+    public ImageIcon getIcon() { return supplier.getGroup().getIcon(); }
 
     void highlight(boolean isHighlighted) {
         if (isHighlighted != highlighted) {
@@ -88,16 +71,11 @@ public class JHVRelatedEvents implements ClickableDrawable {
     }
 
     public static void addHighlightListener(JHVEventListener.Highlight listener) {
-        if (!listeners.contains(listener))
-            listeners.add(listener);
+        if (!listeners.contains(listener)) listeners.add(listener);
     }
 
     public static void removeHighlightListener(JHVEventListener.Highlight listener) {
         listeners.remove(listener);
-    }
-
-    public boolean isHighlighted() {
-        return highlighted;
     }
 
     private static void fireHighlightChanged() {
@@ -106,90 +84,69 @@ public class JHVRelatedEvents implements ClickableDrawable {
 
     public JHVEvent getClosestTo(long timestamp) {
         for (JHVEvent event : events) {
-            if (event.start <= timestamp && timestamp <= event.end) {
-                return event;
-            }
+            if (event.start <= timestamp && timestamp <= event.end) return event;
         }
-        return events.getFirst();
-    }
-
-    void addAssociation(Pair<Integer, Integer> association) {
-        associations.add(association);
-    }
-
-    @Nullable
-    private JHVEvent findEvent(int id) {
-        for (JHVEvent evt : events) {
-            if (evt.getUniqueID() == id) {
-                return evt;
-            }
-        }
-        return null;
+        return events.isEmpty() ? null : events.get(0);
     }
 
     public List<JHVEvent> getNextEvents(JHVEvent event) {
         List<JHVEvent> nEvents = new ArrayList<>();
+        int id = event.getUniqueID();
         for (Pair<Integer, Integer> assoc : associations) {
-            if (assoc.left() == event.getUniqueID()) {
-                JHVEvent newEvt = findEvent(assoc.right());
-                if (newEvt != null) {
-                    nEvents.add(newEvt);
-                }
+            if (assoc.left() == id) {
+                JHVEvent found = findEvent(assoc.right());
+                if (found != null) nEvents.add(found);
             }
         }
         return nEvents;
     }
 
     public List<JHVEvent> getPreviousEvents(JHVEvent event) {
-        List<JHVEvent> nEvents = new ArrayList<>();
+        List<JHVEvent> pEvents = new ArrayList<>();
+        int id = event.getUniqueID();
         for (Pair<Integer, Integer> assoc : associations) {
-            if (assoc.right() == event.getUniqueID()) {
-                JHVEvent newEvt = findEvent(assoc.left());
-                if (newEvt != null) {
-                    nEvents.add(newEvt);
-                }
+            if (assoc.right() == id) {
+                JHVEvent found = findEvent(assoc.left());
+                if (found != null) pEvents.add(found);
             }
         }
-        return nEvents;
+        return pEvents;
     }
 
-    void swapEvent(JHVEvent event, Map<SWEKSupplier, SortedMap<Interval, JHVRelatedEvents>> eventsMap) {
-        int eid = event.getUniqueID();
-        int i = 0;
-        while (events.get(i).getUniqueID() != eid) {
-            i++;
-        }
-        events.remove(i);
+    @Nullable
+    private JHVEvent findEvent(int id) {
+        for (JHVEvent evt : events) if (evt.getUniqueID() == id) return evt;
+        return null;
+    }
+
+    void addAssociation(Pair<Integer, Integer> association) {
+        associations.add(association);
+    }
+
+    void swapEvent(JHVEvent event, Map<SWEKSupplier, TreeMap<Long, List<JHVRelatedEvents>>> eventsMap) {
+        removeFromMap(eventsMap);
+        events.removeIf(e -> e.getUniqueID() == event.getUniqueID());
         events.add(event);
-        resetTime(eventsMap);
+        long start = Long.MAX_VALUE, end = Long.MIN_VALUE;
+        for (JHVEvent evt : events) {
+            start = Math.min(start, evt.start);
+            end = Math.max(end, evt.end);
+        }
+        interval = new Interval(start, end);
+        addToMap(eventsMap);
     }
 
-    private void resetTime(Map<SWEKSupplier, SortedMap<Interval, JHVRelatedEvents>> eventsMap) {
-        long start = Long.MAX_VALUE;
-        long end = Long.MIN_VALUE;
-        for (JHVEvent evt : events) {
-            long time = evt.start;
-            if (time < start) {
-                start = time;
-            }
-            time = evt.end;
-            if (time > end) {
-                end = time;
-            }
-        }
-
-        eventsMap.computeIfAbsent(supplier, k -> new TreeMap<>()).remove(interval);
-
-        interval = new Interval(start, end);
-        eventsMap.get(supplier).put(interval, this);
+    void merge(JHVRelatedEvents found, Map<SWEKSupplier, TreeMap<Long, List<JHVRelatedEvents>>> eventsMap) {
+        removeFromMap(eventsMap);
+        found.removeFromMap(eventsMap);
+        events.addAll(found.events);
+        associations.addAll(found.associations);
+        interval = new Interval(Math.min(interval.start, found.interval.start), Math.max(interval.end, found.interval.end));
+        addToMap(eventsMap);
     }
 
     @Override
-    public void clicked(Point locationOnScreen, long timestamp) {
-        SWEKEventInformationDialog dialog = new SWEKEventInformationDialog(this, getClosestTo(timestamp));
-        dialog.pack();
-        dialog.setLocation(locationOnScreen);
-        dialog.setVisible(true);
+    public void clicked(Point loc, long ts) {
+        new SWEKEventInformationDialog(this, getClosestTo(ts)).setVisible(true);
     }
-
 }
