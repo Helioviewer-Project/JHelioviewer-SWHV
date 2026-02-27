b5b7223cb84fe79e478e5ba886161001e7278f6d
Bogdan Nicula <bogdan@nicula.net>
2026-02-27 10:52:48 +0100

    Formatting

--- src/org/helioviewer/jhv/layers/filters/SliderFilterPanel.java
+++ src/org/helioviewer/jhv/layers/filters/SliderFilterPanel.java
@@ -16,9 +16,7 @@ public class SliderFilterPanel {
     public static class Blend extends AbstractSliderFilterPanel {
         public Blend(ImageLayer layer) {
             super("Blend ",
-                    0,
-                    100,
-                    (int) (layer.getGLImage().getBlend() * 100),
+                    0, 100, (int) (layer.getGLImage().getBlend() * 100),
                     LevelsPanel::formatPercent,
                     value -> layer.getGLImage().setBlend(value / 100.));
         }
@@ -27,9 +25,7 @@ public class SliderFilterPanel {
     public static class DeltaCROTA extends AbstractSliderFilterPanel {
         public DeltaCROTA(ImageLayer layer) {
             super("\u03B4CROTA",
-                    GLImage.MIN_DCROTA * 10,
-                    GLImage.MAX_DCROTA * 10,
-                    (int) (layer.getGLImage().getDeltaCROTA() * 10),
+                    GLImage.MIN_DCROTA * 10, GLImage.MAX_DCROTA * 10, (int) (layer.getGLImage().getDeltaCROTA() * 10),
                     value -> formatDegree(value / 10.0),
                     value -> layer.getGLImage().setDeltaCROTA(value / 10.0));
         }
@@ -38,9 +34,7 @@ public class SliderFilterPanel {
     public static class DeltaCRVAL1 extends AbstractSliderFilterPanel {
         public DeltaCRVAL1(ImageLayer layer) {
             super("\u03B4CRVAL1",
-                    GLImage.MIN_DCRVAL,
-                    GLImage.MAX_DCRVAL,
-                    layer.getGLImage().getDeltaCRVAL1(),
+                    GLImage.MIN_DCRVAL, GLImage.MAX_DCRVAL, layer.getGLImage().getDeltaCRVAL1(),
                     SliderFilterPanel::formatArcsec,
                     layer.getGLImage()::setDeltaCRVAL1);
         }
@@ -49,9 +43,7 @@ public class SliderFilterPanel {
     public static class DeltaCRVAL2 extends AbstractSliderFilterPanel {
         public DeltaCRVAL2(ImageLayer layer) {
             super("\u03B4CRVAL2",
-                    GLImage.MIN_DCRVAL,
-                    GLImage.MAX_DCRVAL,
-                    layer.getGLImage().getDeltaCRVAL2(),
+                    GLImage.MIN_DCRVAL, GLImage.MAX_DCRVAL, layer.getGLImage().getDeltaCRVAL2(),
                     SliderFilterPanel::formatArcsec,
                     layer.getGLImage()::setDeltaCRVAL2);
         }
@@ -60,9 +52,7 @@ public class SliderFilterPanel {
     public static class Opacity extends AbstractSliderFilterPanel {
         public Opacity(ImageLayer layer) {
             super("Opacity ",
-                    0,
-                    100,
-                    (int) (layer.getGLImage().getOpacity() * 100),
+                    0, 100, (int) (layer.getGLImage().getOpacity() * 100),
                     LevelsPanel::formatPercent,
                     value -> layer.getGLImage().setOpacity(value / 100.));
         }
@@ -71,9 +61,7 @@ public class SliderFilterPanel {
     public static class Sharpen extends AbstractSliderFilterPanel {
         public Sharpen(ImageLayer layer) {
             super("Sharpen ",
-                    -100,
-                    100,
-                    (int) (layer.getGLImage().getSharpen() * 100),
+                    -100, 100, (int) (layer.getGLImage().getSharpen() * 100),
                     LevelsPanel::formatPercent,
                     value -> layer.getGLImage().setSharpen(value / 100.));
         }
@@ -95,9 +83,7 @@ public class SliderFilterPanel {
 
         protected AbstractSliderFilterPanel(
                 String titleText,
-                int min,
-                int max,
-                int initial,
+                int min, int max, int initial,
                 IntFunction<String> formatter,
                 IntConsumer onValueChange) {
             title = new JLabel(titleText, JLabel.RIGHT);
