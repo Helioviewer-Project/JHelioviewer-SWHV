ee552b28235e9f388b1fe0ba871435a73133ce3c
Bogdan Nicula <bogdan@nicula.net>
2026-02-27 18:06:30 +0100

    Guard also InteractionPan from mouseDragged before mousePressed

--- src/org/helioviewer/jhv/camera/InteractionPan.java
+++ src/org/helioviewer/jhv/camera/InteractionPan.java
@@ -11,6 +11,7 @@ class InteractionPan implements Interaction.Type {
     private final Camera camera;
     private int lastX;
     private int lastY;
+    private boolean dragStartSet; // avoid freak mouseDragged before mousePressed
 
     InteractionPan(Camera _camera) {
         camera = _camera;
@@ -20,10 +21,14 @@ class InteractionPan implements Interaction.Type {
     public void mousePressed(MouseEvent e) {
         lastX = e.getX();
         lastY = e.getY();
+        dragStartSet = true;
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        if (!dragStartSet)
+            return;
+
         int x = e.getX() - lastX;
         int y = e.getY() - lastY;
         lastX = e.getX();
@@ -33,6 +38,11 @@ class InteractionPan implements Interaction.Type {
         double m = 1 / CameraHelper.getImagePixelFactor(camera, Display.getActiveViewport());
         camera.setTranslation(pan.x + x * m, pan.y - y * m);
         MovieDisplay.display();
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        dragStartSet = false;
     }
 
 }
