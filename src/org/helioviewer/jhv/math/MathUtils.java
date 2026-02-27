70df0e785155b2f36c71bd4943b78c39d6959368
Bogdan Nicula <bogdan@nicula.net>
2026-02-27 21:11:50 +0100

    Guard invSqrt

--- src/org/helioviewer/jhv/math/MathUtils.java
+++ src/org/helioviewer/jhv/math/MathUtils.java
@@ -75,6 +75,8 @@ public class MathUtils {
 
     // https://stackoverflow.com/questions/11513344/how-to-implement-the-fast-inverse-square-root-in-java
     public static float invSqrt(float x) {
+        if (!(x > 0f))
+            return 0f;
         float xhalf = 0.5f * x;
         int i = Float.floatToIntBits(x);
         i = 0x5f3759df - (i >> 1);
@@ -84,6 +86,8 @@ public class MathUtils {
     }
 
     public static double invSqrt(double x) {
+        if (!(x > 0d))
+            return 0d;
         double xhalf = 0.5d * x;
         long i = Double.doubleToLongBits(x);
         i = 0x5fe6ec85e7de30daL - (i >> 1);
