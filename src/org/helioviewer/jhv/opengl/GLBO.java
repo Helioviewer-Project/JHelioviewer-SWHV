b7aedc040de164d2c8469382b40d16603d12eea6
Bogdan Nicula <bogdan@nicula.net>
2026-04-04 16:48:00 +0200

    Port GLBO to LWJGL GL33

--- src/org/helioviewer/jhv/opengl/GLBO.java
+++ src/org/helioviewer/jhv/opengl/GLBO.java
@@ -6,6 +6,8 @@ import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.nio.ShortBuffer;
 
+import org.helioviewer.jhv.base.BufferUtils;
+
 import com.jogamp.opengl.GL3;
 import org.lwjgl.opengl.GL33;
 
@@ -33,12 +35,48 @@ class GLBO {
         GL33.glBindBuffer(target, bufferID);
         GL33.glBufferData(target, capacity, usage); // orphan, https://www.khronos.org/opengl/wiki/Buffer_Object_Streaming#Buffer_re-specification
         switch (buffer) {
-            case ByteBuffer byteBuffer -> GL33.glBufferSubData(target, 0, byteBuffer);
-            case FloatBuffer floatBuffer -> GL33.glBufferSubData(target, 0, floatBuffer);
-            case IntBuffer intBuffer -> GL33.glBufferSubData(target, 0, intBuffer);
-            case ShortBuffer shortBuffer -> GL33.glBufferSubData(target, 0, shortBuffer);
+            case ByteBuffer byteBuffer -> GL33.glBufferSubData(target, 0, directByteBuffer(byteBuffer));
+            case FloatBuffer floatBuffer -> GL33.glBufferSubData(target, 0, directFloatBuffer(floatBuffer));
+            case IntBuffer intBuffer -> GL33.glBufferSubData(target, 0, directIntBuffer(intBuffer));
+            case ShortBuffer shortBuffer -> GL33.glBufferSubData(target, 0, directShortBuffer(shortBuffer));
             default -> throw new IllegalArgumentException("Unsupported buffer type: " + buffer.getClass().getName());
         }
+    }
+
+    private static ByteBuffer directByteBuffer(ByteBuffer buffer) {
+        if (buffer.isDirect())
+            return buffer;
+
+        ByteBuffer copy = BufferUtils.newByteBuffer(buffer.remaining());
+        copy.put(buffer.duplicate());
+        return copy.flip();
+    }
+
+    private static FloatBuffer directFloatBuffer(FloatBuffer buffer) {
+        if (buffer.isDirect())
+            return buffer;
+
+        FloatBuffer copy = BufferUtils.newFloatBuffer(buffer.remaining());
+        copy.put(buffer.duplicate());
+        return copy.flip();
+    }
+
+    private static IntBuffer directIntBuffer(IntBuffer buffer) {
+        if (buffer.isDirect())
+            return buffer;
+
+        IntBuffer copy = BufferUtils.newIntBuffer(buffer.remaining());
+        copy.put(buffer.duplicate());
+        return copy.flip();
+    }
+
+    private static ShortBuffer directShortBuffer(ShortBuffer buffer) {
+        if (buffer.isDirect())
+            return buffer;
+
+        ShortBuffer copy = BufferUtils.newShortBuffer(buffer.remaining());
+        copy.put(buffer.duplicate());
+        return copy.flip();
     }
 
     int getID() {
