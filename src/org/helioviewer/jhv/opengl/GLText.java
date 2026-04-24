420c75fcfe6a66512367532c81d1f77f46b577af
Bogdan Nicula <bogdan@nicula.net>
2026-04-24 20:10:59 +0200

    Cleanup

--- src/org/helioviewer/jhv/opengl/GLText.java
+++ src/org/helioviewer/jhv/opengl/GLText.java
@@ -57,51 +57,38 @@ public class GLText {
 
     private static final int TEXT_SIZE_NORMAL = 14;
 
-    private static final int LEFT_MARGIN_TEXT = 0;//10;
-    private static final int RIGHT_MARGIN_TEXT = 0;//10;
-    private static final int TOP_MARGIN_TEXT = 0;//5;
-    private static final int BOTTOM_MARGIN_TEXT = 0;//5;
+    private static final float LINE_HEIGHT = 1.1f;
 
-    public static void drawTextFloat(Viewport vp, List<String> txts, int pt_x, int pt_y) {
-        if (txts.isEmpty())
+    public static void drawTextFloat(Viewport vp, List<String> lines, int x, int y) {
+        if (lines.isEmpty())
             return;
 
         TextRenderer renderer = getRenderer(TEXT_SIZE_NORMAL);
         int textSize = physicalSize(TEXT_SIZE_NORMAL);
         float textScaleFactor = textSize / renderer.getFontSize();
+        int lineStep = (int) (textSize * LINE_HEIGHT);
 
-        double boundW = 0;
-        int ct = 0;
-        for (String txt : txts) {
-            double w = renderer.measureWidth(txt) * textScaleFactor;
-            if (boundW < w)
-                boundW = w;
-            ct++;
+        float width = 0;
+        for (String line : lines) {
+            width = Math.max(width, renderer.measureWidth(line) * textScaleFactor);
         }
+        float height = textSize * LINE_HEIGHT * lines.size();
 
-        float w = (float) (boundW + LEFT_MARGIN_TEXT + RIGHT_MARGIN_TEXT);
-        float h = (float) (textSize * 1.1 * ct + BOTTOM_MARGIN_TEXT + TOP_MARGIN_TEXT);
-        int textInit_x = pt_x;
-        int textInit_y = pt_y;
+        int textX = x;
+        int textY = y;
+        if (textX + width > vp.width)
+            textX -= (int) (textX + width - vp.width);
+        if (textY + height - textSize > vp.height)
+            textY -= (int) (textY + height - textSize - vp.height);
 
-        // Correct if out of view
-        if (w + pt_x - LEFT_MARGIN_TEXT > vp.width) {
-            textInit_x -= (int) (w + pt_x - LEFT_MARGIN_TEXT - vp.width);
-        }
-        if (h + pt_y - textSize - TOP_MARGIN_TEXT > vp.height) {
-            textInit_y -= (int) (h + pt_y - textSize - TOP_MARGIN_TEXT - vp.height);
-        }
-        // float left = textInit_x - LEFT_MARGIN_TEXT;
-        // float bottom = textInit_y - textSize - TOP_MARGIN_TEXT;
-
-        int deltaY = 0, dY = (int) (textSize * 1.1);
         renderer.beginRendering(vp.width, vp.height);
-        for (String txt : txts) {
+        int baselineY = vp.height - textY;
+        for (String line : lines) {
             renderer.setColor(shadowColor);
-            renderer.draw(txt, textInit_x + shadowOffset[0], vp.height - textInit_y + shadowOffset[1] - deltaY, 0, textScaleFactor);
+            renderer.draw(line, textX + shadowOffset[0], baselineY + shadowOffset[1], 0, textScaleFactor);
             renderer.setColor(Colors.LightGrayFloat);
-            renderer.draw(txt, textInit_x, vp.height - textInit_y - deltaY, 0, textScaleFactor);
-            deltaY += dY;
+            renderer.draw(line, textX, baselineY, 0, textScaleFactor);
+            baselineY -= lineStep;
         }
         renderer.endRendering();
     }
