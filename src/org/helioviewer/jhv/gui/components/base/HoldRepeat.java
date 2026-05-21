8cbfe1faa87afb6f3170a89492c367f301e246a8
Bogdan Nicula <bogdan@nicula.net>
2026-05-21 20:14:15 +0200

    Simplify HoldRepeat

--- src/org/helioviewer/jhv/gui/components/base/HoldRepeat.java
+++ src/org/helioviewer/jhv/gui/components/base/HoldRepeat.java
@@ -1,35 +1,27 @@
 package org.helioviewer.jhv.gui.components.base;
 
-import java.awt.EventQueue;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 import javax.swing.AbstractButton;
-
-import org.helioviewer.jhv.threads.JHVThread;
+import javax.swing.Timer;
 
 public final class HoldRepeat {
-
-    private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor(new JHVThread.NamedThreadFactory("JHV-HoldRepeat"));
 
     private HoldRepeat() {}
 
     public static void install(AbstractButton button, int repeatMs) {
-        final ScheduledFuture<?>[] future = {null};
+        Timer timer = new Timer(repeatMs, e -> {
+            if (button.isEnabled())
+                button.doClick(0);
+        });
+        timer.setInitialDelay(repeatMs);
+
         button.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                if (!button.isEnabled() || future[0] != null)
-                    return;
-
-                future[0] = executor.scheduleAtFixedRate(() -> EventQueue.invokeLater(() -> {
-                    if (button.isEnabled())
-                        button.doClick(0);
-                }), repeatMs, repeatMs, TimeUnit.MILLISECONDS);
+                if (button.isEnabled() && !timer.isRunning())
+                    timer.start();
             }
 
             @Override
@@ -43,10 +35,7 @@ public final class HoldRepeat {
             }
 
             private void stop() {
-                if (future[0] != null) {
-                    future[0].cancel(false);
-                    future[0] = null;
-                }
+                timer.stop();
             }
         });
     }
