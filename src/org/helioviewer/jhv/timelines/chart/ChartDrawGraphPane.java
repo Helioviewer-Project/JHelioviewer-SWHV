bb16864af4cabdcd1f3666e3f420630b0a0e7ef6
Bogdan Nicula <bogdan@nicula.net>
2026-05-25 10:32:43 +0200

    Extract timeline label painter

--- src/org/helioviewer/jhv/timelines/chart/ChartDrawGraphPane.java
+++ src/org/helioviewer/jhv/timelines/chart/ChartDrawGraphPane.java
@@ -1,6 +1,5 @@
 package org.helioviewer.jhv.timelines.chart;
 
-import java.awt.BasicStroke;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
@@ -9,7 +8,6 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.RenderingHints;
-import java.awt.Stroke;
 import java.awt.Transparency;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
@@ -17,18 +15,15 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 import javax.swing.event.MouseInputListener;
 
-import org.helioviewer.jhv.base.Regex;
 import org.helioviewer.jhv.display.Display;
 import org.helioviewer.jhv.events.JHVEventCache;
 import org.helioviewer.jhv.export.ExportMovie;
 import org.helioviewer.jhv.gui.UIGlobals;
-import org.helioviewer.jhv.time.TimeUtils;
 import org.helioviewer.jhv.timelines.TimelineLayer;
 import org.helioviewer.jhv.timelines.TimelineLayers;
 import org.helioviewer.jhv.timelines.draw.ClickableDrawable;
@@ -37,7 +32,6 @@ import org.helioviewer.jhv.timelines.draw.DrawController;
 import org.helioviewer.jhv.timelines.draw.GraphGeometry;
 import org.helioviewer.jhv.timelines.draw.GraphGeometry.YAxisHit;
 import org.helioviewer.jhv.timelines.draw.TimeAxis;
-import org.helioviewer.jhv.timelines.draw.YAxis;
 
 @SuppressWarnings("serial")
 final class ChartDrawGraphPane extends JComponent implements MouseInputListener, MouseWheelListener, ComponentListener, DrawController.Listener {
@@ -51,8 +45,7 @@ final class ChartDrawGraphPane extends JComponent implements MouseInputListener,
 
     private BufferedImage screenImage;
 
-    private final Stroke boldStroke = new BasicStroke(2);
-    private final Stroke thinStroke = new BasicStroke(0.5f);
+    private final TimelineLabelPainter labelPainter = new TimelineLabelPainter();
     private Point mousePosition;
     private int lastWidth = -1;
     private int lastHeight = -1;
@@ -116,7 +109,6 @@ final class ChartDrawGraphPane extends JComponent implements MouseInputListener,
     protected void paintComponent(Graphics g1) {
         super.paintComponent(g1);
         GraphGeometry geometry = DrawController.getGeometry();
-        Rectangle graphArea = geometry.area();
 
         boolean axisHighlightChanged = toggleAxisHighlight(geometry);
         if (redrawGraphArea || axisHighlightChanged) {
@@ -131,7 +123,7 @@ final class ChartDrawGraphPane extends JComponent implements MouseInputListener,
             g.setFont(DrawConstants.font);
             g.drawImage(screenImage, 0, 0, getWidth(), getHeight(), null);
             drawMovieLine(g);
-            drawTimelineValues(g, geometry, DrawController.selectedAxis);
+            labelPainter.drawMouseValues(g, geometry, DrawController.selectedAxis, mousePosition);
         }
     }
 
@@ -162,7 +154,7 @@ final class ChartDrawGraphPane extends JComponent implements MouseInputListener,
             plotG.setClip(graphArea);
             TimeAxis xAxis = DrawController.selectedAxis;
             TimelineLayers.get().forEach(layer -> layer.draw(plotG, graphArea, xAxis, mousePosition));
-            drawLabels(fullG, geometry, xAxis);
+            labelPainter.drawStaticLabels(fullG, geometry, xAxis);
 
             plotG.dispose();
             fullG.dispose();
@@ -172,200 +164,6 @@ final class ChartDrawGraphPane extends JComponent implements MouseInputListener,
     private static void drawBackground(Graphics2D g, int width, int height) {
         g.setColor(UIGlobals.TL_SELECTED_INTERVAL_BACKGROUND_COLOR);
         g.fillRect(0, 0, width, height);
-    }
-
-    private void drawLabels(Graphics2D g, GraphGeometry geometry, TimeAxis xAxis) {
-        Rectangle graphArea = geometry.area();
-        Stroke stroke = g.getStroke();
-        g.setStroke(thinStroke);
-        int ht = 0;
-        drawHorizontalLabels(g, geometry, xAxis, ht, null);
-        ht++;
-        for (TimelineLayer tl : TimelineLayers.get()) {
-            if (tl.isPropagated()) {
-                g.setColor(tl.getDataColor());
-                drawHorizontalLabels(g, geometry, xAxis, ht, tl);
-                ht++;
-            }
-        }
-
-        int ct = -1;
-        for (TimelineLayer tl : TimelineLayers.get()) {
-            if (tl.showYAxis()) {
-                drawVerticalLabels(g, geometry, tl, ct, tl.getYAxis().isHighlighted());
-                ct++;
-            }
-        }
-        g.setStroke(stroke);
-    }
-
-    private void drawTimelineValues(Graphics2D g, GraphGeometry geometry, TimeAxis xAxis) {
-        Rectangle graphArea = geometry.area();
-        if (mousePosition == null || !graphArea.contains(mousePosition)) {
-            return;
-        }
-        long ts = geometry.xMapper(xAxis).toValue(mousePosition.x);
-        int x = graphArea.width / 2;
-        int y = DrawConstants.GRAPH_TOP_SPACE / 2;
-
-        g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
-        int currWidth = drawString(g, "(" + TimeUtils.format(TimeUtils.sqlTimeFormatter, ts), x, y);
-
-        for (TimelineLayer tl : TimelineLayers.get()) {
-            if (!tl.isEnabled()) {
-                continue;
-            }
-
-            String value = tl.getStringValue(ts);
-            if (value != null) {
-                g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
-                currWidth += drawString(g, ", ", x + currWidth, y);
-
-                g.setColor(tl.getDataColor());
-                currWidth += drawString(g, value, x + currWidth, y);
-            }
-        }
-
-        g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
-        drawString(g, ")", x + currWidth, y);
-    }
-
-    private static int drawString(Graphics2D g, String text, int x, int y) {
-        g.drawString(text, x, y);
-        return (int) g.getFontMetrics().getStringBounds(text, g).getWidth();
-    }
-
-    private static void drawHorizontalLabels(Graphics2D g, GraphGeometry geometry, TimeAxis xAxis, int ht, TimelineLayer tl) {
-        Rectangle graphArea = geometry.area();
-        String tickText = TimeUtils.format(DrawConstants.FULL_DATE_TIME_FORMAT, xAxis.start());
-        Rectangle2D tickTextBounds = g.getFontMetrics().getStringBounds(tickText, g);
-        int tickTextWidth = (int) tickTextBounds.getWidth();
-        int tickTextHeight = (int) tickTextBounds.getHeight() + ht * DrawConstants.GRAPH_BOTTOM_AXIS_SPACE;
-        int horizontalTickCount = Math.max(2, (graphArea.width - tickTextWidth * 2) / tickTextWidth);
-        int xend = geometry.rightEdge();
-        TimeAxis.Mapper xMapper;
-        if (tl == null) {
-            xMapper = geometry.xMapper(xAxis);
-        } else {
-            xMapper = new TimeAxis.Mapper(tl.getObservationTime(xAxis.start()), tl.getObservationTime(xAxis.end()), graphArea.x, graphArea.width);
-        }
-        long tickDifferenceHorizontal = (xAxis.end() - xAxis.start()) / (horizontalTickCount - 1);
-
-        long previousDate = Long.MIN_VALUE;
-        for (int i = 0; i < horizontalTickCount; ++i) {
-            long tickValue = xAxis.start() + i * tickDifferenceHorizontal;
-            if (tl != null) {
-                tickValue = tl.getObservationTime(tickValue);
-            }
-
-            int x = xMapper.toPixel(tickValue);
-            if (previousDate == Long.MIN_VALUE) {
-                tickText = TimeUtils.format(DrawConstants.FULL_DATE_TIME_FORMAT_REVERSE, tickValue);
-            } else {
-                long tickDayNumber = tickValue / TimeUtils.DAY_IN_MILLIS;
-                long prevDayNumber = previousDate / TimeUtils.DAY_IN_MILLIS;
-                tickText = TimeUtils.format(tickDayNumber == prevDayNumber ? DrawConstants.HOUR_TIME_FORMAT : DrawConstants.FULL_DATE_TIME_FORMAT_REVERSE, tickValue);
-            }
-
-            if (tl == null) {
-                g.setColor(UIGlobals.TL_TICK_LINE_COLOR);
-                g.drawLine(x, graphArea.y, x, graphArea.y + graphArea.height + 3);
-                g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
-            } else {
-                g.setColor(tl.getDataColor());
-            }
-
-            int yl = graphArea.y + graphArea.height + 2 + tickTextHeight;
-            for (String line : Regex.Return.split(tickText)) {
-                tickTextBounds = g.getFontMetrics().getStringBounds(line, g);
-                tickTextWidth = (int) tickTextBounds.getWidth();
-                int xl = x - (tickTextWidth / 2);
-                if (xl > xend - tickTextWidth) {
-                    xl = xend - tickTextWidth;
-                }
-                g.drawString(line, xl, yl);
-                yl += g.getFontMetrics().getHeight() * 2 / 3;
-            }
-
-            previousDate = tickValue;
-        }
-    }
-
-    private void drawVerticalLabels(Graphics2D g, GraphGeometry geometry, TimelineLayer tl, int leftSide, boolean highlight) {
-        Rectangle graphArea = geometry.area();
-        int axis_x_offset = graphArea.x;
-        if (leftSide != -1) {
-            axis_x_offset += graphArea.width + leftSide * DrawConstants.RIGHT_AXIS_WIDTH;
-        }
-
-        g.setColor(tl.getDataColor());
-        YAxis yAxis = tl.getYAxis();
-        YAxis.Mapper yMapper = geometry.yMapper(yAxis);
-        double start = yMapper.pixelToScaled(geometry.graphBottom());
-        double end = yMapper.pixelToScaled(graphArea.y);
-        if (start > end) {
-            double temp = start;
-            start = end;
-            end = temp;
-        }
-        int decade = (int) Math.floor(Math.log10(end - start));
-        double step = Math.pow(10, decade);
-        double startv = Math.floor(start / step) * step;
-        double endv = Math.ceil(end / step) * step;
-        if ((endv - startv) / step < 5) {
-            step /= 2;
-        }
-        double tick = startv;
-        int ct = 0;
-        drawHorizontalTickline(g, graphArea, yMapper, start, axis_x_offset, leftSide, false, highlight);
-        while (tick <= endv && ct < 20) {
-            if (tick >= start && tick <= end) {
-                drawHorizontalTickline(g, graphArea, yMapper, tick, axis_x_offset, leftSide, true, highlight);
-            }
-            tick += step;
-            ct++;
-        }
-        drawHorizontalTickline(g, graphArea, yMapper, end, axis_x_offset, leftSide, false, highlight);
-
-        String verticalLabel = yAxis.getLabel();
-        Rectangle2D verticalLabelBounds = g.getFontMetrics().getStringBounds(verticalLabel, g);
-        int vWidth = (int) verticalLabelBounds.getWidth();
-        int vHeight = (int) verticalLabelBounds.getHeight();
-        int labelCompensation = vWidth / 2;
-        if (highlight) {
-            Stroke s = g.getStroke();
-            g.setStroke(boldStroke);
-            g.setFont(DrawConstants.fontBold);
-            g.drawLine(axis_x_offset, graphArea.y, axis_x_offset, graphArea.y + graphArea.height + 3);
-            g.drawString(verticalLabel, axis_x_offset - labelCompensation, vHeight);
-            g.setStroke(s);
-            g.setFont(DrawConstants.font);
-        } else {
-            g.drawLine(axis_x_offset, graphArea.y, axis_x_offset, graphArea.y + graphArea.height + 3);
-            g.drawString(verticalLabel, axis_x_offset - labelCompensation, vHeight);
-        }
-    }
-
-    private static void drawHorizontalTickline(Graphics g, Rectangle graphArea, YAxis.Mapper yMapper, double tick, int axis_x_offset, int leftSide, boolean needTxt, boolean highlight) {
-        String tickText = DrawConstants.valueFormatter.format(tick);
-        int y = yMapper.scaledToPixel(tick);
-        Rectangle2D bounds = g.getFontMetrics().getStringBounds(tickText, g);
-        int x_str;
-        if (leftSide == -1) {
-            x_str = axis_x_offset - 6 - (int) bounds.getWidth();
-            g.drawLine(axis_x_offset - 3, y, graphArea.x + graphArea.width, y);
-        } else {
-            x_str = axis_x_offset;
-        }
-        if (needTxt) {
-            if (highlight) {
-                g.setFont(DrawConstants.fontBold);
-                g.drawString(tickText, x_str, y + (int) (bounds.getHeight() / 2));
-                g.setFont(DrawConstants.font);
-            } else {
-                g.drawString(tickText, x_str, y + (int) (bounds.getHeight() / 2));
-            }
-        }
     }
 
     private static void drawMovieLine(Graphics2D g) {
---
+++ src/org/helioviewer/jhv/timelines/chart/TimelineLabelPainter.java
@@ -0,0 +1,243 @@
+package org.helioviewer.jhv.timelines.chart;
+
+import java.awt.BasicStroke;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.geom.Rectangle2D;
+
+import org.helioviewer.jhv.base.Regex;
+import org.helioviewer.jhv.gui.UIGlobals;
+import org.helioviewer.jhv.time.TimeUtils;
+import org.helioviewer.jhv.timelines.TimelineLayer;
+import org.helioviewer.jhv.timelines.TimelineLayers;
+import org.helioviewer.jhv.timelines.draw.DrawConstants;
+import org.helioviewer.jhv.timelines.draw.GraphGeometry;
+import org.helioviewer.jhv.timelines.draw.TimeAxis;
+import org.helioviewer.jhv.timelines.draw.YAxis;
+
+final class TimelineLabelPainter {
+
+    private final Stroke boldStroke = new BasicStroke(2);
+    private final Stroke thinStroke = new BasicStroke(0.5f);
+
+    void drawStaticLabels(Graphics2D g, GraphGeometry geometry, TimeAxis xAxis) {
+        Stroke stroke = g.getStroke();
+        g.setStroke(thinStroke);
+
+        drawTimeLabels(g, geometry, xAxis);
+        drawYAxisLabels(g, geometry);
+
+        g.setStroke(stroke);
+    }
+
+    void drawMouseValues(Graphics2D g, GraphGeometry geometry, TimeAxis xAxis, Point mousePosition) {
+        Rectangle graphArea = geometry.area();
+        if (mousePosition == null || !graphArea.contains(mousePosition)) {
+            return;
+        }
+
+        long ts = geometry.xMapper(xAxis).toValue(mousePosition.x);
+        int x = graphArea.width / 2;
+        int y = DrawConstants.GRAPH_TOP_SPACE / 2;
+
+        g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
+        int currWidth = drawString(g, "(" + TimeUtils.format(TimeUtils.sqlTimeFormatter, ts), x, y);
+
+        for (TimelineLayer tl : TimelineLayers.get()) {
+            if (!tl.isEnabled()) {
+                continue;
+            }
+
+            String value = tl.getStringValue(ts);
+            if (value != null) {
+                g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
+                currWidth += drawString(g, ", ", x + currWidth, y);
+
+                g.setColor(tl.getDataColor());
+                currWidth += drawString(g, value, x + currWidth, y);
+            }
+        }
+
+        g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
+        drawString(g, ")", x + currWidth, y);
+    }
+
+    private void drawTimeLabels(Graphics2D g, GraphGeometry geometry, TimeAxis xAxis) {
+        int row = 0;
+        drawHorizontalLabels(g, geometry, xAxis, row, null);
+        row++;
+
+        for (TimelineLayer tl : TimelineLayers.get()) {
+            if (tl.isPropagated()) {
+                g.setColor(tl.getDataColor());
+                drawHorizontalLabels(g, geometry, xAxis, row, tl);
+                row++;
+            }
+        }
+    }
+
+    private void drawYAxisLabels(Graphics2D g, GraphGeometry geometry) {
+        int axisIndex = -1;
+        for (TimelineLayer tl : TimelineLayers.get()) {
+            if (tl.showYAxis()) {
+                drawVerticalLabels(g, geometry, tl, axisIndex, tl.getYAxis().isHighlighted());
+                axisIndex++;
+            }
+        }
+    }
+
+    private static int drawString(Graphics2D g, String text, int x, int y) {
+        g.drawString(text, x, y);
+        return (int) g.getFontMetrics().getStringBounds(text, g).getWidth();
+    }
+
+    private static void drawHorizontalLabels(Graphics2D g, GraphGeometry geometry, TimeAxis xAxis, int row, TimelineLayer tl) {
+        Rectangle graphArea = geometry.area();
+        String tickText = TimeUtils.format(DrawConstants.FULL_DATE_TIME_FORMAT, xAxis.start());
+        Rectangle2D tickTextBounds = g.getFontMetrics().getStringBounds(tickText, g);
+        int tickTextWidth = (int) tickTextBounds.getWidth();
+        int tickTextHeight = (int) tickTextBounds.getHeight() + row * DrawConstants.GRAPH_BOTTOM_AXIS_SPACE;
+        int horizontalTickCount = Math.max(2, (graphArea.width - tickTextWidth * 2) / tickTextWidth);
+        TimeAxis.Mapper xMapper = xMapper(geometry, xAxis, tl);
+        long tickDifferenceHorizontal = (xAxis.end() - xAxis.start()) / (horizontalTickCount - 1);
+
+        long previousDate = Long.MIN_VALUE;
+        for (int i = 0; i < horizontalTickCount; ++i) {
+            long tickValue = xAxis.start() + i * tickDifferenceHorizontal;
+            if (tl != null) {
+                tickValue = tl.getObservationTime(tickValue);
+            }
+
+            int x = xMapper.toPixel(tickValue);
+            tickText = formatTickText(tickValue, previousDate);
+            drawTimeTick(g, geometry, graphArea, x, tickText, tickTextHeight, tl);
+            previousDate = tickValue;
+        }
+    }
+
+    private static TimeAxis.Mapper xMapper(GraphGeometry geometry, TimeAxis xAxis, TimelineLayer tl) {
+        if (tl == null) {
+            return geometry.xMapper(xAxis);
+        }
+        return new TimeAxis.Mapper(tl.getObservationTime(xAxis.start()), tl.getObservationTime(xAxis.end()), geometry.area().x, geometry.area().width);
+    }
+
+    private static String formatTickText(long tickValue, long previousDate) {
+        if (previousDate == Long.MIN_VALUE) {
+            return TimeUtils.format(DrawConstants.FULL_DATE_TIME_FORMAT_REVERSE, tickValue);
+        }
+
+        long tickDayNumber = tickValue / TimeUtils.DAY_IN_MILLIS;
+        long prevDayNumber = previousDate / TimeUtils.DAY_IN_MILLIS;
+        return TimeUtils.format(tickDayNumber == prevDayNumber ? DrawConstants.HOUR_TIME_FORMAT : DrawConstants.FULL_DATE_TIME_FORMAT_REVERSE, tickValue);
+    }
+
+    private static void drawTimeTick(Graphics2D g, GraphGeometry geometry, Rectangle graphArea, int x, String tickText, int tickTextHeight, TimelineLayer tl) {
+        if (tl == null) {
+            g.setColor(UIGlobals.TL_TICK_LINE_COLOR);
+            g.drawLine(x, graphArea.y, x, geometry.graphBottom() + 3);
+            g.setColor(UIGlobals.TL_LABEL_TEXT_COLOR);
+        } else {
+            g.setColor(tl.getDataColor());
+        }
+
+        int y = geometry.graphBottom() + 2 + tickTextHeight;
+        for (String line : Regex.Return.split(tickText)) {
+            int lineWidth = (int) g.getFontMetrics().getStringBounds(line, g).getWidth();
+            int xl = Math.min(x - lineWidth / 2, geometry.rightEdge() - lineWidth);
+            g.drawString(line, xl, y);
+            y += g.getFontMetrics().getHeight() * 2 / 3;
+        }
+    }
+
+    private void drawVerticalLabels(Graphics2D g, GraphGeometry geometry, TimelineLayer tl, int leftSide, boolean highlight) {
+        Rectangle graphArea = geometry.area();
+        int axisX = graphArea.x;
+        if (leftSide != -1) {
+            axisX += graphArea.width + leftSide * DrawConstants.RIGHT_AXIS_WIDTH;
+        }
+
+        g.setColor(tl.getDataColor());
+        YAxis yAxis = tl.getYAxis();
+        YAxis.Mapper yMapper = geometry.yMapper(yAxis);
+        YTicks ticks = yTicks(yMapper, geometry);
+
+        drawHorizontalTickline(g, graphArea, yMapper, ticks.start, axisX, leftSide, false, highlight);
+        int count = 0;
+        for (double tick = ticks.first; tick <= ticks.last && count < 20; tick += ticks.step, count++) {
+            if (ticks.start <= tick && tick <= ticks.end) {
+                drawHorizontalTickline(g, graphArea, yMapper, tick, axisX, leftSide, true, highlight);
+            }
+        }
+        drawHorizontalTickline(g, graphArea, yMapper, ticks.end, axisX, leftSide, false, highlight);
+
+        drawVerticalTitle(g, graphArea, axisX, yAxis.getLabel(), highlight);
+    }
+
+    private static YTicks yTicks(YAxis.Mapper yMapper, GraphGeometry geometry) {
+        double start = yMapper.pixelToScaled(geometry.graphBottom());
+        double end = yMapper.pixelToScaled(geometry.area().y);
+        if (start > end) {
+            double temp = start;
+            start = end;
+            end = temp;
+        }
+
+        int decade = (int) Math.floor(Math.log10(end - start));
+        double step = Math.pow(10, decade);
+        double first = Math.floor(start / step) * step;
+        double last = Math.ceil(end / step) * step;
+        if ((last - first) / step < 5) {
+            step /= 2;
+        }
+        return new YTicks(start, end, first, last, step);
+    }
+
+    private void drawVerticalTitle(Graphics2D g, Rectangle graphArea, int axisX, String verticalLabel, boolean highlight) {
+        Rectangle2D verticalLabelBounds = g.getFontMetrics().getStringBounds(verticalLabel, g);
+        int vWidth = (int) verticalLabelBounds.getWidth();
+        int vHeight = (int) verticalLabelBounds.getHeight();
+        int labelCompensation = vWidth / 2;
+
+        if (highlight) {
+            Stroke s = g.getStroke();
+            g.setStroke(boldStroke);
+            g.setFont(DrawConstants.fontBold);
+            g.drawLine(axisX, graphArea.y, axisX, graphArea.y + graphArea.height + 3);
+            g.drawString(verticalLabel, axisX - labelCompensation, vHeight);
+            g.setStroke(s);
+            g.setFont(DrawConstants.font);
+        } else {
+            g.drawLine(axisX, graphArea.y, axisX, graphArea.y + graphArea.height + 3);
+            g.drawString(verticalLabel, axisX - labelCompensation, vHeight);
+        }
+    }
+
+    private static void drawHorizontalTickline(Graphics g, Rectangle graphArea, YAxis.Mapper yMapper, double tick, int axisX, int leftSide, boolean needTxt, boolean highlight) {
+        String tickText = DrawConstants.valueFormatter.format(tick);
+        int y = yMapper.scaledToPixel(tick);
+        Rectangle2D bounds = g.getFontMetrics().getStringBounds(tickText, g);
+        int xText;
+        if (leftSide == -1) {
+            xText = axisX - 6 - (int) bounds.getWidth();
+            g.drawLine(axisX - 3, y, graphArea.x + graphArea.width, y);
+        } else {
+            xText = axisX;
+        }
+        if (needTxt) {
+            if (highlight) {
+                g.setFont(DrawConstants.fontBold);
+                g.drawString(tickText, xText, y + (int) (bounds.getHeight() / 2));
+                g.setFont(DrawConstants.font);
+            } else {
+                g.drawString(tickText, xText, y + (int) (bounds.getHeight() / 2));
+            }
+        }
+    }
+
+    private record YTicks(double start, double end, double first, double last, double step) {}
+
+}
