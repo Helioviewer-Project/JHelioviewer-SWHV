36c545947beae4683071ddbe2d8cae75cbda7bb8
Bogdan Nicula <bogdan@nicula.net>
2026-06-19 11:21:33 +0200

    Preserve SWEKSupplier identity semantics

--- src/org/helioviewer/jhv/event/SWEKSupplier.java
+++ src/org/helioviewer/jhv/event/SWEKSupplier.java
@@ -10,4 +10,14 @@ public record SWEKSupplier(SWEKGroup group, String supplierName, String name, SW
         return name == "CACTus" && "HEK".equals(source.name());
     }
 
+    @Override
+    public boolean equals(Object other) {
+        return other == this;
+    }
+
+    @Override
+    public int hashCode() {
+        return System.identityHashCode(this);
+    }
+
 }
