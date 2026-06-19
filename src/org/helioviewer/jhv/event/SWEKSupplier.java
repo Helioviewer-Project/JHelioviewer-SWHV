8952d85e191b91efb15f9910489f061a3bfe3ac8
Bogdan Nicula <bogdan@nicula.net>
2026-06-19 12:38:32 +0200

    Make SWEKSupplier a class again

--- src/org/helioviewer/jhv/event/SWEKSupplier.java
+++ src/org/helioviewer/jhv/event/SWEKSupplier.java
@@ -1,23 +1,44 @@
 package org.helioviewer.jhv.event;
 
-public record SWEKSupplier(SWEKGroup group, String supplierName, String displayName, SWEK.Source source, String dbName) {
+public final class SWEKSupplier {
 
-    public SWEKSupplier {
-        displayName = displayName.intern();
+    private final SWEKGroup group;
+    private final String supplierName;
+    private final String displayName;
+    private final SWEK.Source source;
+    private final String dbName;
+    private final boolean isCactus;
+
+    public SWEKSupplier(SWEKGroup _group, String _supplierName, String _displayName, SWEK.Source _source, String _dbName) {
+        group = _group;
+        supplierName = _supplierName;
+        displayName = _displayName.intern();
+        source = _source;
+        dbName = _dbName;
+        isCactus = displayName == "CACTus" && "HEK".equals(source.name());
+    }
+
+    public SWEKGroup group() {
+        return group;
+    }
+
+    public String supplierName() {
+        return supplierName;
+    }
+
+    public String displayName() {
+        return displayName;
+    }
+
+    public SWEK.Source source() {
+        return source;
+    }
+
+    public String dbName() {
+        return dbName;
     }
 
     public boolean isCactus() {
-        return displayName == "CACTus" && "HEK".equals(source.name());
+        return isCactus;
     }
-
-    @Override
-    public boolean equals(Object other) {
-        return other == this;
-    }
-
-    @Override
-    public int hashCode() {
-        return System.identityHashCode(this);
-    }
-
 }
