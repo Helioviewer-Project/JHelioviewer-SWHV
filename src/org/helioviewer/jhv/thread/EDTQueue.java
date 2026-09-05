f64afb9dc1774fa20337907350d2723e2204ce5c
Bogdan Nicula <bogdan@nicula.net>
2026-09-05 13:16:14 +0200

    Simplify EDTQueue result propagation

--- src/org/helioviewer/jhv/thread/EDTQueue.java
+++ src/org/helioviewer/jhv/thread/EDTQueue.java
@@ -3,38 +3,26 @@ package org.helioviewer.jhv.thread;
 import java.awt.EventQueue;
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
 
 public final class EDTQueue {
 
-    // https://kotek.net/blog/swingutilities.invokeandwait_with_return_value
-    public static <E> E invokeAndWait(Callable<E> r) throws InterruptedException, InvocationTargetException {
-        if (EventQueue.isDispatchThread()) {
-            try {
-                return r.call();
-            } catch (Exception e) {
-                throw new InvocationTargetException(e);
-            }
+    public static <E> E invokeAndWait(Callable<E> callable) throws InterruptedException, InvocationTargetException {
+        FutureTask<E> task = new FutureTask<>(callable);
+        if (EventQueue.isDispatchThread())
+            task.run();
+        else
+            EventQueue.invokeAndWait(task);
+
+        try {
+            return task.get();
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            if (cause instanceof Error error)
+                throw error;
+            throw new InvocationTargetException(cause);
         }
-
-        AtomicReference<E> ret = new AtomicReference<>();
-        AtomicReference<Throwable> thrown = new AtomicReference<>();
-
-        EventQueue.invokeAndWait(() -> {
-            try {
-                ret.set(r.call());
-            } catch (Throwable t) {
-                // pass exception to original thread
-                thrown.set(t);
-            }
-        });
-
-        Throwable t = thrown.get();
-        if (t == null)
-            return ret.get();
-        if (t instanceof Error error)
-            throw error;
-        throw new InvocationTargetException(t);
     }
 
     public static void invokeAndWait(Runnable r) throws InterruptedException, InvocationTargetException {
