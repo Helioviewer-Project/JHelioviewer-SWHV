583af91f3e4909adb1aed6a24c370f57d38aeb73
Bogdan Nicula <bogdan@nicula.net>
2026-09-05 09:59:09 +0200

    Move orthographic map implementation into OrthographicView

--- src/org/helioviewer/jhv/display/MapView.java
+++ src/org/helioviewer/jhv/display/MapView.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.helioviewer.jhv.astronomy.Position;
 import org.helioviewer.jhv.math.Quat;
+import org.helioviewer.jhv.math.SphericalCoords;
 import org.helioviewer.jhv.math.Vec2;
 import org.helioviewer.jhv.math.Vec3;
 import org.helioviewer.jhv.opengl.BufVertex;
@@ -135,12 +136,22 @@ public abstract class MapView {
 
         @Override
         public Vec2 mouseToMap(Viewport vp, int x, int y) {
-            return OrthographicMap.mouseToMap(camera, viewpoint, cameraWidth(vp), vp, gridType, x, y);
+            Quat rotation = gridType == GridType.Viewpoint
+                    ? Quat.ZERO
+                    : Quat.rotateWithConjugate(viewpoint.toQuat(), gridType.toCarrington(viewpoint));
+
+            Vec3 p = ViewportMath.unprojectToOutputSphere(camera, vp, cameraWidth(vp), x, y, rotation);
+            if (p == null)
+                return Vec2.NAN;
+
+            double theta = Math.toDegrees(SphericalCoords.latitude(p));
+            double phi = Math.toDegrees(SphericalCoords.longitude(p));
+            return new Vec2(phi, theta);
         }
 
         @Override
         public Vec3 mouseToSurface(Viewport vp, int x, int y) {
-            return OrthographicMap.mouseToSurface(camera, viewpoint, cameraWidth(vp), vp, x, y);
+            return ViewportMath.unprojectToOutputSphere(camera, vp, cameraWidth(vp), x, y, viewpoint.toQuat());
         }
 
         @Override
@@ -150,12 +161,26 @@ public abstract class MapView {
 
         @Override
         public void emitMapLine(Viewport vp, List<Vec3> vertices, double radius, byte[] color, BufVertex vexBuf) {
-            OrthographicMap.emitMapLine(vertices, radius, color, vexBuf);
+            if (vertices.isEmpty())
+                return;
+
+            Vec3 first = vertices.getFirst();
+            vexBuf.startLine((float) (first.x * radius), (float) (first.y * radius), (float) (first.z * radius), 1, color);
+            for (int i = 1; i < vertices.size(); i++) {
+                Vec3 vertex = vertices.get(i);
+                float x = (float) (vertex.x * radius);
+                float y = (float) (vertex.y * radius);
+                float z = (float) (vertex.z * radius);
+                vexBuf.putVertex(x, y, z, 1, color);
+            }
+            vexBuf.endLine();
         }
 
         @Override
         public void emitMapPoints(Viewport vp, List<Vec3> vertices, double size, double radius, byte[] color, BufVertex vexBuf) {
-            OrthographicMap.emitMapPoints(vertices, size, radius, color, vexBuf);
+            float pointSize = (float) size;
+            for (Vec3 vertex : vertices)
+                vexBuf.putVertex((float) (vertex.x * radius), (float) (vertex.y * radius), (float) (vertex.z * radius), pointSize, color);
         }
     }
 
