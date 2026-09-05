3ca23d392484a9a713216aa3dcdf8b2824f9e6f4
Bogdan Nicula <bogdan@nicula.net>
2026-09-05 10:16:16 +0200

    Use an ordered set for event associations

--- src/org/helioviewer/jhv/event/JHVRelatedEvents.java
+++ src/org/helioviewer/jhv/event/JHVRelatedEvents.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import org.helioviewer.jhv.base.Colors;
@@ -14,7 +15,7 @@ public class JHVRelatedEvents {
     private static final Colors.Data eventColors = new Colors.Data();
 
     private final LinkedHashMap<Integer, JHVEvent> events = new LinkedHashMap<>();
-    private final List<JHVEvent.Link> associations = new ArrayList<>();
+    private final LinkedHashSet<JHVEvent.Link> associations = new LinkedHashSet<>();
     private final Color color;
 
     private Interval interval;
@@ -84,11 +85,10 @@ public class JHVRelatedEvents {
     }
 
     void addAssociation(JHVEvent.Link link) {
-        if (!associations.contains(link))
-            associations.add(link);
+        associations.add(link);
     }
 
-    List<JHVEvent.Link> getAssociations() {
+    Collection<JHVEvent.Link> getAssociations() {
         return associations;
     }
 
@@ -104,7 +104,7 @@ public class JHVRelatedEvents {
 
     void merge(JHVRelatedEvents found) {
         events.putAll(found.events);
-        found.associations.forEach(this::addAssociation);
+        associations.addAll(found.associations);
         interval = new Interval(Math.min(interval.start(), found.interval.start()), Math.max(interval.end(), found.interval.end()));
     }
 
