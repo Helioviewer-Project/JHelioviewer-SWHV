2cc41f5a0d2e181a36b7158f0636592cd2fe4958
Bogdan Nicula <bogdan@nicula.net>
2026-09-10 23:45:52 +0200

    Clear JPIP scan-start flag before skipping metadata

--- src/org/helioviewer/jhv/view/j2k/jpip/JPIPCache.java
+++ src/org/helioviewer/jhv/view/j2k/jpip/JPIPCache.java
@@ -22,10 +22,10 @@ public class JPIPCache extends Kdu_cache {
 
         JPIPStream stream = new JPIPStream();
         while (Scan_databins(flags, klassID, codestreamID, binID, binLen, complete, null, 0)) {
+            flags &= ~Kdu_global.KDU_CACHE_SCAN_START;
             if (klassID[0] == Constants.KDU.META_DATABIN)
                 continue;
 
-            flags &= ~Kdu_global.KDU_CACHE_SCAN_START;
             byte[] data = new byte[binLen[0]];
             if (!Scan_databins(flags | Kdu_global.KDU_CACHE_SCAN_NO_ADVANCE, klassID, codestreamID, binID, binLen, complete, data, binLen[0]))
                 break;
