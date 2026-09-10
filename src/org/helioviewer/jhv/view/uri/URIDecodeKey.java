45a454269433b9b909027dbc241e9d0353a3937f
Bogdan Nicula <bogdan@nicula.net>
2026-09-10 11:15:26 +0200

    Reduce

--- src/org/helioviewer/jhv/view/uri/URIDecodeKey.java
+++
@@ -1,9 +0,0 @@
-package org.helioviewer.jhv.view.uri;
-
-import javax.annotation.Nullable;
-
-import org.helioviewer.jhv.image.ImageFilter;
-import org.helioviewer.jhv.io.DataUri;
-import org.helioviewer.jhv.view.ClipSet;
-
-record URIDecodeKey(DataUri uri, ImageFilter.Type filter, @Nullable FITSViewState.Data fitsData, @Nullable ClipSet.Range clipRange) {}
